5e95129f897b1cf076dab13a2fa55b6cee89bc0e
Eddie Ridwan <eddie.ridwan@amc.org.au>
2016-06-07 17:21:56 +1200

    Upload surat export ke folder surat export ubahan desa

--- donjo-app/models/surat_master_model.php
+++ donjo-app/models/surat_master_model.php
@@ -3,12 +3,12 @@
 	function __construct(){
 		parent::__construct();
 	}
-	
+
 	function autocomplete(){
 		$sql   = "SELECT nama FROM tweb_surat_format";
 		$query = $this->db->query($sql);
 		$data  = $query->result_array();
-		
+
 		$i=0;
 		$outp='';
 		while($i<count($data)){
@@ -19,8 +19,8 @@
 		$outp = '[' .$outp. ']';
 		return $outp;
 	}
-	
-	
+
+
 	function search_sql(){
 		if(isset($_SESSION['cari'])){
 		$cari = $_SESSION['cari'];
@@ -30,62 +30,62 @@
 			return $search_sql;
 			}
 		}
-	
-	function filter_sql(){		
+
+	function filter_sql(){
 		if(isset($_SESSION['filter'])){
 			$kf = $_SESSION['filter'];
 			$filter_sql= " AND u.act_analisis = $kf";
 		return $filter_sql;
 		}
 	}
-	
-	function master_sql(){		
+
+	function master_sql(){
 		if(isset($_SESSION['analisis_master'])){
 			$kf = $_SESSION['analisis_master'];
 			$filter_sql= " AND u.id_master = $kf";
 		return $filter_sql;
 		}
 	}
-	
-	function tipe_sql(){		
+
+	function tipe_sql(){
 		if(isset($_SESSION['tipe'])){
 			$kf = $_SESSION['tipe'];
 			$filter_sql= " AND u.id_tipe = $kf";
 		return $filter_sql;
 		}
 	}
-	
-	function kategori_sql(){		
+
+	function kategori_sql(){
 		if(isset($_SESSION['kategori'])){
 			$kf = $_SESSION['kategori'];
 			$filter_sql= " AND u.id_kategori = $kf";
 		return $filter_sql;
 		}
 	}
-	
+
 	function paging($p=1,$o=0){
-	
+
 		$sql      = "SELECT COUNT(id) AS id FROM tweb_surat_format u WHERE 1";
-		$sql     .= $this->search_sql(); 
-		$sql .= $this->filter_sql();   
-		
+		$sql     .= $this->search_sql();
+		$sql .= $this->filter_sql();
+
 		$sql .= $this->tipe_sql();
 		$sql .= $this->kategori_sql();
 		$query    = $this->db->query($sql);
 		$row      = $query->row_array();
 		$jml_data = $row['id'];
-		
+
 		$this->load->library('paging');
 		$cfg['page']     = $p;
 		$cfg['per_page'] = $_SESSION['per_page'];
 		$cfg['num_rows'] = $jml_data;
 		$this->paging->init($cfg);
-		
+
 		return $this->paging;
 	}
-	
+
 	function list_data($o=0,$offset=0,$limit=500){
-	
+
 		//Ordering SQL
 		switch($o){
 			case 1: $order_sql = ' ORDER BY u.nomor'; break;
@@ -96,24 +96,24 @@
 			case 6: $order_sql = ' ORDER BY u.id_kategori DESC'; break;
 			default:$order_sql = ' ORDER BY u.id';
 		}
-	
+
 		//Paging SQL
 		$paging_sql = ' LIMIT ' .$offset. ',' .$limit;
-		
+
 		//Main Query
 		$sql   = "SELECT u.* FROM tweb_surat_format u  WHERE 1 ";
-			
+
 		$sql .= $this->search_sql();
 		$sql .= $this->filter_sql();
-		
+
 		$sql .= $this->tipe_sql();
 		$sql .= $this->kategori_sql();
 		$sql .= $order_sql;
 		$sql .= $paging_sql;
-		
+
 		$query = $this->db->query($sql);
 		$data=$query->result_array();
-		
+
 		//Formating Output
 		$i=0;
 		$j=$offset;
@@ -124,77 +124,77 @@
 		}
 		return $data;
 	}
-	
+
 	function insert(){
 		$data = $_POST;
-		
+
 		$data['url_surat'] = str_replace(" ","_",$data['nama']);
 		$data['url_surat'] = strtolower($data['url_surat']);
 		$data['url_surat'] = "surat_".$data['url_surat'];
 		$outp = $this->db->insert('tweb_surat_format',$data);
-		
+
 		$mypath="surat\\".$data['url_surat']."\\";
 		$path = "".str_replace("\\","/",$mypath)."/";
-		
+
 		if (!file_exists($mypath)) {
 			mkdir($mypath, 0777, true);
 		}
-		
+
 		//doc
 		$raw="surat\\raw\\";
 		$raw_path = "".str_replace("\\","/",$raw);
 		$file = $raw_path."template.rtf";
 		$handle = fopen($file,'r');
-		
+
 		$buffer = stream_get_contents($handle);
 		//$handle = fopen($path.$data['url_surat'],'w+');
-		
+
 		$berkas = $path.$data['url_surat'].".rtf";
 		$handle = fopen($berkas,'w+');
 		fwrite($handle,$buffer);
 		fclose($handle);
-		
+
 		//form
 		$mypath="donjo-app\\views\\surat\\form\\";
 		$path_form = "".str_replace("\\","/",$mypath)."/";
-		
+
 		$raw="surat\\raw\\";
 		$raw_path = "".str_replace("\\","/",$raw);
 		$file = $raw_path."form.raw";
 		$handle = fopen($file,'r');
-		
+
 		$buffer = stream_get_contents($handle);
 		//$handle = fopen($path_form.$data['url_surat'],'w+');
-		
+
 		$berkas = $path_form.$data['url_surat'].".php";
 		$handle = fopen($berkas,'w+');
 		$buffer=str_replace("[nama_surat]","Surat $data[nama]",$buffer);
 		fwrite($handle,$buffer);
 		fclose($handle);
-		
+
 		//cetak
 		$mypath="donjo-app\\views\\surat\\print\\";
 		$path_form = "".str_replace("\\","/",$mypath)."/";
-		
+
 		$raw="surat\\raw\\";
 		$raw_path = "".str_replace("\\","/",$raw);
 		$file = $raw_path."print.raw";
 		$handle = fopen($file,'r');
-		
+
 		$buffer = stream_get_contents($handle);
 		//$handle = fopen($path_form.$data['url_surat'],'w+');
-		
+
 		$berkas = $path_form."print_".$data['url_surat'].".php";
 		$handle = fopen($berkas,'w+');
 		$nama_surat = strtoupper($data['nama']);
 		$buffer=str_replace("[nama_surat]","SURAT $nama_surat",$buffer);
 		fwrite($handle,$buffer);
 		fclose($handle);
-		
+
 		if($outp) $_SESSION['success']=1;
 			else $_SESSION['success']=-1;
 	}
-	
+
 	function update($id=0){
 		$data = $_POST;
 		$this->db->where('id',$id);
@@ -203,33 +203,34 @@
 		if($outp) $_SESSION['success']=1;
 			else $_SESSION['success']=-1;
 	}
-	
+
 	function upload($url=""){
+		$_SESSION['error_msg'] = '';
 		$tipe_file   = $_FILES['foto']['type'];
-		//echo $tipe_file;
-		if ($tipe_file != "application/rtf"){
+		$mime_type_rtf = array("application/rtf", "text/rtf");
+		if(!in_array($tipe_file, $mime_type_rtf)){
+			$_SESSION['error_msg'].= " -> Jenis file salah: " . $tipe_file;
 			$_SESSION['success']=-1;
 		} else {
-			$vdir_upload = "surat\\$url\\$url.rtf";
-			//$vdir_upload = "surat/$url/$url.rtf";
-			unlink($vdir_upload); 
+			// Upload ke folder surat export ubahan desa
+			$vdir_upload = LOKASI_SURAT_EXPORT_DESA . $url . ".rtf";
 			move_uploaded_file($_FILES["foto"]["tmp_name"], $vdir_upload);
 			$_SESSION['success']=1;
 		}
-		
+
 	}
-	
+
 	function delete($id=''){
 		$sql  = "DELETE FROM tweb_surat_format WHERE id=?";
 		$outp = $this->db->query($sql,array($id));
-		
+
 		if($outp) $_SESSION['success']=1;
 			else $_SESSION['success']=-1;
 	}
-	
+
 	function delete_all(){
 		$id_cb = $_POST['id_cb'];
-		
+
 		if(count($id_cb)){
 			foreach($id_cb as $id){
 				$sql  = "DELETE FROM tweb_surat_format WHERE id=?";
@@ -237,51 +238,51 @@
 			}
 		}
 		else $outp = false;
-		
+
 		if($outp) $_SESSION['success']=1;
 			else $_SESSION['success']=-1;
 	}
-	
+
 	function list_atribut($id=0){
 		$sql   = "SELECT * FROM tweb_surat_atribut WHERE id_surat = ?";
 		$query = $this->db->query($sql,$id);
 		$data= $query->result_array();
-		
+
 		//Formating Output
 		$i=0;
 		while($i<count($data)){
 			$data[$i]['no']=$i+1;
-			
+
 			$i++;
 		}
 		return $data;
 	}
-		
+
 	function get_surat_format($id=0){
 		$sql   = "SELECT * FROM tweb_surat_format WHERE id=?";
 		$query = $this->db->query($sql,$id);
 		$data  = $query->row_array();
 		return $data;
 	}
-	
+
 	function get_analisis_master(){
 		$sql   = "SELECT * FROM analisis_master WHERE id=?";
 		$query = $this->db->query($sql,$_SESSION['analisis_master']);
 		return $query->row_array();
-	}	
-	
+	}
+
 	function get_tweb_surat_atribut($id=''){
 		$sql   = "SELECT * FROM tweb_surat_atribut WHERE id=?";
 		$query = $this->db->query($sql,$id);
 		return $query->row_array();
-	}	
+	}
 
 	function list_tipe(){
 		$sql   = "SELECT * FROM analisis_tipe_indikator";
 		$query = $this->db->query($sql);
 		return $query->result_array();
 	}
-	
+
 	function list_kategori(){
 		$sql   = "SELECT u.* FROM analisis_kategori_indikator u WHERE 1";
 		$sql .= $this->master_sql();
