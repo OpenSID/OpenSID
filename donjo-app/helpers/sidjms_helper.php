79f56950bc73c778cac215c7185a8ad4c8a23d4f
Eddie Ridwan <eddie.ridwan@amc.org.au>
2016-06-07 15:23:12 +1200

    Pakai surat export ubahan desa apabila ada

--- donjo-app/helpers/sidjms_helper.php
+++ donjo-app/helpers/sidjms_helper.php
@@ -5,6 +5,7 @@ define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
 define("LOKASI_SURAT_PRINT_DESA", 'desa/surat/print/');
+define("LOKASI_SURAT_EXPORT_DESA", 'desa/surat/export/');
 
 /**
  * Ambil Versi
@@ -45,5 +46,24 @@ define("LOKASI_SURAT_PRINT_DESA", 'desa/surat/print/');
   {
     $konfigurasi_database = LOKASI_CONFIG_DESA . 'database.php';
     return $konfigurasi_database;
+  }
+
+/**
+ * SuratExportDesa
+ *
+ * Mengembalikan path surat ubahan desa apabila ada
+ *
+ * @access  public
+ * @return  string
+ */
+  function SuratExportDesa($nama_surat)
+  {
+    $surat_export_desa = LOKASI_SURAT_EXPORT_DESA . $nama_surat . ".rtf";
+    if(is_file($surat_export_desa)){
+      return $surat_export_desa;
+    } else {
+      return "";
+    }
+
   }
 ?>
