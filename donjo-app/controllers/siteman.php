74c7fc1af0c5a638f22cc528bb6b92c2c741da0b
Abdul Gani <buabatudaa@gmail.com>
2016-06-17 08:30:49 +0800

    FEATURE : Apabila menggunakan offline mode, maka modul yang tidak digunakan seperti halaman website dan gis akan dinonaktifkan.

--- donjo-app/controllers/siteman.php
+++ donjo-app/controllers/siteman.php
@@ -1,18 +1,18 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Siteman extends CI_Controller {
-	
+
 	function __construct(){
 		parent::__construct();
 		session_start();
 		$this->load->model('header_model');
 		$this->load->model('user_model');
 	}
-	
+
 	function index(){
 		$this->user_model->logout();
 		$header = $this->header_model->get_config();
-		
+
 		//Initialize Session ------------
 		if(!isset($_SESSION['siteman']))
 		$_SESSION['siteman']=0;
@@ -22,16 +22,16 @@ class Siteman extends CI_Controller {
 		$_SESSION['pengumuman'] = 0;
 		$_SESSION['sesi'] = "kosong";
 		//-------------------------------
-		
+
 		$this->load->view('siteman',$header);
 		$_SESSION['siteman']=0;
 	}
-	
+
 	function auth(){
 		$this->user_model->siteman();
 		redirect('main');
 	}
-	
+
 	function login(){
 		$this->user_model->login();
 		$header = $this->header_model->get_config();
@@ -41,5 +41,5 @@ class Siteman extends CI_Controller {
 	function flash(){
 		$this->load->view('config');
 	}
-	
+
 }
--- donjo-app/controllers/web.php
+++ donjo-app/controllers/web.php
@@ -4,6 +4,12 @@ class web extends CI_Controller{
 	function __construct(){
 		parent::__construct();
 		session_start();
+
+		// Jika offline_mode aktif, tidak perlu menampilkan modul website
+		if ($this->config->item("offline_mode")===TRUE) {
+			redirect('hom_desa');
+		}
+
 		$this->load->model('user_model');
 		$grup	= $this->user_model->sesi_grup($_SESSION['sesi']);
 		if($grup!=1 AND $grup!=2 AND $grup!=3 AND $grup!=4) redirect('siteman');
@@ -11,37 +17,37 @@ class web extends CI_Controller{
 		$this->load->model('web_artikel_model');
 
 	}
-	
+
 	function clear(){
 		unset($_SESSION['cari']);
 		unset($_SESSION['filter']);
 		redirect('web');
 	}
-	
+
 	function pager($cat=1){
-		if(isset($_POST['per_page'])) 
+		if(isset($_POST['per_page']))
 			$_SESSION['per_page']=$_POST['per_page'];
 		redirect("web/index/$cat");
 	}
-	
+
 	function index($cat=1,$p=1,$o=0){
-	
+
 		$data['p']        = $p;
 		$data['o']        = $o;
 		$data['cat']	  = $cat;
-		
+
 		if(isset($_SESSION['cari']))
 			$data['cari'] = $_SESSION['cari'];
 		else $data['cari'] = '';
-		
+
 		if(isset($_SESSION['filter']))
 			$data['filter'] = $_SESSION['filter'];
 		else $data['filter'] = '';
-	
-		if(isset($_POST['per_page'])) 
+
+		if(isset($_POST['per_page']))
 			$_SESSION['per_page']=$_POST['per_page'];
 		$data['per_page'] = $_SESSION['per_page'];
-		
+
 		$data['paging']  = $this->web_artikel_model->paging($cat,$p,$o);
 		$data['main']    = $this->web_artikel_model->list_data($cat,$o, $data['paging']->offset, $data['paging']->per_page);
 		$data['keyword'] = $this->web_artikel_model->autocomplete();
@@ -51,19 +57,19 @@ class web extends CI_Controller{
 
 		$header = $this->header_model->get_data();
 		$nav['act']=0;
-		
+
 		$this->load->view('header', $header);
 		$this->load->view('web/nav',$nav);
 		$this->load->view('web/artikel/table',$data);
 		$this->load->view('footer');
 	}
-	
+
 	function form($cat=1,$p=1,$o=0,$id=''){
-	
+
 		$data['p'] = $p;
 		$data['o'] = $o;
 		$data['cat'] = $cat;
-		
+
 		if($id){
 			$data['artikel']        = $this->web_artikel_model->get_artikel($id);
 			$data['form_action'] = site_url("web/update/$cat/$id/$p/$o");
@@ -72,11 +78,11 @@ class web extends CI_Controller{
 			$data['artikel']        = null;
 			$data['form_action'] = site_url("web/insert/$cat");
 		}
-			
+
 		$data['kategori'] = $this->web_artikel_model->get_kategori($cat);
-		
+
 		$header = $this->header_model->get_data();
-		
+
 		$nav['act']=0;
 		$this->load->view('header', $header);
 		//$this->load->view('web/spacer');
@@ -85,7 +91,7 @@ class web extends CI_Controller{
 			$this->load->view('web/artikel/form',$data);
 		else
 			$this->load->view('web/artikel/widget-form',$data);
-		
+
 		$this->load->view('footer');
 	}
 
@@ -96,7 +102,7 @@ class web extends CI_Controller{
 		else unset($_SESSION['cari']);
 		redirect("web/index/$cat");
 	}
-	
+
 	function filter($cat=1){
 		$filter = $this->input->post('filter');
 		if($filter!=0)
@@ -104,32 +110,32 @@ class web extends CI_Controller{
 		else unset($_SESSION['filter']);
 		redirect("web/index/$cat");
 	}
-	
+
 	function insert($cat=1){
 		$this->web_artikel_model->insert($cat);
 		redirect("web/index/$cat");
 	}
-	
+
 	function update($cat=0,$id='',$p=1,$o=0){
 		$this->web_artikel_model->update($id);
 		redirect("web/index/$cat/$p/$o");
 	}
-	
+
 	function delete($cat=1,$p=1,$o=0,$id=''){
 		$this->web_artikel_model->delete($id);
 		redirect("web/index/$cat/$p/$o");
 	}
-		
+
 	function hapus($cat=1,$p=1,$o=0){
 		$this->web_artikel_model->hapus($cat);
 		redirect("web/index/1/$p/$o");
 	}
-	
+
 	function delete_all($p=1,$o=0){
 		$this->web_artikel_model->delete_all();
 		redirect("web/index/$p/$o");
 	}
-	
+
 	function artikel_lock($cat=1,$id=0){
 		$this->web_artikel_model->artikel_lock($id,1);
 		redirect("web/index/$cat");
@@ -139,9 +145,9 @@ class web extends CI_Controller{
 		$this->web_artikel_model->artikel_lock($id,2);
 		redirect("web/index/$cat");
 	}
-	
+
 	function ajax_add_kategori($cat=1,$p=1,$o=0){
-	
+
 		$data['form_action'] = site_url("web/insert_kategori/$cat/$p/$o");
 		$this->load->view('web/artikel/ajax_add_kategori_form',$data);
 	}
@@ -150,7 +156,7 @@ class web extends CI_Controller{
 		$this->web_artikel_model->insert_kategori();
 		redirect("web/index/$cat/$p/$o");
 	}
-	
+
 	function headline($cat=1,$p=1,$o=0,$id=0){
 		$this->web_artikel_model->headline($id);
 		redirect("web/index/$cat/$p/$o");
