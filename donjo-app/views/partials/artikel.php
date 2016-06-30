7c44199a308cf45e0d5f8ce151e1d3ac3d54ec13
Eddie Ridwan <eddie.ridwan@amc.org.au>
2016-06-30 23:49:28 +0100

    Pindahkan styling form komentar ke first.css

--- donjo-app/views/partials/artikel.php
+++ donjo-app/views/partials/artikel.php
@@ -103,20 +103,20 @@ if($single_artikel["id"]){
 				}
 		echo "
 				<div class=\"box-body\">
-					<form name=\"form\" action=\"".site_url("first/add_comment/".$single_artikel["id"])."\" method=POST onSubmit=\"return validasi(this)\">
+					<form id=\"form-komentar\" name=\"form\" action=\"".site_url("first/add_comment/".$single_artikel["id"])."\" method=POST onSubmit=\"return validasi(this)\">
 					<table width=100%>
-						<tr class=\"komentar\"><td>Nama</td><td style=\"padding-top: 5px;\"> <input type=text name=\"owner\" size=20 maxlength=30 value=\"".$_SESSION['post']['owner']."\"></td></tr>
-						<tr class=\"komentar\"><td>Alamat e-mail</td><td style=\"padding-top: 5px;\"> <input type=text name=\"email\" size=20 maxlength=30 value=\"".$_SESSION['post']['email']."\"></td></tr>
-						<tr class=\"komentar\"><td valign=top>Komentar</td><td style=\"padding-top: 5px;\"> <textarea name=\"komentar\" style=\"width: 300px; height: 100px;\">".$_SESSION['post']['komentar']."</textarea></td></tr>
-						<tr><td>&nbsp;</td>
-							<td style=\"padding-top: 10px;\">
-								<img id=\"captcha\" src=\"/securimage/securimage_show.php\" alt=\"CAPTCHA Image\" style=\"height: 40px;\"/>
-								<a href=\"#\" onclick=\"document.getElementById('captcha').src = '/securimage/securimage_show.php?' + Math.random(); return false\" style=\"margin-left:10px;\">[ Ganti gambar ]</a>
+						<tr class=\"komentar nama\"><td>Nama</td><td> <input type=text name=\"owner\" size=20 maxlength=30 value=\"".$_SESSION['post']['owner']."\"></td></tr>
+						<tr class=\"komentar alamat\"><td>Alamat e-mail</td><td> <input type=text name=\"email\" size=20 maxlength=30 value=\"".$_SESSION['post']['email']."\"></td></tr>
+						<tr class=\"komentar pesan\"><td valign=top>Komentar</td><td> <textarea name=\"komentar\">".$_SESSION['post']['komentar']."</textarea></td></tr>
+						<tr class=\"captcha\"><td>&nbsp;</td>
+							<td>
+								<img id=\"captcha\" src=\"/securimage/securimage_show.php\" alt=\"CAPTCHA Image\"/>
+								<a href=\"#\" onclick=\"document.getElementById('captcha').src = '/securimage/securimage_show.php?' + Math.random(); return false\">[ Ganti gambar ]</a>
 							</td></tr>
-						<tr><td>&nbsp;</td><td style=\"padding-top: 5px;\">
+						<tr class=\"captcha_code\"><td>&nbsp;</td><td>
 								<input type=\"text\" name=\"captcha_code\" size=\"10\" maxlength=\"6\" value=\"".$_SESSION['post']['captcha_code']."\"/> Isikan kode di gambar
 							</td></tr>
-						<tr><td>&nbsp;</td><td style=\"padding-top: 5px;\"><input type=\"submit\" value=\"Kirim\" style=\"font-size: 15px; padding: 3px 10px; \"></td></tr>
+						<tr class=\"submit\"><td>&nbsp;</td><td><input type=\"submit\" value=\"Kirim\"></td></tr>
 					</table>
 					</form>
 				</div>
