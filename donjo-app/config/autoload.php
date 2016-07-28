769d4303b48b88b88d70640cb070d183be0a3576
Eddie Ridwan <eddie.ridwan@amc.org.au>
2016-07-28 11:13:34 +0800

    Ganti nama helper file menjadi opensid_helper.php

--- donjo-app/config/autoload.php
+++ donjo-app/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('session','database');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url','donjolib','date','pict','sidjms');
+$autoload['helper'] = array('url','donjolib','date','pict','opensid');
 
 
 /*
