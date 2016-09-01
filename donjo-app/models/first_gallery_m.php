b1ec00954b7c0bab40e88775a18f0c74007ebeb3
Eddie Ridwan <eddie.ridwan@amc.org.au>
2016-09-01 10:44:16 +0800

    #108: galeri kosong sekarang tampil di arsip galeri apabila mempunyai sub-galeri bergambar

--- donjo-app/models/first_gallery_m.php
+++ donjo-app/models/first_gallery_m.php
@@ -5,76 +5,86 @@ class First_Gallery_M extends CI_Model{
 	function __construct(){
 		parent::__construct();
 	}
-	
+
 	function paging($p=1){
-	
+
 		$sql      = "SELECT COUNT(id) AS id FROM gambar_gallery WHERE enabled=1 AND tipe='0'";
 		$query    = $this->db->query($sql);
 		$row      = $query->row_array();
 		$jml_data = $row['id'];
-		
+
 		$this->load->library('paging');
 		$cfg['page']     = $p;
 		$cfg['per_page'] = 8;
 		$cfg['num_rows'] = $jml_data;
 		$this->paging->init($cfg);
-		
+
 		return $this->paging;
 	}
-	
+
 	function gallery_show($offset=0,$limit=50){
-	
+
 		$paging_sql = ' LIMIT ' .$offset. ',' .$limit;
-		
+
 		$sql   = "SELECT * FROM gambar_gallery WHERE enabled=1 AND tipe='0' ";
 		$sql .= $paging_sql;
-		
+
+		$query = $this->db->query($sql);
+		$data  = $query->result_array();
+		// Untuk album yang tidak ada gambar cover, cari gambar di sub-gallery
+		for ($i=0; $i<count($data); $i++) {
+			if ($data[$i]['gambar'] == '') {
+				$galeri = $data[$i]['id'];
+				$sql   = "SELECT gambar FROM gambar_gallery WHERE ((enabled='1') AND ((parrent='".$galeri."') OR (id='".$galeri."')) AND (gambar<>'')) LIMIT 1";
+				$query = $this->db->query($sql);
+				$row  = $query->row_array();
+				$data[$i]['gambar'] = $row['gambar'];
+			}
+		}
+		return $data;
+	}
+
+	function paging2($gal=0,$p=1){
+
+		$sql      = "SELECT COUNT(id) AS id FROM gambar_gallery WHERE enabled=1 AND parrent=?";
+		$query    = $this->db->query($sql,$gal);
+		$row      = $query->row_array();
+		$jml_data = $row['id'];
+
+		$this->load->library('paging');
+		$cfg['page']     = $p;
+		$cfg['per_page'] = 8;
+		$cfg['num_rows'] = $jml_data;
+		$this->paging->init($cfg);
+
+		return $this->paging;
+	}
+
+	function sub_gallery_show($gal=0,$offset=0,$limit=50){
+		$paging_sql = ' LIMIT ' .$offset. ',' .$limit;
+
+		$sql   = "SELECT * FROM gambar_gallery WHERE ((enabled='1') AND ((parrent='".$gal."') OR (id='".$gal."'))) ";
+		$sql .= $paging_sql;
+
 		$query = $this->db->query($sql);
 		$data  = $query->result_array();
 		return $data;
 	}
 
-	function paging2($gal=0,$p=1){
-	
-		$sql      = "SELECT COUNT(id) AS id FROM gambar_gallery WHERE enabled=1 AND parrent=?";
-		$query    = $this->db->query($sql,$gal);
-		$row      = $query->row_array();
-		$jml_data = $row['id'];
-		
-		$this->load->library('paging');
-		$cfg['page']     = $p;
-		$cfg['per_page'] = 8;
-		$cfg['num_rows'] = $jml_data;
-		$this->paging->init($cfg);
-		
-		return $this->paging;
-	}
-	
-	function sub_gallery_show($gal=0,$offset=0,$limit=50){
-		$paging_sql = ' LIMIT ' .$offset. ',' .$limit;
-		
-		$sql   = "SELECT * FROM gambar_gallery WHERE ((enabled='1') AND ((parrent='".$gal."') OR (id='".$gal."'))) ";
-		$sql .= $paging_sql;
-		
-		$query = $this->db->query($sql);
-		$data  = $query->result_array();
-		return $data;
-	}
-	
 	function get_parrent($parrent){
 		$sql   = "SELECT * FROM gambar_gallery WHERE id='$parrent'";
 		$query = $this->db->query($sql);
 		$data  = $query->row_array();
 		return $data;
 	}
-	
+
 	function gallery_widget(){
-		
+
 		$sql   = "SELECT * FROM gambar_gallery WHERE enabled='1' ORDER BY RAND() LIMIT 4";
 		$query = $this->db->query($sql);
 		$data  = $query->result_array();
 		return $data;
 	}
-	
+
 }
 
