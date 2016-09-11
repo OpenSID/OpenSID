76a99e786b505763da95d3653775432d613b4dc9
Eddie Ridwan <eddie.ridwan@amc.org.au>
2016-09-11 13:12:50 +0800

    #132: fitur cari di laman Laporan Masuk (layanan mandiri) sudah diperbaiki

--- donjo-app/views/lapor/table.php
+++ donjo-app/views/lapor/table.php
@@ -34,8 +34,8 @@ source: keyword
 </select>
 </div>
 <div class="right">
-<input name="cari" id="cari" type="text" class="inputbox help tipped" size="20" value="<?php echo $cari?>" title="Cari.." onkeypress="if (event.keyCode == 13) {$('#'+'mainform').attr('action','<?php echo site_url('komentar/search')?>');$('#'+'mainform').submit();}" />
-<button type="button" onclick="$('#'+'mainform').attr('action','<?php echo site_url('komentar/search')?>');$('#'+'mainform').submit();" class="uibutton tipsy south"title="Cari Data"><span class="icon-search icon-large">&nbsp;</span>Cari</button>
+<input name="cari" id="cari" type="text" class="inputbox help tipped" size="20" value="<?php echo $cari?>" title="Cari.." onkeypress="if (event.keyCode == 13) {$('#'+'mainform').attr('action','<?php echo site_url('lapor/search')?>');$('#'+'mainform').submit();}" />
+<button type="button" onclick="$('#'+'mainform').attr('action','<?php echo site_url('lapor/search')?>');$('#'+'mainform').submit();" class="uibutton tipsy south"title="Cari Data"><span class="icon-search icon-large">&nbsp;</span>Cari</button>
 </div>
 </div>
 <table class="list">
