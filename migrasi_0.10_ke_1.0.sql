cf0b70e5613dce764a8a89079368135092d7780f
Eddie Ridwan <eddie.ridwan@amc.org.au>
2016-09-21 14:07:07 +0800

    Tambahkan migrasi_0.10_ke_1.0.sql. Hapus migrasi yang lama

---
+++ migrasi_0.10_ke_1.0.sql
@@ -0,0 +1,10 @@
+--
+-- --------------------------------------------------------
+--
+-- Mengubah struktur database SID:
+--   1. menambahkan kode untuk pekerjaan LAINNYA di tweb_penduduk_pekerjaan
+--
+-- --------------------------------------------------------
+--
+
+INSERT INTO tweb_penduduk_pekerjaan(id, nama) VALUES (89, 'LAINNYA');
