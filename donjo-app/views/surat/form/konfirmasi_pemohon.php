202bdeb98c567841a5426739d775c8047e812820
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-02-17 21:21:03 +1300

    #303:(SID 3.10 Tampilkan daftar dokumen dan manajemen dokumen di masing2 form cetak surat

--- donjo-app/views/surat/form/konfirmasi_pemohon.php
+++ donjo-app/views/surat/form/konfirmasi_pemohon.php
@@ -23,3 +23,9 @@
     <?php echo $individu['warganegara']?> / <?php echo $individu['agama']?>
   </td>
 </tr>
+<tr>
+  <th>Dokumen Kelengkapan / Syarat</th>
+  <td>
+    <a header="Dokumen" target="ajax-modal" rel="dokumen" href="<?php echo site_url("penduduk/dokumen_list/$individu[id]")?>" class="uibutton special">Daftar Dokumen</a><a target="_blank" href="<?php echo site_url("penduduk/dokumen/$individu[id]")?>" class="uibutton confirm">Manajemen Dokumen</a> )* Atas Nama <?php echo $individu['nama']?> [<?php echo $individu['nik']?>]
+  </td>
+</tr>
