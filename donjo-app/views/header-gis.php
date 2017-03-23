65229345fea903d4882ed6f67a5bae6a49c75817
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-03-23 12:58:21 +0900

    Ganti http://maps.googleapis.com/maps/api/js ke https untuk yang ada hosting ssl

--- donjo-app/views/header-gis.php
+++ donjo-app/views/header-gis.php
@@ -37,7 +37,7 @@
 <script type="text/javascript" src="<?php echo base_url()?>assets/js/donjoscript/donjo.ui.attribut.js"></script>
 <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.validate.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url()?>assets/js/validasi.js"></script>
-<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=<?php echo config_item('google_key'); ?>"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<?php echo config_item('google_key'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url()?>assets/js/jscolor/jscolor.js"></script>
 </head>
 <body>
@@ -87,7 +87,7 @@
 
 
 <!-- NOTIFICATION
-<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=<?php echo config_item('google_key'); ?>"></script>--><?php  if($_SESSION['success']==1): ?>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<?php echo config_item('google_key'); ?>"></script>--><?php  if($_SESSION['success']==1): ?>
 <script type="text/javascript">
 $('document').ready(function(){
 notification('success','Data Berhasil Disimpan')();
