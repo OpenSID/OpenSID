4849d516fb089b50013bd69ead7ccedbea775324
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-03-28 15:20:08 +0900

    Ganti ke versi 1.12. Mutakhirkan contoh data awal. Tambahkan catatan rilis.

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", 'pra-1.12');
+define("VERSION", '1.12');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
