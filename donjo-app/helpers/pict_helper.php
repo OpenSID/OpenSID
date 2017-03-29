12fcb7a5dfaf543d90603128618aaad1a2f9345a
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-03-29 21:24:24 +0900

    Upload foto pengguna sekarang bisa menggunakan gambar .png

--- donjo-app/helpers/pict_helper.php
+++ donjo-app/helpers/pict_helper.php
@@ -5,36 +5,11 @@ function AmbilFoto($foto){
   return $file_foto;
 }
 
-function UploadFoto($fupload_name,$old_foto){
-  $vdir_upload = LOKASI_USER_PICT;
-  if($old_foto!="")
-	unlink($vdir_upload."kecil_".$old_foto);
-
-  $vfile_upload = $vdir_upload . $fupload_name;
-  move_uploaded_file($_FILES["foto"]["tmp_name"], $vfile_upload);
-  $im_src = imagecreatefromjpeg($vfile_upload);
-  $src_width = imageSX($im_src);
-  $src_height = imageSY($im_src);
-  if($src_width < $src_height){
-	  $dst_width = 100;
-	  $dst_height = ($dst_width/$src_width)*$src_height;
-	  $cut_height = ($dst_height - 100)/2;
-	  $im = imagecreatetruecolor(100,100);
-	  imagecopyresampled($im, $im_src, 0, -($cut_height), 0, $cut_height, $dst_width, $dst_height, $src_width, $src_height);
-
-  }else{
-	  $dst_height = 100;
-	  $dst_width = ($dst_height/$src_height)*$src_width;
-	  $cut_width = ($dst_width - 100)/2;
-	  $im = imagecreatetruecolor(100,100);
-	  imagecopyresampled($im, $im_src, -($cut_width), 0, $cut_width, 0, $dst_width, $dst_height, $src_width, $src_height);
-  }
-  imagejpeg($im,$vdir_upload ."kecil_".$fupload_name);
-  imagedestroy($im_src);
-  imagedestroy($im);
-
-  unlink($vfile_upload);
-  return true;
+function UploadFoto($fupload_name,$old_foto,$tipe_file=""){
+  $dimensi = array("width"=>100, "height"=>100);
+  if($old_foto!="") $old_foto = "kecil_".old_foto;
+  $nama_simpan = "kecil_".$fupload_name;
+  return UploadResizeImage(LOKASI_USER_PICT,$dimensi,"foto",$fupload_name,$nama_simpan,$old_foto,$tipe_file);
 }
 
 function UploadGambar($fupload_name,$old_gambar){
@@ -437,7 +412,7 @@ function UploadArea($fupload_name){
 
 function UploadLogo($fupload_name,$old_foto,$tipe_file){
   $dimensi = array("width"=>100, "height"=>100);
-  return UploadResizeImage(LOKASI_LOGO_DESA,$dimensi,"logo",$fupload_name,$old_foto,$tipe_file);
+  return UploadResizeImage(LOKASI_LOGO_DESA,$dimensi,"logo",$fupload_name,$fupload_name,$old_foto,$tipe_file);
 }
 
 function UploadLogox($fupload_name){
@@ -450,8 +425,9 @@ function UploadLogox($fupload_name){
 * $jenis_upload contoh "logo", "foto"
 * $dimensi = array("width"=>lebar, "height"=>tinggi)
 * $lokasi contoh LOKASI_LOGO_DESA
+* $nama_simpan contoh "kecil_".$fupload_name
 */
-function UploadResizeImage($lokasi,$dimensi,$jenis_upload,$fupload_name,$old_foto,$tipe_file){
+function UploadResizeImage($lokasi,$dimensi,$jenis_upload,$fupload_name,$nama_simpan,$old_foto,$tipe_file){
   // Hanya bisa upload jpeg atau png
   $mime_type_image = array("image/jpeg", "image/pjpeg", "image/png", "image/x-png");
   if(!in_array($tipe_file, $mime_type_image)){
@@ -490,9 +466,9 @@ function UploadResizeImage($lokasi,$dimensi,$jenis_upload,$fupload_name,$old_fot
     imagecopyresampled($im, $im_src, 0, 0, $cut_width, 0, $dst_width, $dst_height, $src_width, $src_height);
   }
   if($tipe_file == "image/jpeg" OR $tipe_file == "image/pjpeg")
-    imagejpeg($im,$vdir_upload .$fupload_name);
+    imagejpeg($im,$vdir_upload .$nama_simpan);
   elseif($tipe_file == "image/png" OR $tipe_file == "image/x-png")
-    imagepng($im,$vdir_upload .$fupload_name);
+    imagepng($im,$vdir_upload .$nama_simpan);
 
   imagedestroy($im_src);
   imagedestroy($im);
