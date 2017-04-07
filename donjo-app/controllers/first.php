56b0d489c535d3b7bb8f77ab5525af7d54859c7e
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-04-07 12:29:24 +1200

    #355: Sekarang hasil analisis bisa ditampilkan di modul web melalui menu statis (dari SID 3.10)

--- donjo-app/controllers/first.php
+++ donjo-app/controllers/first.php
@@ -229,25 +229,46 @@ class First extends CI_Controller{
 	}
 
 	function statistik($stat=0,$tipe=0){
-		$data['heading'] = $this->laporan_penduduk_model->judul_statistik($stat);
 		$data['teks_berjalan'] = $this->first_artikel_m->get_teks_berjalan();
 		$data['slide'] = $this->first_artikel_m->slide_show();
 		$data['desa'] = $this->first_m->get_data();
 		$data['menu_atas'] = $this->first_menu_m->list_menu_atas();
 		$data['menu_kiri'] = $this->first_menu_m->list_menu_kiri();
-		$data['stat'] = $this->laporan_penduduk_model->list_data($stat);
-		$data['jenis_laporan'] = $this->laporan_penduduk_model->jenis_laporan($stat);
-		$data['tipe'] = $tipe;
-
 		$data['w_cos']  = $this->first_artikel_m->cos_widget();
 		$this->web_widget_model->get_widget_data($data);
-
 		$data['data_config'] = $this->config_model->get_data();
+
+		$data['heading'] = $this->laporan_penduduk_model->judul_statistik($stat);
+		$data['jenis_laporan'] = $this->laporan_penduduk_model->jenis_laporan($stat);
+		$data['stat'] = $this->laporan_penduduk_model->list_data($stat);
+		$data['tipe'] = $tipe;
 		$data['st'] = $stat;
 
 		$this->load->view('layouts/stat.tpl.php',$data);
 	}
 
+	function data_analisis($stat="",$sb=0,$per=0){
+		$data['teks_berjalan'] = $this->first_artikel_m->get_teks_berjalan();
+		$data['slide'] = $this->first_artikel_m->slide_show();
+		$data['desa'] = $this->first_m->get_data();
+		$data['menu_atas'] = $this->first_menu_m->list_menu_atas();
+		$data['menu_kiri'] = $this->first_menu_m->list_menu_kiri();
+		$data['w_cos']  = $this->first_artikel_m->cos_widget();
+		$this->web_widget_model->get_widget_data($data);
+		$data['data_config'] = $this->config_model->get_data();
+
+		if($stat == ""){
+			$data['list_indikator'] = $this->first_penduduk_m->list_indikator();
+			$data['list_jawab'] = null;
+			$data['indikator'] = null;
+		}else{
+			$data['list_indikator'] = "";
+			$data['list_jawab'] = $this->first_penduduk_m->list_jawab($stat,$sb,$per);
+			$data['indikator'] = $this->first_penduduk_m->get_indikator($stat);
+		}
+
+		$this->load->view('layouts/analisis.tpl.php',$data);
+	}
 
 	function wilayah(){
 
