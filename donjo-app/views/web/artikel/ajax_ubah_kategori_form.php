1c7b5f4d5d3d239293966dd9027333d5e1675b24
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-05-02 08:02:35 +1000

    Sekarang kategori artikel dinamis bisa diubah

---
+++ donjo-app/views/web/artikel/ajax_ubah_kategori_form.php
@@ -0,0 +1,23 @@
+<form action="<?php echo $form_action?>" method="post" id="validasi">
+	<table style="width:100%">
+		<tr>
+			<th style="white-space:nowrap; padding-right: 15px;">Nama Kategori</th>
+			<td>
+				<select name="kategori">
+					<option value="">Kategori</option>
+					<?php  foreach($list_kategori AS $kategori){?>
+						<option
+							<?php  if($kategori_sekarang['id_kategori']==$kategori['id']) :?>selected<?php  endif?> value="<?php  echo $kategori['id']?>"><?php  echo $kategori['judul']?>
+						</option>
+					<?php  }?>
+				</select>
+			</td>
+		</tr>
+	</table>
+	<div class="buttonpane" style="text-align: right; width: 95%; position:absolute;bottom:0px;">
+	    <div class="uibutton-group">
+	        <button class="uibutton" type="button" onclick="$('#window').dialog('close');">Tutup</button>
+	        <button class="uibutton confirm" type="submit">Simpan</button>
+	    </div>
+	</div>
+</form>
