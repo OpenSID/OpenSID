0445d54b2bae3b4e054e02cf760a090daa603b10
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-05-05 22:04:08 +1000

    #414: Form peta lokasi penduduk sudah berfungsi lagi

--- donjo-app/views/sid/kependudukan/maps.php
+++ donjo-app/views/sid/kependudukan/maps.php
@@ -1,5 +1,4 @@
 <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery-1.5.2.min.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<?php echo config_item('google_key'); ?>"></script>
 <script>
 (function() {
         var mapOptions = {
