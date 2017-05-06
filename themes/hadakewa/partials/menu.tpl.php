62938c9cb990756141df2d1c502ec2e73b76b6bc
ahmad afandi <ahmad.afandi@sisi.id>
2017-05-06 07:10:56 +0700

    perbaikan themes setelah update fork dari server

--- themes/hadakewa/partials/menu.tpl.php
+++ themes/hadakewa/partials/menu.tpl.php
@@ -13,12 +13,12 @@
 	<?php foreach($menu_atas as $data){?>
 		<?php echo $data['menu']?>
 	<?php }?>
-  <li><a href="https://www.facebook.com/desahadakewa/" target="_blank"><span style="color:#083d74"><i class="fa fa-facebook-square fa-2x"></i></span></a></li>
-  <li><a href="#"><span style="color:#701b0b"><i class="fa fa-instagram fa-2x"></i></span></a></li>
-  <li><a href="#"><span style="color:#0ccdd9"><i class="fa fa-twitter-square fa-2x"></i></span></a></li>
-  <li><a href="#"><span style="color:#c2080c"><i class="fa fa-youtube-square fa-2x"></i></span></a></li>
-  <li><a href="#"><span style="color:#c2080c"><i class="fa fa-google-plus-square fa-2x"></i></span></a></li>
-  <li><a href="#"><span style="color:#e3a206"><i class="fa fa-rss-square fa-2x"></i></span></a></li>
+  <li><a href="https://www.facebook.com/desahadakewa/" target="_blank"><span style="color:#083d74"><i class="fa fa-facebook-square fa-3x"></i></span></a></li>
+  <li><a href="#"><span style="color:#701b0b"><i class="fa fa-instagram fa-3x"></i></span></a></li>
+  <li><a href="#"><span style="color:#0ccdd9"><i class="fa fa-twitter-square fa-3x"></i></span></a></li>
+  <li><a href="#"><span style="color:#c2080c"><i class="fa fa-youtube-square fa-3x"></i></span></a></li>
+  <li><a href="#"><span style="color:#c2080c"><i class="fa fa-google-plus-square fa-3x"></i></span></a></li>
+  <li><a href="#"><span style="color:#e3a206"><i class="fa fa-rss-square fa-3x"></i></span></a></li>
   <li><a href="<?php echo site_url('siteman') ?>"><i class="fa fa-lock fa-lg"></i>  Login Admin</a></li>
 	</ul>
 </div>
