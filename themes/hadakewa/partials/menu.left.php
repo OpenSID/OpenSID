d625c963b2c2b8a91250cfbadd97a4f4746f54bb
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-05-09 10:17:19 +1000

    Perbaikan tema hadakewa: css, dan tampilkan menu Beranda

--- themes/hadakewa/partials/menu.left.php
+++ themes/hadakewa/partials/menu.left.php
@@ -1,5 +1,6 @@
 <div id='cssmenu'>
 	<ul id="global-nav" class="main">
+		<li><a href="<?php echo site_url()."first"?>">Beranda</a></li>
 	<?php foreach($menu_kiri as $data){?>
 		<?php echo $data['menu']?>
 	<?php }?>
