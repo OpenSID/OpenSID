3acfd12680b19adb8df278eb6f0105d670360204
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-06-04 18:01:30 +1000

    #444: Sekarang gambar artikel di tampilkan ke slider-header. Jika diklik, artikel terkait ditampilkan.

--- themes/default/layouts/slider.php
+++ themes/default/layouts/slider.php
@@ -5,8 +5,16 @@
     <span class="cycle-prev"><img src="<?php echo base_url()?>assets/images/back_button.png" alt="Back"></span> <!-- Untuk membuat tanda panah di kiri slider -->
     <span class="cycle-next"><img src="<?php echo base_url()?>assets/images/next_button.png" alt="Next"></span><!-- Untuk membuat tanda panah di kanan slider -->
     <span class="cycle-pager"></span>  <!-- Untuk membuat tanda bulat atau link pada slider -->
-    <?php foreach($slider_photos as $foto): ?>
-			<img src="<?php echo base_url()?>desa/upload/galeri/<?php echo 'sedang_'.$foto['gambar']?>" alt="Logo dan kata kata">
-    <?php endforeach; ?>
+    <?php if($slide) : ?>
+	  	<?php foreach ($slide as $gambar) : ?>
+	    	<?php if(is_file(LOKASI_FOTO_ARTIKEL.'kecil_'.$gambar['gambar'])) : ?>
+			    <img src="<?php echo base_url().LOKASI_FOTO_ARTIKEL.'kecil_'.$gambar['gambar']?>" data-artikel="<?php echo $gambar['id']?>" onclick="tampil_artikel($(this).data('artikel'));">
+			   <?php endif; ?>
+		   <?php endforeach; ?>
+		<?php else : ?>
+	    <?php foreach($slider_photos as $foto): ?>
+				<img src="<?php echo base_url()?>desa/upload/galeri/<?php echo 'sedang_'.$foto['gambar']?>" alt="Logo dan kata kata">
+	    <?php endforeach; ?>
+	  <?php endif; ?>
 	</div>
 </div>
