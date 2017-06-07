1afea51739b1bd4921af5e1b20caf1a18173d21f
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-06-08 05:45:15 +1000

    Lengkapi catatan rilis 1.17. Ganti ke versi 1.17

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", 'pasca-1.16');
+define("VERSION", '1.17');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
