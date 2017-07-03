605e5751cd1365f6fcbe6bc12e3625351ab5e3de
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-07-03 22:23:30 +1000

    Tambah catatan rilis v2.1. Mutakhirkan contoh data awal. Ganti ke versi pra-v2.1

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", 'pasca-2.0');
+define("VERSION", 'pra-2.1');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
