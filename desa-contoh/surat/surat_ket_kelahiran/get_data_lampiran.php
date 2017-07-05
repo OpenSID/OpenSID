a9f621f068ea63543548c8e8ee25480d16b0d14e
fathurvbf <fathur_mail@yahoo.co.id>
2017-07-05 22:56:56 +0700

    updatedatasuratketerangankelahiran

---
+++ desa-contoh/surat/surat_ket_kelahiran/get_data_lampiran.php
@@ -0,0 +1,13 @@
+<?php
+$this->load->model('keluarga_model');
+$this->load->model('pamong_model');
+$anggota = $this->keluarga_model->list_anggota($individu['id_kk']);
+$desa = $this->keluarga_model->get_desa();
+// Gunakan data identitas desa, jika ada
+if ($desa['nip_kepala_desa']){
+	$kepala_desa['pamong_nama'] = $desa['nama_kepala_desa'];
+	$kepala_desa['pamong_nip'] = $desa['nip_kepala_desa'];
+} else
+	$kepala_desa = $this->pamong_model->get_pamong_by_nama($desa['nama_kepala_desa']);
+
+?>
