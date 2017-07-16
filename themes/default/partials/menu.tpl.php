938ce3797dc760490918d9be73322943833fbcf1
Happy Agung <metalabmetadata@gmail.com>
2017-07-16 19:31:32 +0800

    geser kekiri menu tema atas

--- themes/default/partials/menu.tpl.php
+++ themes/default/partials/menu.tpl.php
@@ -8,7 +8,7 @@
 <?php endif; ?>
 
 <nav class="navbar navbar-inverse">
-	<div class="container-fluid">
+	<div class="container-fluid-inverse">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 				<span class="sr-only">Toggle navigation</span>
