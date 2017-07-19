5a785f49ede6a6f40875fb67068c398d02b3c5a9
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-07-20 09:22:47 +1000

    Ganti ke versi 2.2

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", 'pra-2.2');
+define("VERSION", '2.2');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
