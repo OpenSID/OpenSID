8184b5b2d7cbfc3c4099f4e215491893a9551f0a
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-08-06 19:57:33 +1000

    Gunakan partial konfirmasi_pemohon.php untuk menampilkan data pemohon di surat_ket_kurang_mampu dan surat_ket_beda_identitas_kis

--- surat/surat_ket_beda_identitas_kis/surat_ket_beda_identitas_kis.php
+++ surat/surat_ket_beda_identitas_kis/surat_ket_beda_identitas_kis.php
@@ -58,32 +58,9 @@
 							<form id="validasi" action="" method="POST" target="_blank">
 								<input type="hidden" name="nik" value="<?php echo $individu['id']?>" class="inputbox required" >
 								<table class="form">
-									<?php if($individu){?>
-										<tr>
-											<th width="10">Tempat Tanggal Lahir (Umur)</th>
-											<td>
-												<?php echo $individu['tempatlahir']?> <?php echo tgl_indo($individu['tanggallahir'])?> (<?php echo $individu['umur']?> Tahun)
-											</td>
-										</tr>
-										<tr>
-											<th>Alamat</th>
-											<td><?php echo unpenetration($individu['alamat']); ?></td>
-										</tr>
-										<tr>
-											<th>Pendidikan</th>
-											<td><?php echo $individu['pendidikan']; ?></td>
-										</tr>
-										<tr>
-											<th>Warganegara / Agama</th>
-											<td><?php echo $individu['warganegara']?> / <?php echo $individu['agama']?></td>
-										</tr>
-										<tr>
-											<th>Dokumen Kelengkapan / Syarat</th>
-											<td>
-											<a header="Dokumen" target="ajax-modal" rel="dokumen" href="<?php echo site_url("penduduk/dokumen_list/$individu[id]")?>" class="uibutton special">Daftar Dokumen</a><a target="_blank" href="<?php echo site_url("penduduk/dokumen/$individu[id]")?>" class="uibutton confirm">Manajemen Dokumen</a> )* Atas Nama <?php echo $individu['nama']?> [<?php echo $individu['nik']?>]
-											</td>
-										</tr>
-									<?php }?>
+									<?php if($individu){
+										include("donjo-app/views/surat/form/konfirmasi_pemohon.php");
+									} ?>
 									<tr>
 										<th>DATA KELUARGA / KK </th>
 										<td></td>
--- surat/surat_ket_kurang_mampu/surat_ket_kurang_mampu.php
+++ surat/surat_ket_kurang_mampu/surat_ket_kurang_mampu.php
@@ -68,25 +68,8 @@
 					<table class="form">
 						<?php
 						if($individu){
+							include("donjo-app/views/surat/form/konfirmasi_pemohon.php");
 							?>
-							<tr>
-								<th width="200">Tempat Tanggal Lahir (Umur)</th>
-								<td>
-									<?php echo $individu['tempatlahir']?> <?php echo tgl_indo($individu['tanggallahir'])?> (<?php echo $individu['umur']?> Tahun)
-								</td>
-							</tr>
-							<tr>
-								<th>Alamat</th>
-								<td><?php echo unpenetration($individu['alamat']); ?></td>
-							</tr>
-							<tr>
-								<th>Pendidikan</th>
-								<td><?php echo $individu['pendidikan']; ?></td>
-							</tr>
-							<tr>
-								<th>Warganegara / Agama</th>
-								<td><?php echo $individu['warganegara']?> / <?php echo $individu['agama']?></td>
-							</tr>
 							<tr>
 								<th>Data Keluarga / KK </th>
 								<td>
@@ -134,12 +117,6 @@
 											</tbody>
 										</table>
 									</div>
-								</td>
-							</tr>
-							<tr>
-								<th>Dokumen Kelengkapan / Syarat</th>
-								<td>
-									<a header="Dokumen" target="ajax-modal" rel="dokumen" href="<?php echo site_url("penduduk/dokumen_list/$individu[id]")?>" class="uibutton special">Daftar Dokumen</a><a target="_blank" href="<?php echo site_url("penduduk/dokumen/$individu[id]")?>" class="uibutton confirm">Manajemen Dokumen</a> )* Atas Nama <?php echo $individu['nama']?> [<?php echo $individu['nik']?>]
 								</td>
 							</tr>
 						<?php }?>
