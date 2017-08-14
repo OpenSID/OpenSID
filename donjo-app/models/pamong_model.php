bf327267a1599f9e27242641189f3d4b9c7f9365
Ahmad Afandi <ahmad.afandi85@gmail.com>
2017-08-14 08:16:29 +0700

    perbaikan bisa menambah foto ketika tambah pengurus baru

--- donjo-app/models/pamong_model.php
+++ donjo-app/models/pamong_model.php
@@ -80,11 +80,27 @@
 		$nik        		= penetration($this->input->post('pamong_nik'));
 		$jabatan  	= penetration($this->input->post('jabatan'));
 		$status  		= penetration($this->input->post('pamong_status'));
+		$nama_file = '';
+		$lokasi_file = $_FILES['foto']['tmp_name'];
+		$tipe_file   = $_FILES['foto']['type'];
+		$nama_file   = $_FILES['foto']['name'];
+		$nama_file   = str_replace(' ', '-', $nama_file); 	 // normalkan nama file
 
-		$sql = "INSERT INTO tweb_desa_pamong (pamong_nama,pamong_nip,pamong_nik,jabatan,pamong_status,pamong_tgl_terdaftar)
-				VALUES (?,?,?,?,?,NOW())";
+		if(!empty($nama_file)){
+			if (!empty($lokasi_file) AND in_array($tipe_file, unserialize(MIME_TYPE_GAMBAR))){
+				UploadFoto($nama_file,$old_foto,$tipe_file);
+			} else {
+				$nama_file = '';
+				$_SESSION['success'] = -1;
+				$_SESSION['error_msg'] = " -> Jenis file salah: " . $tipe_file;
+			}
+		}
 
-		$outp = $this->db->query($sql, array($nama,$nip,$nik,$jabatan,$status));
+
+		$sql = "INSERT INTO tweb_desa_pamong (pamong_nama,pamong_nip,pamong_nik,jabatan,pamong_status,pamong_tgl_terdaftar,foto)
+				VALUES (?,?,?,?,?,NOW(),?)";
+
+		$outp = $this->db->query($sql, array($nama,$nip,$nik,$jabatan,$status,$nama_file));
 
 		if($outp) $_SESSION['success']=1;
 			else $_SESSION['success']=-1;
@@ -108,7 +124,7 @@
 				$_SESSION['error_msg'] = " -> Jenis file salah: " . $tipe_file;
 			}
 		}
-		
+
 		$data = array(
 			"pamong_nip" => $this->input->post('pamong_nip'),
 			"pamong_nama" =>$this->input->post('pamong_nama'),
