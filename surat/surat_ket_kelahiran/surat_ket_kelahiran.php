30a2c8b563ae663736e0d7925a7c81eed6f81f7e
Ayi Sumarna <ayisumarna@gmail.com>
2017-09-26 10:44:45 +0700

    ubah tulisan tombol dari clear menjadi bersihkan

--- surat/surat_ket_kelahiran/surat_ket_kelahiran.php
+++ surat/surat_ket_kelahiran/surat_ket_kelahiran.php
@@ -677,7 +677,7 @@ $('document').ready(function(){
             <a href="<?php echo site_url()?>surat" class="uibutton icon prev">Kembali</a>        </div>
         <div class="right">
             <div class="uibutton-group">
-                <button class="uibutton" type="reset">Clear</button>
+                <button class="uibutton" type="reset">Bersihkan</button>
 
 							<button type="button" onclick="$('#'+'validasi').attr('action','<?php echo $form_action?>');$('#'+'validasi').submit();" class="uibutton special"><span class="ui-icon ui-icon-print">&nbsp;</span>Cetak</button>
 							<?php if (SuratExport($url)) { ?><button type="button" onclick="$('#'+'validasi').attr('action','<?php echo $form_action2?>');$('#'+'validasi').submit();" class="uibutton confirm"><span class="ui-icon ui-icon-document">&nbsp;</span>Export Doc</button><?php } ?>
