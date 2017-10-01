d74a8a0b060f14fabb133efd14f93aa93cc38dfa
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-10-01 22:25:31 +1100

    Perbaiki penampilan tombol nonaktif di menu Sekretariat

--- donjo-app/views/sekretariat/nav.php
+++ donjo-app/views/sekretariat/nav.php
@@ -1,6 +1,6 @@
 <div id="nav">
 <ul>
-	<li class="disabled" <?php if($this->tab_ini == 1){?>class="selected"<?php }?>>
+	<li class="nonaktif" <?php if($this->tab_ini == 1){?>class="selected"<?php }?>>
 		<a href="#">Surat Keluar</a>
 	</li>
 	<li <?php if($this->tab_ini == 2){?>class="selected"<?php }?>>
@@ -12,7 +12,7 @@
 	<li <?php if($this->tab_ini == 4){?>class="selected"<?php }?>>
 		<a href="<?php echo site_url("dokumen_sekretariat/index/3")?>">PERDES</a>
 	</li>
-	<li class="disabled" <?php if($this->tab_ini == 5){?>class="selected"<?php }?>>
+	<li class="nonaktif" <?php if($this->tab_ini == 5){?>class="selected"<?php }?>>
 		<a href="#">Inventaris Desa</a>
 	</li>
 </ul>
