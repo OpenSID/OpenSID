d1bb108b75569940f6668a4440b7013b25cd2abc
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-10-02 22:10:18 +1100

    Di fitur lapor Layanan Mandiri sekarang nama Pengirim dan NIK tidak bisa diubah [security-fix]

--- themes/default/partials/lapor.php
+++ themes/default/partials/lapor.php
@@ -9,12 +9,12 @@ Silahkan laporkan perubahan data kependudukan anda.
 
 <tr>
 <th>Pengirim </th>
-<td> <input class="inputbox" type="text" name="owner" value="<?php echo $_SESSION['nama']?>" size="30"/></td>
+<td> <input class="inputbox" type="text" readonly="readonly" name="owner" value="<?php echo $_SESSION['nama']?>" size="30"/></td>
 </tr>
 
 <tr>
 <th>NIK</th>
-<td> <input class="inputbox" type="text" name="email" value="<?php echo $_SESSION['nik']?>" size="30"/></td>
+<td> <input class="inputbox" type="text" readonly="readonly" name="email" value="<?php echo $_SESSION['nik']?>" size="30"/></td>
 </tr>
 
 <tr>
