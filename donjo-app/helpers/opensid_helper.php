9063f97c9c1d2210eea17f1c1798242a5ea04b1c
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-11-01 15:38:25 +1100

    Ganti ke versi pasca-2.6

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", '2.6');
+define("VERSION", 'pasca-2.6');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
