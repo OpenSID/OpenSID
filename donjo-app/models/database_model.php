5c75eaee3db38dec5ceaf59f9d4451f5af8fa55d
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-11-02 12:29:10 +1100

    #684: Sesuaikan judul statistik kelompok umur dengan SID 3.10. Sesuaikan tampilan statistik di modul web dengan SID 3.10

--- donjo-app/models/database_model.php
+++ donjo-app/models/database_model.php
@@ -5,7 +5,8 @@
   private $versionMigrate = array(
     '2.4' => array('migrate' => 'migrasi_24_ke_25','nextVersion' => '2.5'),
     'pra-2.5' => array('migrate' => 'migrasi_24_ke_25','nextVersion' => NULL),
-    '2.5' => array('migrate' => 'migrasi_25_ke_26','nextVersion' => NULL)
+    '2.5' => array('migrate' => 'migrasi_25_ke_26','nextVersion' => '2.6'),
+    '2.6' => array('migrate' => 'migrasi_26_ke_27','nextVersion' => NULL)
   );
 
   function __construct(){
@@ -126,6 +127,37 @@
     $this->migrasi_23_ke_24();
     $this->migrasi_24_ke_25();
     $this->migrasi_25_ke_26();
+    $this->migrasi_26_ke_27();
+  }
+
+  function migrasi_26_ke_27(){
+    // Sesuaikan judul kelompok umur dengan SID 3.10 versi Okt 2017
+    $this->db->truncate('tweb_penduduk_umur');
+    $sql = '
+      INSERT INTO tweb_penduduk_umur VALUES
+      ("1","BALITA","0","5","0"),
+      ("2","ANAK-ANAK","6","17","0"),
+      ("3","DEWASA","18","30","0"),
+      ("4","TUA","31","120","0"),
+      ("6","Di bawah 1 Tahun","0","1","1"),
+      ("9","2 s/d 4 Tahun","2","4","1"),
+      ("12","5 s/d 9 Tahun","5","9","1"),
+      ("13","10 s/d 14 Tahun","10","14","1"),
+      ("14","15 s/d 19 Tahun","15","19","1"),
+      ("15","20 s/d 24 Tahun","20","24","1"),
+      ("16","25 s/d 29 Tahun","25","29","1"),
+      ("17","30 s/d 34 Tahun","30","34","1"),
+      ("18","35 s/d 39 Tahun ","35","39","1"),
+      ("19","40 s/d 44 Tahun","40","44","1"),
+      ("20","45 s/d 49 Tahun","45","49","1"),
+      ("21","50 s/d 54 Tahun","50","54","1"),
+      ("22","55 s/d 59 Tahun","55","59","1"),
+      ("23","60 s/d 64 Tahun","60","64","1"),
+      ("24","65 s/d 69 Tahun","65","69","1"),
+      ("25","70 s/d 74 Tahun","70","74","1"),
+      ("26","Di atas 75 Tahun","75","99999","1");
+    ';
+    $this->db->query($sql);
   }
 
   function migrasi_25_ke_26(){
