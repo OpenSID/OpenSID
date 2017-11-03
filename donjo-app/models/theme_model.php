7e7a33e145c400f08df8e07c916db68fca7dabf2
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-11-03 13:17:15 +1100

    Sekarang di halaman Setting Aplikasi, setting tema disediakan pilihan tema yang terpasang

---
+++ donjo-app/models/theme_model.php
@@ -0,0 +1,24 @@
+<?php class Theme_Model extends CI_Model{
+
+	function __construct(){
+		parent::__construct();
+	}
+
+	/*
+	 * Tema sistem ada di subfolder themes/
+	 * Tema buatan sistem ada di subfolder desa/themes/
+	*/
+	function list_all(){
+		$tema_sistem = glob('themes/*' , GLOB_ONLYDIR);
+		$tema_desa = glob('desa/themes/*' , GLOB_ONLYDIR);
+		$tema_semua = array_merge($tema_sistem, $tema_desa);
+		$list_tema = array();
+		foreach ($tema_semua as $tema){
+			$list_tema[] = str_replace('themes/', '', $tema);
+		}
+		return $list_tema;
+	}
+
+}
+?>
+
