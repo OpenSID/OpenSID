2bd51b32429d8777e3c1b3906aa39d3f2da17f5f
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-11-22 12:07:27 +1100

    Sediakan menu Pengaturan > Info Sistem untuk menampilkan info PHP untuk membantu melacak masalah

---
+++ donjo-app/views/setting/info_php.php
@@ -0,0 +1,20 @@
+<style type="text/css">
+	div#phpinfo {
+		font-size: 10pt;
+	}
+	#nav a:link {
+		background-color: transparent;
+	}
+</style>
+
+<div id="pageC">
+	<div id="contentpane">
+    <div class="ui-layout-center" id="maincontent" style="padding: 5px;">
+			<div id="phpinfo">
+				<?php
+					phpinfo();
+				?>
+			</div>
+    </div>
+	</div>
+</div>
