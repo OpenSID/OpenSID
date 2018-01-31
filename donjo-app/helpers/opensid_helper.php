8c64a48b1ff5427f379bfc870ee45179fb3035b9
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-02-01 05:39:28 +0700

    Ganti ke versi 2.9

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", 'pasca-2.8');
+define("VERSION", '2.9');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
