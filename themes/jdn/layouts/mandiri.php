70ef27a0232c8e1f5db9e21702cea49eb19a5bc9
adirahman <kodingku@github.com>
2018-02-26 19:28:56 +0700

    update theme baru jdn

---
+++ themes/jdn/layouts/mandiri.php
@@ -0,0 +1,39 @@
+<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<?php $this->load->view($folder_themes.'/layouts/header.php');?>
+			<div id="contentwrapper">
+				<div id="contentcolumn">
+					<div class="innertube">
+						<?php
+							$views_partial_layout = '';
+							switch($m){
+								case 1 :
+									$views_partial_layout = $folder_themes.'/partials/mandiri.php';
+									break;
+								case 2 :
+									$views_partial_layout = $folder_themes.'/partials/layanan.php';
+									break;
+								case 4 :
+									$views_partial_layout = $folder_themes.'/partials/bantuan.php';
+									break;
+								default:
+									$views_partial_layout = $folder_themes.'/partials/lapor.php';
+							}
+							$this->load->view($views_partial_layout);
+						?>
+					</div>
+				</div>
+			</div>
+
+			<div id="rightcolumn">
+				<div class="innertube">
+					<?php $this->load->view($folder_themes.'/partials/side.right.php');?>
+				</div>
+			</div>
+
+			<div id="footer">
+				<?php $this->load->view($folder_themes.'/partials/copywright.tpl.php');?>
+			</div>
+		</div>
+	</body>
+</html>
