8aeb7f97310d37015b55a9042f6e3e74a72f07fb
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-04-29 21:39:55 +1000

    Ubah config supaya environment default adalah production. Perbaiki tampilkan halaman error system

--- donjo-app/views/errors/html/error_db.php
+++ donjo-app/views/errors/html/error_db.php
@@ -1,3 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php
+	$ci = new CI_Controller();
+	$ci =& get_instance();
+	$ci->load->helper('url');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
