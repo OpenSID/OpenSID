8d03810803f62244032a361037a45785f49417b2
Yusuphm <supalpuket@gmail.com>
2018-05-08 15:10:55 +0700

    Hapus folder "themes" dan "asset" yang lama.

--- assets/front/js/layout.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  $_wrapperHeight = parseInt($('body').height());
-  $('#content').css({'height':$_wrapperHeight-20});
-});
--- themes/default/partials/breadcrumb.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-<?php if ($this->breadcrumbs->generate_breadcrumb()):?>
-	<?php echo $this->breadcrumbs->generate_breadcrumb();?><?php endif;?>
