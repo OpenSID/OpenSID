c14063a747a29295fac8a8c2fc1cdb5825e491ea
Ego Dafma Dasa <egodasa@gmail.com>
2018-05-12 23:39:36 +0700

    Perbaikan skrip

--- donjo-app/views/widgets/peta_wilayah_desa.php
+++ donjo-app/views/widgets/peta_wilayah_desa.php
@@ -1,23 +1,23 @@
-<!-- widget Peta Lokasi Kantor Desa -->
+<!-- widget Peta Wilayah Desa -->
   <div class="box box-default box-solid">
     <div class="box-header">
       <h3 class="box-title">
         <i class="fa fa-map-marker"></i>
-        <?php echo "Lokasi Kantor ". ucwords($this->setting->sebutan_desa); ?>"</h3>
+        <?php echo "Wilayah ". ucwords($this->setting->sebutan_desa); ?>"</h3>
     </div>
     <div class="box-body">
-      <div id="map_canvas" style="height:200px;"></div>
+      <div id="map_wilayah" style="height:200px;"></div>
       <a href="https://www.openstreetmap.org/#map=15/<?php echo $data_config['lat']."/".$data_config['lng']; ?>">Buka peta</a>
     </div>
   </div>
 
   <script>
-    <?php
+        <?php
 			//Jika posisi kantor desa belum ada, maka posisi peta akan menampilkan seluruh Indonesia
 			if(!empty($data_config['lat']) && !empty($data_config['lng'])){
 		?>
 			var posisi = [<?php echo $data_config['lat'].",".$data_config['lng']; ?>];
-      var zoom = <?php echo $data_config['zoom']; ?>;
+            var zoom = <?php echo $data_config['zoom']; ?>;
 		<?
 			}else{
 		?>
@@ -27,17 +27,19 @@
 			}
 		?>
 
-    var lokasi_kantor = L.map('map_canvas').setView(posisi, zoom);
+    var wilayah_desa = L.map('map_wilayah').setView(posisi, zoom);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      id: 'mapbox.streets'
-    }).addTo(lokasi_kantor);
+      id: 'wilayah_desa'
+    }).addTo(wilayah_desa);
     <?php
-			//Jika posisi kantor desa belum ada, maka posisi peta akan menampilkan seluruh Indonesia
-			if(!empty($data_config['lat']) && !empty($data_config['lng'])){
-		?>
-			var kantor_desa = L.marker(posisi).addTo(lokasi_kantor);
+			//Jika wilayah belum ada, maka posisi peta akan menampilkan seluruh Indonesia
+			if(!empty($data_config['path'])){
+		?>  
+            var polygon_desa = <?php echo $data_config['path']; ?>;
+			var kantor_desa = L.polygon(polygon_desa).addTo(wilayah_desa);
+            wilayah_desa.fitBounds(kantor_desa.getBounds());
 		<?
 			}
 		?>
