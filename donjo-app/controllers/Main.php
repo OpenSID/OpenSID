dd4c276d0f880240aa70aa6ec42ee72601a31650
Yusuphm <supalpuket@gmail.com>
2018-05-14 22:50:13 +0700

    Keterbacaan kode lebih baik.
    - Ganti metode pengecekan sesi; cukup membaca variabel ```$this->session->user_id```
    - Gunakan konstanta untuk grup user

--- donjo-app/controllers/Main.php
+++ donjo-app/controllers/Main.php
@@ -1,36 +1,33 @@
 <?php
-class Main extends CI_Controller {
+define('GRUP_ADMINISTRATOR', 1);
+define('GRUP_OPERATOR',      2);
+define('GRUP_REDAKSI',       3);
+define('GRUP_KONTRIBUTOR',   4);
+ 
+class Main extends CI_Controller
+{
 
-	function __construct(){
-		parent::__construct();
-	}
 
-	function index(){
-		if (isset($_SESSION['siteman']) AND $_SESSION['siteman'] == 1){
-			$this->load->model('user_model');
-			$grup	= $this->user_model->sesi_grup($_SESSION['sesi']);
-			switch($grup){
-				case 1: redirect('hom_desa'); break;
-				case 2: redirect('hom_desa'); break;
-				case 3: redirect('web'); break;
-				case 4: redirect('web'); break;
-				default: {
-					if ($this->setting->offline_mode > 0) {
-						redirect('siteman');
-
-					} else {
-						redirect('first');
-					}
-				}
-			}
-
-		// Jika offline_mode aktif, tidak perlu menampilkan halaman website
-		} else if ($this->setting->offline_mode > 0) {
-			redirect('siteman');
-
-		} else {
-			redirect('first');
-		}
-
-	}
+    function index() {
+        if ($this->session->user_id) {
+            $this->load->model('user_model');
+            switch($this->session->role) {
+                case GRUP_ADMINISTRATOR:
+                case GRUP_OPERATOR:
+                    redirect('hom_desa');
+                    break;
+                
+                case GRUP_REDAKSI:
+                case GRUP_KONTRIBUTOR:
+                    redirect('web');
+            }
+            return;
+        }
+        // Jika offline_mode aktif, tidak perlu menampilkan halaman website
+        if ($this->setting->offline_mode > 0) {
+            redirect('siteman');
+        } else {
+            redirect('first');
+        }
+    }
 }
