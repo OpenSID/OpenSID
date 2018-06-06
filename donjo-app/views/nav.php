75b28021bc45320ac31126f70456b846b69c0d3c
fitamrin <fitamrin@yahoo.com>
2018-06-06 14:29:29 +0700

    #1051 Penambahan Lambang Desa dan identitas desa (nama desa, kec dan kab)

--- donjo-app/views/nav.php
+++ donjo-app/views/nav.php
@@ -2,11 +2,32 @@
 <!-- Nantinya seluruh file nav.php (sub modul) yang ada di masing-masing folder modul utama akan dihapus (sudah tidak digunakan lagi)  -->
 <aside class="main-sidebar">		
 	<section class="sidebar">	
+		<div class="user-panel">
+			<div class="pull-left image">
+				<img src="<?php echo LogoDesa($desa['logo']);?>" class="img-circle" alt="User Image">
+			</div>        
+			<div class="pull-left info">
+				<p><?php echo ucwords($this->setting->sebutan_desa." ".$desa['nama_desa']); ?></p>
+				<?php 
+					$nam_kec = strlen(unpenetration($desa['nama_kecamatan']));
+					$nam_kab = strlen(unpenetration($desa['nama_kabupaten']));
+					if ($nam_kec<=12 AND $nam_kab<=12){
+						echo ucwords($this->setting->sebutan_kecamatan." ".unpenetration($desa['nama_kecamatan']));
+						echo "</br>";
+						echo ucwords($this->setting->sebutan_kabupaten." ".unpenetration($desa['nama_kabupaten']));
+					}else{
+						echo ucwords(substr($this->setting->sebutan_kecamatan,0,3).". ".unpenetration($desa['nama_kecamatan']));
+						echo "</br>";
+						echo ucwords(substr($this->setting->sebutan_kabupaten,0,3).". ".unpenetration($desa['nama_kabupaten']));
+					}
+					
+				?>				
+				
+			</div>
+		</div>	
 		<ul class="sidebar-menu" data-widget="tree">
 			<li class="header">MENU UTAMA</li>
-			<?php foreach ($modul AS $mod){?>
-				
-
+			<?php foreach ($modul AS $mod){?>	
 			<li class="<?php if($this->modul_ini==$mod['id']){?>active<?php }?> <?php if(count($mod['submodul'])>0) {?>treeview<?php } ?>">
 				<a href="<?php echo site_url()?><?php echo $mod['url']?>">
 					<i class="fa <?php echo $mod['ikon']?> <?php if($this->modul_ini==$mod['id']){?>text-aqua<?php }?>"></i> <span><?php echo $mod['modul']?></span>	
