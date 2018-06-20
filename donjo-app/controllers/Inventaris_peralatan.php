438304e111c2c52626a0daa2f8089a82f9e7b45a
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-06-20 11:55:04 +1000

    Perbaiki coding style file controller dan model modul Inventaris

--- donjo-app/controllers/Inventaris_peralatan.php
+++ donjo-app/controllers/Inventaris_peralatan.php
@@ -1,13 +1,16 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-class inventaris_peralatan extends CI_Controller{
+class inventaris_peralatan extends CI_Controller
+{
 
-	function __construct(){
+	function __construct()
+	{
 		parent::__construct();
 		session_start();
 		$this->load->model('user_model');
 		$grup	= $this->user_model->sesi_grup($_SESSION['sesi']);
-		if($grup!=1 AND $grup!=2) {
+		if ($grup!=1 AND $grup!=2)
+		{
 			$_SESSION['request_uri'] = $_SERVER['REQUEST_URI'];
 			redirect('siteman');
 		}
@@ -21,14 +24,15 @@ class inventaris_peralatan extends CI_Controller{
 		$this->controller = 'Inventaris_peralatan';
 	}
 
-	function clear(){
+	function clear()
+	{
 		unset($_SESSION['cari']);
 		unset($_SESSION['filter']);
 		redirect('Inventaris_peralatan');
 	}
 
-	function index(){
-	
+	function index()
+	{
 		$data['main'] = $this->inventaris_peralatan_model->list_inventaris();
 		$data['total'] = $this->inventaris_peralatan_model->sum_inventaris();
 		$data['pamong'] = $this->surat_model->list_pamong();
@@ -39,8 +43,8 @@ class inventaris_peralatan extends CI_Controller{
 		$this->load->view('footer');
 	}
 
-	function view($id){
-		
+	function view($id)
+	{
 		$data['main'] = $this->inventaris_peralatan_model->view($id);
 		$header = $this->header_model->get_data();
 		$this->load->view('header', $header);
@@ -49,8 +53,8 @@ class inventaris_peralatan extends CI_Controller{
 		$this->load->view('footer');
 	}
 
-	function view_mutasi($id){
-	
+	function view_mutasi($id)
+	{
 		$data['main'] = $this->inventaris_peralatan_model->view_mutasi($id);
 		$header = $this->header_model->get_data();
 		$this->load->view('header', $header);
@@ -59,8 +63,8 @@ class inventaris_peralatan extends CI_Controller{
 		$this->load->view('footer');
 	}
 
-	function edit($id){
-	
+	function edit($id)
+	{
 		$data['main'] = $this->inventaris_peralatan_model->view($id);
 		$header = $this->header_model->get_data();
 		$this->load->view('header', $header);
@@ -69,8 +73,8 @@ class inventaris_peralatan extends CI_Controller{
 		$this->load->view('footer');
 	}
 
-	function edit_mutasi($id){
-	
+	function edit_mutasi($id)
+	{
 		$data['main'] = $this->inventaris_peralatan_model->edit_mutasi($id);
 		$header = $this->header_model->get_data();
 		$this->load->view('header', $header);
@@ -78,8 +82,8 @@ class inventaris_peralatan extends CI_Controller{
 		$this->load->view('inventaris/peralatan/edit_mutasi',$data);
 		$this->load->view('footer');
 	}
-	function form(){
-	
+	function form()
+	{
 		$header = $this->header_model->get_data();
 		$this->load->view('header', $header);
 		$this->load->view('inventaris/nav',$nav);
@@ -87,8 +91,8 @@ class inventaris_peralatan extends CI_Controller{
 		$this->load->view('footer');
 	}
 
-	function form_mutasi($id){
-	
+	function form_mutasi($id)
+	{
 		$data['main'] = $this->inventaris_peralatan_model->view($id);
 		$header = $this->header_model->get_data();
 		$this->load->view('header', $header);
@@ -97,11 +101,9 @@ class inventaris_peralatan extends CI_Controller{
 		$this->load->view('footer');
 	}
 
-	
-	function mutasi(){
-
+	function mutasi()
+	{
 		$data['main'] = $this->inventaris_peralatan_model->list_mutasi_inventaris();
-
 		$header = $this->header_model->get_data();
 		$this->load->view('header', $header);
 		$this->load->view('inventaris/nav',$nav);
@@ -109,22 +111,22 @@ class inventaris_peralatan extends CI_Controller{
 		$this->load->view('footer');
 	}
 
-	function cetak($tahun,$penandatangan){
+	function cetak($tahun, $penandatangan)
+	{
 		$data['header'] = $this->header_model->get_config();
 		$data['total'] = $this->inventaris_peralatan_model->sum_print($tahun);
 		$data['print'] = $this->inventaris_peralatan_model->cetak($tahun);
 		$data['pamong'] = $this->inventaris_peralatan_model->pamong($penandatangan);
 		$this->load->view('inventaris/peralatan/inventaris_print',$data);
-
 	}
 
-	function download($tahun,$penandatangan){
+	function download($tahun, $penandatangan)
+	{
 		$data['header'] = $this->header_model->get_config();
 		$data['total'] = $this->inventaris_peralatan_model->sum_print($tahun);
 		$data['print'] = $this->inventaris_peralatan_model->cetak($tahun);
 		$data['pamong'] = $this->inventaris_peralatan_model->pamong($penandatangan);
 		$this->load->view('inventaris/peralatan/inventaris_excel',$data);
-
 	}
 
 }
