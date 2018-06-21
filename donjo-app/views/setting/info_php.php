215fe4c24408fb2ff819893d66fcf8010043bcbb
fitamrin <fitamrin@yahoo.com>
2018-06-21 11:48:04 +0700

    Perbaikan issue #1076 pada tampilan info php

--- donjo-app/views/setting/info_php.php
+++ donjo-app/views/setting/info_php.php
@@ -1,26 +1,3 @@
-<style type="text/css">
-	.tdc {
-		padding-top:5px;
-		padding-bottom:5px;
-		border-bottom: 1px solid #e5e5e5;
-	}
-	.tdc2 {
-		border-bottom: 1px solid #e5e5e5;
-	}
-</style>
-<?php
-   ob_start();
-   phpinfo();
-   $Ausgabe = ob_get_contents();
-   ob_end_clean();
-   preg_match_all("=<body[^>]*>(.*)</body>=siU", $Ausgabe, $a);
-	 $phpinfo = $a[1][0];
-   $phpinfo = str_replace( '<table border="0" cellpadding="3" width="600">', '<div class="table-responsive"><table class="table table-striped table-hover"><tbdoy>', $phpinfo );
-	 $phpinfo = str_replace( '<tr class="h">', '<tr class="text-center">', $phpinfo );
-	 $phpinfo = str_replace( '<td class="e">', '<td class="tdc">', $phpinfo );
-   $phpinfo = str_replace( '<td class="v">', '<td class="tdc2">', $phpinfo );
-	 $phpinfo = str_replace( '</table>', '</tbody></table></div>', $phpinfo );
-?>
 <div class="content-wrapper">
 	<section class="content-header">
 		<h1>Info Sistem</h1>
@@ -35,8 +12,42 @@
 				<div class="box box-primary">
 					<div class="box-body">
 						<div class="row">
-							<div class="col-sm-10">
-								<?=$phpinfo?>
+							<div class="col-sm-11">
+                <?php
+									ob_start();
+									phpinfo();
+									$phpinfo = array('phpinfo' => array());
+									if(preg_match_all('#(?:<h2>(?:<a name=".*?">)?(.*?)(?:</a>)?</h2>)|(?:<tr(?: class=".*?")?><t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>)?)?</tr>)#s', ob_get_clean(), $matches, PREG_SET_ORDER)):
+										foreach($matches as $match):
+											if(strlen($match[1])):
+												$phpinfo[$match[1]] = array();
+											elseif(isset($match[3])):
+												$phpinfo[end(array_keys($phpinfo))][$match[2]] = isset($match[4]) ? array($match[3], $match[4]) : $match[3];
+											else:
+												$phpinfo[end(array_keys($phpinfo))][] = $match[2];
+											endif;
+										endforeach;
+								?>
+										<?php $i = 0;?>
+                    <?php foreach($phpinfo as $name => $section):?>
+											<?php $i++;?>
+											<?php if($i==1):?>
+                      	<h3><div class='table-responsive'><table class='table table-bordered dataTable table-hover'>
+											<?php else:?>
+												<h3><?=$name?></h3><div class='table-responsive'><table class='table table-bordered dataTable table-hover'>
+											<?php endif?>
+											<?php foreach($section as $key => $val): ?>
+												<?php if(is_array($val)):?>
+                          <tr><td class="col-md-4 info"><?=$key?></td><td><?=$val[0]?></td><td><?=$val[1]?></td></tr>
+												<?php elseif(is_string($key)):?>
+                          <tr><td class="col-md-4 info"><?=$key?></td><td colspan='2'><?=$val?></td></tr>
+												<?php else:?>
+                          <tr><td class="btn-primary" colspan='3'><?=$val?></td></tr>
+												<?php endif;?>
+                      <?php endforeach;?>
+                      </table></div>
+										<?php endforeach;?>
+									<?php endif;?>
 							</div>
 						</div>
 					</div>
