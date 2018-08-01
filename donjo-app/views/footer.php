c9617d91b0a713f2493602bc04a1cd9cacabe7f5
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-08-02 09:50:24 +1000

    Perbaiki tulisan pengembang aplikasi di footer

--- donjo-app/views/footer.php
+++ donjo-app/views/footer.php
@@ -2,7 +2,7 @@
 				<div class="pull-right hidden-xs">
 			  	<b>Version</b> <?= AmbilVersi()?>
 				</div>
-				<strong>Aplikasi <a href="https://github.com/OpenSID/OpenSID" target="_blank"> OpenSID</a> Berbasis SID, Dikembangkan oleh <a href="http://www.combine.or.id" target="_blank">Combine.or.id</a>.</strong>
+				<strong>Aplikasi <a href="https://github.com/OpenSID/OpenSID" target="_blank"> OpenSID</a>, dikembangkan oleh <a href="https://www.facebook.com/groups/OpenSID/" target="_blank">Komunitas OpenSID</a>.</strong>
 			</footer>
 		</div>
 
