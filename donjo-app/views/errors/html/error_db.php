795d50767579e13647a3f518613ecc42b8b36f22
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-08-11 11:57:55 +1000

    #1265, #1046: Sekarang pesan error akan tampil secara lebih informatif. [bug-fix]

--- donjo-app/views/errors/html/error_db.php
+++ donjo-app/views/errors/html/error_db.php
@@ -1,16 +1,11 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<?php
-	$ci = new CI_Controller();
-	$ci =& get_instance();
-	$ci->load->helper('url');
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Database Error</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/errors.css'); ?>" />
+	<link rel="stylesheet" type="text/css" href="/assets/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="/assets/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="/assets/css/AdminLTE.css" />
 </head>
 <body>
 <div class="container">
@@ -18,15 +13,15 @@
 		<h2 class="headline text-danger"> 500</h2>
 
 		<div class="error-content">
-			<h3><i class="fa fa-warning text-danger"></i> <?php echo strip_tags($heading); ?></h3>
+			<h3><i class="fa fa-warning text-danger"></i> <?= strip_tags($heading); ?></h3>
 			<?php
 			error_log(strip_tags($message));
 			?>
 			<p>
-				<?php echo $message; ?>
+				<?= $message; ?>
 
 				Harap laporkan masalah ini, agar kami dapat mencarikan solusinya.
-				Untuk sementara Anda dapat kembali ke halaman <a href="<?php echo base_url(); ?>">awal</a>.
+				Untuk sementara Anda dapat kembali ke halaman <a href="<?= APP_URL ?>">awal</a>.
 			</p>
 		</div>
 
