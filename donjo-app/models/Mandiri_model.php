dd8ac8673681cb66b07747976a4055ff51193c2f
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-08-13 21:04:23 +1000

    Sesuaikan dgn aturan penulisan script. Haruskan penggunaan angka. Sederhanakan struktur.

--- donjo-app/models/Mandiri_model.php
+++ donjo-app/models/Mandiri_model.php
@@ -116,32 +116,39 @@
 
 
 	public function insert()
-    {
-        if ($_POST['nik'] == "") {
-            redirect("mandiri");
-        }
-        // load library form_validation
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('pin', 'Pin', 'trim|required|min_length[6]|max_length[6]');
-
-        if ($this->form_validation->run() == true) {
-            $sql  = "DELETE FROM tweb_penduduk_mandiri WHERE nik=?";
-            $outp = $this->db->query($sql, array($_POST['nik']));
-            $rpin            = $this->generate_pin($_POST['pin']);
-            $hash_pin        = hash_pin($rpin);
-            $data['pin']     = $hash_pin;
-            $data['nik']     = $_POST['nik'];
-            $data['id_pend'] = $this->db->select('id')->where('nik', $_POST['nik'])
-                ->get('tweb_penduduk')->row()->id;
-            $data['tanggal_buat'] = date("Y-m-d H:i:s");
-            $outp = $this->db->insert('tweb_penduduk_mandiri', $data);
-            if ($_POST['pin'] != "") {
-                return $_POST['pin'];
-            } else {
-                return $rpin;
-            }
-        }
+  {
+    if ($_POST['nik'] == "") {
+        redirect("mandiri");
     }
+    // load library form_validation
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('pin', 'Pin', 'trim|numeric|required|min_length[6]|max_length[6]');
+
+    if ($this->form_validation->run() !== true)
+    {
+    	$_SESSION['error_msg'] = 'PIN harus 6 (enam) digit angka.';
+    	return;
+    }
+
+    $sql  = "DELETE FROM tweb_penduduk_mandiri WHERE nik=?";
+    $outp = $this->db->query($sql, array($_POST['nik']));
+    $rpin            = $this->generate_pin($_POST['pin']);
+    $hash_pin        = hash_pin($rpin);
+    $data['pin']     = $hash_pin;
+    $data['nik']     = $_POST['nik'];
+    $data['id_pend'] = $this->db->select('id')->where('nik', $_POST['nik'])
+        ->get('tweb_penduduk')->row()->id;
+    $data['tanggal_buat'] = date("Y-m-d H:i:s");
+    $outp = $this->db->insert('tweb_penduduk_mandiri', $data);
+    if ($_POST['pin'] != "")
+    {
+      return $_POST['pin'];
+    }
+    else
+    {
+      return $rpin;
+    }
+  }
 
 	function delete($id_pend=''){
 		$sql  = "DELETE FROM tweb_penduduk_mandiri WHERE id_pend=?";
