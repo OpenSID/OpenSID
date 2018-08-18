10e1ff0c55ccdeae43ac38655e164ee3ef4b2a37
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-08-19 08:37:50 +1000

    Setelah login tampilkan SID home

--- donjo-app/controllers/Main.php
+++ donjo-app/controllers/Main.php
@@ -11,8 +11,8 @@ class Main extends CI_Controller {
 			$this->load->model('user_model');
 			$grup	= $this->user_model->sesi_grup($_SESSION['sesi']);
 			switch($grup){
-				case 1: redirect('hom_desa'); break;
-				case 2: redirect('hom_desa'); break;
+				case 1: redirect('hom_sid'); break;
+				case 2: redirect('hom_sid'); break;
 				case 3: redirect('web'); break;
 				case 4: redirect('web'); break;
 				default: {
