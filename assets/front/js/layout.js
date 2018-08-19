f9f5925e4251dad5e1461ad2e4f93f86b98c040a
Yusuphm <supalpuket@gmail.com>
2018-08-20 01:13:34 +0700

    Setting anti CSRF di frontend

--- assets/front/js/layout.js
+++ assets/front/js/layout.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
-  $_wrapperHeight = parseInt($('body').height());
-  $('#content').css({'height':$_wrapperHeight-20});
-});
+	$_wrapperHeight = parseInt($('body').height());
+	$('#content').css({'height':$_wrapperHeight-20});
+
+	var csrf_param = '_csrf'
+	var csrf_token = document.cookie.match(new RegExp(csrf_param +'=(\\w+)'))[1]
+	$('form').each(function(i, form) {
+		if (form.method.toLowerCase() !== 'post') {
+			return
+		}
+		if (!form[csrf_param]) {
+			$(form).append($('<input type=hidden name='+ csrf_param +'>'))
+		}
+		form[csrf_param].value = csrf_token
+	})
+})
