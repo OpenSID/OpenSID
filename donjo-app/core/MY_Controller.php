498d6ae9ebcad90a28d4ceef24aa9e78679f187b
Yusuphm <supalpuket@gmail.com>
2018-08-23 12:34:20 +0700

    Perbaikan tidak tampil data statistik di frontend

--- donjo-app/core/MY_Controller.php
+++ donjo-app/core/MY_Controller.php
@@ -100,31 +100,30 @@ class MY_Controller extends CI_Controller {
 
 }
 
-class Web_Controller extends MY_Controller {
+class Web_Controller extends MY_Controller
+{
 	/**
-  	* Constructor
- 	*/
-	function __construct()
-  {
-    parent::__construct();
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		parent::__construct();
 		$this->includes['folder_themes'] = '../../'.$this->theme_folder.'/'.$this->theme;
-  }
-
-  // Jika file theme/view tidak ada, gunakan file default/view
-  // Supaya tidak semua layout atau partials harus diulangi untuk setiap tema
-  function fallback_default($theme, $view)
-  {
-	if (is_object($this)) {
-		$theme_folder = $this->theme_folder;
-	} else {
-		$tmp = new self();
-		$theme_folder = $tmp->theme_folder;
 	}
 
-    $theme_file = FCPATH . $theme_folder . '/' . $theme . $view;
-    if (!is_file($theme_file)) $theme_view = '../../themes/default' . $view;
-    else $theme_view = '../../' . $theme_folder . '/' . $theme . $view;
-    return $theme_view;
-  }
+	// Jika file theme/view tidak ada, gunakan file default/view
+	// Supaya tidak semua layout atau partials harus diulangi untuk setiap tema
+	public static function fallback_default($theme, $view)
+	{
+		$view = trim($view, '/');
+		$theme_folder = self::get_instance()->theme_folder;
+		$theme_view = "../../$theme_folder/$theme/$view";
 
+		if (!is_file(APPPATH .'views/'. $theme_view))
+		{
+			$theme_view = "../../themes/default/$view";
+		}
+
+		return $theme_view;
+	}
 }
