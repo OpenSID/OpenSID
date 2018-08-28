7940f190a257c7a6bb77573ef3ed0cfab57fcb1c
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-08-29 09:31:31 +1000

    #1648: Sekarang apabila tidak diisi, PIN acak akan dihasilkan pada waktu membuat PIN untuk pendaftar Layanan Mandiri. [bug-fix]

--- donjo-app/models/Mandiri_model.php
+++ donjo-app/models/Mandiri_model.php
@@ -106,36 +106,43 @@
 		return $data;
 	}
 
-	function generate_pin($pin=""){
-		if($pin==""){
-			$pin = rand(100000,999999);
+	private function generate_pin($pin="")
+	{
+		if ($pin == "")
+		{
+			$pin = rand(100000, 999999);
 			$pin = strrev($pin);
 		}
 		return $pin;
 	}
-
 
 	public function insert()
   {
     if ($_POST['nik'] == "") {
         redirect("mandiri");
     }
-    // load library form_validation
-    $this->load->library('form_validation');
-    $this->form_validation->set_rules('pin', 'Pin', 'trim|numeric|required|min_length[6]|max_length[6]');
-
-    if ($this->form_validation->run() !== true)
+    if (empty($_POST['pin']))
     {
-    	$_SESSION['error_msg'] = 'PIN harus 6 (enam) digit angka.';
-    	return;
+    	$rpin = $this->generate_pin($_POST['pin']);
+    }
+    else
+    {
+	    // load library form_validation
+	    $this->load->library('form_validation');
+	    $this->form_validation->set_rules('pin', 'Pin', 'trim|numeric|required|min_length[6]|max_length[6]');
+	    if ($this->form_validation->run() !== true)
+	    {
+	    	$_SESSION['error_msg'] = 'PIN harus 6 (enam) digit angka.';
+	    	return;
+	    }
+	    $rpin = $_POST['pin'];
     }
 
-    $sql  = "DELETE FROM tweb_penduduk_mandiri WHERE nik=?";
+    $sql = "DELETE FROM tweb_penduduk_mandiri WHERE nik=?";
     $outp = $this->db->query($sql, array($_POST['nik']));
-    $rpin            = $this->generate_pin($_POST['pin']);
-    $hash_pin        = hash_pin($rpin);
-    $data['pin']     = $hash_pin;
-    $data['nik']     = $_POST['nik'];
+    $hash_pin = hash_pin($rpin);
+    $data['pin'] = $hash_pin;
+    $data['nik'] = $_POST['nik'];
     $data['id_pend'] = $this->db->select('id')->where('nik', $_POST['nik'])
         ->get('tweb_penduduk')->row()->id;
     $data['tanggal_buat'] = date("Y-m-d H:i:s");
