81bfcbf695a7b569a7bd8a6ebf6367d0d9eef5f3
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-09-05 21:34:32 +1200

    Teruskan merapikan controller sesuai dengan aturan penulisan script

--- donjo-app/controllers/Hom_desa.php
+++ donjo-app/controllers/Hom_desa.php
@@ -1,15 +1,17 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Hom_desa extends CI_Controller{
+class Hom_desa extends CI_Controller {
 
-	function __construct(){
+	public function __construct()
+	{
 		parent::__construct();
 		session_start();
 		$this->load->model('user_model');
 		$grup	= $this->user_model->sesi_grup($_SESSION['sesi']);
-		if($grup!=1 AND $grup!=2) {
-			if(empty($grup))
+		if ($grup != 1 AND $grup != 2)
+		{
+			if (empty($grup))
 				$_SESSION['request_uri'] = $_SERVER['REQUEST_URI'];
 			else
 				unset($_SESSION['request_uri']);
@@ -20,14 +22,16 @@ class Hom_desa extends CI_Controller{
 		$this->modul_ini = 200;
 	}
 
-	function index(){
+	public function index()
+	{
 		redirect('hom_desa/konfigurasi');
 	}
 
-	function konfigurasi(){
+	public function konfigurasi()
+	{
 		$this->load->model('provinsi_model');
 		// Menampilkan menu dan sub menu aktif
-		$nav['act']= 1;
+		$nav['act'] = 1;
 		$nav['act_sub'] = 17;
 		$header = $this->header_model->get_data();
 
@@ -42,34 +46,40 @@ class Hom_desa extends CI_Controller{
 		$this->load->view('footer');
 	}
 
-	function insert(){
+	public function insert()
+	{
 		$this->config_model->insert();
 		redirect('hom_desa/konfigurasi');
 	}
 
-	function update($id=''){
+	public function update($id='')
+	{
 		$this->config_model->update($id);
 		redirect("hom_desa/konfigurasi");
 	}
 
-	function ajax_kantor_maps(){
+	public function ajax_kantor_maps()
+	{
 		$data['desa'] = $this->config_model->get_data();
 		$data['form_action'] = site_url("hom_desa/update_kantor_maps/");
 		$this->load->view("home/ajax_kantor_desa_maps", $data);
 	}
 
-	function ajax_wilayah_maps(){
+	public function ajax_wilayah_maps()
+	{
 		$data['desa'] = $this->config_model->get_data();
 		$data['form_action'] = site_url("hom_desa/update_wilayah_maps/");
 		$this->load->view("home/ajax_wilayah_desa_maps", $data);
 	}
 
-	function update_kantor_maps(){
+	public function update_kantor_maps()
+	{
 		$this->config_model->update_kantor();
 		redirect("hom_desa/konfigurasi");
 	}
 
-	function update_wilayah_maps(){
+	public function update_wilayah_maps()
+	{
 		$this->config_model->update_wilayah();
 			redirect("hom_desa/konfigurasi");
 	}
--- donjo-app/controllers/Hom_sid.php
+++ donjo-app/controllers/Hom_sid.php
@@ -1,15 +1,17 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Hom_sid extends CI_Controller{
+class Hom_sid extends CI_Controller {
 
-	function __construct(){
+	public function __construct()
+	{
 		parent::__construct();
 		session_start();
 		$this->load->model('user_model');
 		$grup	= $this->user_model->sesi_grup($_SESSION['sesi']);
-		if($grup!=1 AND $grup!=2) {
-			if(empty($grup))
+		if ($grup != 1 AND $grup != 2)
+		{
+			if (empty($grup))
 				$_SESSION['request_uri'] = $_SERVER['REQUEST_URI'];
 			else
 				unset($_SESSION['request_uri']);
@@ -20,7 +22,8 @@ class Hom_sid extends CI_Controller{
 		$this->modul_ini = 1;
 	}
 
-	function index(){
+	public function index()
+	{
 		// Pengambilan data penduduk untuk ditampilkan widget Halaman Dashboard (modul Home SID)
 		$data['penduduk'] = $this->header_model->penduduk_total();
 		$data['keluarga'] = $this->header_model->keluarga_total();
@@ -29,26 +32,26 @@ class Hom_sid extends CI_Controller{
 		$data['rtm'] = $this->header_model->rtm_total();
 		$data['dusun'] = $this->header_model->dusun_total();
 		// Menampilkan menu dan sub menu aktif
-		$nav['act']= 1;
+		$nav['act'] = 1;
 		$nav['act_sub'] = 16;
 		$header = $this->header_model->get_data();
 
-		$this->load->view('header',$header);
-		$this->load->view('nav',$nav);
-		$this->load->view('home/desa',$data);
+		$this->load->view('header', $header);
+		$this->load->view('nav', $nav);
+		$this->load->view('home/desa', $data);
 		$this->load->view('footer');
 	}
 
-	function donasi(){
+	public function donasi()
+	{
 		// Menampilkan menu dan sub menu aktif
-		$nav['act']= 1;
+		$nav['act'] = 1;
 		$nav['act_sub'] = 19;
 		$header = $this->header_model->get_data();
 
-		$this->load->view('header',$header);
-		$this->load->view('nav',$nav);
+		$this->load->view('header', $header);
+		$this->load->view('nav', $nav);
 		$this->load->view('home/donasi');
 		$this->load->view('footer');
 	}
-
 }
