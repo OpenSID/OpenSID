c3e3e7c0cde4496529826c965b5bef1614ea3813
siayi <ayisumarna@gmail.com>
2018-10-31 13:18:20 +0700

    Surat Keterangan Domisili

--- surat/surat_ket_domisili/surat_ket_domisili.php
+++ surat/surat_ket_domisili/surat_ket_domisili.php
@@ -1,136 +1,76 @@
-<script>
-$(function(){
-var nik = {};
-nik.results = [
-<?php foreach($penduduk as $data){?>
-{id:'<?php echo $data['id']?>',name:"<?php echo $data['nik']." - ".($data['nama'])?>",info:"<?php echo ($data['alamat'])?>"},
-<?php }?>
-];
-
-$('#nik').flexbox(nik, {
-resultTemplate: '<div><label>No nik : </label>{name}</div><div>{info}</div>',
-watermark: <?php if($individu){?>'<?php echo $individu['nik']?> - <?php echo spaceunpenetration($individu['nama'])?>'<?php }else{?>'Ketik no nik di sini..'<?php }?>,
-width: 260,
-noResultsText :'Tidak ada no nik yang sesuai..',
-onSelect: function() {
-$('#'+'main').submit();
-}
-});
-
-});
-</script>
-
-
-<style>
-table.form.detail th{
-padding:5px;
-background:#fafafa;
-border-right:1px solid #eee;
-}
-table.form.detail td{
-padding:5px;
-}
-</style>
-<div id="pageC">
-<table class="inner">
-<tr style="vertical-align:top">
-
-<td style="background:#fff;padding:5px;">
-<div class="content-header">
-
-</div>
-<div id="contentpane">
-<div class="ui-layout-north panel">
-<h3>Surat Keterangan Domisili</h3>
-</div>
-<div class="ui-layout-center" id="maincontent" style="padding: 5px;">
-<table class="form">
-<tr>
-<th>NIK / Nama Pemohon</th>
-<td>
-<form action="" id="main" name="main" method="POST">
-<div id="nik" name="nik"></div>
-</form>
-</tr>
-
-<form id="validasi" action="<?php echo $form_action?>" method="POST" target="_blank">
-<input type="hidden" name="nik" value="<?php echo $individu['id']?>" class="inputbox required" >
-<?php if($individu){ //bagian info setelah terpilih?>
-<?php include("donjo-app/views/surat/form/konfirmasi_pemohon.php"); ?>
-<?php }?>
-<tr>
-<th>Nomor Surat</th>
-<td>
-<input name="nomor" type="text" class="inputbox required" size="12"/>
-</td>
-</tr>
-<tr>
-<tr>
-<th>Keperluan</th>
-<td>
-<input name="keperluan" type="text" class="inputbox" size="40"/>
-</td>
-</tr>
-<tr>
-</td>
-</tr>
-<tr>
-<th>Atas Nama</th>
-<td>
-<select name="atas_nama"  type="text" class="inputbox">
-<option value="">Atas Nama</option>
-<option value=""> </option>
-<option value="An. Kepala Desa <?php echo unpenetration($desa['nama_desa'])?>"> An. Kepala Desa <?php echo unpenetration($desa['nama_desa'])?> </option>
-<option value="Ub. Kepala Desa <?php echo unpenetration($desa['nama_desa'])?>"> Ub. Kepala Desa <?php echo unpenetration($desa['nama_desa'])?> </option>
-</select>
-</tr>
-<tr>
-<th>Sebagai</th>
-<td>
-<select name="jabatan"  class="inputbox required">
-<option value="">Pilih Jabatan</option>
-<?php foreach($pamong AS $data){?>
-<option ><?php echo unpenetration($data['jabatan'])?></option>
-<?php }?>
-</select>
-<tr>
-<th>Staf Pemerintah Desa</th>
-<td>
-<select name="pamong"  class="inputbox required">
-<option value="">Pilih Staf Pemerintah Desa</option>
-<?php foreach($pamong AS $data){?>
-<option value="<?php echo $data['pamong_nama']?>"><font style="bold"><?php echo unpenetration($data['pamong_nama'])?></font> (<?php echo unpenetration($data['jabatan'])?>)</option>
-<?php }?>
-<tr>
-<th>N I P</th>
-<td>
-<select name="pamong_nip"  type="text" class="inputbox">
-<option value="">Pilih No NIP</option>
-<?php foreach($pamong AS $data){?>
-<option ><?php echo($data['pamong_nip'])?></option>
-<?php }?>
-</select>
-
-</td>
-</tr>
-</td>
-</tr>
-</table>
-</div>
-   
-<div class="ui-layout-south panel bottom">
-<div class="left">     
-<a href="<?php echo site_url()?>surat" class="uibutton icon prev">Kembali</a>
-</div>
-<div class="right">
-<div class="uibutton-group">
-<button class="uibutton" type="reset">Clear</button>
-
-<button type="button" onclick="$('#'+'validasi').attr('action','<?php echo $form_action?>');$('#'+'validasi').submit();" class="uibutton special"><span class="ui-icon ui-icon-print">&nbsp;</span>Cetak</button>
-<?php if (file_exists("surat/$url/$url.rtf")) { ?><button type="button" onclick="$('#'+'validasi').attr('action','<?php echo $form_action2?>');$('#'+'validasi').submit();" class="uibutton confirm"><span class="ui-icon ui-icon-document">&nbsp;</span>Export Doc</button><?php } ?>
-</div>
-</div>
-</div> </form>
-</div>
-</td></tr></table>
+<div class="content-wrapper">
+	<section class="content-header">
+		<h1>Surat Keterangan Domisili</h1>
+		<ol class="breadcrumb">
+			<li><a href="<?= site_url('hom_desa/about')?>"><i class="fa fa-dashboard"></i> Home</a></li>
+			<li><a href="<?= site_url('surat')?>"> Daftar Cetak Surat</a></li>
+			<li class="active">Surat Keterangan Domisili</li>
+		</ol>
+	</section>
+	<section class="content">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="box box-info">
+					<div class="box-header with-border">
+						<a href="<?=site_url("surat")?>" class="btn btn-social btn-flat btn-info btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"  title="Kembali Ke Daftar Wilayah">
+							<i class="fa fa-arrow-circle-left "></i>Kembali Ke Daftar Cetak Surat
+           	</a>
+					</div>
+					<div class="box-body">
+						<form action="" id="main" name="main" method="POST" class="form-horizontal">
+							<div class="form-group">
+								<label for="nik"  class="col-sm-3 control-label">NIK / Nama</label>
+								<div class="col-sm-6 col-lg-4">
+									<select class="form-control  input-sm select2" id="nik" name="nik" style ="width:100%;" onchange="formAction('main')">
+										<option value="">--  Cari NIK / Nama Penduduk--</option>
+										<?php foreach ($penduduk as $data): ?>
+											<option value="<?= $data['id']?>" <?php if ($individu['nik']==$data['nik']): ?>selected<?php endif; ?>>NIK : <?= $data['nik']." - ".$data['nama']?></option>
+										<?php endforeach;?>
+									</select>
+								</div>
+							</div>
+						</form>
+						<form id="validasi" action="<?= $form_action?>" method="POST" target="_blank" class="form-surat form-horizontal">
+							<input type="hidden" id="url_surat" name="url_surat" value="<?= $url ?>">
+							<input type="hidden" id="url_remote" name="url_remote" value="<?= site_url('surat/nomor_surat_duplikat')?>">
+							<div class="row jar_form">
+								<label for="nomor" class="col-sm-3"></label>
+								<div class="col-sm-8">
+									<input class="required" type="hidden" name="nik" value="<?= $individu['id']?>">
+								</div>
+							</div>
+							<?php if ($individu): ?>
+								<?php include("donjo-app/views/surat/form/konfirmasi_pemohon.php"); ?>
+							<?php	endif; ?>
+							<div class="form-group">
+								<label for="nomor"  class="col-sm-3 control-label">Nomor Surat</label>
+								<div class="col-sm-8">
+									<input  id="nomor" class="form-control input-sm required" type="text" placeholder="Nomor Surat" name="nomor" value="<?= $surat_terakhir['no_surat_berikutnya'];?>">
+									<p class="help-block text-red small"><?= $surat_terakhir['ket_nomor']?><strong><?= $surat_terakhir['no_surat'];?></strong> (tgl: <?= $surat_terakhir['tanggal']?>)</p>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="keperluan"  class="col-sm-3 control-label">Keperluan</label>
+								<div class="col-sm-8">
+									<textarea name="keperluan" class="form-control input-sm required" placeholder="Keperluan"></textarea>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">Tertanda Atas Nama</label>
+								<div class="col-sm-6 col-lg-4">
+									<select class="form-control input-sm" name="atas_nama">
+										<option value="">Atas Nama</option>
+										<option value="An. Kepala Desa <?= unpenetration($desa['nama_desa'])?>"> An. Kepala Desa <?= unpenetration($desa['nama_desa'])?> </option>
+										<option value="Ub. Kepala Desa <?= unpenetration($desa['nama_desa'])?>"> Ub. Kepala Desa <?= unpenetration($desa['nama_desa'])?> </option>
+									</select>
+								</div>
+							</div>
+							<?php include("donjo-app/views/surat/form/_pamong.php"); ?>
+						</form>
+					</div>
+					<?php include("donjo-app/views/surat/form/tombol_cetak.php"); ?>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
