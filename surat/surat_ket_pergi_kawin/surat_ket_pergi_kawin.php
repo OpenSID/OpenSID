d9ac9e3eb1548238c7a5d0e62abf87e9ac6b01f8
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-12-26 12:04:51 +1100

    Sekarang di semua form surat, isian tgl-berlaku-akhir harus sesudah tgl-berlaku-mulai.

--- surat/surat_ket_pergi_kawin/surat_ket_pergi_kawin.php
+++ surat/surat_ket_pergi_kawin/surat_ket_pergi_kawin.php
@@ -61,25 +61,7 @@
 									<textarea  id="keterangan" class="form-control input-sm required" placeholder="Keperluan" name="keterangan"></textarea>
 								</div>
 							</div>
-							<div class="form-group">
-								<label for="berlaku_dari"  class="col-sm-3 control-label">Berlaku Dari - Sampai</label>
-								<div class="col-sm-3 col-lg-2">
-									<div class="input-group input-group-sm date">
-										<div class="input-group-addon">
-											<i class="fa fa-calendar"></i>
-										</div>
-										<input title="Pilih Tanggal" class="form-control input-sm datepicker required" name="berlaku_dari" type="text"/>
-									</div>
-								</div>
-								<div class="col-sm-3 col-lg-2">
-									<div class="input-group input-group-sm date">
-										<div class="input-group-addon">
-											<i class="fa fa-calendar"></i>
-										</div>
-										<input title="Pilih Tanggal" class="form-control input-sm datepicker required" name="berlaku_sampai" type="text"/>
-									</div>
-								</div>
-							</div>
+							<?php include("donjo-app/views/surat/form/tgl_berlaku.php"); ?>
 							<?php include("donjo-app/views/surat/form/_pamong.php"); ?>
 						</form>
 					</div>
