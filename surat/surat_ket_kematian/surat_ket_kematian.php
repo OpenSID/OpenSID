d6a6ba595ba6e16ff504a2a2e9b06a4cc062acff
Boedy Irhadtanto <boedyirh@yahoo.co.id>
2018-12-29 19:46:52 +0700

    Update surat_ket_kematian.php

--- surat/surat_ket_kematian/surat_ket_kematian.php
+++ surat/surat_ket_kematian/surat_ket_kematian.php
@@ -121,7 +121,7 @@
 									<div class="col-sm-6 col-lg-4">
 										<select class="form-control  input-sm select2" id="nik" name="nik" style ="width:100%;" onchange="formAction('main')">
 											<option value="">--  Cari NIK / Nama Penduduk--</option>
-											<?php foreach ($mati as $data): ?>
+											<?php foreach ($penduduk as $data): ?>
 												<option value="<?= $data['id']?>" <?php if ($individu['nik']==$data['nik']): ?>selected<?php endif; ?>>NIK : <?= $data['nik']." - ".$data['nama']?></option>
 											<?php endforeach;?>
 										</select>
