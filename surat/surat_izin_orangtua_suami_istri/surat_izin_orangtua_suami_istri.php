161dfb651e63d56242fbdc9a68b4ad7c4e51f9f5
ahmad afandi <ahmad.afandi85@gmail.com>
2019-01-13 11:30:24 +0700

    ubah cari nik menjadi include file

--- surat/surat_izin_orangtua_suami_istri/surat_izin_orangtua_suami_istri.php
+++ surat/surat_izin_orangtua_suami_istri/surat_izin_orangtua_suami_istri.php
@@ -56,17 +56,7 @@
 								<div class="form-group subtitle_head">
 									<label class="col-sm-3 text-right"><strong>PIHAK YANG MEMBERI IZIN</strong></label>
 								</div>
-								<div class="form-group">
-									<label for="nik"  class="col-sm-3 control-label">NIK / Nama</label>
-									<div class="col-sm-6 col-lg-4">
-										<select class="form-control input-sm select2-nik" id="nik" name="nik" style ="width:100%;" onchange="formAction('main')">
-											<option value="">--  Cari NIK / Nama Penduduk--</option>
-											<?php foreach ($penduduk as $data): ?>
-												<option value="<?= $data['id']?>" <?php if ($individu['nik']==$data['nik']): ?>selected<?php endif; ?>>NIK : <?= $data['nik']." - ".$data['nama']."\n".$data['alamat']?></option>
-											<?php endforeach;?>
-										</select>
-									</div>
-								</div>
+								<?php include("donjo-app/views/surat/form/_cari_nik.php"); ?>
 							</div>
 						</form>
 						<form id="validasi" action="<?= $form_action?>" method="POST" target="_blank" class="form-surat form-horizontal">
--- surat/surat_ket_domisili_usaha/surat_ket_domisili_usaha.php
+++ surat/surat_ket_domisili_usaha/surat_ket_domisili_usaha.php
@@ -18,17 +18,7 @@
 					</div>
 					<div class="box-body">
 						<form action="" id="main" name="main" method="POST" class="form-horizontal">
-							<div class="form-group">
-								<label for="nik"  class="col-sm-3 control-label">NIK / Nama</label>
-								<div class="col-sm-6 col-lg-4">
-									<select class="form-control  input-sm select2-nik" id="nik" name="nik" style ="width:100%;" onchange="formAction('main')">
-										<option value="">--  Cari NIK / Nama Penduduk--</option>
-										<?php foreach ($penduduk as $data): ?>
-											<option value="<?= $data['id']?>" <?php if ($individu['nik']==$data['nik']): ?>selected<?php endif; ?>>NIK : <?= $data['nik']." - ".$data['nama']."\n".$data['alamat']?></option>
-										<?php endforeach;?>
-									</select>
-								</div>
-							</div>
+							<?php include("donjo-app/views/surat/form/_cari_nik.php"); ?>
 						</form>
 						<form id="validasi" action="<?= $form_action?>" method="POST" target="_blank" class="form-surat form-horizontal">
 							<input type="hidden" id="url_surat" name="url_surat" value="<?= $url ?>">
