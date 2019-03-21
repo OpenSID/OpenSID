b5fb4106d4f445e70e1a2705220656ef9d56fe16
Diki Siswanto <dickyachmady@gmail.com>
2019-03-21 15:21:23 +0800

    Perbaiki kesalahan tipe data

--- donjo-app/models/Laporan_bulanan_model.php
+++ donjo-app/models/Laporan_bulanan_model.php
@@ -71,18 +71,18 @@
 	{
 		switch ($bulan)
 		{
-	    case 01 : $bulan = "Januari"; break;
-	    case 02 : $bulan = "Februari"; break;
-	    case 03 : $bulan = "Maret"; break;
-	    case 04 : $bulan = "April"; break;
-	    case 05 : $bulan = "Mei"; break;
-	    case 06 : $bulan = "Juni"; break;
-	    case 07 : $bulan = "Juli"; break;
-	    case 08 : $bulan = "Agustus"; break;
-	    case 09 : $bulan = "September"; break;
-	    case 10 : $bulan = "Oktober"; break;
-	    case 11 : $bulan = "November"; break;
-	    case 12 : $bulan = "Desember"; break;
+	    case '01' : $bulan = "Januari"; break;
+	    case '02' : $bulan = "Februari"; break;
+	    case '03' : $bulan = "Maret"; break;
+	    case '04' : $bulan = "April"; break;
+	    case '05' : $bulan = "Mei"; break;
+	    case '06' : $bulan = "Juni"; break;
+	    case '07' : $bulan = "Juli"; break;
+	    case '08' : $bulan = "Agustus"; break;
+	    case '09' : $bulan = "September"; break;
+	    case '10' : $bulan = "Oktober"; break;
+	    case '11' : $bulan = "November"; break;
+	    case '12' : $bulan = "Desember"; break;
     }
 		return $bulan;
 	}
