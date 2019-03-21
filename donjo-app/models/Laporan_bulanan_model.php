6533705f119d5d78f98d498dc25aa6a0773fde53
Diki Siswanto <dickyachmady@gmail.com>
2019-03-21 14:58:09 +0800

    Perbaiki method bulan()

--- donjo-app/models/Laporan_bulanan_model.php
+++ donjo-app/models/Laporan_bulanan_model.php
@@ -71,15 +71,15 @@
 	{
 		switch ($bulan)
 		{
-	    case 1 : $bulan = "Januari"; break;
-	    case 2 : $bulan = "Februari"; break;
-	    case 3 : $bulan = "Maret"; break;
-	    case 4 : $bulan = "April"; break;
-	    case 5 : $bulan = "Mei"; break;
-	    case 6 : $bulan = "Juni"; break;
-	    case 7 : $bulan = "Juli"; break;
-	    case 8 : $bulan = "Agustus"; break;
-	    case 9 : $bulan = "September"; break;
+	    case 01 : $bulan = "Januari"; break;
+	    case 02 : $bulan = "Februari"; break;
+	    case 03 : $bulan = "Maret"; break;
+	    case 04 : $bulan = "April"; break;
+	    case 05 : $bulan = "Mei"; break;
+	    case 06 : $bulan = "Juni"; break;
+	    case 07 : $bulan = "Juli"; break;
+	    case 08 : $bulan = "Agustus"; break;
+	    case 09 : $bulan = "September"; break;
 	    case 10 : $bulan = "Oktober"; break;
 	    case 11 : $bulan = "November"; break;
 	    case 12 : $bulan = "Desember"; break;
