a71551d5fc376cfd7a3993be0a52459d57b73d18
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-04-03 09:52:28 +1100

    Perbaiki filter Dusun di halaman Data Calon Pemilih [bug-fix]

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", '19.04');
+define("VERSION", '19.04-pasca');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
