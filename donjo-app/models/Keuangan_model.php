93ea1281dfa0a6277c3dfc73a8e6ec9836e1e3c1
hasanarofid <hasanarofid@gmail.com>
2019-04-06 11:44:42 +0700

    menambah data function untuk widget grafik

--- donjo-app/models/Keuangan_model.php
+++ donjo-app/models/Keuangan_model.php
@@ -26,29 +26,24 @@ class Keuangan_model extends CI_model {
 				$data2['versi_database'] = $versi_siskeudes[1][0];
 				$data2['tahun_anggaran'] = $data['tahun_anggaran'];
 				if ($this->db->insert('keuangan_master', $data2)) {
-					$anggaran = $client->getDatabaseTableRows($inputFiles,'Ta_Anggaran');
+					$anggaran = $client->getDatabaseTableRows($inputFiles,'Ta_RAB');
 					if ($anggaran) {
 						$i = 1;
 						$id_master_keuangan = $this->db->insert_id();
 						foreach ($anggaran as $value) {
 							if ($i > 1) {
 								$dataanggaran['id_keuangan_master'] = $id_master_keuangan;
-								$dataanggaran['KURincianSD'] = $value[2];
-								$dataanggaran['KD_Rincian'] = $value[3];
-								$dataanggaran['RincianSD'] = $value[4];
-								$dataanggaran['anggaran'] = $value[5];
-								$dataanggaran['anggaranPAK'] = $value[6];
-								$dataanggaran['anggaranStlhPAK'] = $value[7];
-								$dataanggaran['Belanja'] = $value[8];
-								$dataanggaran['Kd_keg'] = $value[9];
-								$dataanggaran['SumberDana'] = $value[10];
-								$dataanggaran['kd_desa'] = $value[11];
-								$dataanggaran['tgl_posting'] = $value[12];
-								$this->db->insert('keuangan_anggaran', $dataanggaran);
+								$dataanggaran['kd_desa'] = $value[1];
+								$dataanggaran['kd_keg'] = $value[2];
+								$dataanggaran['kd_rincian'] = $value[3];
+								$dataanggaran['anggaran'] = $value[4];
+								$dataanggaran['anggaranPAK'] = $value[5];
+								$dataanggaran['anggaranStlhPAK'] = $value[6];
+								$this->db->insert('keuangan_ta_rab', $dataanggaran);
 							}
 							$i++;
 						}
-		
+
 					}
 
 				}
@@ -64,6 +59,36 @@ class Keuangan_model extends CI_model {
 			return;
 		}
 
+	}
+	public function tahun_anggaran()
+	{
+		$this->db->select('tahun_anggaran');
+		$result = $this->db->get('keuangan_master')->row();
+		return $result->tahun_anggaran;
+	}
+
+	public function anggaran_keuangan()
+	{
+		$this->db->select_sum('anggaran');
+		$this->db->where('keuangan_ta_rab.id_keuangan_master ', 1);
+		$result = $this->db->get('keuangan_ta_rab')->row();
+		return $result->anggaran;
+	}
+
+	public function anggaranPAK()
+	{
+		$this->db->select_sum('anggaranPAK');
+		$this->db->where('keuangan_ta_rab.id_keuangan_master ', 1);
+		$result = $this->db->get('keuangan_ta_rab')->row();
+		return $result->anggaranPAK;
+	}
+
+	public function anggaranStlhPAK()
+	{
+		$this->db->select_sum('anggaranStlhPAK');
+		$this->db->where('keuangan_ta_rab.id_keuangan_master ', 1);
+		$result = $this->db->get('keuangan_ta_rab')->row();
+		return $result->anggaranStlhPAK;
 	}
 
 	public function cekMasterKeuangan($versi_database,$tahun_anggaran)
