85b2402f391dc25c1b65c640443a571996997524
hasanarofid <hasanarofid@gmail.com>
2019-04-09 15:05:12 +0700

    update view import keuangan dari siskuedes

--- donjo-app/views/keuangan/import_data.php
+++ donjo-app/views/keuangan/import_data.php
@@ -1,17 +1,6 @@
 <style type="text/css">
 	.nowrap { white-space: nowrap; }
 </style>
-<script>
-	$(function()
-	{
-		var keyword = <?= $keyword?> ;
-		$( "#cari" ).autocomplete(
-		{
-			source: keyword,
-			maxShowItems: 10,
-		});
-	});
-</script>
 <div class="content-wrapper">
 	<section class="content-header">
 		<h1>Keuangan</h1>
@@ -29,7 +18,7 @@
   					<div class="box-body">
   						<div class="form-group">
   							<label for="file"  class="control-label">Versi Database</label>
-  							<select class="form-control" name="versi_database">
+  							<select class="form-control" name="versi_database" id="versi_database">
                   <option value="">.:Pilih Versi Database:.</option>
                   <option value="1">Siskuedes versi 1</option>
                   <option value="2">Siskuedes versi 2</option>
@@ -37,7 +26,7 @@
   						</div>
   						<div class="form-group">
   							<label for="file"  class="control-label">Tahun Anggaran</label>
-  							<select class="form-control" name="tahun_anggaran">
+  							<select class="form-control" name="tahun_anggaran" id="tahun_anggaran">
                   <option value="">.:Pilih Tahun Anggaran:.</option>
                   <option value="2018">2018</option>
                   <option value="2019">2019</option>
@@ -56,14 +45,67 @@
   						</div>
   					</div>
   					<div class="modal-footer">
-  						<button type="submit" class="btn btn-social btn-flat btn-info btn-sm" id="ok"><i class='fa fa-check'></i> Simpan</button>
+  						<button type="button" class="btn btn-social btn-flat btn-info btn-sm" id="ok" onclick="simpan()"><i class='fa fa-check'></i> Simpan</button>
   					</div>
   				</div>
   			</div>
   		</div>
   	</div>
   </form>
-
-
 	</section>
 </div>
+
+ <div class="modal modal-danger fade"  id="getCodeModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Peringatan</h4>
+              </div>
+              <div class="modal-body">
+                <p>Versi Database yang akan anda import sudah ada pad sistem!</p>
+                <p>Apakah anda ingin melanjutkan proses import untuk menindih datanya?</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Batalkan</button>
+                <button type="button" class="btn btn-outline" onclick="simpanDataUpdate()">Lanjutkan Import</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+<script type="text/javascript">
+
+		function simpan() {
+			if ($("#versi_database").val() == '') {
+				alert('Pilih Versi Database!');
+				$("#versi_database").focus();
+			}else if ($("#tahun_anggaran").val() == '') {
+				alert('Pilih Tahun Anggaran!');
+				$("#tahun_anggaran").focus();
+			}else if ($("#file2").val() == '') {
+				alert('Pilih Berkas File Import!');
+				$("#file2").focus();
+			}else{
+				$.ajax({
+						 url: '<?= site_url("keuangan/cekVersiDatabase")?>',
+							type: "POST",
+							datatype:"json",
+						 data: $('#validasi').serialize(),
+						 success: function(response) {
+								 if (response == 1) {
+									 $("#getCodeModal").modal('show');
+								 }else{
+										 $('#validasi').submit();
+								 }
+						 }
+				 });
+			}
+		}
+		function simpanDataUpdate() {
+			$('#validasi').submit();
+		}
+
+
+</script>
