2f800444fe16210b8675aab1601dd1eae86207f4
Rudi Purwanto <45786885+roaddee@users.noreply.github.com>
2019-04-13 11:00:09 +0700

    Update donjolib-helper.php
    Penambahan Fungsi Pembilang dan rupiah untuk surat yang membutuhkan pembilang

--- donjo-app/helpers/donjolib_helper.php
+++ donjo-app/helpers/donjolib_helper.php
@@ -547,3 +547,103 @@ function hash_pin($pin="")
 	return $pin;
 }
 
+// Terbilang
+if ( ! function_exists('number_to_words'))
+{
+	function number_to_words($number)
+	{
+		$before_comma = trim(to_word($number));
+		//$after_comma = trim(comma($number));
+		//return ucwords($results = $before_comma.' koma '.$after_comma);
+		return ucwords($results = $before_comma.' Rupiah');
+	}
+
+	function to_word($number)
+	{
+		$words = "";
+		$arr_number = array(
+		"",
+		"satu",
+		"dua",
+		"tiga",
+		"empat",
+		"lima",
+		"enam",
+		"tujuh",
+		"delapan",
+		"sembilan",
+		"sepuluh",
+		"sebelas");
+
+		if($number<12)
+		{
+			$words = " ".$arr_number[$number];
+		}
+		else if($number<20)
+		{
+			$words = to_word($number-10)." belas";
+		}
+		else if($number<100)
+		{
+			$words = to_word($number/10)." puluh ".to_word($number%10);
+		}
+		else if($number<200)
+		{
+			$words = "seratus ".to_word($number-100);
+		}
+		else if($number<1000)
+		{
+			$words = to_word($number/100)." ratus ".to_word($number%100);
+		}
+		else if($number<2000)
+		{
+			$words = "seribu ".to_word($number-1000);
+		}
+		else if($number<1000000)
+		{
+			$words = to_word($number/1000)." ribu ".to_word($number%1000);
+		}
+		else if($number<1000000000)
+		{
+			$words = to_word($number/1000000)." juta ".to_word($number%1000000);
+		}
+		else
+		{
+			$words = "undefined";
+		}
+		return $words;
+	}
+
+function comma($number)
+	{
+		$after_comma = stristr($number,',');
+		$arr_number = array(
+		"nol",
+		"satu",
+		"dua",
+		"tiga",
+		"empat",
+		"lima",
+		"enam",
+		"tujuh",
+		"delapan",
+		"sembilan");
+
+		$results = "";
+		$length = strlen($after_comma);
+		$i = 1;
+		while($i<$length)
+		{
+			$get = substr($after_comma,$i,1);
+			$results .= " ".$arr_number[$get];
+			$i++;
+		}
+		return $results;
+	}
+
+	function rupiah24($angka){
+	
+	$hasil_rupiah = "Rp " . number_format($angka,2,',','.');
+	return $hasil_rupiah;
+	}
+}
