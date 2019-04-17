8235b1c411373028494d60e7913f02d34b992887
galiarta <kadekgaliarta@gmail.com>
2019-04-17 19:47:44 +0800

    Ubah tampilan widget statistik

--- donjo-app/views/widgets/statistik.php
+++ donjo-app/views/widgets/statistik.php
@@ -2,9 +2,9 @@
 
 <div class="box box-info box-solid">
   <div class="box-header">
-    <h3 class="box-title"><a href="<?php echo site_url("first/statistik/1")?>"><i class="fa fa-bar-chart"></i> Statistik <?php echo $desa["nama_desa"];?></a></h3>
+    <h3 class="box-title"><a href="<?php echo site_url("first/statistik/1")?>"><i class="fa fa-bar-chart"></i> Statistik <?php echo ucwords($this->setting->sebutan_desa),' ', $desa["nama_desa"];?></a></h3>
   </div>
-  <div class="box-body">
+  <div class="box-body" width="100%" height="100%">
     <script type="text/javascript">
     $(function () {
         var chart_widget;
@@ -18,12 +18,22 @@
                     plotShadow: false
                 },
                 title: {
-                    text: 'Statistik Penduduk'
+                    text: 'Jumlah Penduduk'
                 },
           yAxis: {
                 title: {
                   text: 'Jumlah'
                 }
+          },
+          xAxis: {
+            categories: 
+            [ 
+            <?php  foreach($stat_widget as $data){?>
+              <?php if($data['jumlah'] != "-" AND $data['nama']!= "JUMLAH"){?>
+                ['<?php echo $data['jumlah']?> <br> <?php echo $data['nama']?>'],
+              <?php }?>
+            <?php }?>
+            ]
           },
           legend: {
             enabled:false
