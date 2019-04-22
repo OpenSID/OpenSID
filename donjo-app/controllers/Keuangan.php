aed3e2006981ad7a4f2f992b45e65b1443e96d47
hasanarofid <hasanarofid@gmail.com>
2019-04-22 14:34:38 +0700

    update fungsi unzip file import

--- donjo-app/controllers/Keuangan.php
+++ donjo-app/controllers/Keuangan.php
@@ -1,26 +1,13 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Keuangan extends CI_Controller {
-
+class Keuangan extends Admin_Controller {
   public function __construct()
 	{
-    parent::__construct();
-    session_start();
-    $this->load->model('user_model');
-    $grup = $this->user_model->sesi_grup($_SESSION['sesi']);
-    if ($grup != 1 AND $grup != 2)
-    {
-      if (empty($grup))
-        $_SESSION['request_uri'] = $_SERVER['REQUEST_URI'];
-      else
-        unset($_SESSION['request_uri']);
-      redirect('siteman');
-    }
-
-    $this->load->model('header_model');
-    $this->load->model('keuangan_model');
-    $this->modul_ini = 201;
-    $this->controller = 'keuangan';
+		parent::__construct();
+		session_start();
+		$this->load->model('keuangan_model');
+		$this->load->model('header_model');
+		$this->modul_ini = 201;
 	}
 
   public function widget()
@@ -51,26 +38,18 @@ class Keuangan extends CI_Controller {
 
   public function proses_impor()
   {
+    // print_r($_FILES);die();
     $data['versi_database'] = $_POST['versi_database'];
     $data['tahun_anggaran'] = $_POST['tahun_anggaran'];
-      $inputFiles = $_FILES['keuangan']['tmp_name'];
-      $ekstensi_diperbolehkan	= array('mde');
-      $nama = $_FILES['keuangan']['name'];
-      $x = explode('.', $nama);
-      $ekstensi = strtolower(end($x));
-      $ukuran	= $_FILES['keuangan']['size'];
-      $file_tmp = $_FILES['keuangan']['tmp_name'];
-      if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){
-        if (move_uploaded_file($file_tmp, 'upload/keuangan/'.$nama)) {
-          $file = 'upload/keuangan/'.$nama;
-          $this->keuangan_model->convertMDE($file,$data);
-          redirect('keuangan/import_data');
-        }
-      }else{
-        $_SESSION['success'] = -1;
-        $_SESSION['error_msg'] = 'File Harus Extension .mde';
-        return;
-      }
+    $inputFiles = $_FILES['keuangan']['tmp_name'];
+    $ekstensi_diperbolehkan	= array('zip');
+    $nama = $_FILES['keuangan'];
+
+    if($_FILES['keuangan']['name'] !='')
+            			{
+                    $this->keuangan_model->extract($nama);
+                  }
+            		        	                
   }
 
   public function cekVersiDatabase()
