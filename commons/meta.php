05558f998d2acacf44f017602ad3671402469289
Ariandi Ryan Kahfi <46939846+ariandii@users.noreply.github.com>
2019-05-14 16:35:28 +0700

    Update meta.php
    Kembalikan default URL favicon dan perbaiki meta property.

--- commons/meta.php
+++ commons/meta.php
@@ -8,9 +8,9 @@
 <meta name='theme' content='Natra' />
 <meta name='designer' content='Ariandi Ryan Kahfi, S.Pd.' />
 <meta name='theme:designer' content='Ariandi Ryan Kahfi, S.Pd.' />
-<meta name='theme:version' content='3.1' />
-<meta name="keywords" content="Website Sistem Informasi Desa <?php echo $desa['nama_desa'];?>" />
-<meta property="og:site_name" content="<?php echo $desa['nama_desa'];?>"/>
+<meta name='theme:version' content='3.2' />
+<meta name="keywords" content="<?php echo $this->setting->website_title.' '.ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'];?>" />
+<meta property="og:site_name" content="<?php echo ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'];?>"/>
 <meta property="og:type" content="article"/>
 <meta property="fb:app_id" content="147912828718">
 
@@ -23,7 +23,7 @@ else echo $single_artikel["judul"]. ' - ' . ucwords($this->setting->sebutan_desa
 		. (($desa['nama_desa']) ? ' ' . $desa['nama_desa'] : '');
 ?></title>
 
-<link rel="shortcut icon" href="<?php echo base_url("$this->theme_folder/natra/images/favicon.png"); ?>" />
+<link rel="shortcut icon" href="<?php echo base_url()?>favicon.ico" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url("$this->theme_folder/natra/assets/css/bootstrap.min.css"); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url("$this->theme_folder/natra/assets/css/font-awesome.min.css"); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url("$this->theme_folder/natra/assets/css/animate.css"); ?>">
@@ -38,15 +38,16 @@ else echo $single_artikel["judul"]. ' - ' . ucwords($this->setting->sebutan_desa
 
 <?php if(isset($single_artikel)): ?>
     <meta property="og:title" content="<?php echo $single_artikel["judul"];?>"/>
-    <meta property="og:url" content="<?php echo base_url()?>index.php/first/artikel/<?php echo $single_artikel['id'];?>"/>
+    <meta property="og:url" content="<?php echo site_url()?>first/artikel/<?php echo $single_artikel['id'];?>"/>
     <meta property="og:image" content="<?php echo base_url()?><?php echo LOKASI_FOTO_ARTIKEL?>sedang_<?php echo $single_artikel['gambar'];?>"/>
     <meta property="og:description" content="<?php echo potong_teks($single_artikel['isi'], 300)?> ..."/>
-    <meta name="description" content="<?php echo potong_teks($single_artikel['isi'], 300)?> ..."/>
 <?php else: ?>
-<meta name="description" content="Website Sistem Informasi <?php echo ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'];?>"/>
+    <meta property="og:title" content="<?php echo ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'];?>"/>
+    <meta property="og:url" content="<?php echo site_url()?>"/>
+    <meta property="og:image" content="<?php echo LogoDesa($desa['logo']);?>"/>
+    <meta property="og:description" content="<?php echo $this->setting->website_title.' '.ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'];?>"/>
 <?php endif; ?>
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-
 <script language='javascript' src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
 <script language='javascript' src="<?php echo base_url('assets/bootstrap/js/bootstrap.js'); ?>"></script>
 <script language='javascript' src="<?php echo base_url('assets/js/jquery.cycle2.min.js') ?>"></script>
@@ -79,7 +80,6 @@ daym = "0" + daym;
 var hours = mydate.getHours();
 var minutes = mydate.getMinutes();
 var seconds = mydate.getSeconds();
-
 if (hours <= 9)
 hours = "0" + hours;
 if (minutes <= 9)
