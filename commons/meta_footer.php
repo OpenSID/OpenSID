2cdda4129dfd101626bd712a20144a58d5c14c0f
Ariandi Ryan Kahfi <46939846+ariandii@users.noreply.github.com>
2019-05-19 03:11:48 +0700

    Update meta_footer.php
    Sekarang script dapat menyesuaikan nama folder tema.

--- commons/meta_footer.php
+++ commons/meta_footer.php
@@ -1,8 +1,8 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<script src="<?php echo base_url("$this->theme_folder/natra/assets/js/jquery.min.js"); ?>"></script> 
-<script src="<?php echo base_url("$this->theme_folder/natra/assets/js/wow.min.js"); ?>"></script> 
-<script src="<?php echo base_url("$this->theme_folder/natra/assets/js/slick.min.js"); ?>"></script> 
-<script src="<?php echo base_url("$this->theme_folder/natra/assets/js/custom.js"); ?>"></script>
+<script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/jquery.min.js") ?>"></script> 
+<script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/wow.min.js") ?>"></script> 
+<script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/slick.min.js") ?>"></script> 
+<script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/custom.js") ?>"></script>
 <!--
 <script type="text/javascript">
     (function () {
@@ -11,14 +11,14 @@
             facebook: "kulinersemut",
             telegram: "ariandi",
             email: "natai.raya@mail.com",
-            sms: "+628115222660",
-            call: "+628115222660",
+            //sms: "+628115222660",
+            //call: "+628115222660",
             company_logo_url: "<?php echo LogoDesa($desa['logo']);?>", // URL of company logo (png, jpg, gif)
-            greeting_message: "Selamat Datang di Website <?php echo ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'];?>.",
+            greeting_message: "Selamat Datang di <?php echo ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'];?>.",
             //call_to_action: "Kontak",
             button_color: "#e64946",
             position: "left", // Position may be 'right' or 'left'
-            order: "whatsapp,facebook,telegram,email,sms,call" // Order of buttons
+            order: "whatsapp,facebook,telegram,email" // Order of buttons
         };
         
         var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
