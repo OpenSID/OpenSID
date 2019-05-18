dc334348f8f9b5368daf3b215fe7dc8269816da2
Ariandi Ryan Kahfi <46939846+ariandii@users.noreply.github.com>
2019-05-19 03:16:35 +0700

    Update footer_bottom.php
    Sekarang script dapat menyesuaikan nama folder tema.

--- partials/footer_bottom.php
+++ partials/footer_bottom.php
@@ -7,7 +7,7 @@
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
           <div class="footer_bottom_right">
 		   <a target="_blank" href="https://github.com/OpenSID/OpenSID">OpenSID <?php echo AmbilVersi()?></a>
-		   <a target="_blank" href="<?php echo site_url(); ?>siteman"> &copy; 2018 Tema Natra 3.1</a>
+		   <a target="_blank" href="<?php echo site_url(); ?>siteman"> &copy; 2018 Tema Natra 4.1</a>
           </div>
         </div>
       </div>
