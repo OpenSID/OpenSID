7d13401cb335348e5ec37de79f2aad28863dc7e4
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-05-19 17:02:48 +1200

    Sesuaikan dengan aturan penulisan script. Tambah keterangan asal konsep sitemap yg digunakan

--- donjo-app/controllers/Sitemap.php
+++ donjo-app/controllers/Sitemap.php
@@ -1,32 +1,22 @@
 <?php
 
+// https://itsolutionstuff.com/post/how-to-create-dynamic-sitemap-in-php-codeigniterexample.html
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class Sitemap extends CI_Controller {
 
-
     /**
-
      * Index Page for this controller.
-
      *
-
      */
-
     public function index()
-
     {
-
         $this->load->database();
-
         $query = $this->db->get("artikel");
-
         $data['artikel'] = $query->result();
 
-
         $this->load->view('sitemap', $data);
-
     }
-
 }
