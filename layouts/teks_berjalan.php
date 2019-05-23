af4c94ee708d65d3233843f1de23d7d6df0976ad
Ariandi Ryan Kahfi <ariandi@mail.com>
2019-05-23 08:07:38 +0700

    Menyesuaikan teks berjalan terbaru pada OpenSID

--- layouts/teks_berjalan.php
+++ layouts/teks_berjalan.php
@@ -1,13 +1,10 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<?php foreach ($teks_berjalan AS $isi): ?>
-<?php $pecah = explode(" ", $isi);
-for ($i=0; $i<=sizeof($pecah)-1; $i++)
-{
-    if ((substr($pecah[$i], 0, 7) == 'http://') && ($pecah[$i] != 'http://'));
-    else if ((substr($pecah[$i], 0, 8) == 'https://') && ($pecah[$i] != 'https://'));
-    else if ((substr($pecah[$i], 0, 3) == 'www') && ($pecah[$i] != 'www'))
-    $isi = str_replace($pecah[$i], "<a href='http://".$pecah[$i]."'>Selengkapnya...</a>", $isi);
-} ?>
-<span style="font-family: Oswald; padding-right: 50px;"><?= $isi?></span>
+<?php foreach ($teks_berjalan AS $teks): ?>
+    <span class="teks" style="font-family: Oswald; padding-right: 50px;">
+        <?= $teks['teks']?>
+        <?php if ($teks['tautan']): ?>
+        <a href="<?= site_url('first/artikel/'.$teks['tautan']) ?>"><?= $teks['judul_tautan']?></a>
+        <?php endif; ?>
+    </span>
 <?php endforeach; ?>
