7b4ea04a1b56fe8a1f279cc0c011ce089c2e15c4
Ariandi Ryan Kahfi <ariandi@mail.com>
2019-05-28 12:05:42 +0700

    #2067 Sebutan desa sesuai yang sudah disetting
    Muncul sebutan desa pada header menu Ekspor Database sama dengan sebutan seharusnya

--- donjo-app/views/export/exp.php
+++ donjo-app/views/export/exp.php
@@ -2,7 +2,7 @@
 											<div class="row">
 												<div class="col-md-12">
 													<div class="box-header with-border">
-														<h3 class="box-title"><strong>Ekspor Data Desa</strong></h3>
+														<h3 class="box-title"><strong>Ekspor Data <?php echo ucwords($this->setting->sebutan_desa); ?></strong></h3>
 													</div>
 													<div class="box-body">
 														<div class="row">
