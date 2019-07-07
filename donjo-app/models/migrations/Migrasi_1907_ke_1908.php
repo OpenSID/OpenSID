c08b90c10f00c75217ee9b8c43ac03cf18638b72
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-07-07 17:40:36 +1000

    Perbaiki komentar di file migrasi

--- donjo-app/models/migrations/Migrasi_1907_ke_1908.php
+++ donjo-app/models/migrations/Migrasi_1907_ke_1908.php
@@ -29,7 +29,7 @@ class Migrasi_1907_ke_1908 extends CI_model {
 		$fields['path'] = array('type' => 'TEXT', 'constraint' => 11, 'null' => TRUE, 'default' => NULL);
 		$fields['map_tipe'] = array('type' => 'VARCHAR', 'constraint' => 20, 'null' => TRUE, 'default' => NULL);
 	  $this->dbforge->modify_column('tweb_wil_clusterdesa', $fields);
-		// Tambah kolom pengurus untuk ttd u.b
+		// Tambah kolom kode untuk setting_aplikasi_options
 	 	if (!$this->db->field_exists('kode', 'setting_aplikasi_options'))
 	 	{
 			$fields = array();
