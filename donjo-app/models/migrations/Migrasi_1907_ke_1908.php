7dcd1362147998d10c5fa1717af66e44eb5b63df
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-07-07 15:36:55 +1000

    Setting aplikasi offline_mode tidak lagi membatasi akses ke menu Pemetaan > Peta.

--- donjo-app/models/migrations/Migrasi_1907_ke_1908.php
+++ donjo-app/models/migrations/Migrasi_1907_ke_1908.php
@@ -29,5 +29,24 @@ class Migrasi_1907_ke_1908 extends CI_model {
 		$fields['path'] = array('type' => 'TEXT', 'constraint' => 11, 'null' => TRUE, 'default' => NULL);
 		$fields['map_tipe'] = array('type' => 'VARCHAR', 'constraint' => 20, 'null' => TRUE, 'default' => NULL);
 	  $this->dbforge->modify_column('tweb_wil_clusterdesa', $fields);
+		// Tambah kolom pengurus untuk ttd u.b
+	 	if (!$this->db->field_exists('kode', 'setting_aplikasi_options'))
+	 	{
+			$fields = array();
+			$fields['kode'] = array('type' => 'TINYINT', 'constraint' => 4, 'null' => TRUE, 'default' => NULL);
+		  $this->dbforge->add_column('setting_aplikasi_options', $fields);
+	 	}
+	  // Perbaiki setting offline_mode
+	  $this->db->where('key', 'offline_mode')->update('setting_aplikasi', array('jenis' => 'option-kode'));
+	  $setting_id = $this->db->select('id')->where('key', 'offline_mode')->get('setting_aplikasi')->row()->id;
+	  $this->db->where('id_setting', $setting_id)->delete('setting_aplikasi_options');
+		$this->db->insert_batch(
+			'setting_aplikasi_options',
+			array(
+				array('id_setting'=>$setting_id, 'kode'=>'0', 'value'=>'Web bisa diakses publik'),
+				array('id_setting'=>$setting_id, 'kode'=>'1', 'value'=>'Web hanya bisa diakses admin'),
+				array('id_setting'=>$setting_id, 'kode'=>'2', 'value'=>'Web non-aktif sama sekali'),
+			)
+		);
   }
 }
