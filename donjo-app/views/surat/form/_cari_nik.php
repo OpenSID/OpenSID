605cdd26c246496e6a69f73482e0727d2095da63
Rismawan Junandia <junandiagroup@gmail.com>
2019-07-12 18:42:48 +0700

    revisi

--- donjo-app/views/surat/form/_cari_nik.php
+++ donjo-app/views/surat/form/_cari_nik.php
@@ -3,7 +3,14 @@
 	<div class="col-sm-6 col-lg-4">
     	<select class="form-control required input-sm select2-nik" id="nik" name="nik" style ="width:100%;" onchange="formAction('main')">
 			<option value="">--  Cari NIK / Tag ID Card / Nama Penduduk --</option>
-				<?php foreach ($penduduk as $data): ?>
+			<?php 
+				if ($this->uri->segment(3) == "surat_ket_lahir_mati") {
+					$datas = $perempuan;
+				}else{
+					$datas = $penduduk;
+				}
+			?>
+				<?php foreach ($datas as $data): ?>
 					<option value="<?= $data['id']?>" <?php selected($individu['nik'], $data['nik']); ?>><?= $data['info_pilihan_penduduk']?></option>
 				<?php endforeach;?>
 		</select>
