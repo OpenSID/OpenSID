91ee625e2f19b9e69ad8f5b1d6e6c8ec1e87d293
kerupuksambel <kerupuksambel.2000@gmail.com>
2019-07-22 13:08:42 +0700

    Widget - Memperbaiki error karena penamaan

--- donjo-app/views/widgets/keuangan.php
+++ donjo-app/views/widgets/keuangan.php
@@ -1,138 +1,121 @@
-<!-- widget Statistik -->
+<!-- Widget Statistik -->
+<?php
+  $widget_data = json_decode($widget_keuangan, true);
+?>
 <style type="text/css">
-  .highcharts-xaxis-labels tspan {font-size: 8px;}
+  .box-body > select{
+    margin-left: 0;
+  }
+
+  .progress{
+    margin-bottom: 10px;
+    height: 30px;
+  }
+
+  .progress #progress-pendapatan{
+    background-color: rgb(32, 124, 229);
+  }
+
+  .progress #progress-belanja{
+    background-color: #28a745;
+  }
+
+  .keuangan-sub{
+    padding-top: 20px;
+  }
+
+  .keuangan-angka-sub{
+    padding-top: 8px;
+    padding-bottom: 12px;
+    text-align: center;
+  }
+
+  .keuangan-angka-sub span{
+    padding-bottom: 10px;
+    display: block;
+    font-size: 12px;
+  }
+
+  .keuangan-angka-sub h3{
+    font-size: 2rem;
+  }
+
 </style>
 <div class="box box-info box-solid">
   <div class="box-header">
     <h3 class="box-title"><a href="<?= site_url("first/keuangan/1")?>"><i class="fa fa-bar-chart"></i> Statistik Keuangan Desa</a></h3>
   </div>
   <div class="box-body">
-    <select id="keuangan-selector">
-      <option value="chart">Pagu Anggaran (PA)</option>
-      <option value="realisasi">Realisasi Pendapatan Desa</option>
+    Tahun <select id="keuangan-selector">
+      <option value="2016">2016</option>
+      <option value="2017">2017</option>
     </select>
-    <div id="chart"></div>
-    <div id="realisasi"></div>
+    <div class="keuangan-sub">
+      <h3>Pendapatan Desa</h3>
+      <div class="progress">
+        <div id="progress-pendapatan" class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 0%"><span style="vertical-align: middle;">100 %</span></div>
+      </div>
+
+      <div class="row keuangan-angka">
+        <div class="col-md-6 keuangan-angka-sub">
+          <span>Realisasi</span>
+          <h3 id="realisasi-pendapatan"></h3>
+        </div>
+        <div class="col-md-6 keuangan-angka-sub">
+          <span>Anggaran</span>
+          <h3 id="pagu-pendapatan"></h3>
+        </div>
+      </div>
+    </div>
+    <div class="keuangan-sub">
+      <h3>Belanja Desa</h3>
+      <div class="progress">
+        <div id="progress-belanja" class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 0%"></div>
+      </div>
+
+      <div class="row keuangan-angka">
+        <div class="col-md-6 keuangan-angka-sub">
+          <span>Realisasi</span>
+          <h3 id="realisasi-belanja"></h3>
+        </div>
+        <div class="col-md-6 keuangan-angka-sub">
+          <span>Anggaran</span>
+          <h3 id="pagu-belanja"></h3>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
 <script type="text/javascript">
-	$(document).ready(function ()
-	{
-		Highcharts.chart('chart', {
-		    chart: {
-		        type: 'bar'
-		    },
-		    title: {
-		        text: 'Pagu Anggaran (PA)'
-		    },
-		    subtitle: {
-		        text: 'Tahun <?= $widget_keuangan['tahun_anggaran'] ?>'
-		    },
-		    xAxis: {
-		        categories: ['(PA) Pendapatan Desa', '(PA) Belanja Desa', '(PA) Pembiayaan Desa'],
-		        // title: {
-		        //     text: null
-		        // }
-		    },
-		    yAxis: {
-		        min: 0,
-		        title: {
-		            text: 'Juta',
-		            align: 'high'
-		        },
-		        labels: {
-		            overflow: 'justify'
-		        }
-		    },
-		    tooltip: {
-		        valueSuffix: ' juta'
-		    },
-		    plotOptions: {
-		        bar: {
-		            dataLabels: {
-		                enabled: true
-		            }
-		        }
-		    },
-		    credits: {
-		        enabled: false
-		    },
-		    series: [{
-		        name: 'Anggaran',
-						color: '#2E8B57',
-		        data: [<?= $widget_keuangan['pendapatan_desa'] ?>, 31, 635]
-		    }, {
-		        name: 'Realisasi',
-						color: '#FFD700',
-		        data: [<?= $widget_keuangan['realisasi_pendapatan_desa'] ?>, 156, 947]
-		    }]
-		});
-	});
+  var data = <?= $widget_keuangan ?>;
 
-  Highcharts.chart('realisasi', {
-      chart: {
-          type: 'bar'
-      },
-      title: {
-          text: 'Realisasi Anggaran (RA)'
-      },
-      subtitle: {
-          text: 'Tahun <?= $widget_keuangan['tahun_anggaran'] ?>'
-      },
-      xAxis: {
-          categories: ['(PA) Pendapatan Desa', '(PA) Belanja Desa', '(PA) Pembiayaan Desa'],
-      },
-      yAxis: {
-          min: 0,
-          title: {
-              text: 'Population (millions)',
-              align: 'high'
-          },
-          labels: {
-              overflow: 'justify'
-          }
-      },
-      tooltip: {
-          valueSuffix: ' juta'
-      },
-      plotOptions: {
-          bar: {
-              dataLabels: {
-                  enabled: true
-              }
-          }
-      },
-      credits: {
-          enabled: false
-      },
-      series: [{
-          name: 'Anggaran',
-          color: '#2E8B57',
-          data: [<?= $pendapatan_desa->AnggaranStlhPAK ?>, 31, 635]
-      }, {
-          name: 'Realisasi',
-          color: '#FFD700',
-          data: [<?= $realisasi_pendapatan_desa->AnggaranStlhPAK ?>, 156, 947]
-      }]
+  $(document).ready(function()
+  {
+    var tahun = $("#keuangan-selector").val();
+    var realisasiPendapatan = data[tahun]['pendapatan']['realisasi'];
+    var paguPendapatan = data[tahun]['pendapatan']['pagu'];
+    var realisasiBelanja = data[tahun]['belanja']['realisasi'];
+    var paguBelanja = data[tahun]['belanja']['pagu'];
+    $("#progress-pendapatan").css("width", realisasiPendapatan / paguPendapatan * 100 + "%");
+    $("#progress-belanja").css("width", realisasiBelanja / paguBelanja * 100 + "%");
+    $("#pagu-pendapatan").text(paguPendapatan);
+    $("#realisasi-pendapatan").text(realisasiPendapatan);
+    $("#pagu-belanja").text(paguBelanja);
+    $("#realisasi-belanja").text(realisasiBelanja);
   });
-
-</script>
-<script type="text/javascript">
+  
   $("#keuangan-selector").change(function(){
-    var sel = $("#keuangan-selector").val();
-    if(sel == "chart"){
-      $("#chart").show();
-      $("#realisasi").hide();
-    }else if(sel == "realisasi"){
-      $("#chart").hide();
-      $("#realisasi").show();
-    }
+    var tahun = $("#keuangan-selector").val();
+    var realisasiPendapatan = data[tahun]['pendapatan']['realisasi'];
+    var paguPendapatan = data[tahun]['pendapatan']['pagu'];
+    var realisasiBelanja = data[tahun]['belanja']['realisasi'];
+    var paguBelanja = data[tahun]['belanja']['pagu'];
+    $("#progress-pendapatan").css("width", realisasiPendapatan / paguPendapatan * 100 + "%");
+    $("#progress-belanja").css("width", realisasiBelanja / paguBelanja * 100 + "%");
+    $("#pagu-pendapatan").text(paguPendapatan);
+    $("#realisasi-pendapatan").text(realisasiPendapatan);
+    $("#pagu-belanja").text(paguBelanja);
+    $("#realisasi-belanja").text(realisasiBelanja);
   });
-
-  $("#realisasi").hide();
 </script>
-<!-- Highcharts -->
-<script src="<?= base_url()?>assets/js/highcharts/highcharts.js"></script>
-<script src="<?= base_url()?>assets/js/highcharts/exporting.js"></script>
-<script src="<?= base_url()?>assets/js/highcharts/highcharts-more.js"></script>
