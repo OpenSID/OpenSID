35cd624a4e06fe47516a45b3ef8cadb9dc5f2cae
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-08-01 21:01:22 +1000

    Ganti ke versi v19.08-pasca

--- donjo-app/models/Database_model.php
+++ donjo-app/models/Database_model.php
@@ -27,7 +27,7 @@
 		'19.05' => array('migrate' => 'migrasi_1905_ke_1906', 'nextVersion' => '19.06'),
 		'19.06' => array('migrate' => 'migrasi_1906_ke_1907', 'nextVersion' => '19.07'),
 		'19.07' => array('migrate' => 'migrasi_1907_ke_1908', 'nextVersion' => '19.08'),
-		'19.08' => array('migrate' => NULL, 'nextVersion' => NULL)
+		'19.08' => array('migrate' => 'migrasi_1908_ke_1909', 'nextVersion' => NULL)
 	);
 
 	public function __construct()
@@ -186,6 +186,12 @@
 	$this->migrasi_1905_ke_1906();
 	$this->migrasi_1906_ke_1907();
 	$this->migrasi_1907_ke_1908();
+	$this->migrasi_1908_ke_1909();
+  }
+
+  private function migrasi_1908_ke_1909()
+  {
+  	//
   }
 
   private function migrasi_1907_ke_1908()
