72f1328c7302100e7e36552af41c0105fa059f3a
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-09-02 17:20:49 +1000

    Ganti ke versi v19.09-pasca

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", '19.09');
+define("VERSION", '19.09-pasca');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
