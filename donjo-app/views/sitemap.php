f1de4bf101ef8557b89f8d81ae82cd3a2f51c0e4
arfarells <k.jjones8588@student.valleycollege.edu>
2019-09-16 21:26:08 +0700

    Perbaikan Sitemap

--- donjo-app/views/sitemap.php
+++ donjo-app/views/sitemap.php
@@ -1,3 +1,4 @@
+<?php header('Content-type: text/xml'); ?>
 <?php echo'<?xml version="1.0" encoding="UTF-8" ?>' ?>
 
 <!-- https://itsolutionstuff.com/post/how-to-create-dynamic-sitemap-in-php-codeigniterexample.html -->
