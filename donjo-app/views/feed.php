feaa06213a567bc9646d221dec6e9b0889c11988
arfarells <k.jjones8588@student.valleycollege.edu>
2019-09-17 00:10:50 +0700

    #1661 Tambahkand dan perbaiki fitur RSS feed untuk artikel baru

---
+++ donjo-app/views/feed.php
@@ -0,0 +1,47 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/"
+    xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+    xmlns:slash="http://purl.org/rss/1.0/modules/slash/">
+    <?php 
+	$details = "<channel>
+    <title>Desa ". $data_config["nama_desa"] ."</title>
+    <link>". base_url() ."</link>
+    <description>Situs Web Desa ". $data_config["nama_desa"] ." Kec. ". $data_config["nama_kecamatan"] ."
+        <?= ucwords($this->setting->sebutan_kabupaten_singkat)
+        ". $data_config["nama_kabupaten"] ." - ". $data_config["nama_propinsi"] ."</description>
+    <language>ID</language>
+    <generator>Sistem Informasi Desa</generator>
+    <pubDate>".date(DATE_RFC2822)."</pubDate>
+    <image>
+        <title>Desa ". $data_config["nama_desa"] ."</title>
+        <url>". base_url("assets/files/logo/".$data_config["logo"]."") ."</url>
+        <link>". base_url() ."</link>
+    </image>
+    <atom:link href=\"".htmlspecialchars(site_url("feed"))."\" rel=\"self\" type=\"application/rss+xml\" />
+    ";
+    foreach ($feeds as $key=>$item):
+    if (strlen(trim($item["judul"]))>0):
+    $kategori = (strlen(trim($item["kategori"]))==0)? "Artikel":$item["kategori"];
+    $details .= "
+    <item>
+        <title>".htmlspecialchars($item["judul"])."</title>
+        <link>".$item["url"]."</link>
+        <source url=\"".htmlspecialchars(site_url("feed"))."\">Situs Web Desa ". $data_config["nama_desa"] ."</source>
+        <pubDate>".date(DATE_RFC2822,strtotime($item["tgl"]))."</pubDate>
+        <dc:creator>
+            <![CDATA[ ".$item["author"]." ]]>
+        </dc:creator>
+        <category>
+            <![CDATA[ ".htmlspecialchars($kategori)." ]]>
+        </category>
+        <guid isPermaLink=\"false\">".htmlspecialchars($item["url"])."</guid>
+        <description>
+            <![CDATA[ ".htmlspecialchars($data_config["nama_desa"]).", ".htmlspecialchars($item["isi"])." ]]>
+        </description>
+    </item>";
+    endif;
+    endforeach;
+    $details .="</channel>
+</rss>";
+echo($details);
