b276e25678d540c4ea21c7dd244cf1aec41607d8
arfarells <k.jjones8588@student.valleycollege.edu>
2019-09-19 23:35:04 +0700

    perbaikan RSS lebih ringkas

--- donjo-app/views/feed.php
+++ donjo-app/views/feed.php
@@ -8,16 +8,11 @@
     <title>Desa ". $data_config["nama_desa"] ."</title>
     <link>". base_url() ."</link>
     <description>Situs Web Desa ". $data_config["nama_desa"] ." Kec. ". $data_config["nama_kecamatan"] ."
-        <?= ucwords($this->setting->sebutan_kabupaten_singkat)
+        ". ucwords($this->setting->sebutan_kabupaten_singkat)."
         ". $data_config["nama_kabupaten"] ." - ". $data_config["nama_propinsi"] ."</description>
     <language>ID</language>
     <generator>Sistem Informasi Desa</generator>
-    <pubDate>".date(DATE_RFC2822)."</pubDate>
-    <image>
-        <title>Desa ". $data_config["nama_desa"] ."</title>
-        <url>". base_url("assets/files/logo/".$data_config["logo"]."") ."</url>
-        <link>". base_url() ."</link>
-    </image>
+
     <atom:link href=\"".htmlspecialchars(site_url("feed"))."\" rel=\"self\" type=\"application/rss+xml\" />
     ";
     foreach ($feeds as $key=>$item):
@@ -27,17 +22,8 @@
     <item>
         <title>".htmlspecialchars($item["judul"])."</title>
         <link>".$item["url"]."</link>
-        <source url=\"".htmlspecialchars(site_url("feed"))."\">Situs Web Desa ". $data_config["nama_desa"] ."</source>
-        <pubDate>".date(DATE_RFC2822,strtotime($item["tgl"]))."</pubDate>
-        <dc:creator>
-            <![CDATA[ ".$item["author"]." ]]>
-        </dc:creator>
-        <category>
-            <![CDATA[ ".htmlspecialchars($kategori)." ]]>
-        </category>
-        <guid isPermaLink=\"false\">".htmlspecialchars($item["url"])."</guid>
         <description>
-            <![CDATA[ ".htmlspecialchars($data_config["nama_desa"]).", ".htmlspecialchars($item["isi"])." ]]>
+            ".htmlspecialchars($data_config["nama_desa"]).", ".htmlspecialchars($item["isi"])." 
         </description>
     </item>";
     endif;
