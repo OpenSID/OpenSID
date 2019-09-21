05711fd92620d84c142e975758ff165e5e3c6999
arfarells <k.jjones8588@student.valleycollege.edu>
2019-09-21 22:18:18 +0700

    Fix Feed Model #1661

--- donjo-app/models/Feed_model.php
+++ donjo-app/models/Feed_model.php
@@ -14,9 +14,9 @@
 		$this->db->from('artikel a LEFT JOIN user u ON (a.id_user = u.id)
 		LEFT JOIN kategori k ON (a.id_kategori = k.id)');
 		$this->db->where('a.enabled=1');
-		$this->db->order_by('a.id', 'DEC');
-       $this->db->limit('20');
-	   return $this->db->get()->result();		
+		$this->db->order_by('a.id', 'desc');
+        $this->db->limit('20');
+	    return $this->db->get()->result();		
 	}
 }
 ?>
