db74f601feb649e59659724601249cf5be4746bb
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-10-06 12:08:20 +1100

    Hapus menu modul 'dokumen_sekretariat/peraturan_desa' yang salah ditambah

--- donjo-app/models/migrations/Migrasi_1910_ke_1911.php
+++ donjo-app/models/migrations/Migrasi_1910_ke_1911.php
@@ -5,6 +5,8 @@ class Migrasi_1910_ke_1911 extends CI_model {
   {
   	// WNI sebagai nilai default untuk kolom kewarganegaraan
 	  $this->dbforge->modify_column('tweb_penduduk', array('warganegara_id' => array('type' => 'TINYINT', 'constraint' => 4, 'null' => false, 'default' => 1)));
+	  // Hapus modul yg salah tambah
+	  $this->db->where('url', 'dokumen_sekretariat/peraturan_desa')->delete('setting_modul');
 	  // Aktifkan submodul Pemetaan
 		$submodul_peta = array('88'=>'plan', '89'=>'point', '90'=>'garis', '91'=>'line', '92'=>'area', '93'=>'polygon');
 		foreach ($submodul_peta as $key => $submodul)
