aac1acaf046189addad051413b9d54b905662c98
Zukrodin <zukrodin@gmail.com>
2019-10-19 18:47:38 +0700

    Update layanan_mandiri.php
    [kab50kota] Tampilkan form lapor di layanan mandiri sebagai popup #2405

--- donjo-app/views/web/mandiri/layanan_mandiri.php
+++ donjo-app/views/web/mandiri/layanan_mandiri.php
@@ -20,7 +20,7 @@
         <td><h4><a href="<?= site_url();?>first/mandiri/1/2" class=""><button type="button" class="btn btn-primary btn-block"><i class="fa fa-history"></i>Riwayat Layanan</button></a> </h4></td>
       </tr>
       <tr>
-        <td><h4><a href="<?= site_url();?>first/mandiri/1/3" class=""><button type="button" class="btn btn-primary btn-block"><i class="fa fa-bullhorn"></i>Lapor</button></a> </h4></td>
+        <td><h4><button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-bullhorn"></i>Lapor</button></h4></td>
       </tr>
       <tr>
         <td><h4><a href="<?= site_url();?>first/mandiri/1/4" class=""><button type="button" class="btn btn-primary btn-block"><i class="fa fa-handshake-o"></i>Bantuan</button></a></h4></td>
