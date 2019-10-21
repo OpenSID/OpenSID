dfdc8bbc6e000c994e0ecfa11883bc104ee90295
Diki Siswanto <dickyachmady@gmail.com>
2019-10-21 13:47:23 +0800

    Timpa bahasa bawaan datatable menjadi Bahasa Indonesia

--- donjo-app/views/web/mandiri/layanan.php
+++ donjo-app/views/web/mandiri/layanan.php
@@ -3,7 +3,10 @@
 <script>
 	$(document).ready(function() {
 		$('#list-rekam').DataTable({
-			'pageLength' : 10
+			'pageLength' : 10,
+			'language': {
+				'url': BASE_URL + '/assets/bootstrap/js/dataTables.indonesian.lang'
+				}
 		});
 	})
 </script>
