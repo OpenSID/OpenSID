8f72c536d836eb7e7fad52fdb5a31f8165f04ebd
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-10-28 12:24:50 +1100

    Perbaiki istilah kategori informasi publik

--- donjo-app/views/dokumen/_informasi_publik.php
+++ donjo-app/views/dokumen/_informasi_publik.php
@@ -1,8 +1,8 @@
 <div class="form-group">
-	<label class="control-label col-sm-4" for="nama">Kategori Publik</label>
+	<label class="control-label col-sm-4" for="nama">Kategori Informasi Publik</label>
 	<div class="col-sm-6">
 		<select name="attr[kategori_publik]" class="form-control select2 input-sm required">
-			<option value="">Pilih Kategori Dokumen</option>
+			<option value="">Pilih Kategori Informasi Publik</option>
 			<?php foreach ($list_kategori_publik AS $key => $value): ?>
 				<option value="<?= $key ?>" <?php selected($dokumen['attr']['kategori_publik'], $key) ?>><?= $value ?></option>
 			<?php endforeach; ?>
