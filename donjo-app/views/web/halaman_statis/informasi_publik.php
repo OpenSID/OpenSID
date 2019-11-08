4189a91eb7da68fb66db9d1b5365c02eafe10d5d
fusionid <zeppyshiddiq@gmail.com>
2019-11-08 17:53:36 +0700

    Perbaikan issue #2429 , #2463 dan Relayout Informasi Publik di Admin dan Frontpage

--- donjo-app/views/web/halaman_statis/informasi_publik.php
+++ donjo-app/views/web/halaman_statis/informasi_publik.php
@@ -1,55 +1,136 @@
-<style type="text/css">
-  <!-- Bug? paksa kotak cari ke sisi kanan -->
-  #info_publik_wrapper .row .col-sm-6 { width: 50% !important; }
-</style>
-
 <div class="box box-danger" style="padding-bottom: 2rem;">
-	<div class="box-header with-border" style="margin-bottom: 15px;">
+	<div class="box-header with-border">
 		<h3 class="box-title"><?= $heading ?></h3>
 	</div>
-  </style>
-	<div  style="margin-right: 1rem; margin-left: 1rem;">
-		<table class="table table-striped table-bordered" id="info_publik">
-			<thead>
-				<tr>
-          <th>No</th>
-					<th>Judul Informasi</th>
-          <th>Tahun</th>
-          <th>Kategori</th>
-          <th>Tanggal Upload</th>
-				</tr>
-			</thead>
-      <tfoot>
-      </tfoot>
-		</table>
+</style>
+<div class="box-body">
+	<div class="row">
+		<form id="peraturanForm" onsubmit="formAction(); return false;">
+			<div class="col-md-3">
+				<div class="form-group">
+					<label for="jenis_dokumen">Jenis</label>
+					<select class="form-control" name="kategori" id="kategori" onchange="formAction()">
+						<option value="">Semua</option>
+						<?php foreach($kategori as $s): ?>
+							<option value="<?= $s['id'] ?>" <?php selected($s['id'], $kategori_dokumen) ?>><?= $s['nama'] ?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="form-group">
+					<label for="jenis_dokumen">Tahun</label>
+					<select class="form-control" name="tahun" id="tahun" onchange="formAction()">
+						<option value="">Semua</option>
+						<?php foreach($tahun as $t): ?>
+							<option value="<?= $t['tahun'] ?>" <?php selected($t['tahun'], $tahun_dokumen) ?> ><?= $t['tahun'] ?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="form-group">
+					<label for="jenis_dokumen">Tentang</label>
+					<input type="text" name="tentang" id="tentang" class="form-control" style="margin-top: 0rem;">
+				</div>
+			</div>
+			<div class="col-md-3">
+				<button type="submit" class="btn btn-info" style="margin-top: 2.5rem;"><i class="fa fa-search"></i> Cari</button>
+			</div>
+		</form>
 	</div>
 </div>
-
+<div  style="margin-right: 1rem; margin-left: 1rem;">
+	<table class="table table-striped table-bordered" id="info_publik">
+		<thead>
+			<tr>
+				<th>Judul</th>
+				<th>Jenis</th>
+				<th>Tahun</th>
+			</tr>
+		</thead>
+		<tbody id="tbody-dokumen">
+		</tbody>
+	</table>
+</div>
+</div>
 <script type="text/javascript">
 $(document).ready(function() {
+	$('#jdih-table').DataTable({
+		"dom": 'rt<"bottom"p><"clear">',
+		"destroy": true,
+		"paging": false,
+		"ordering": false
+	});
 
-  var url = "<?= site_url('first/ajax_informasi_publik')?>";
-    table = $('#info_publik').DataTable({
-      'processing': true,
-      'serverSide': true,
-      "pageLength": 10,
-      'order': [],
-      "ajax": {
-        "url": url,
-        "type": "POST"
-      },
-      //Set column definition initialisation properties.
-      "columnDefs": [
-        {
-          "targets": [ 0 ], //first column / numbering column
-          "orderable": false, //set not orderable
-        },
-      ],
-      'language': {
-        'url': BASE_URL + '/assets/bootstrap/js/dataTables.indonesian.lang'
-      }
-    });
-
+	get_table();
 } );
 
+function get_table() {
+	var url = "<?= site_url('first/ajax_table_peraturan_public')?>";
+
+	$.ajax({
+		type: "GET",
+		url: url,
+		dataType: "JSON",
+		success: function(data)
+		{
+			var html;
+			if (data.length == 0)
+			{
+				html = "<tr><td colspan='3' align='center'>No Data Available</td></tr>";
+			}
+			for (var i = 0; i < data.length; i++)
+			{
+				html += "<tr>"+"<td><a href='<?= base_url('desa/upload/dokumen/')?>"+data[i].satuan+"'>"+data[i].nama+"</a></td>"+
+				"<td>"+data[i].kategori+"</td>"+
+				"<td>"+data[i].tahun+"</td>";
+			}
+			$('#tbody-dokumen').html(html);
+		},
+		error: function(err, jqxhr, errThrown)
+		{
+			console.log(err);
+		}
+	})
+}
+
+function formAction()
+{
+	$('#tbody-dokumen').html('');
+	var url = "<?= site_url('first/filter_peraturan_public') ?>";
+	var kategori = $('#kategori').val();
+	var tahun = $('#tahun').val();
+	var tentang = $('#tentang').val();
+
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: {
+			kategori: kategori,
+			tahun: tahun,
+			tentang: tentang
+		},
+		dataType: "JSON",
+		success: function(data)
+		{
+			var html;
+			if (data.length == 0)
+			{
+				html = "<tr><td colspan='3' align='center'>No Data Available</td></tr>";
+			}
+			for (var i = 0; i < data.length; i++)
+			{
+				html += "<tr>"+"<td><a href='<?= base_url('desa/upload/dokumen/')?>"+data[i].satuan+"'>"+data[i].nama+"</a></td>"+
+				"<td>"+data[i].kategori+"</td>"+
+				"<td>"+data[i].tahun+"</td>";
+			}
+			$('#tbody-dokumen').html(html);
+		},
+		error: function(err, jqxhr, errThrown)
+		{
+			console.log(err);
+		}
+	})
+}
 </script>
