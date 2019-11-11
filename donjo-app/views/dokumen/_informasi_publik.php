16d5ebff1e177fd559a17af53ee596cea36640fd
Eddie Ridwan <eddie.ridwan@gmail.com>
2019-11-11 14:10:25 +1100

    Perbaiki isian Pilih Kategori Informasi Publik melebar pada layar kecil

--- donjo-app/views/dokumen/_informasi_publik.php
+++ donjo-app/views/dokumen/_informasi_publik.php
@@ -1,7 +1,7 @@
 <div class="form-group">
 	<label class="control-label col-sm-4" for="nama">Kategori Informasi Publik</label>
 	<div class="col-sm-6">
-		<select name="kategori_info_publik" class="form-control select2 input-sm required">
+		<select name="kategori_info_publik" class="form-control select2 input-sm required" style="width: 100%">
 			<option value="">Pilih Kategori Informasi Publik</option>
 			<?php foreach ($list_kategori_publik AS $key => $value): ?>
 				<option value="<?= $key ?>" <?php selected($dokumen['kategori_info_publik'], $key) ?>><?= $value ?></option>
