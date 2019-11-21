5be159a67f950a61429f76049ae9ac4e2a966be3
fusionid <zeppyshiddiq@gmail.com>
2019-11-21 19:36:54 +0700

    Perbaikan script export peta ke GPX

--- donjo-app/views/area/maps.php
+++ donjo-app/views/area/maps.php
@@ -164,6 +164,19 @@ window.onload = function()
 			document.getElementById('path').value = getLatLong('Poly', e.target).toString();
 		})
 		setTimeout(function() {peta_area.invalidateSize();peta_area.fitBounds(poligon_area.getBounds());}, 500);
+
+		var layer = poligon_area;
+		var geojson = layer.toGeoJSON();
+		var shape_for_db = JSON.stringify(geojson);
+		var gpxData = togpx(JSON.parse(shape_for_db));
+
+		$("#exportGPX").on('click', function (event) {
+			data = 'data:text/xml;charset=utf-8,' + encodeURIComponent(gpxData);
+			$(this).attr({
+				'href': data,
+				'target': '_blank'
+			});
+		});
 	<?php endif; ?>
 
 	//Fungsi
@@ -238,4 +251,3 @@ window.onload = function()
 	<script src="<?= base_url()?>assets/js/jquery.validate.min.js"></script>
 	<script src="<?= base_url()?>assets/js/leaflet.filelayer.js"></script>
 	<script src="<?= base_url()?>assets/js/togeojson.js"></script>
-	<script src="<?= base_url()?>assets/js/togpx.js"></script>
