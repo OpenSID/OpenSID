aadf31aa86f5a49ad690da3ef389dc6e795b918e
Nur Hidayat <51182125+uncu29@users.noreply.github.com>
2019-12-01 10:25:39 +0700

    Update Database_model.php

--- donjo-app/models/Database_model.php
+++ donjo-app/models/Database_model.php
@@ -31,7 +31,7 @@
 		'19.09' => array('migrate' => 'migrasi_1909_ke_1910', 'nextVersion' => '19.10'),
 		'19.10' => array('migrate' => 'migrasi_1910_ke_1911', 'nextVersion' => '19.11'),
 		'19.11' => array('migrate' => 'migrasi_1911_ke_1912', 'nextVersion' => '19.12'),
-		'19.12' => array('migrate' => NULL, 'nextVersion' => NULL)
+		'19.12' => array('migrate' => 'migrasi_1912_ke_2001', 'nextVersion' => NULL)
 	);
 
 	public function __construct()
@@ -194,8 +194,15 @@
 		$this->migrasi_1909_ke_1910();
 		$this->migrasi_1910_ke_1911();
 		$this->migrasi_1911_ke_1912();
+		$this->migrasi_1912_ke_2001();
   }
-
+  private function migrasi_1912_ke_2001()
+  {
+  	$this->load->model('migrations/migrasi_default_value');
+  	$this->migrasi_default_value->up();
+  	$this->load->model('migrations/migrasi_1912_ke_2001');
+  	$this->migrasi_1912_ke_2001->up();
+  }
   private function migrasi_1911_ke_1912()
   {
   	$this->load->model('migrations/migrasi_default_value');
