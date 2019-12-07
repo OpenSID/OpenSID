73daf40681b8d02103c8a305a54b4c62ec19d926
Ariandi Ryan Kahfi <ariandi@mail.com>
2019-12-07 12:18:33 +0700

    Merapikan tampilan widget Arsip Artikel

--- widgets/arsip_artikel.php
+++ widgets/arsip_artikel.php
@@ -9,13 +9,14 @@
               <div id="mostPopular" class="tab-pane fade in active" role="tabpanel">
                   <table id="ul-menu">
                   <?php foreach ($arsip as $l): ?>
-                    <tr><td>
+                    <tr><td width="25%" valign="top">
                         <a href="<?= site_url("first/artikel/$l[id]")?>">
                             <?php if (is_file(LOKASI_FOTO_ARTIKEL."kecil_$l[gambar]")): ?>
-                                <img width="30%" style="float:left; margin:0 8px 4px 0;" class="img-fluid img-thumbnail" src="<?= base_url("desa/upload/artikel/kecil_$l[gambar]")?>"/>
+                                <img width="100%" style="float:left; margin:0 8px 4px 0;" class="img-fluid img-thumbnail" src="<?= base_url("desa/upload/artikel/kecil_$l[gambar]")?>"/>
                             <?php else: ?>
-                                <img width="30%" style="float:left; margin:0 8px 4px 0;" class="img-fluid img-thumbnail" src="<?= base_url("assets/images/404-image-not-found.jpg")?>"/>
+                                <img width="100%" style="float:left; margin:0 8px 4px 0;" class="img-fluid img-thumbnail" src="<?= base_url("assets/images/404-image-not-found.jpg")?>"/>
                             <?php endif;?>
+                        </td><td width="2%"></td><td valign="top">
                             <small><span class="meta_date"><font color="green"><?= tgl_indo2($l['tgl_upload']) ?></font></span><br><?= $l['judul']?></a></small>
                     </td></tr>
                   <?php endforeach; ?>
@@ -24,13 +25,14 @@
               <div id="recentComent" class="tab-pane fade" role="tabpanel">
                 <table id="ul-menu">
                   <?php foreach ($arsip_rand as $l): ?>
-                    <tr><td>
+                    <tr><td width="25%" valign="top">
                         <a href="<?= site_url("first/artikel/$l[id]")?>">
                             <?php if (is_file(LOKASI_FOTO_ARTIKEL."kecil_$l[gambar]")): ?>
-                                <img width="30%" style="float:left; margin:0 8px 4px 0;" class="img-fluid img-thumbnail" src="<?= base_url("desa/upload/artikel/kecil_$l[gambar]")?>"/>
+                                <img width="100%" style="float:left; margin:0 8px 4px 0;" class="img-fluid img-thumbnail" src="<?= base_url("desa/upload/artikel/kecil_$l[gambar]")?>"/>
                             <?php else: ?>
-                                <img width="30%" style="float:left; margin:0 8px 4px 0;" class="img-fluid img-thumbnail" src="<?= base_url("assets/images/404-image-not-found.jpg")?>"/>
+                                <img width="100%" style="float:left; margin:0 8px 4px 0;" class="img-fluid img-thumbnail" src="<?= base_url("assets/images/404-image-not-found.jpg")?>"/>
                             <?php endif;?>
+                        </td><td width="2%"></td><td valign="top">
                             <small><span class="meta_date"><font color="green"><?= tgl_indo2($l['tgl_upload']) ?></font></span><br><?= $l['judul']?></a></small>
                     </td></tr>
                   <?php endforeach; ?>
