bb0425fe0a0c3ca0a518e42695a0cd91b348f947
Ariandi Ryan Kahfi <ariandi@mail.com>
2020-01-03 23:30:35 +0700

    #27 Menampilkan Produk Hukum dan Informasi Publik

---
+++ layouts/halaman_statis.tpl.php
@@ -0,0 +1,13 @@
+<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<?php $this->load->view($folder_themes.'/layouts/header.php');?>
+      <div id="contentwrapper">
+        <div id="contentcolumn">
+          <div class="innertube">
+            <?php $this->load->view($halaman_statis); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,29 @@
+<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php $this->load->view("$folder_themes/commons/meta.php"); ?>
+</head>
+<body>
+<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+<div class="container"style="background-color: #f6f6f6;">
+    <header id="header">
+        <?php $this->load->view("$folder_themes/partials/header.php"); ?>
+    </header>
+    <div id="navarea">
+        <?php $this->load->view("$folder_themes/partials/menu_head.php"); ?>
+    </div>
+    <section id="mainContent">
+        <div class="content_middle">
+        </div>
+        <div class="content_bottom">
+            <div class="col-lg-12 col-md-12">
+                <div class="content_bottom_left">
+                    <script src="<?= base_url()?>assets/front/js/jquery.js"></script>
+                    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/bootstrap/css/dataTables.bootstrap.min.css">
+                    <script src="<?= base_url() ?>assets/bootstrap/js/jquery.dataTables.min.js"></script>
+                    <script src="<?= base_url() ?>assets/bootstrap/js/dataTables.bootstrap.min.js"></script>
+                    <script type="text/javascript">
+                      var BASE_URL = "<?= base_url(); ?>";
+                    </script>
+                    
