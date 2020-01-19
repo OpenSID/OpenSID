cb3ab796e1cfa82bdad22f08f6310e62c555a19e
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-01-19 18:09:12 +0700

    Tambah kolom data siskeudes 2019

---
+++ donjo-app/models/migrations/Migrasi_2001_ke_2002.php
@@ -0,0 +1,12 @@
+<?php
+class Migrasi_2001_ke_2002 extends CI_model {
+
+	public function up()
+	{
+		// Tambah kolom data siskeudes
+		if (!$this->db->field_exists('ID_Bank','keuangan_ta_sts'))
+		{
+			$this->db->query("ALTER TABLE keuangan_ta_sts ADD ID_Bank varchar(10) NULL");
+		}	
+	}
+}
