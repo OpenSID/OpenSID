3998cdc956fd3f0e81009bc27acf7c1d5493604c
INVALID\riyan <blueshiftzenith@gmail.com>
2020-01-30 13:42:48 +0700

    add event listener setelah create polygon

--- donjo-app/views/sid/wilayah/ajax_wilayah_maps.php
+++ donjo-app/views/sid/wilayah/ajax_wilayah_maps.php
@@ -170,6 +170,10 @@
       });
 
       peta_wilayah.fitBounds(polygon.getBounds());
+
+      // set value setelah create polygon
+      document.getElementById('path').value = getLatLong('Poly', layer).toString();
+      document.getElementById('zoom').value = peta_wilayah.getZoom();
     });
 
     //Unggah Peta dari file GPX/KML
