4fc601ea7b4865e4d83bb360d641947a6e67c9e2
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-02-03 09:37:07 +0700

    Tambah Judul Semester di Header Tabel

--- donjo-app/views/keuangan/tabel_laporan_rp_apbd.php
+++ donjo-app/views/keuangan/tabel_laporan_rp_apbd.php
@@ -51,6 +51,7 @@
 			<h4>LAPORAN REALISASI PELAKSANAAN</h4>
 			<h4>ANGGARAN PENDAPATAN DAN BELANJA DESA</h4>
 			<h4>PEMERINTAH <?= strtoupper(ucwords($this->setting->sebutan_desa))?> <?= strtoupper($desa['nama_desa'])?></h4>
+			<h4>SEMESTER <?= $sm ?></h4>
 			<h4>TAHUN ANGGARAN <?= $ta ?></h4>
 		</td>
 	</tr>
