c40380c493651ee534cea79b2599c31fa15ff5af
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-02-05 11:55:43 +1100

    Perbaiki judul laporan di artikel keuangan

--- donjo-app/views/keuangan/tabel_laporan_rp_apbd_artikel.php
+++ donjo-app/views/keuangan/tabel_laporan_rp_apbd_artikel.php
@@ -1,8 +1,13 @@
-<style>
-	table#judul-laporan h4 {
-		font-size: 14px;
-	}
-</style>
+<table width="100%" style="border: solid 0px black; text-align: center;">
+	<tr>
+		</td>
+		<td>
+			<h5>LAPORAN REALISASI PELAKSANAAN</h5>
+			<h5>ANGGARAN PENDAPATAN DAN BELANJA DESA</h5>
+			<h5>TAHUN ANGGARAN <?= $thn ?></h5>
+		</td>
+	</tr>
+</table>
 
 <?php include('donjo-app/views/keuangan/tabel_laporan_rp_apbd_isi.php'); ?>
 
--- donjo-app/views/keuangan/tabel_laporan_rp_apbd_isi.php
+++ donjo-app/views/keuangan/tabel_laporan_rp_apbd_isi.php
@@ -41,17 +41,6 @@
 	}
 </style>
 
-<table id="judul-laporan" width="100%" style="border: solid 0px black; text-align: center;">
-	<tr>
-		<td>
-			<h4>LAPORAN REALISASI PELAKSANAAN</h4>
-			<h4>ANGGARAN PENDAPATAN DAN BELANJA DESA</h4>
-			<h4>PEMERINTAH <?= strtoupper(ucwords($this->setting->sebutan_desa))?> <?= strtoupper($desa['nama_desa'])?></h4>
-			<h4>SEMESTER <?= $sm ?></h4>
-			<h4>TAHUN ANGGARAN <?= $ta ?></h4>
-		</td>
-	</tr>
-</table>
 <table class='blueTable' width='100%'>
 	<thead>
 		<tr>
