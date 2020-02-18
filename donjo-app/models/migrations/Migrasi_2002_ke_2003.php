e0b0fc54580a2e4a7f192441435d70140e519a32
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-02-18 15:43:03 +1100

    #2679: Sekarang tidak error pada waktu menambah Album baru walaupun sql_mode STRICT. [bug-fix]

--- donjo-app/models/migrations/Migrasi_2002_ke_2003.php
+++ donjo-app/models/migrations/Migrasi_2002_ke_2003.php
@@ -6,5 +6,13 @@ class Migrasi_2002_ke_2003 extends CI_model {
 		// Hapus setting tombol cetak surat langsung
 			$this->db->where('key', 'tombol_cetak_surat')
 				->delete('setting_aplikasi');
+		// Setting nilai default supaya tidak error pada sql strict mode
+		$fields = array();
+		$fields['parrent'] = array('type' => 'INT', 'constraint' => 4, 'default' => 0);
+		$fields['tipe'] = array('type' => 'INT', 'constraint' => 4, 'default' => 0);
+	  $this->dbforge->modify_column('gambar_gallery', $fields);
+		$fields = array();
+		$fields['kode_surat'] = array('type' => 'VARCHAR', 'constraint' => 10, 'null' => TRUE, 'default' => NULL);
+	  $this->dbforge->modify_column('tweb_surat_format', $fields);
 	}
 }
