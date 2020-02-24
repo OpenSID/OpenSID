942a462b54610c7851b14120e471073174c88091
afa28 <57283157+afa28@users.noreply.github.com>
2020-02-24 19:15:21 +0700

    Update Web_pengunjung_model.php
    Hapus Penggunaan Session Yang Berlebihan
    Ikuti Aturan Penulisan
    Ganti If dgn Switch

--- donjo-app/models/Web_pengunjung_model.php
+++ donjo-app/models/Web_pengunjung_model.php
@@ -12,48 +12,63 @@
 			$type = $_SESSION['filter'];
 		}
 		
-		if($type == ''){//Semua Data
-			$_SESSION['lblx'] = "Tahun";
-			$_SESSION['judul'] = "SETIAP TAHUN";
-			$parm1 = "YEAR(`Tanggal`) AS `tgl`, ";
-		}else if($type == '1'){//Data 7 Hari Terakhir (Format Y-m-d), Hari ini dan 6 hari kebelakang
-			$awal = $this->op_tgl('-6 days', date('Y-m-d'));
-			$_SESSION['lblx'] = "Tanggal";
-			$_SESSION['judul'] = "MINGGU INI";
-			$parm1 = "`Tanggal` AS `tgl`, ";
-			$parm2 = "WHERE `Tanggal` BETWEEN '".$awal."' AND '".date('Y-m-d')."'";//7 hari
-		}else if($type == '2'){//Data Bulan Ini (m), Mulai Tgl 1 sampai Akhir
-			$_SESSION['lblx'] = "Tanggal";
-			$_SESSION['judul'] = "BULAN ".strtoupper(getBulan(date('m')));
-			$parm1 = "`Tanggal` AS `tgl`, ";
-			$parm2 = "WHERE MONTH(`Tanggal`) = '".date('m')."'";//7 hari
-		}else if($type == '3'){//Data Tahun Ini (Y), Mulai bulan 1 sampai 12
-			$_SESSION['lblx'] = "Bulan";
-			$_SESSION['judul'] = "TAHUN ".date('Y');
-			$parm1 = "MONTH(`Tanggal`) AS `tgl`, ";
-			$parm2 = "WHERE YEAR(`Tanggal`) = '".date('Y')."'";//12 Bulan
-		}else{
-			//$_SESSION['lblx'] = "Rentang";
-			//$parm1 = "`Tanggal` AS `tgl`, ";
-			//$parm2 = "WHERE `Tanggal` BETWEEN '".$awal."' AND '".date('Y-m-d')."'";//Dari Tanggal Sampai Tanggal
+		switch ($type)
+		{
+			case 1: 
+				$awal = $this->op_tgl('-6 days', date('Y-m-d'));
+				$data['lblx'] = "Tanggal";
+				$data['judul'] = "DARI TANGGAL ".$awal." SAMPAI ".date('Y-m-d');
+				$parm1 = "`Tanggal` AS `tgl`, ";
+				$parm2 = "WHERE `Tanggal` BETWEEN '".$awal."' AND '".date('Y-m-d')."'";//7 hari
+				break; //Data Bulan Ini
+			case 2: 
+				$data['lblx'] = "Tanggal";
+				$data['judul'] = "BULAN ".strtoupper(getBulan(date('m')))." ".date('Y');
+				$parm1 = "`Tanggal` AS `tgl`, ";
+				$parm2 = "WHERE MONTH(`Tanggal`) = '".date('m')."'";//7 harib
+				break;//Data Tahun Ini
+			case 3: 
+				$data['lblx'] = "Bulan";
+				$data['judul'] = "TAHUN ".date('Y');
+				$parm1 = "MONTH(`Tanggal`) AS `tgl`, ";
+				$parm2 = "WHERE YEAR(`Tanggal`) = '".date('Y')."'";//12 Bulan
+				break; //Semua Data
+			default: 
+				$data['lblx'] = "Tahun";
+				$data['judul'] = "SETIAP TAHUN";
+				$parm1 = "YEAR(`Tanggal`) AS `tgl`, ";
+				break; //Semua Data
+					
+			//case untuk rentang tgl
+				//$data['lblx'] = "Rentang";
+				//$parm1 = "`Tanggal` AS `tgl`, ";
+				//$parm2 = "WHERE `Tanggal` BETWEEN '".$awal."' AND '".date('Y-m-d')."'";//Dari Tanggal Sampai Tanggal
 		}
+		
 		$query = "SELECT ".$parm1." SUM(`Jumlah`) AS `Total` FROM sys_traffic ".$parm2." GROUP BY `tgl` ORDER BY `Tanggal` ASC ";
-		$data = $this->db->query($query)->result_array();
+		$data ['pengunjung'] = $this->db->query($query)->result_array();
 		return $data;
 	}
 	
 	public function get_count($tgl)
 	{
-		if($tgl == 1){//Semua Data
-			$parm = "";
-		}else if($tgl == 2){//Data Bulan Ini
-			$parm = "WHERE MONTH(`Tanggal`) = '".date('m')."'";
-		}else if($tgl == 3){//Data Tahun Ini
-			$parm = "WHERE YEAR(`Tanggal`) = '".date('Y')."'";
-		}else{//Data Dengan Rentang Jarak
-			$hari = $this->op_tgl($tgl, date('Y-m-d'));
-			$parm = "WHERE `Tanggal` BETWEEN '".$hari."' AND '".date('Y-m-d')."'";
+		switch ($tgl)
+		{
+			case 1: 
+				$parm = ""; 
+				break; //Semua Data
+			case 2: 
+				$parm = "WHERE MONTH(`Tanggal`) = '".date('m')."'"; 
+				break; //Data Bulan Ini
+			case 3: 
+				$parm = "WHERE YEAR(`Tanggal`) = '".date('Y')."'"; 
+				break; //Data Tahun Ini
+			default: 
+				$hari = $this->op_tgl($tgl, date('Y-m-d')); 
+				$parm = "WHERE `Tanggal` BETWEEN '".$hari."' AND '".date('Y-m-d')."'"; 
+				break; //Data Dengan Rentang Jarak
 		}
+
 		$query = "SELECT SUM(`Jumlah`) AS `Total` FROM sys_traffic ".$parm;
 		$data = $this->db->query($query)->result_array();
 		$data = $data [0]['Total'];
@@ -62,6 +77,7 @@
 	
 	public function op_tgl($op, $tgl)
 	{
+		//Membuat Rentang Tgl
 		$data = date('Y-m-d', strtotime($op, strtotime($tgl)));
 		return $data;
 	}	
