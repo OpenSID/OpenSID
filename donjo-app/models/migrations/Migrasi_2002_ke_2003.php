1ede92797c950d5653942dbb05251ce6b1b2e65d
afa28 <57283157+afa28@users.noreply.github.com>
2020-02-24 22:56:27 +0700

    Update Migrasi_2002_ke_2003.php

--- donjo-app/models/migrations/Migrasi_2002_ke_2003.php
+++ donjo-app/models/migrations/Migrasi_2002_ke_2003.php
@@ -4,15 +4,32 @@ class Migrasi_2002_ke_2003 extends CI_model {
 	public function up()
 	{
 		// Hapus setting tombol cetak surat langsung
-			$this->db->where('key', 'tombol_cetak_surat')
+		$this->db->where('key', 'tombol_cetak_surat')
 				->delete('setting_aplikasi');
 		// Setting nilai default supaya tidak error pada sql strict mode
 		$fields = array();
 		$fields['parrent'] = array('type' => 'INT', 'constraint' => 4, 'default' => 0);
 		$fields['tipe'] = array('type' => 'INT', 'constraint' => 4, 'default' => 0);
-	  $this->dbforge->modify_column('gambar_gallery', $fields);
+	  	$this->dbforge->modify_column('gambar_gallery', $fields);
 		$fields = array();
 		$fields['kode_surat'] = array('type' => 'VARCHAR', 'constraint' => 10, 'null' => TRUE, 'default' => NULL);
-	  $this->dbforge->modify_column('tweb_surat_format', $fields);
+	  	$this->dbforge->modify_column('tweb_surat_format', $fields);
+		//tambah modul tema
+		if ($this->db->table_exists('setting_modul'))
+		{
+			$object = array(
+				'id' => 206,
+				'modul' => 'Tema',
+				'url' => 'tema',
+				'aktif' => 1,
+				'ikon' => 'fa-object-group',
+				'urut' => 11,
+				'level' => 4,
+				'hidden' => 0,
+				'ikon_kecil' => '',
+				'parent' => 13
+			);
+			$this->db->insert('setting_modul', $object);
+		}
 	}
 }
