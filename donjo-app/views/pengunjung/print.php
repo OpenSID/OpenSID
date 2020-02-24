d4a4342c308481526182c78dec1dab4407fcf039
afa28 <57283157+afa28@users.noreply.github.com>
2020-02-24 19:26:52 +0700

    Update print.php
    Hapus Session Berlebihan
    Ikuti Aturan Penulisan
    Tambahkan Total Data Pada Tabel
    Hapus Yang Tidak Perlu

--- donjo-app/views/pengunjung/print.php
+++ donjo-app/views/pengunjung/print.php
@@ -20,7 +20,7 @@
 								<h1 style="text-transform: uppercase;"></h1>
 								<h1><?= strtoupper($this->setting->sebutan_kecamatan)?> <?= strtoupper($config['nama_kecamatan'])?> </h1>
 								<h1><?= strtoupper($this->setting->sebutan_desa)." ".strtoupper($config['nama_desa'])?></h1>
-								<h1>LAPORAN DATA STATISTIK PENGUNJUNG WEBSITE <?= $_SESSION['judul'];?></h1>
+								<h1>LAPORAN DATA STATISTIK PENGUNJUNG WEBSITE <?= $main['judul'];?></h1>
 							</td>
 						</tr>
 						<tr>
@@ -34,67 +34,30 @@
 										</tr>
 									</thead>
 									<tbody>
-										<?php $no = 1; $total = 0; foreach ($main as $data):
+										<?php $no = 1; $total = 0; foreach ($main['pengunjung'] as $data):
 											$total = $total + $data['Total'];
 										?>
 										<tr>
 											<td class="thick" align="center" width="2"><?= $no++;?></td>
 											<td class="thick" align="center">
-											<?php
-												if($_SESSION['lblx']=='Bulan'){
-													echo getBulan($data['tgl']);
-												}else{
-													echo tgl_indo2($data['tgl']);
-												}
+											<?php if($main['lblx']=='Bulan'):
+													  echo getBulan($data['tgl'])." ".date('Y');
+												  else :
+													  echo tgl_indo2($data['tgl']);
+												  endif;
 											?>
 											</td>
 											<td class="thick" align="center"><?= $data['Total'];?></td>
 										</tr>
 										<?php endforeach;?>
 									</tbody>
-								</table>
-								<br>
-								<table class="noborder">
-									<tbody>
+									<tfoot class="bg-gray disabled color-palette">
 										<tr>
-											<td class="top">
-												<div class="nowrap">
-													<label>Laporan data statistik pengunjung website <?= strtolower($_SESSION['judul'])?> pada tanggal</label>
-													<label>:</label>
-													<strong><?= tgl_indo(date("Y m d"))?></strong>
-												</div>
-											</td>
+											<th colspan="2" class="text-center">Total</th>
+											<th class="text-center"><?= $total?></th>
 										</tr>
-									</tbody>
-								</table>
-								<br>
-								<table class="noborder">
-									<tbody>
-										<tr>
-											<td class="top" align="center" width="30%">
-												<div class="nowrap"><label></label></div>
-												<div class="nowrap"><label><br></label></div>
-												<div style="height: 70px;"></div>
-												<div class="nowrap"><strong style="text-transform: uppercase;"></strong></div>
-												<div class="nowrap"><label></label>	</div>
-											</td>
-											<td class="top" align="center" width="40%">
-												<div class="nowrap"><label>&nbsp;</label></div>
-												<div class="nowrap"><label><br></label></div>
-											</td>
-											<td class="top" align="center" width="30%">
-												<div class="nowrap"><label>&nbsp;</label></div>
-												<div class="nowrap"><label><?= ucwords($this->setting->sebutan_desa)?> <?= $config['nama_desa']?>,
-													<?= tgl_indo(date("Y m d"))?><br>KEPALA DESA/LURAH <?= (strtoupper($config['nama_desa']))?><br>
-												</div>
-												<div style="height: 50px;"></div>
-												<div class="nowrap"><strong style="text-transform: uppercase;"></strong></div>
-												<div class="nowrap">( <?= $pamong_ttd['pamong_nama']?> )<br>NIP <?= $pamong_ttd['pamong_niap_nip']?></div>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-								<br>
+									</tfoot>
+								</table>								
 							</td>
 						</tr>
 					</tbody>
