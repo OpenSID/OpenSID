8213b0d81327cac6badf2a980738c7d3924f4339
afa28 <57283157+afa28@users.noreply.github.com>
2020-02-25 13:55:09 +0700

    Update Web_pengunjung_model.php

--- donjo-app/models/Web_pengunjung_model.php
+++ donjo-app/models/Web_pengunjung_model.php
@@ -12,66 +12,81 @@
 			$type = $_SESSION['filter'];
 		}
 		
+		$this->db->select_sum('Jumlah');
+		
 		switch ($type)
 		{
 			case 1: 
 				$awal = $this->op_tgl('-6 days', date('Y-m-d'));
 				$data['lblx'] = "Tanggal";
 				$data['judul'] = "DARI TANGGAL ".$awal." SAMPAI ".date('Y-m-d');
-				$parm1 = "`Tanggal` AS `tgl`, ";
-				$parm2 = "WHERE `Tanggal` BETWEEN '".$awal."' AND '".date('Y-m-d')."'";//7 hari
-				break; //Data Bulan Ini
+				
+				$this->db->select('Tanggal');
+				$this->db->where('Tanggal >=', $awal);
+				$this->db->where('Tanggal <=', date('Y-m-d'));
+				$this->db->group_by('Tanggal');
+				break; //7 Hari
 			case 2: 
 				$data['lblx'] = "Tanggal";
 				$data['judul'] = "BULAN ".strtoupper(getBulan(date('m')))." ".date('Y');
-				$parm1 = "`Tanggal` AS `tgl`, ";
-				$parm2 = "WHERE MONTH(`Tanggal`) = '".date('m')."'";//7 harib
-				break;//Data Tahun Ini
+				
+				$this->db->select('Tanggal');
+				$this->db->where('MONTH(`Tanggal`) =', date('m'));
+				$this->db->group_by('Tanggal');
+				break;//1 bulan(tgl 1 sampai akhir bulan)
 			case 3: 
 				$data['lblx'] = "Bulan";
 				$data['judul'] = "TAHUN ".date('Y');
-				$parm1 = "MONTH(`Tanggal`) AS `tgl`, ";
-				$parm2 = "WHERE YEAR(`Tanggal`) = '".date('Y')."'";//12 Bulan
-				break; //Semua Data
+
+				$this->db->select('MONTH(`Tanggal`) AS Tanggal');
+				$this->db->where('YEAR(Tanggal) =', date('Y'));
+				$this->db->group_by('MONTH(`Tanggal`)');
+				break; //1 tahun / 12 Bulan
 			default: 
 				$data['lblx'] = "Tahun";
 				$data['judul'] = "SETIAP TAHUN";
-				$parm1 = "YEAR(`Tanggal`) AS `tgl`, ";
+
+				$this->db->select('YEAR(`Tanggal`) AS Tanggal');
+				$this->db->group_by('YEAR(`Tanggal`)');
 				break; //Semua Data
-					
-			//case untuk rentang tgl
-				//$data['lblx'] = "Rentang";
-				//$parm1 = "`Tanggal` AS `tgl`, ";
-				//$parm2 = "WHERE `Tanggal` BETWEEN '".$awal."' AND '".date('Y-m-d')."'";//Dari Tanggal Sampai Tanggal
 		}
+		$this->db->order_by('Tanggal', 'asc');
+		$sql = $this->db->get('sys_traffic')->result_array();
+		$data ['pengunjung'] = $sql;
 		
-		$query = "SELECT ".$parm1." SUM(`Jumlah`) AS `Total` FROM sys_traffic ".$parm2." GROUP BY `tgl` ORDER BY `Tanggal` ASC ";
-		$data ['pengunjung'] = $this->db->query($query)->result_array();
+		foreach($sql as $total):
+			$jml = $jml + $total['Jumlah'];
+		endforeach;
+		
+		$data ['Total'] = $jml;
 		return $data;
 	}
 	
 	public function get_count($tgl)
 	{
+		$this->db->select_sum('Jumlah');
+		
 		switch ($tgl)
 		{
 			case 1: 
-				$parm = ""; 
+				//null
 				break; //Semua Data
 			case 2: 
-				$parm = "WHERE MONTH(`Tanggal`) = '".date('m')."'"; 
+				$this->db->where('MONTH(Tanggal) =', date('m'));
 				break; //Data Bulan Ini
 			case 3: 
-				$parm = "WHERE YEAR(`Tanggal`) = '".date('Y')."'"; 
+				$this->db->where('YEAR(Tanggal) =', date('Y'));
 				break; //Data Tahun Ini
 			default: 
 				$hari = $this->op_tgl($tgl, date('Y-m-d')); 
-				$parm = "WHERE `Tanggal` BETWEEN '".$hari."' AND '".date('Y-m-d')."'"; 
+				$this->db->where('Tanggal >=', $hari);
+				$this->db->where('Tanggal <=', date('Y-m-d'));
 				break; //Data Dengan Rentang Jarak
 		}
-
-		$query = "SELECT SUM(`Jumlah`) AS `Total` FROM sys_traffic ".$parm;
-		$data = $this->db->query($query)->result_array();
-		$data = $data [0]['Total'];
+		$sql = $this->db->get('sys_traffic');
+		
+		$data = $sql->result_array();
+		$data = $data [0]['Jumlah'];
 		if(empty($data)):
 			$data = 0;
 		endif;
@@ -84,5 +99,13 @@
 		$data = date('Y-m-d', strtotime($op, strtotime($tgl)));
 		return $data;
 	}	
+	
+	public function get_config()
+	{
+		$sql = "SELECT * FROM config WHERE 1";
+		$query = $this->db->query($sql);
+		$data = $query->row_array();
+		return $data;
+	}
 }
 ?>
