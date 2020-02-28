9687a7304744f907e02dd3a050a143dfc81debc2
Ariandi Ryan Kahfi <ariandi@mail.com>
2020-02-29 01:42:49 +0700

    Sembunyikan grafik keuangan yang bernilai 0

--- partials/apbdesa-tema.php
+++ partials/apbdesa-tema.php
@@ -1,34 +1,19 @@
-<style type="text/css">
-  .progress-bar span
-  {
-    position: absolute;
-    right: 20px;
-    color: #000000;
-    font-weight: bold;
-  }
-  .box-info {
-    border-top-width: 5px;
-    padding-top: 0px
-  }
-</style>
-<div class="container" id="transparansi-footer" style="width: 100%; padding-top: 10px; background: #fff; color: #222">
-  <div class="box box-info">
-    <?php foreach ($data_widget as $subdata_name => $subdatas): ?>
-      <div class="col-md-4">
-      <div align="center" style="height: 3.5em;"><h2><?= ($subdatas['laporan'])?></h2></div><hr/>
-      <div align="center" style="height: 1em;"><h4>Realisasi | Anggaran</h4></div><hr/>
-      <?php foreach ($subdatas as $key => $subdata): ?>
-        <?php if($subdata['judul'] != NULL and $key != 'laporan'): ?>
-          <div class="progress-group">
-            <?= $subdata['judul']; ?><br>
-            <b>Rp. <?= number_format($subdata['realisasi']); ?> | Rp. <?= number_format($subdata['anggaran']); ?></b>
-            <div class="progress progress-bar-striped" align="right" style="background-color: #27b2c8"><small></small>
-              <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" style="width: <?= $subdata['persen'] ?>%" aria-valuenow="<?= $subdata['persen'] ?>" aria-valuemin="0" aria-valuemax="100"><span><?= $subdata['persen'] ?> %</span></div>
-            </div>
-          </div>
-        <?php endif; ?>
-      <?php endforeach; ?>
-      </div>
+<div class="container" id="transparansi-footer" style="width: 100%; padding-top: 10px;">
+<?php foreach ($data_widget as $subdata_name => $subdatas): ?>
+<div class="col-md-4">
+    <div align="center"><h2><?= ($subdatas['laporan'])?></h2></div><hr>
+    <div align="center"><h4>Realisasi | Anggaran</h4></div><hr>
+    <?php foreach ($subdatas as $key => $subdata): ?>
+    <?php if($subdata['judul'] != NULL and $key != 'laporan' and $subdata['realisasi'] != 0 and $subdata['anggaran'] != 0): ?>
+    <div class="progress-group"><?= $subdata['judul']; ?><br>
+		<b>Rp. <?= number_format($subdata['realisasi']); ?> | Rp. <?= number_format($subdata['anggaran']); ?></b>
+        <div class="progress progress-bar-striped" align="right" style="background-color: #27c8a2"><small></small>
+            <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" style="width: <?= $subdata['persen'] ?>%" aria-valuenow="<?= $subdata['persen'] ?>" aria-valuemin="0" aria-valuemax="100"><span><?= $subdata['persen'] ?> %</span></div>
+        </div>
+
+    </div>
+    <?php endif; ?>
     <?php endforeach; ?>
-  </div>
 </div>
+<?php endforeach; ?>
+</div><hr>
