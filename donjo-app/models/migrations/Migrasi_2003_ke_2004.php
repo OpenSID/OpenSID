5e8521382aec01ac16af059cb2682606fb74d279
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-03-05 15:16:41 +1100

    Lakukan migrasi mengubah panjang jalan dari KM menjadi M. Ubah KM di cetak inventaris jalam menjadi M. Sederhanakan penambahan  migrasi baru.

--- donjo-app/models/migrations/Migrasi_2003_ke_2004.php
+++ donjo-app/models/migrations/Migrasi_2003_ke_2004.php
@@ -3,6 +3,10 @@ class Migrasi_2003_ke_2004 extends CI_model {
 
 	public function up()
 	{
-		var_dump('hello'); die();
+		// Ubah panjang jalan dari KM menjadi M.
+		// Untuk mencegah diubah berkali-kali buat asumsi panjang jalan sebelum konversi maksimal 100 KM dan sesudah menggunakan M, minimal 100 M.
+		$this->db->where('panjang < 100')
+			->set('panjang', 'panjang * 1000', false)
+			->update('inventaris_jalan');
 	}
 }
