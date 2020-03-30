0f9380db1eb00cc048385e38ffaf5249c30cf32d
Mohamad Abdul Haris <46555658+M-A-Haris@users.noreply.github.com>
2020-03-30 07:58:43 +0700

    Update opensid_helper.php

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-define("VERSION", '20.03-pasca');
+define("VERSION", '20.04');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
