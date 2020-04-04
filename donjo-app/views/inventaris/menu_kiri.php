42d48563b4ba329f6ef2924262c2388d8ff7c3ad
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-04-04 22:59:22 +1100

    Perbaiki menu_kiri inventaris

--- donjo-app/views/inventaris/menu_kiri.php
+++ donjo-app/views/inventaris/menu_kiri.php
@@ -7,10 +7,15 @@
 	</div>
 	<div class="box-body no-padding">
 		<ul class="nav nav-pills nav-stacked">
-			<li class="<?php ($tip==1) and print('active')?>"><a href="<?=site_url($this->tipe);?>"><i class="fa fa-list"></i> Daftar Inventaris</a></li>
-			<?php if($this->tab_ini != 6):?>
+			<?php if ($this->tab_ini == 7): ?>
+				<li <?php if ($tip==1): ?>class="active"<?php endif; ?>><a href="<?=site_url('laporan_inventaris')?>"><i class="fa fa-list"></i> Laporan Semua Asset</a></li>
+				<li <?php if ($tip==2): ?>class="active"<?php endif; ?>><a href="<?=site_url('laporan_inventaris/mutasi')?>"><i class="fa fa-list"></i> Laporan Asset Yang Dihapus</a></li>
+			<?php else: ?>
+				<li class="<?php ($tip==1) and print('active')?>"><a href="<?=site_url($this->tipe);?>"><i class="fa fa-list"></i> Daftar Inventaris</a></li>
+				<?php if($this->tab_ini != 6):?>
 	  			<li class="<?php ($tip==2) and print('active')?>"><a href="<?=site_url($this->tipe.'/mutasi')?>"><i class="fa fa-share"></i> Mutasi Inventaris</a></li>
-			<?php endif ?>
+				<?php endif ?>
+			<?php endif ?>}
 		</ul>
 	</div>
 </div>
