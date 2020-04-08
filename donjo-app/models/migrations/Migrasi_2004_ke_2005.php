1e33259d1bfc0fc159ed0d979a4570259f70aabc
afa28 <andifahruddinakas@gmail.com>
2020-04-08 12:34:07 +0700

    Update Migrasi_2004_ke_2005.php

--- donjo-app/models/migrations/Migrasi_2004_ke_2005.php
+++ donjo-app/models/migrations/Migrasi_2004_ke_2005.php
@@ -19,6 +19,8 @@ class Migrasi_2004_ke_2005 extends CI_model {
 		// Perbaiki nama aset salah
 		$this->db->where('id_aset', 3423)->update('tweb_aset', array('nama' => 'JALAN'));
 		$this->db->where('id', 79)->update('setting_modul', array('url'=>'api_inventaris_kontruksi', 'aktif'=>'1'));
+		// Hapus field urut di tabel artikel krn tdk dibutuhkan
+		$this->db->query('ALTER TABLE `artikel` DROP COLUMN `urut`');
 	}
 	
 }
