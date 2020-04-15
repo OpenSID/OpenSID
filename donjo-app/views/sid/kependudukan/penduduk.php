c1e5ff73807dc66fc1a01b2f6cb6991e6a66dcb9
afa28 <andifahruddinakas@gmail.com>
2020-04-16 06:52:24 +0700

    Hapus modal confirmasi status yg tdk digunakan
    Yang tidak menggunakan modal confirm-status:
    1. suplemen - suplement
    2. suplement - daftar
    3. kependudukan - rtm
    4. kependudukan - keluarga
    5. analisis_kategori
    6. analisis_indikator
    7. analisis_klasifikasii
    8. analisis_priode
    9. kelompok
    10. dpt
    11. kelompok_master
    12. analisis_respon
    13. kelompok - anggota
    
    Catatan : modal confirmasi status hanya digunakan di penduduk dan log penduduk

--- donjo-app/views/sid/kependudukan/penduduk.php
+++ donjo-app/views/sid/kependudukan/penduduk.php
@@ -310,25 +310,6 @@
 								</div>
 							</div>
 						</div>
-						<div class='modal fade' id='confirm-status' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-							<div class='modal-dialog'>
-								<div class='modal-content'>
-									<div class='modal-header'>
-										<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-										<h4 class='modal-title' id='myModalLabel'><i class='fa fa-exclamation-triangle text-red'></i> Konfirmasi</h4>
-									</div>
-									<div class='modal-body btn-info'>
-										Apakah Anda yakin ingin mengembalikan status data penduduk ini?
-									</div>
-									<div class='modal-footer'>
-										<button type="button" class="btn btn-social btn-flat btn-danger btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
-										<a class='btn-ok'>
-											<button type="button" class="btn btn-social btn-flat btn-info btn-sm" id="ok-status"><i class='fa fa-check'></i> Simpan</button>
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
 					</div>
 				</div>
 			</div>
@@ -336,3 +317,22 @@
 	</section>
 </div>
 <?php $this->load->view('global/confirm_delete');?>
+<div class='modal fade' id='confirm-status' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
+	<div class='modal-dialog'>
+		<div class='modal-content'>
+			<div class='modal-header'>
+				<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
+					<h4 class='modal-title' id='myModalLabel'><i class='fa fa-exclamation-triangle text-red'></i> Konfirmasi</h4>
+			</div>
+			<div class='modal-body btn-info'>
+				Apakah Anda yakin ingin mengembalikan status data penduduk ini?
+			</div>
+			<div class='modal-footer'>
+				<button type="button" class="btn btn-social btn-flat btn-danger btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
+				<a class='btn-ok'>
+					<button type="button" class="btn btn-social btn-flat btn-info btn-sm" id="ok-status"><i class='fa fa-check'></i> Simpan</button>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
