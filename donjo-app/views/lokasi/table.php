7f78e97210ceed70c754000fd8fac6278a993c32
afa28 <andifahruddinakas@gmail.com>
2020-04-16 07:00:29 +0700

    Hapus modal mapBox yg tidak terpakai

--- donjo-app/views/lokasi/table.php
+++ donjo-app/views/lokasi/table.php
@@ -168,17 +168,6 @@
 									</div>
 								</div>
 							</div>
-							<div  class="modal fade" id="mapBox" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-								<div class='modal-dialog'>
-									<div class='modal-content'>
-										<div class='modal-header'>
-											<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-											<h4 class='modal-title' id='myModalLabel'></h4>
-										</div>
-										<div class="fetched-data"></div>
-									</div>
-								</div>
-							</div>
 						</div>
 					</div>
 				</div>
