e2ebf41bdeb7f199e394af86ba3b44b1ea2b3a88
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-04-20 16:57:13 +1000

    Hapus dialog confirm-status yg tidak digunakan

--- donjo-app/views/sid/kependudukan/rtm_anggota.php
+++ donjo-app/views/sid/kependudukan/rtm_anggota.php
@@ -91,25 +91,6 @@
 								</div>
 							</div>
 						</div>
-						<div class='modal fade' id='confirm-status' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-							<div class='modal-dialog'>
-								<div class='modal-content'>
-									<div class='modal-header'>
-										<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-										<h4 class='modal-title' id='myModalLabel'><i class='fa fa-exclamation-triangle text-red'></i> Konfirmasi</h4>
-									</div>
-									<div class='modal-body btn-info'>
-										Apakah Anda yakin ingin memecah Data Keluarga ini?
-									</div>
-									<div class='modal-footer'>
-										<button type="button" class="btn btn-social btn-flat btn-danger btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
-										<a class='btn-ok'>
-											<button type="button" class="btn btn-social btn-flat btn-info btn-sm" id="ok-delete"><i class='fa fa-check'></i> Simpan</button>
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
 						<div  class="modal fade" id="modalBox" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 							<div class='modal-dialog'>
 								<div class='modal-content'>
