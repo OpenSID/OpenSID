e90bf7b7c8510a03af326e0c7806b0af013304f0
ariandii <ariandi@mail.com>
2020-04-26 00:49:30 +0700

    Implementasi ajax untuk mengambil Jadwal Sholat

--- widgets/jadwal_sholat.php
+++ widgets/jadwal_sholat.php
@@ -1,91 +1,57 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <?php if (config_item('kode_kota')): ?>
-<?php
-$tgl = date('Y-m-d');
-$tgl_besok = mktime(0,0,0,date("n"),date("j")+1,date("Y"));
-$besok = date("Y-m-d", $tgl_besok);
-$kota = config_item('kode_kota');
 
-$kode = json_decode(file_get_contents('https://api.banghasan.com/sholat/format/json/kota/kode/'.$kota), true);
-$nama = $kode['kota'][0]['nama'];
+<link rel="stylesheet" href="<?= base_url("$this->theme_folder/$this->theme/assets/css/widget.min.css") ?>">
+<script>
+	const KODE_KOTA = "<?= config_item('kode_kota') ?>";
+	const TANGGAL = "<?= date('Y-m-d') ?>";  
+	const BESOK = "<?= date("Y-m-d", mktime(0,0,0,date("n"),date("j")+1,date("Y"))) ?>";  
+</script>
+<script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/widget.min.js") ?>"></script>
 
-$waktu = json_decode(file_get_contents('https://api.banghasan.com/sholat/format/json/jadwal/kota/'.$kota.'/tanggal/'.$tgl), true);
-$waktu2 = json_decode(file_get_contents('https://api.banghasan.com/sholat/format/json/jadwal/kota/'.$kota.'/tanggal/'.$besok), true);
-?>
-<style>
-	.data-case-container {
-		background-color: #FFF8DC; width:100%; height: 100%; padding: 0px; margin: 0px;
-	}
-	.data-case-container li,
-	.data-case-container li p,
-	.data-case-container li td {
-		font-family: "ants-thin", "Century Gothic", Arial; font-size: 12px;
-	}
-	.data-case-container li.info-date {
-		font-family: "ants-strong", "Century Gothic", Arial; font-size: 14px;
-		font-weight: bold; padding: 5px; text-align: center;
-	}
-	.data-case-container li.info-region {
-		padding: 4px 5px; background-color: #e64946; color: #fff; text-align: center;
-		font-family: "ants-strong", "Century Gothic", Arial; font-size: 10px; font-weight: bold;
-	}
-		
-	.data-case-container table td {
-		padding: 3px 5px; border-bottom: 1px solid #e4bd2e;
-	}
-	.data-case-container table tr:last-child td {
-		border-bottom: none;
-	}
-	.data-case-container table td.dot {
-		width: 5px; white-space: nowrap; text-align: center;
-	}
-	.data-case-container table td.case {
-		width: 5px; white-space: nowrap; text-align: right;
-	}
-</style>
 <div class="archive_style_1">
     <div class="single_bottom_rightbar">
         <h2 class="box-title"><i class="fa fa-calendar"></i> JADWAL SHOLAT & IMSAK</h2>
         <div class="data-case-container">
             <ul class="ants-right-headline">
-                <li class="info-date"><?= $nama ?></li>
+                <li class="info-date"><span data-name="kota"><i class="fa fa-spinner fa-pulse"></i></span></li>
                 <li class="info-case">
                     <table style="width: 100%;" cellpadding="0" cellspacing="0">
                         <tr>
-                            <td colspan="3" class="description"><li class="info-region"><?= $waktu['jadwal']['data']['tanggal'] ?></li></td>
-                            <td colspan="3" class="description"><li class="info-region"><?= $waktu2['jadwal']['data']['tanggal'] ?></li></td>
+                            <td id="jadwal-shalat" colspan="3" class="description"><li class="info-region"><span data-name="tanggal"><i class="fa fa-spinner fa-pulse"></i></span></li></td>
+                            <td id="jadwal-shalat2" colspan="3" class="description"><li class="info-region"><span data-name="tanggal2"><i class="fa fa-spinner fa-pulse"></i></span></li></td>
                         </tr>
                         <tr>
-                            <td class="description">Imsak</td><td class="dot">:</td><td class="case"><?= $waktu['jadwal']['data']['imsak'] ?></td>
-                            <td class="description">Imsak</td><td class="dot">:</td><td class="case"><?= $waktu2['jadwal']['data']['imsak'] ?></td>
+                            <td id="jadwal-shalat" class="description">Imsak</td><td class="dot">:</td><td class="case"><span data-name="imsak"><i class="fa fa-spinner fa-pulse"></i></span></td>
+                            <td id="jadwal-shalat2" class="description">Imsak</td><td class="dot">:</td><td class="case"><span data-name="imsak2"><i class="fa fa-spinner fa-pulse"></i></span></td>
                         </tr>
                         <tr>
-                            <td class="description">Subuh</td><td class="dot">:</td><td class="case"><?= $waktu['jadwal']['data']['subuh'] ?></td>
-                            <td class="description">Subuh</td><td class="dot">:</td><td class="case"><?= $waktu2['jadwal']['data']['subuh'] ?></td>
+                            <td id="jadwal-shalat" class="description">Subuh</td><td class="dot">:</td><td class="case"><span data-name="subuh"><i class="fa fa-spinner fa-pulse"></i></span></td>
+                            <td id="jadwal-shalat2" class="description">Subuh</td><td class="dot">:</td><td class="case"><span data-name="subuh2"><i class="fa fa-spinner fa-pulse"></i></span></td>
                         </tr>
                         <tr>
-                            <td class="description">Terbit</td><td class="dot">:</td><td class="case"><?= $waktu['jadwal']['data']['terbit'] ?></td>
-                            <td class="description">Terbit</td><td class="dot">:</td><td class="case"><?= $waktu2['jadwal']['data']['terbit'] ?></td>
+                            <td id="jadwal-shalat" class="description">Terbit</td><td class="dot">:</td><td class="case"><span data-name="terbit"><i class="fa fa-spinner fa-pulse"></i></span></td>
+                            <td id="jadwal-shalat2" class="description">Terbit</td><td class="dot">:</td><td class="case"><span data-name="terbit2"><i class="fa fa-spinner fa-pulse"></i></span></td>
                         </tr>
                         <tr>
-                            <td class="description">Dhuha</td><td class="dot">:</td><td class="case"><?= $waktu['jadwal']['data']['dhuha'] ?></td>
-                            <td class="description">Dhuha</td><td class="dot">:</td><td class="case"><?= $waktu2['jadwal']['data']['dhuha'] ?></td>
+                            <td id="jadwal-shalat" class="description">Dhuha</td><td class="dot">:</td><td class="case"><span data-name="dhuha"><i class="fa fa-spinner fa-pulse"></i></span></td>
+                            <td id="jadwal-shalat2" class="description">Dhuha</td><td class="dot">:</td><td class="case"><span data-name="dhuha2"><i class="fa fa-spinner fa-pulse"></i></span></td>
                         </tr>
                         <tr>
-                            <td class="description">Dzuhur</td><td class="dot">:</td><td class="case"><?= $waktu['jadwal']['data']['dzuhur'] ?></td>
-                            <td class="description">Dzuhur</td><td class="dot">:</td><td class="case"><?= $waktu2['jadwal']['data']['dzuhur'] ?></td>
+                            <td id="jadwal-shalat" class="description">Dzuhur</td><td class="dot">:</td><td class="case"><span data-name="dzuhur"><i class="fa fa-spinner fa-pulse"></i></span></td>
+                            <td id="jadwal-shalat2" class="description">Dzuhur</td><td class="dot">:</td><td class="case"><span data-name="dzuhur2"><i class="fa fa-spinner fa-pulse"></i></span></td>
                         </tr>
                         <tr>
-                            <td class="description">Ashar</td><td class="dot">:</td><td class="case"><?= $waktu['jadwal']['data']['ashar'] ?></td>
-                            <td class="description">Ashar</td><td class="dot">:</td><td class="case"><?= $waktu2['jadwal']['data']['ashar'] ?></td>
+                            <td id="jadwal-shalat" class="description">Ashar</td><td class="dot">:</td><td class="case"><span data-name="ashar"><i class="fa fa-spinner fa-pulse"></i></span></td>
+                            <td id="jadwal-shalat2" class="description">Ashar</td><td class="dot">:</td><td class="case"><span data-name="ashar2"><i class="fa fa-spinner fa-pulse"></i></span></td>
                         </tr>
                         <tr>
-                            <td class="description">Maghrib</td><td class="dot">:</td><td class="case"><?= $waktu['jadwal']['data']['maghrib'] ?></td>
-                            <td class="description">Maghrib</td><td class="dot">:</td><td class="case"><?= $waktu2['jadwal']['data']['maghrib'] ?></td>
+                            <td id="jadwal-shalat" class="description">Maghrib</td><td class="dot">:</td><td class="case"><span data-name="maghrib"><i class="fa fa-spinner fa-pulse"></i></span></td>
+                            <td id="jadwal-shalat2" class="description">Maghrib</td><td class="dot">:</td><td class="case"><span data-name="maghrib2"><i class="fa fa-spinner fa-pulse"></i></span></td>
                         </tr>
                         <tr>
-                            <td class="description">Isya</td><td class="dot">:</td><td class="case"><?= $waktu['jadwal']['data']['isya'] ?></td>
-                            <td class="description">Isya</td><td class="dot">:</td><td class="case"><?= $waktu2['jadwal']['data']['isya'] ?></td>
+                            <td id="jadwal-shalat" class="description">Isya</td><td class="dot">:</td><td class="case"><span data-name="isya"><i class="fa fa-spinner fa-pulse"></i></span></td>
+                            <td id="jadwal-shalat2" class="description">Isya</td><td class="dot">:</td><td class="case"><span data-name="isya2"><i class="fa fa-spinner fa-pulse"></i></span></td>
                         </tr>
             		</table>
             	</li>
