f8d5687c834d98fc3cd010b6f58d03203a16f7c5
afa28 <andifahruddinakas@gmail.com>
2020-05-02 18:44:19 +0700

    Update bottom_content_right.php

--- partials/bottom_content_right.php
+++ partials/bottom_content_right.php
@@ -8,7 +8,7 @@ if ($w_cos):
 	foreach ($w_cos as $data):
 		$widget = trim($data['isi']);
 		if ($data["jenis_widget"] == 1):
-			include('$this->theme_folder/$this->theme/widgets/'.$widget);
+			include("$this->theme_folder/$this->theme/widgets/".$widget);
 		elseif ($data["jenis_widget"] == 2):
 			include($widget);
 		else: ?>
