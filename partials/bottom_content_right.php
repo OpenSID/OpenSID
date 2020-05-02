a35d6dbae0751fcea7c33440a07281919ed25e73
afa28 <andifahruddinakas@gmail.com>
2020-05-02 18:19:01 +0700

    perbaikan widget

--- partials/bottom_content_right.php
+++ partials/bottom_content_right.php
@@ -4,22 +4,23 @@
 
 <!-- Tampilkan Widget -->
 <?php
-
-if($w_cos){
-	foreach($w_cos as $data){
-		if(($data["jenis_widget"] == 1) OR ($data["jenis_widget"] == 2)){
-			include("$this->theme_folder/$this->theme/widgets/".trim($data['isi']));
-		} else {
-			echo "
-			<div class=\"single_bottom_rightbar\">
-			    <h2><i class='fa fa-folder-open'></i> ".$data["judul"]."</h2>
-				<div class=\"box-body\">
-				".html_entity_decode($data['isi'])."
+if ($w_cos):
+	foreach ($w_cos as $data):
+		$widget = trim($data['isi']);
+		if ($data["jenis_widget"] == 1):
+			include('$this->theme_folder/$this->theme/widgets/'.$widget);
+		elseif ($data["jenis_widget"] == 2):
+			include($widget);
+		else: ?>
+			<div class="box box-primary box-solid">
+				<div class="box-header">
+					<h3 class="box-title"><?=$data["judul"]?></h3>
+				</div>
+				<div class="box-body">
+					<?=html_entity_decode($data['isi'])?>
 				</div>
 			</div>
-			";
-		}
-	}
-}
+		<?php endif;
+	endforeach;
+endif;
 ?>
-</div>
