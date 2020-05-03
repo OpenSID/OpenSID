460b2c7d962b4099afcb63121989a3743959d261
Diki Siswanto <dickyachmady@gmail.com>
2020-05-03 09:34:09 +0800

    refactor: perbaiki penulisan view dalam format php in html dan sederhakan script js di halaman statistik

--- themes/hadakewa/partials/gallery.php
+++ themes/hadakewa/partials/gallery.php
@@ -8,55 +8,58 @@
 		<div class="box-body">
 			<ul class="thumbnail">
 				<?php $i = 1 ?>
-				<?php foreach ($gallery as $data) : ?>
-					<?php if (is_file(LOKASI_GALERI . 'sedang_' . $data['gambar'])) : ?>
-						<li>
-							<div class="entry">
-								<a href="<?= AmbilGaleri($data['gambar'],'sedang') ?>" class="group2">
-									<img src="<?= AmbilGaleri($data['gambar'], 'kecil') ?>" alt="">
-								</a>
-							</div>
-							<div class="title">
-								<a href="<?= site_url('first/sub_gallery/' . $data['id']) ?>" title="<?= $data['nama'] ?>">Album : <?= $data['nama'] ?></a>
-							</div>
-						</li>
-					<?php endif ?>
-					<?php if (fmod($i, 2) == 0) : ?>
-						<div class="clearboth"></div>
-					<?php endif ?>
-					<?php $i++ ?>
+				<?php foreach($gallery as $data) : ?>
+				<?php if(is_file(LOKASI_GALERI."sedang_{$data['gambar']}")) : ?>
+				<li>
+					<div class="entry">
+						<a href="<?= AmbilGaleri($data['gambar'], 'sedang') ?>" class="group2">
+							<img src="<?= AmbilGaleri($data['gambar'], 'kecil') ?>" alt="">
+						</a>
+						<div class="title">
+							<a href="<?= site_url("first/sub_gallery/{$data['id']}") ?>" title="<?= $data['nama'] ?>">
+								Album: <?= $data['nama'] ?>
+							</a>
+						</div>
+					</div>
+				</li>
+				<div class="<?php fmod($i, 2) == 0 and print('clearboth') ?>"></div>
+				<?php $i++ ?>
+				<?php endif ?>
 				<?php endforeach ?>
 			</ul>
 			<div class="clearboth"></div>
 		</div>
 		<div class="box-footer">
-			<p>Halaman <?= $p ?> dari <?= $paging->end_link ?></p>
-			<ul class="pagination	pagination-sm no-margin">
-				<?php if ($paging->start_link) : ?>
-					<li>
-						<a href="<?= site_url('first/gallery/'.$paging->start_link)?>" title="Halaman Awal">
-							<i class="fa fa-fast-backward"></i>&nbsp;
-						</a>
-					</li>
+			<div>Halaman <?= $p ?> dari <?= $paging->end_link ?></div>
+			<ul class="pagination pagination-sm no-margin">
+				<?php if($paging->start_link) : ?>
+				<li>
+					<a href="<?= site_url("first/gallery/{$paging->start_link}") ?>" title="Halaman pertama">
+						<i class="fa fa-fast-backward"></i>&nbsp;</a>
+				</li>
 				<?php endif ?>
-				<?php foreach ($pages as $i): ?>
-					<li class="<?php ($p == $i) and print('active') ?>">
-						<a class="page-link" href="<?= site_url('first/gallery/'.$i. $paging->suffix) ?>" title="Halaman <?= $i ?>"><?= $i ?></a>&nbsp;
-					</li>
-				<?php endforeach; ?>
-				<?php if ($paging->next) : ?>
-					<li>
-						<a href="<?= site_url('first/gallery/'.$paging->next . $paging->suffix)?>" title="Halaman Selanjutnya">
-							<i class="fa fa-forward"></i>
-						</a>
-					</li>
+				<?php if($paging->prev) : ?>
+				<li>
+					<a href="<?= site_url("first/gallery/{$paging->prev}") ?>" title="Halaman sebelumnya">
+						<i class="fa fa-fast-backward"></i>&nbsp;</a>
+				</li>
 				<?php endif ?>
-				<?php if ($paging->end_link) : ?>
-					<li>
-						<a href="<?= site_url('first/gallery/'.$paging->end_link . $paging->suffix)?>" title="Halaman Akhir">
-							<i class="fa fa-fast-forward"></i>&nbsp;
-						</a>
-					</li>
+				<?php foreach($pages as $page) : ?>
+				<li class="<?php $p === $page and print('active') ?>">
+					<a class="page-link" href="<?= site_url("first/gallery/{$page}") ?>" title="Halaman <?= $page ?>"><?= $page ?></a>
+				</li>
+				<?php endforeach ?>
+				<?php if($paging->next) : ?>
+				<li>
+					<a href="<?= site_url("first/gallery/{$paging->next}") ?>" title="Halaman selanjutnya">
+						<i class="fa fa-fast-backward"></i>&nbsp;</a>
+				</li>
+				<?php endif ?>
+				<?php if($paging->end_link) : ?>
+				<li>
+					<a href="<?= site_url("first/gallery/{$paging->prev}") ?>" title="Halaman terakhir">
+						<i class="fa fa-fast-forward"></i>&nbsp;</a>
+				</li>
 				<?php endif ?>
 			</ul>
 		</div>
