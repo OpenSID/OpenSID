af7f65111810d8648c447f4e4fec964211c54689
ariandii <ariandi@mail.com>
2020-05-03 07:01:36 +0700

    #42 Penambahan kolom dusun pada tabel DPT

--- partials/dpt.php
+++ partials/dpt.php
@@ -1,56 +1,45 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<?php
-echo "
-<div class='single_page_area'>
+<div class="single_page_area">
 
-	<div class='single_page_area'>
-		<h2>Daftar Calon Pemilih (pada tgl pemilihan $tanggal_pemilihan)</h2>
+	<div class="single_page_area">
+		<h2>Daftar Calon Pemilih (pada tgl pemilihan <?= $tanggal_pemilihan ?>)</h2>
 
-		<div class='table-responsive'>
-		<table id='dpt' class='table table-bordered table-striped'>
+		<div class="table-responsive">
+		<table id="dpt" class="table table-bordered table-striped">
 		<thead>
 		<tr>
-			<th>No</th>
-			<th>RW</th>
-			<th>Jiwa</th>
-			<th>L</th>
-			<th>P</th>
+			<th class="text-center">No</th>
+			<th class="text-center">Nama Dusun</th>
+			<th class="text-center">RW</th>
+			<th class="text-center">Jiwa</th>
+			<th class="text-center">L</th>
+			<th class="text-center">P</th>
 		</tr>
-		</thead>";
-		if(count($main) > 0){
-			echo "
-			<tbody>";
-
-			foreach($main as $data){
-				echo "<tr>
-					<td>".$data['no']."</td>
-					<td>".strtoupper($data['rw'])."</td>
-					<td class='text-right'>".$data['jumlah_warga']."</td>
-					<td class='text-right'>".$data['jumlah_warga_l']."</td>
-					<td class='text-right'>".$data['jumlah_warga_p']."</td>
-				</tr>";
-			}
-			echo "
+		</thead><?php
+		if(count($main) > 0){ ?>
+			<tbody><?php
+			foreach($main as $data){ ?>
+				<tr>
+					<td class="text-center"><?= $data["no"] ?></td>
+					<td class="text-left"><?= strtoupper($data["dusun"]) ?></td>
+					<td class="text-center"><?= strtoupper($data["rw"]) ?></td>
+					<td class="text-right"><?= $data["jumlah_warga"] ?></td>
+					<td class="text-right"><?= $data["jumlah_warga_l"] ?></td>
+					<td class="text-right"><?= $data["jumlah_warga_p"] ?></td>
+				</tr><?php
+			} ?>
 			</tbody>
-
-			<tfooter>
 			<tr>
-				<th colspan=2 class='text-right'>TOTAL</th>
-				<th class='text-right'>".$total['total_warga']."</th>
-				<th class='text-right'>".$total['total_warga_l']."</th>
-				<th class='text-right'>".$total['total_warga_p']."</th>
-			</tr>
-			</tfooter>";
-
-		} else {
-			echo "<tr><td colspan=6 class='text-center'>Daftar masih kosong</td></tr>";
-		}
-
-		echo "
+				<th colspan="3" class="text-right">TOTAL</th>
+				<th class="text-right"><?= $total["total_warga"] ?></th>
+				<th class="text-right"><?= $total["total_warga_l"] ?></th>
+				<th class="text-right"><?= $total["total_warga_p"] ?></th>
+			</tr><?php
+		} else { ?>
+			<tr><td colspan=6 class="text-center">Daftar masih kosong</td></tr><?php
+		} ?>
 		</table>
 		</div>
 	</div>
-
 </div> <!-- .list-frame -->";
-
