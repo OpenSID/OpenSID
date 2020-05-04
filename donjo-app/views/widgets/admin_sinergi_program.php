d8f042ab7732f7050aafc4b9ddd6dd41d8b59767
afa28 <andifahruddinakas@gmail.com>
2020-05-04 21:55:43 +0700

    Hapus </div> yg berlebihan

--- donjo-app/views/widgets/admin_sinergi_program.php
+++ donjo-app/views/widgets/admin_sinergi_program.php
@@ -138,7 +138,6 @@
 						<div class="col-xs-12">
 							<button type="reset" class="btn btn-social btn-flat btn-danger btn-sm"><i class="fa fa-times"></i> Batal</button>
 							<button type="submit" class="btn btn-social btn-flat btn-info btn-sm pull-right"><i class="fa fa-check"></i> Simpan</button>
-							</div>
 						</div>
 					</div>
 				</div>
