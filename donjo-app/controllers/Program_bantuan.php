fce3ce28561cdc3d75c0122c63cbb5c4d3a48642
afa28 <andifahruddinakas@gmail.com>
2020-05-04 20:02:40 +0700

    Perbaikan program bantuan

--- donjo-app/controllers/Program_bantuan.php
+++ donjo-app/controllers/Program_bantuan.php
@@ -28,7 +28,7 @@ class Program_bantuan extends Admin_Controller {
 	{
 		if (isset($_POST['per_page']))
 			$_SESSION['per_page'] = $_POST['per_page'];
-		
+
 		$data = $this->program_bantuan_model->get_program($p, FALSE);
 		$data['tampil'] = 0;
 		$data['list_sasaran'] = unserialize(SASARAN);
@@ -53,7 +53,7 @@ class Program_bantuan extends Admin_Controller {
 		{
 			$data['individu'] = NULL;
 		}
-		$data['form_action'] = site_url("program_bantuan/add_peserta");
+		$data['form_action'] = site_url("program_bantuan/add_peserta/".$program_id);
 		$header = $this->header_model->get_data();
 
 		$this->load->view('header', $header);
@@ -110,7 +110,7 @@ class Program_bantuan extends Admin_Controller {
 		$header = $this->header_model->get_data();
 
 		$this->load->view('header', $header);
-		$this->load->view('nav', $nav);		
+		$this->load->view('nav', $nav);
 		$this->load->view('program_bantuan/peserta', $data);
 		$this->load->view('footer');
 	}
@@ -122,7 +122,7 @@ class Program_bantuan extends Admin_Controller {
 		$data['detail'] = $this->program_bantuan_model->get_data_program($data['peserta']['program_id']);
 		$header = $this->header_model->get_data();
 		$header['minsidebar'] = 1;
-		
+
 		$this->load->view('header', $header);
 		$this->load->view('nav', $nav);
 		$this->load->view('program_bantuan/data_peserta', $data);
@@ -201,7 +201,7 @@ class Program_bantuan extends Admin_Controller {
 		$this->load->view('nav', $nav);
 		$data['program'] = $this->program_bantuan_model->get_program(1, $id);
 		$data['jml'] = $this->program_bantuan_model->jml_peserta_program($id);
-		
+
 		if ($this->form_validation->run() === FALSE)
 		{
 			$this->load->view('program_bantuan/edit', $data);
--- donjo-app/models/migrations/Migrasi_2005_ke_2006.php
+++ donjo-app/models/migrations/Migrasi_2005_ke_2006.php
@@ -7,14 +7,14 @@ class Migrasi_2005_ke_2006 extends CI_model {
 
 		// Ubah nama kode status penduduk
 		$this->db->where('id', 2)
-			->update('tweb_penduduk_status', array('nama' => 'TIDAK TETAP'));
+		->update('tweb_penduduk_status', array('nama' => 'TIDAK TETAP'));
 
 		//Ganti nama folder widget menjadi widgets
 		rename('desa/widget', 'desa/widgets');
 		rename('desa/upload/widget', 'desa/upload/widgets');
 		// Arahkan semua widget statis ubahan desa ke folder desa/widgets
 		$list_widgets = $this->db->where('jenis_widget', 2)->get('widget')->result_array();
-		foreach ($list_widgets as $widgets) 
+		foreach ($list_widgets as $widgets)
 		{
 			$ganti = str_replace('desa/widget', 'desa/widgets', $widgets['isi']); // Untuk versi 20.04-pasca ke atas
 			$cek = explode('/', $ganti); // Untuk versi 20.04 ke bawah
@@ -23,8 +23,17 @@ class Migrasi_2005_ke_2006 extends CI_model {
 				$this->db->where('id', $widgets['id'])->update('widget', array('isi' => 'desa/widgets/'.$widgets['isi']));
 			}
 		}
-  	// Sesuaikan dengan sql_mode STRICT_TRANS_TABLES
+		// Sesuaikan dengan sql_mode STRICT_TRANS_TABLES
 		$this->db->query("ALTER TABLE outbox MODIFY COLUMN CreatorID text NULL");
+
+		// Hapus field sasaran
+		if ($this->db->field_exists('sasaran', 'program_peserta'))
+			$this->db->query('ALTER TABLE `program_peserta` DROP COLUMN `sasaran`');
+
+		// Hapus field sasaran
+		if ($this->db->field_exists('sasaran', 'program_peserta'))
+			$this->db->query('ALTER TABLE `program_peserta` DROP COLUMN `sasaran`');
+
 	}
 
 	private function grup_akses_covid19()
@@ -44,5 +53,5 @@ class Migrasi_2005_ke_2006 extends CI_model {
 			$this->db->query($sql);
 		}
 	}
-	
+
 }
