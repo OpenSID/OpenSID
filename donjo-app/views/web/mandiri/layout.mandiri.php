a48053922aa778cc75e78bbab7e0741c6088531c
Erick Surya Dinata <ericksuryadinata@gmail.com>
2020-05-06 05:41:52 +0700

    solusi terkait #2872, #2920

--- donjo-app/views/web/mandiri/layout.mandiri.php
+++ donjo-app/views/web/mandiri/layout.mandiri.php
@@ -4,13 +4,23 @@
   #wrapper-mandiri .tdk-permohonan { display: none !important; }
   a.btn { color: #fff; }
   .unread > td { background-color: #ffeeaa !important; }
+  .show-only-mobile{
+    visibility: hidden;
+  }
 </style>
 <div class="content-wrapper" id="wrapper-mandiri">
   <section class="content">
     <div class="row">
       <div class="col-md-3">
-        <div class="box box-info">
+        <div class="box box-info" id="box-mandiri">
           <div class="box-header with-border">
+            <div class="show-only-mobile" id="mobile-only">
+              <h3 class="box-title">NIK: <?= $_SESSION['nik'];?></h3>
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-bars"></i>
+                </button>
+              </div>
+            </div>
           </div>
           <div class="box-body no-padding">
             <?php include('donjo-app/views/web/mandiri/layanan_mandiri.php'); ?>
@@ -64,7 +74,26 @@
     $("#wrapper-mandiri form#validasi").removeAttr('target');
     $("#wrapper-mandiri .tdk-permohonan textarea").removeClass('required');    
     $("#wrapper-mandiri .tdk-permohonan select").removeClass('required');    
-    $("#wrapper-mandiri .tdk-permohonan input").removeClass('required');    
+    $("#wrapper-mandiri .tdk-permohonan input").removeClass('required');
+    
+    // untuk keperluan settings collapsible data mandiri
+    // entah kalau di adminLTE ada cara yang lebih mudah
+    function detectMobile(x) {
+      const box = document.getElementById('box-mandiri');
+      const hidden = document.getElementById('mobile-only');
+      if (x.matches) { // If media query matches
+        box.classList.add('collapsed-box');
+        hidden.style.visibility = "visible";
+      } else {
+        const box = document.getElementById('box-mandiri');
+        hidden.style.visibility = "hidden";
+        box.classList.remove('collapsed-box');
+      }
+    }
+
+    var x = window.matchMedia("(max-width: 700px)")
+    detectMobile(x) // Call listener function at run time
+    x.addListener(detectMobile)
   });
 </script>
 
