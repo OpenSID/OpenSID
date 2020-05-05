00c00c9a82cddfc3d8f33da278dcdb1ada859257
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-05-05 21:05:07 +0700

    Perbaikan modal aparatur desa di peta

--- donjo-app/views/gis/aparatur_desa_web.php
+++ donjo-app/views/gis/aparatur_desa_web.php
@@ -20,11 +20,11 @@
 <div class="modal-body" id="maincontent">
 	<div class="box box-warning box-solid">
 
-		<div class="box-body">
-			<div id="aparatur_desa" class="cycle-slideshow"
+			<div class="box-body">
+				<div id="aparatur_desa" class="cycle-slideshow"
 				data-cycle-pause-on-hover=true
 				data-cycle-fx=scrollHorz
-				data-cycle-timeout=1000
+				data-cycle-timeout=2000
 				data-cycle-caption-plugin=caption2
 				data-cycle-overlay-fx-out="slideUp"
 				data-cycle-overlay-fx-in="slideDown"
@@ -34,19 +34,13 @@
 					<div class="cycle-caption"></div>
 					<div class="cycle-overlay"></div>
 				<?php else: ?>
-				  <span class="cycle-pager"></span>  <!-- Untuk membuat tanda bulat atau link pada slider -->
+					<span class="cycle-pager"></span>  <!-- Untuk membuat tanda bulat atau link pada slider -->
 				<?php endif; ?>
 
-			  <?php foreach($aparatur_desa as $data) : ?>
-			  	<?php if(AmbilFoto($data['foto'],"besar") AND is_file(LOKASI_USER_PICT.$data['foto'])) : ?>
-						<img src="<?php echo AmbilFoto($data['foto'],"besar") ?>"
-							data-cycle-title="<span class='cycle-overlay-title'><?= $data['nama'] ?></span>"
-							data-cycle-desc="<?= $data['jabatan'] ?>">
-					<?php else: ?>
-						<img src="<?= base_url("assets/files/user_pict/kuser.png") ?>"
+				<?php foreach ($aparatur_desa['daftar_perangkat'] as $data) : ?>
+						<img src="<?= $data['foto'] ?>"
 						data-cycle-title="<span class='cycle-overlay-title'><?= $data['nama'] ?></span>"
 						data-cycle-desc="<?= $data['jabatan'] ?>">
-					<?php endif; ?>
 				<?php endforeach; ?>
 			</div>
 		</div>
