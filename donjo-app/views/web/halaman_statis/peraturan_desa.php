c96b7dbf94af8330d7e9dfa232651fd5858adbaa
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-05-06 05:54:25 +1000

    Hapus script tidak digunakan. Perbaiki tampilan status sukses di update dokumen. Perbaiki supaya unduh peraturan tidak melalui lokasi dokumen langsung

--- donjo-app/views/web/halaman_statis/peraturan_desa.php
+++ donjo-app/views/web/halaman_statis/peraturan_desa.php
@@ -121,7 +121,7 @@ function formAction()
       }
       for (var i = 0; i < data.length; i++)
       {
-        html += "<tr>"+"<td><a href='<?= base_url(LOKASI_DOKUMEN)?>"+data[i].satuan+"'>"+data[i].nama+"</a></td>"+
+        html += "<tr>"+"<td><a href='<?= site_url('dokumen_web/unduh_berkas/')?>"+data[i].id+"'>"+data[i].nama+"</a></td>"+
         "<td>"+data[i].kategori+"</td>"+
         "<td>"+data[i].tahun+"</td>";
       }
