f795c7a483f03ebd9659e96f075c20cb78d69879
ariandii <ariandi@mail.com>
2020-05-08 10:28:13 +0700

    Tambah Cycle Next & Prev pada Widget Aparatur Desa

--- widgets/aparatur_desa.php
+++ widgets/aparatur_desa.php
@@ -1,7 +1,5 @@
 <link type='text/css' href="<?= base_url()?>assets/front/css/slider.css" rel='Stylesheet' />
-<!--
-<script type="text/javascript" src= "<?= base_url()?>assets/front/js/jquery.cycle2.caption2.min.js" type= "text/javascript"></script>
--->
+<script src="<?= base_url()?>assets/front/js/jquery.cycle2.caption2.min.js"></script>
 <style type="text/css">
 	#aparatur_desa .cycle-pager span
 	{
@@ -14,6 +12,10 @@
 		margin-bottom: 0px;
 		border: 0px;
 	}
+	.cycle-next, .cycle-prev
+	{
+		mix-blend-mode: difference;
+	}
 </style>
 
 <!-- widget Aparatur Desa -->
@@ -23,31 +25,27 @@
 		<div id="aparatur_desa" class="cycle-slideshow"
 		data-cycle-pause-on-hover=true
 		data-cycle-fx=scrollHorz
-		data-cycle-timeout=3000
+		data-cycle-timeout=2000
 		data-cycle-caption-plugin=caption2
 		data-cycle-overlay-fx-out="slideUp"
 		data-cycle-overlay-fx-in="slideDown"
+		data-cycle-auto-height=<?= $aparatur_desa['foto_pertama'] ?>
 		>
 
 		<?php if ($this->web_widget_model->get_setting('aparatur_desa', 'overlay') == true): ?>
+			<span class="cycle-prev"><img src="<?= base_url()?>assets/images/back_button.png" alt="Back"></span>
+			<span class="cycle-next"><img src="<?= base_url()?>assets/images/next_button.png" alt="Next"></span>
 			<div class="cycle-caption"></div>
 			<div class="cycle-overlay"></div>
 			<?php else: ?>
 				<span class="cycle-pager"></span>  <!-- Untuk membuat tanda bulat atau link pada slider -->
 			<?php endif; ?>
 
-			<?php foreach($aparatur_desa as $data) : ?>
-				<?php if(AmbilFoto($data['foto'],"besar") AND is_file(LOKASI_USER_PICT.$data['foto'])) : ?>
-				<img src="<?= AmbilFoto($data['foto'],"besar") ?>"
+			<?php foreach($aparatur_desa['daftar_perangkat'] as $data) : ?>
+				<img src="<?= $data['foto'] ?>"
 				data-cycle-title="<span class='cycle-overlay-title'><?= $data['nama'] ?></span>"
 				data-cycle-desc="<?= $data['jabatan'] ?>"
 				>
-				<?php else: ?>
-					<img src="<?= base_url("assets/files/user_pict/kuser.png") ?>"
-					data-cycle-title="<span class='cycle-overlay-title'><?= $data['nama'] ?></span>"
-					data-cycle-desc="<?= $data['jabatan'] ?>"
-					>
-				<?php endif; ?>
 			<?php endforeach; ?>
 		</div>
 	</div></div>
