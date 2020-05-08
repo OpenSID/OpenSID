8b28f6d6c473eee9c692d5d6bd36b0a3e330f1d8
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-05-09 06:38:25 +1000

    Perbaiki indentasi

--- donjo-app/views/web/mandiri/surat.php
+++ donjo-app/views/web/mandiri/surat.php
@@ -110,97 +110,98 @@
 		<div class="modal-content">
 			<div class="modal-header btn-danger">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">×</span></button>
-					<h4 class="modal-title" id="myModalLabel"><i class="fa fa-exclamation-triangle"></i> &nbsp;Peringatan</h4>
-				</div>
-				<div class="modal-body">
-					<p id="kata_peringatan"></p>
-				</div>
-				<div class="modal-footer">
-					<button class="btn btn-social btn-flat btn-danger btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
-				</div>
+					<span aria-hidden="true">×</span>
+				</button>
+				<h4 class="modal-title" id="myModalLabel"><i class="fa fa-exclamation-triangle"></i> &nbsp;Peringatan</h4>
+			</div>
+			<div class="modal-body">
+				<p id="kata_peringatan"></p>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-social btn-flat btn-danger btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
 			</div>
 		</div>
 	</div>
+</div>
 
-	<div class="modal fade" id="modal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class='modal-dialog'>
-			<div class='modal-content'>
-				<div class='modal-header'>
-					<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-					<h4 class='modal-title' id='myModalLabel'>Ubah dokumen</h4>
-				</div>
-				<form id="unggah_dokumen" action="" method="POST" enctype="multipart/form-data">
-					<div class='modal-body'>
-						<div class="row">
-							<div class="col-sm-12">
-								<div class="box box-danger">
-									<div class="box-body">
-										<div class="form-group">
-											<label for="nama_dokumen">Nama / Jenis Dokumen</label>
-											<input id="nama_dokumen" name="nama" class="form-control input-sm required" type="text" placeholder="Nama Dokumen" value=""/>
-											<input type="text" class="hidden" name="id" id="id_dokumen" value=""/>
-										</div>
-										<div class="form-group">
-											<select class="form-control required input-sm" name="id_syarat" id="id_syarat">
-												<option> -- Pilih Jenis Dokumen -- </option>
-												<?php foreach ($menu_dokumen_mandiri AS $data): ?>
-													<option value="<?= $data['ref_syarat_id']?>" ><?= $data['ref_syarat_nama']?></option>
-												<?php endforeach;?>
-											</select>
-										</div>
-										<div class="form-group">
-											<label for="file" >Pilih File:</label>
-											<div class="input-group input-group-sm">
-												<input type="text" class="form-control" id="file_path" name="satuan">
-												<input type="file" class="hidden" id="file" name="satuan">
-												<span class="input-group-btn">
-													<button type="button" class="btn btn-info btn-flat" id="file_browser"><i class="fa fa-search"></i> Browse</button>
-												</span>
-											</div>
-											<span class="help-block"><code>Kosongkan jika tidak ingin mengubah dokumen.</code></span>
-										</div>
-										<?php if (!empty($kk)): ?>
-											<hr>
-											<span class="help-block"><code>Centang jika dokumen yang diupload berlaku juga untuk anggota keluarga di bawah ini.</code></span>
-											<div class="table-responsive">
-												<table class="table table-bordered table-hover table-striped table-sm">
-													<thead>
-														<tr>
-															<th scope="col">#</th>
-															<th scope="col">NIK</th>
-															<th scope="col">Nama</th>
-														</tr>
-													</thead>
-													<tbody>
-														<?php foreach ($kk as $item): ?>
-															<?php if ($item['nik'] != $penduduk['nik']): ?>
-																<tr>
-																	<td><input class='anggota_kk' id="anggota_<?=$item['id']?>" type='checkbox' name='anggota_kk[]' value="<?=$item['id']?>"></td>
-																	<td><?=$item['nik']?></td>
-																	<td><?=$item['nama']?></td>
-																</tr>
-															<?php endif; ?>
-														<?php endforeach; ?>
-													</tbody>
-												</table>
-											</div>
-										<?php endif ?>
+<div class="modal fade" id="modal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class='modal-dialog'>
+		<div class='modal-content'>
+			<div class='modal-header'>
+				<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
+				<h4 class='modal-title' id='myModalLabel'>Ubah dokumen</h4>
+			</div>
+			<form id="unggah_dokumen" action="" method="POST" enctype="multipart/form-data">
+				<div class='modal-body'>
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="box box-danger">
+								<div class="box-body">
+									<div class="form-group">
+										<label for="nama_dokumen">Nama / Jenis Dokumen</label>
+										<input id="nama_dokumen" name="nama" class="form-control input-sm required" type="text" placeholder="Nama Dokumen" value=""/>
+										<input type="text" class="hidden" name="id" id="id_dokumen" value=""/>
 									</div>
+									<div class="form-group">
+										<select class="form-control required input-sm" name="id_syarat" id="id_syarat">
+											<option> -- Pilih Jenis Dokumen -- </option>
+											<?php foreach ($menu_dokumen_mandiri AS $data): ?>
+												<option value="<?= $data['ref_syarat_id']?>" ><?= $data['ref_syarat_nama']?></option>
+											<?php endforeach;?>
+										</select>
+									</div>
+									<div class="form-group">
+										<label for="file" >Pilih File:</label>
+										<div class="input-group input-group-sm">
+											<input type="text" class="form-control" id="file_path" name="satuan">
+											<input type="file" class="hidden" id="file" name="satuan">
+											<span class="input-group-btn">
+												<button type="button" class="btn btn-info btn-flat" id="file_browser"><i class="fa fa-search"></i> Browse</button>
+											</span>
+										</div>
+										<span class="help-block"><code>Kosongkan jika tidak ingin mengubah dokumen.</code></span>
+									</div>
+									<?php if (!empty($kk)): ?>
+										<hr>
+										<span class="help-block"><code>Centang jika dokumen yang diupload berlaku juga untuk anggota keluarga di bawah ini.</code></span>
+										<div class="table-responsive">
+											<table class="table table-bordered table-hover table-striped table-sm">
+												<thead>
+													<tr>
+														<th scope="col">#</th>
+														<th scope="col">NIK</th>
+														<th scope="col">Nama</th>
+													</tr>
+												</thead>
+												<tbody>
+													<?php foreach ($kk as $item): ?>
+														<?php if ($item['nik'] != $penduduk['nik']): ?>
+															<tr>
+																<td><input class='anggota_kk' id="anggota_<?=$item['id']?>" type='checkbox' name='anggota_kk[]' value="<?=$item['id']?>"></td>
+																<td><?=$item['nik']?></td>
+																<td><?=$item['nama']?></td>
+															</tr>
+														<?php endif; ?>
+													<?php endforeach; ?>
+												</tbody>
+											</table>
+										</div>
+									<?php endif ?>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="modal-footer">
-						<button type="reset" class="btn btn-social btn-flat btn-danger btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
-						<button type="submit" class="btn btn-social btn-flat btn-info btn-sm" id="upload_btn"><i class='fa fa-check'></i> Simpan</button>
-					</div>
-				</form>
-			</div>
+				</div>
+				<div class="modal-footer">
+					<button type="reset" class="btn btn-social btn-flat btn-danger btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
+					<button type="submit" class="btn btn-social btn-flat btn-info btn-sm" id="upload_btn"><i class='fa fa-check'></i> Simpan</button>
+				</div>
+			</form>
 		</div>
 	</div>
-	<script type='text/javascript'>
-		$(document).ready(function(){
+</div>
+<script type='text/javascript'>
+	$(document).ready(function(){
 
 		// var id_surat = 0;
 		var url = "<?= site_url('first/cek_syarat')?>";
@@ -222,17 +223,17 @@
 			//Set column definition initialisation properties.
 			"columnDefs": [
 			{
-					"targets": [ 0 ], //first column / numbering column
-					"orderable": false, //set not orderable
-				},
-				],
-				'language': {
-					'url': BASE_URL + '/assets/bootstrap/js/dataTables.indonesian.lang'
-				},
-				'drawCallback': function (){
-					$('.dataTables_paginate > .pagination').addClass('pagination-sm no-margin');
-				}
-			});
+				"targets": [ 0 ], //first column / numbering column
+				"orderable": false, //set not orderable
+			},
+			],
+			'language': {
+				'url': BASE_URL + '/assets/bootstrap/js/dataTables.indonesian.lang'
+			},
+			'drawCallback': function (){
+				$('.dataTables_paginate > .pagination').addClass('pagination-sm no-margin');
+			}
+		});
 
 		$('#id_surat').change(function(){
 			table.ajax.reload();
