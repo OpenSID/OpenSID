9da5ace016fb19bbd5009cc54a209ce1bf838b3a
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-05-15 02:21:07 +0700

    Rev. Tampilkan Daftar Penerima Bantuan di Statistik Web

--- themes/klasik/partials/statistik.php
+++ themes/klasik/partials/statistik.php
@@ -83,6 +83,24 @@
 		display: none;
 	}
 </style>
+<style>
+	.input-sm
+	{
+		padding: 4px 4px;
+	}
+	@media (max-width:780px)
+	{
+		.btn-group-vertical
+		{
+			display: block;
+		}
+	}
+	.table-responsive
+	{
+		min-height:275px;
+	}
+	}
+</style>
 
 <div class="box box-danger">
 	<div class="box-header with-border">
@@ -172,37 +190,108 @@
 </div>
 
 <?php if ($program_peserta):?>
-	<div class="box box-danger">
-		<div class="box-header with-border">
-			<h3 class="box-title">Daftar Peserta Program <?=$heading?></h3>
-		</div>
-		<div class="box-body">
-			<?php $peserta = $program_peserta[1];?>
-			<div class="table-responsive">
-				<table class="table table-bordered table-striped dataTable table-hover">
-					<thead class="bg-gray disabled color-palette">
-						<tr>
-							<th rowspan="2" class="text-center">No</th>
-							<th rowspan="2" nowrap class="text-center">Program</th>
-							<th rowspan="2" nowrap class="text-center">Nama</th>
-							<th rowspan="2" class="text-center">Alamat</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php $nomer = $paging->offset;?>
-						<?php if (is_array($peserta)): ?>
-							<?php foreach ($peserta as $key=>$item): $nomer++;?>
-								<tr>
-									<td class="text-center"><?= $nomer?></td>
-									<td><?= strtoupper($item['program_plus']);?></td>
-									<td nowrap><?= $item["peserta_info"]?></td>
-									<td nowrap><?= $item["kartu_alamat"];?></td>
-								</tr>
-							<?php endforeach; ?>
-						<?php endif; ?>
-					</tbody>
-				</table>
+	<section class="content" id="maincontent">
+		<div class="row">
+			<div class="col-md-12">
+				<?php $detail = $program_peserta[0];?>
+				<div class="box box-info">
+					<div class="box-body">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="dataTables_wrapper form-inline dt-bootstrap no-footer">
+								<form id="mainform" name="mainform" action="" method="post">
+									<input type="hidden" name="id" value="<?php echo $this->uri->segment(3) ?>">
+									<div class="row">
+										<div class="col-sm-12">
+													<div class="box-header with-border">
+														<h3 class="box-title">Daftar Peserta Program <?= $heading ?></h3>
+													</div>
+										</div>
+										<div class="col-sm-3">
+											<div class="input-group input-group-sm pull-right">
+												<input name="cari" id="cari" class="form-control" placeholder="Cari..." type="text" value="<?=html_escape($cari_peserta)?>" onkeypress="if (event.keyCode == 13){$('#'+'mainform').attr('action', '<?=site_url("first/search_peserta")?>');$('#'+'mainform').submit();}">
+												<div class="input-group-btn">
+													<button type="submit" class="btn btn-default" onclick="$('#'+'mainform').attr('action', '<?=site_url("first/search_peserta")?>');$('#'+'mainform').submit();"><i class="fa fa-search"></i></button>
+												</div>
+											</div>
+										</div>
+										<div class="col-sm-12">
+										<?php $peserta = $program_peserta[1];?>
+											<div class="table-responsive">
+												<table class="table table-bordered table-striped dataTable table-hover">
+													<thead class="bg-gray disabled color-palette">
+														<tr>
+															<th rowspan="2" class="text-center">No</th>
+															<th rowspan="2" class="text-center">Program</th>
+															<th rowspan="2" nowrap class="text-center"><?= $detail["judul_peserta_info"]?></th>
+															<th rowspan="2" class="text-center">Alamat</th>
+														</tr>
+													</thead>
+													<tbody>
+														<?php $nomer = $paging->offset;?>
+														<?php if (is_array($peserta)): ?>
+															<?php foreach ($peserta as $key=>$item): $nomer++;?>
+																<tr>
+																	<td class="text-center"><?= $nomer?></td>
+																	<td nowrap><?= strtoupper($item['program_plus']);?></td>
+																	<td nowrap><?= $item["peserta_info"]?></td>
+																	<td><?= $item["kartu_alamat"];?></td>
+																</tr>
+															<?php endforeach; ?>
+														<?php endif; ?>
+													</tbody>
+												</table>
+											</div>
+										</div>
+
+									</div>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <div class="dataTables_length">
+                        <form id="paging" action="<?= site_url("first/statistik/$st/0")?>" method="post" class="form-horizontal">
+                         <label>
+                            Tampilkan
+                            <select name="per_page" class="form-control input-sm" onchange="$('#mainform').submit();" id="per_page_input">
+      	                      <option value="10" <?php selected($per_page, 10); ?> >10</option>
+                              <option value="50" <?php selected($per_page, 50); ?> >50</option>
+                              <option value="100" <?php selected($per_page, 100); ?> >100</option>
+                            </select>
+        	                  Dari
+                            <strong><?= $paging->num_rows?></strong>
+                            Total Data
+                          </label>
+                        </form>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="dataTables_paginate paging_simple_numbers">
+                        <ul class="pagination">
+                          <?php if ($paging->start_link): ?>
+                            <li><a href="<?=site_url("first/statistik/$st/0/$paging->start_link")?>" aria-label="First"><span aria-hidden="true">Awal</span></a></li>
+                          <?php endif; ?>
+                          <?php if ($paging->prev): ?>
+                            <li><a href="<?=site_url("first/statistik/$st/0/$paging->prev")?>" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+                          <?php endif; ?>
+                          <?php for ($i=$paging->start_link;$i<=$paging->end_link;$i++): ?>
+                            <li <?=jecho($p, $i, "class='active'")?>><a href="<?= site_url("first/statistik/$st/0/$i")?>"><?= $i?></a></li>
+                          <?php endfor; ?>
+                          <?php if ($paging->next): ?>
+                            <li><a href="<?=site_url("first/statistik/$st/0/$paging->next")?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
+                          <?php endif; ?>
+                          <?php if ($paging->end_link): ?>
+                            <li><a href="<?=site_url("first/statistik/$st/0/$paging->end_link")?>" aria-label="Last"><span aria-hidden="true">Akhir</span></a></li>
+                          <?php endif; ?>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+								</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
+	</section>
 	<?php endif;?>
