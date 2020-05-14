9df6f5653a629742eb7b9a0a1c656c1782199922
afa28 <andifahruddinakas@gmail.com>
2020-05-14 08:26:39 +0700

    Refactor

--- widgets/menu_kategori.php
+++ widgets/menu_kategori.php
@@ -3,16 +3,19 @@
 <div class="single_bottom_rightbar">
 	<h2> Kategori</h2>
 	<ul id="ul-menu" class="sidebar-latest">
-		<?php foreach($menu_kiri as $data){?>
-			<li><a href="<?= site_url()."first/kategori/".$data['id']?>"><?= $data['nama']; if(count($data['submenu'])>0) { echo "<span class='caret'></span>"; } ?></a>
+		<?php foreach($menu_kiri as $data):?>
+			<li>
+				<a href="<?= site_url('first/kategori/'.$data['id']);?>">
+					<?= $data['nama'];?><?php (count($data['submenu'])>0) and print('<span class="caret"></span>');?>
+				</a>
 				<?php if(count($data['submenu'])>0): ?>
 					<ul class="submenu">
-						<?php foreach($data['submenu'] as $submenu): ?>
-							<li><a href="<?= site_url()."first/kategori/".$submenu['id']?>"><?= $submenu['nama']?></a></li>
+						<?php foreach($data['submenu'] as $submenu):?>
+							<li><a href="<?= site_url('first/kategori/'.$submenu['id'])?>"><?= $submenu['nama']?></a></li>
 						<?php endforeach; ?>
 					</ul>
 				<?php endif; ?>
 			</li>
-		<?php }?>
+		<?php endforeach;?>
 	</ul>
 </div>
