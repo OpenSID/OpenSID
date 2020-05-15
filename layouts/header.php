8fda447687c9f9e3a699c675934e376d11931f90
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-05-15 21:45:45 +0700

    Tampilkan Penerima Bantuan dan Perbaiki Statistik di Peta

--- layouts/header.php
+++ layouts/header.php
@@ -19,11 +19,4 @@
 			<div class="content_bottom">
 				<div class="col-lg-12 col-md-12">
 					<div class="content_bottom_left">
-						<script src="<?= base_url()?>assets/front/js/jquery.js"></script>
-						<link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/bootstrap/css/dataTables.bootstrap.min.css">
-						<script src="<?= base_url() ?>assets/bootstrap/js/jquery.dataTables.min.js"></script>
-						<script src="<?= base_url() ?>assets/bootstrap/js/dataTables.bootstrap.min.js"></script>
-						<script type="text/javascript">
-							var BASE_URL = "<?= base_url(); ?>";
-						</script>
 						<?php $this->load->view('head_tags_front') ?>
