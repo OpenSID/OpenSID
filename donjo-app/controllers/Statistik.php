8d7440c172085d91364f8067a159c370a6a86464
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-05-19 02:14:42 +0700

    Perbaiki error reinitialise DataTable

--- donjo-app/controllers/Statistik.php
+++ donjo-app/controllers/Statistik.php
@@ -124,9 +124,6 @@ class Statistik extends Admin_Controller {
 		$this->load->view('header', $header);
 		$this->load->view('nav', $nav);
 		$this->load->view('statistik/penduduk_pie', $data);
-		if (in_array($lap, array('bantuan_keluarga', 'bantuan_penduduk')))
-			$this->load->view('statistik/peserta_bantuan', $data);
-
 		$this->load->view('footer');
 	}
 
