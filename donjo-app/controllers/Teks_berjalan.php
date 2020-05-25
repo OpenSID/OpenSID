9191abbdc3dc38b223d6ec02bff7b95d50c52bcb
afa28 <andifahruddinakas@gmail.com>
2020-05-25 22:32:29 +0700

    Perbaikan Modul Teks Berjalan
    1. Conver tautan menjadi slug artikel
    2. Sederhanakan dan hapus fuction yg tdk digunakan
    3. Ubah data tampil pada tabel

--- donjo-app/controllers/Teks_berjalan.php
+++ donjo-app/controllers/Teks_berjalan.php
@@ -9,6 +9,7 @@ class Teks_berjalan extends Admin_Controller {
 
 		$this->load->model('header_model');
 		$this->load->model('teks_berjalan_model');
+		$this->load->model('web_artikel_model');
 		$this->modul_ini = 13;
 		$this->sub_modul_ini = 64;
 	}
@@ -17,7 +18,7 @@ class Teks_berjalan extends Admin_Controller {
 	{
 		$data['main'] = $this->teks_berjalan_model->list_data();
 		$header = $this->header_model->get_data();
-		
+
 		$this->load->view('header', $header);
 		$this->load->view('nav', $nav);
 		$this->load->view('web/teks_berjalan/table', $data);
@@ -26,8 +27,8 @@ class Teks_berjalan extends Admin_Controller {
 
 	public function form($id = '')
 	{
-		$this->load->model('web_artikel_model');
 		$data['list_artikel'] = $this->web_artikel_model->list_data($cat=-1, $o=6, $offset=0, $limit=500);
+
 		if ($id)
 		{
 			$data['teks'] = $this->teks_berjalan_model->get_teks($id);
