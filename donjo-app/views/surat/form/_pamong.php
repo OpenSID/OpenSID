d986ceb24554a3c76b050231beb7a159bd82146f
afa28 <andifahruddinakas@gmail.com>
2020-06-01 22:21:27 +0700

    Update _pamong.php

--- donjo-app/views/surat/form/_pamong.php
+++ donjo-app/views/surat/form/_pamong.php
@@ -30,7 +30,7 @@
 <div class="form-group tdk-permohonan">
 	<label for="jabatan"  class="col-sm-3 control-label">Menjabat Sebagai</label>
 	<div class="col-sm-6 col-lg-4">
-		<select class="form-control input-sm required" id="jabatan" name="jabatan" disabled>
+		<select class="form-control input-sm required" id="jabatan" name="jabatan">
 			<option value='' selected="selected" >-- Pilih Jabatan--</option>
 			<?php foreach ($pamong AS $data): ?>
 				<option <?php if ($data['pamong_ttd']==1): ?>selected<?php endif; ?>><?= $data['jabatan']?></option>
