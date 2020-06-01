ba46dc79bfb218b5595a660a9091652825157f99
afa28 <andifahruddinakas@gmail.com>
2020-06-01 15:50:24 +0700

    referensi_model dan  helper selected
    1. Gunakan referensi_model
    2. Gunakan helper selected
    3. Ubah judul combobox sesuai judul kategori (gunakan huruf besar)

--- donjo-app/views/sid/kependudukan/penduduk_log.php
+++ donjo-app/views/sid/kependudukan/penduduk_log.php
@@ -41,35 +41,36 @@
 										<div class="row">
 											<div class="col-sm-9">
 												<select class="form-control input-sm" name="status_dasar" onchange="formAction('mainform', '<?=site_url('penduduk_log/filter/status_dasar')?>')">
-													<option value="">Semua</option>
+													<option value="">STATUS DASAR</option>
 													<?php foreach ($list_status_dasar as $data): ?>
-                            <?php if (strtolower($data['nama']) != 'hidup'): ?>
-                              <option value="<?= $data['id']?>" <?php if ($status_dasar==$data['id']): ?>selected<?php endif; ?>><?= ucwords(strtolower($data['nama']))?></option>
-                            <?php endif; ?>
-                          <?php endforeach; ?>
+														<?php if (strtolower($data['nama']) != 'hidup'): ?>
+															<option value="<?= $data['id']?>" <?php selected($status_dasar, $data['id']); ?>><?= set_ucwords($data['nama'])?></option>
+														<?php endif; ?>
+													<?php endforeach; ?>
 												</select>
 												<select class="form-control input-sm" name="sex" onchange="formAction('mainform','<?= site_url('penduduk_log/filter/sex')?>')">
-                          <option value="">Jenis Kelamin</option>
-                          <option value="1" <?php if ($sex==1 ): ?>selected<?php endif ?>>Laki-Laki</option>
-                 	        <option value="2" <?php if ($sex==2 ): ?>selected<?php endif ?>>Perempuan</option>
-                        </select>
-                        <select class="form-control input-sm" name="agama" onchange="formAction('mainform','<?= site_url('penduduk_log/filter/agama')?>')">
-                          <option value="">Agama</option>
-                 					<?php foreach ($list_agama AS $data): ?>
-                            <option value="<?= $data['id']?>" <?php if ($agama == $data['id']): ?>selected<?php endif ?>><?= $data['nama']?></option>
-                					<?php endforeach; ?>
-                        </select>
+													<option value="">JENIS KELAMIN</option>
+													<?php foreach ($list_sex AS $data): ?>
+														<option value="<?= $data['id']?>" <?php selected($sex, $data['id']); ?>><?= set_ucwords($data['nama'])?></option>
+													<?php endforeach; ?>
+												</select>
+												<select class="form-control input-sm" name="agama" onchange="formAction('mainform','<?= site_url('penduduk_log/filter/agama')?>')">
+													<option value="">AGAMA</option>
+													<?php foreach ($list_agama AS $data): ?>
+														<option value="<?= $data['id']?>" <?php selected($agama, $data['id']); ?>><?= set_ucwords($data['nama'])?></option>
+													<?php endforeach; ?>
+												</select>
 												<select class="form-control input-sm " name="dusun" onchange="formAction('mainform','<?= site_url('penduduk_log/filter/dusun')?>')">
-													<option value="">Pilih <?= ucwords($this->setting->sebutan_dusun)?></option>
+													<option value=""><?= strtoupper($this->setting->sebutan_dusun)?></option>
 													<?php foreach ($list_dusun AS $data): ?>
-														<option value="<?= $data['dusun']?>" <?php if ($dusun == $data['dusun']): ?>selected<?php endif ?>><?= strtoupper($data['dusun'])?></option>
+														<option value="<?= $data['dusun']?>" <?php selected($dusun, $data['dusun']); ?>><?= set_ucwords($data['dusun'])?></option>
 													<?php endforeach; ?>
 												</select>
 												<?php if ($dusun): ?>
 													<select class="form-control input-sm" name="rw" onchange="formAction('mainform','<?= site_url('penduduk_log/filter/rw')?>')" >
 														<option value="">RW</option>
 														<?php foreach ($list_rw AS $data): ?>
-															<option value="<?= $data['rw']?>" <?php if ($rw == $data['rw']): ?>selected<?php endif ?>><?= $data['rw']?></option>
+															<option value="<?= $data['rw']?>" <?php selected($rw, $data['rw']); ?>><?= set_ucwords($data['rw'])?></option>
 														<?php endforeach; ?>
 													</select>
 												<?php endif; ?>
@@ -77,7 +78,7 @@
 													<select class="form-control input-sm" name="rt" onchange="formAction('mainform','<?= site_url('penduduk_log/filter/rt')?>')">
 														<option value="">RT</option>
 														<?php foreach ($list_rt AS $data): ?>
-															<option value="<?= $data['rt']?>" <?php if ($rt == $data['rt']): ?>selected<?php endif ?>><?= $data['rt']?></option>
+															<option value="<?= $data['rt']?>" <?php selected($rw, $data['rt']); ?>><?= set_ucwords($data['rt'])?></option>
 														<?php endforeach; ?>
 													</select>
 												<?php endif; ?>
@@ -96,87 +97,85 @@
 												<div class="table-responsive">
 													<table class="table table-bordered dataTable table-hover">
 														<thead class="bg-gray disabled color-palette">
-                              <tr>
-                             		<th>No</th>
-                    	     			<th><input type="checkbox" id="checkall"/></th>
-                           			<th width="85">Aksi</th>
-                           			<?php if ($o==2): ?>
-                             			<th><a href="<?= site_url("penduduk_log/index/$p/1")?>">NIK <i class='fa fa-sort-asc fa-sm'></i></a></th>
-                          			<?php elseif ($o==1): ?>
-                             			<th><a href="<?= site_url("penduduk_log/index/$p/2")?>">NIK <i class='fa fa-sort-desc fa-sm'></i></span></a></th>
-                          			<?php else: ?>
-                             			<th><a href="<?= site_url("penduduk_log/index/$p/1")?>">NIK <i class='fa fa-sort fa-sm'></i></a></th>
-                          			<?php endif; ?>
-                           			<?php if ($o==4): ?>
-                             			<th><a href="<?= site_url("penduduk_log/index/$p/3")?>">Nama <i class='fa fa-sort-asc fa-sm'></i></a></th>
-                          			<?php elseif ($o==3): ?>
-                             			<th><a href="<?= site_url("penduduk_log/index/$p/4")?>">Nama <i class='fa fa-sort-desc fa-sm'></i></a></th>
-                          			<?php else: ?>
-                             			<th><a href="<?= site_url("penduduk_log/index/$p/3")?>">Nama <i class='fa fa-sort fa-sm'></i></a></th>
-                          			<?php endif; ?>
-	                          		<?php if ($o==6): ?>
+															<tr>
+																<th>No</th>
+																<th><input type="checkbox" id="checkall"/></th>
+																<th width="85">Aksi</th>
+																<?php if ($o==2): ?>
+																	<th><a href="<?= site_url("penduduk_log/index/$p/1")?>">NIK <i class='fa fa-sort-asc fa-sm'></i></a></th>
+																<?php elseif ($o==1): ?>
+																	<th><a href="<?= site_url("penduduk_log/index/$p/2")?>">NIK <i class='fa fa-sort-desc fa-sm'></i></span></a></th>
+																<?php else: ?>
+																	<th><a href="<?= site_url("penduduk_log/index/$p/1")?>">NIK <i class='fa fa-sort fa-sm'></i></a></th>
+																<?php endif; ?>
+																<?php if ($o==4): ?>
+																	<th><a href="<?= site_url("penduduk_log/index/$p/3")?>">Nama <i class='fa fa-sort-asc fa-sm'></i></a></th>
+																<?php elseif ($o==3): ?>
+																	<th><a href="<?= site_url("penduduk_log/index/$p/4")?>">Nama <i class='fa fa-sort-desc fa-sm'></i></a></th>
+																<?php else: ?>
+																	<th><a href="<?= site_url("penduduk_log/index/$p/3")?>">Nama <i class='fa fa-sort fa-sm'></i></a></th>
+																<?php endif; ?>
+																<?php if ($o==6): ?>
 																	<th><a href="<?= site_url("penduduk_log/index/$p/5")?>">No. KK / Nama KK <i class='fa fa-sort-asc fa-sm'></i></a></th>
-                                <?php elseif ($o==5): ?>
+																<?php elseif ($o==5): ?>
 																	<th><a href="<?= site_url("penduduk_log/index/$p/6")?>">No. KK / Nama KK <i class='fa fa-sort-desc fa-sm'></i></a></th>
-                               	<?php else: ?>
-																 	<th><a href="<?= site_url("penduduk_log/index/$p/5")?>">No. KK / Nama KK <i class='fa fa-sort fa-sm'></i></a></th>
-                                <?php endif; ?>
-                            		<th><?= ucwords($this->setting->sebutan_dusun)?></th>
-                            		<th>RW</th>
-                            		<th>RT</th>
-
-                                <?php if ($o==8): ?>
+																<?php else: ?>
+																	<th><a href="<?= site_url("penduduk_log/index/$p/5")?>">No. KK / Nama KK <i class='fa fa-sort fa-sm'></i></a></th>
+																<?php endif; ?>
+																<th><?= ucwords($this->setting->sebutan_dusun)?></th>
+																<th>RW</th>
+																<th>RT</th>
+																<?php if ($o==8): ?>
 																	<th><a href="<?= site_url("penduduk_log/index/$p/7")?>">Umur <i class='fa fa-sort-asc fa-sm'></i></a></th>
-                                <?php elseif ($o==7): ?>
+																<?php elseif ($o==7): ?>
 																	<th><a href="<?= site_url("penduduk_log/index/$p/8")?>">Umur <i class='fa fa-sort-desc fa-sm'></i></a></th>
-                                <?php else: ?>
+																<?php else: ?>
 																	<th><a href="<?= site_url("penduduk_log/index/$p/7")?>">Umur <i class='fa fa-sort fa-sm'></i></a></th>
-                                <?php endif; ?>
-
-                            		<th>Status Menjadi</th>
-                            		<?php if ($o==10): ?>
+																<?php endif; ?>
+																<th>Status Menjadi</th>
+																<?php if ($o==10): ?>
 																	<th><a href="<?= site_url("penduduk_log/index/$p/9")?>">Tanggal Peristiwa <i class='fa fa-sort-asc fa-sm'></i></a></th>
-                                <?php elseif ($o==9): ?>
+																<?php elseif ($o==9): ?>
 																	<th><a href="<?= site_url("penduduk_log/index/$p/10")?>">Tanggal Peristiwa <i class='fa fa-sort-desc fa-sm'></i></a></th>
-                                <?php else: ?>
+																<?php else: ?>
 																	<th><a href="<?= site_url("penduduk_log/index/$p/9")?>">Tanggal Peristiwa <i class='fa fa-sort fa-sm'></i></a></th>
-                                <?php endif; ?>
-                                <th>Tanggal Rekam</th>
-                              	<th>Catatan Peristiwa</th>
-                            	</tr>
-                        		</thead>
-                              <tbody>
-                                <?php foreach ($main as $data): ?>
-                               		<tr>
-                                    <td><?= $data['no']?></td>
-                                    <td>
-                                      <input type="checkbox" name="id_cb[]" value="<?= $data['id_log']?>" />
-                                    </td>
-                                    <td nowrap>
-                                      <a href="<?= site_url("penduduk_log/edit/$p/$o/$data[id_log]")?>" class="btn bg-orange btn-flat btn-sm"  title="Edit Log Penduduk" data-remote="false" data-toggle="modal" data-target="#modalBox" data-title="Edit Log Penduduk" ><i class="fa fa-edit"></i></a>
-                                      <a href="#" data-href="<?= site_url("penduduk_log/kembalikan_status/$data[id_log]")?>" class="btn bg-olive btn-flat btn-sm" title="Kembalikan Status"  data-remote="false"  data-toggle="modal" data-target="#confirm-status"><i class="fa fa-undo"></i></a>
-                                    </td>
-                                    <td>
-                                      <a href="<?= site_url("penduduk/detail/$p/$o/$data[id]")?>" id="test" name="<?= $data['id']?>"><?= $data['nik']?></a>
-                                    </td>
-                                    <td>
-                                      <a href="<?= site_url("penduduk/detail/$p/$o/$data[id]")?>"><?= strtoupper($data['nama'])?></a>
-                                    </td>
-                                    <td>
-                                      <a href="<?= site_url("keluarga/kartu_keluarga/$p/$o/$data[id_kk]")?>"><?= $data['no_kk']?> </a> <br>
-                                      <?= $data['nama_kk']?>
-                                    </td>
-                                    <td><?= $data['dusun']?></td>
-                                    <td><?= $data['rw']?></td>
-                                    <td><?= $data['rt']?></td>
-                                    <td><?= $data['umur_pada_peristiwa']?></td>
-                                    <td><?= $data['status_dasar']?></td>
-                                    <td><?= tgl_indo($data['tgl_peristiwa'])?></td>
-                                    <td><?= tgl_indo($data['tanggal'])?></td>
-                                    <td><?= $data['catatan']?></td>
-                                	</tr>
-                                <?php endforeach; ?>
-                        		</tbody>
+																<?php endif; ?>
+																<th>Tanggal Rekam</th>
+																<th>Catatan Peristiwa</th>
+															</tr>
+														</thead>
+														<tbody>
+															<?php foreach ($main as $data): ?>
+																<tr>
+																	<td><?= $data['no']?></td>
+																	<td>
+																		<input type="checkbox" name="id_cb[]" value="<?= $data['id_log']?>" />
+																	</td>
+																	<td nowrap>
+																		<a href="<?= site_url("penduduk_log/edit/$p/$o/$data[id_log]")?>" class="btn bg-orange btn-flat btn-sm"  title="Edit Log Penduduk" data-remote="false" data-toggle="modal" data-target="#modalBox" data-title="Edit Log Penduduk" ><i class="fa fa-edit"></i></a>
+																		<a href="#" data-href="<?= site_url("penduduk_log/kembalikan_status/$data[id_log]")?>" class="btn bg-olive btn-flat btn-sm" title="Kembalikan Status"  data-remote="false"  data-toggle="modal" data-target="#confirm-status"><i class="fa fa-undo"></i></a>
+																	</td>
+																	<td>
+																		<a href="<?= site_url("penduduk/detail/$p/$o/$data[id]")?>" id="test" name="<?= $data['id']?>"><?= $data['nik']?></a>
+																	</td>
+																	<td>
+																		<a href="<?= site_url("penduduk/detail/$p/$o/$data[id]")?>"><?= strtoupper($data['nama'])?></a>
+																	</td>
+																	<td>
+																		<a href="<?= site_url("keluarga/kartu_keluarga/$p/$o/$data[id_kk]")?>"><?= $data['no_kk']?> </a> <br>
+																		<?= $data['nama_kk']?>
+																	</td>
+																	<td><?= $data['dusun']?></td>
+																	<td><?= $data['rw']?></td>
+																	<td><?= $data['rt']?></td>
+																	<td><?= $data['umur_pada_peristiwa']?></td>
+																	<td><?= $data['status_dasar']?></td>
+																	<td><?= tgl_indo($data['tgl_peristiwa'])?></td>
+																	<td><?= tgl_indo($data['tanggal'])?></td>
+																	<td><?= $data['catatan']?></td>
+																</tr>
+															<?php endforeach; ?>
+														</tbody>
 													</table>
 												</div>
 											</div>
@@ -201,26 +200,26 @@
 											</div>
 										</div>
 										<div class="col-sm-6">
-                      <div class="dataTables_paginate paging_simple_numbers">
-                        <ul class="pagination">
-                          <?php if ($paging->start_link): ?>
-                            <li><a href="<?=site_url("penduduk_log/index/$paging->start_link/$o")?>" aria-label="First"><span aria-hidden="true">Awal</span></a></li>
-                          <?php endif; ?>
-                          <?php if ($paging->prev): ?>
-                            <li><a href="<?=site_url("penduduk_log/index/$paging->prev/$o")?>" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
-                          <?php endif; ?>
-                          <?php for ($i=$paging->start_link;$i<=$paging->end_link;$i++): ?>
-               	            <li <?=jecho($p, $i, "class='active'")?>><a href="<?= site_url("penduduk_log/index/$i/$o")?>"><?= $i?></a></li>
-                          <?php endfor; ?>
-                          <?php if ($paging->next): ?>
-                            <li><a href="<?=site_url("penduduk_log/index/$paging->next/$o")?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
-                          <?php endif; ?>
-                          <?php if ($paging->end_link): ?>
-                            <li><a href="<?=site_url("penduduk_log/index/$paging->end_link/$o")?>" aria-label="Last"><span aria-hidden="true">Akhir</span></a></li>
-                          <?php endif; ?>
-                        </ul>
-                      </div>
-                    </div>
+											<div class="dataTables_paginate paging_simple_numbers">
+												<ul class="pagination">
+													<?php if ($paging->start_link): ?>
+														<li><a href="<?=site_url("penduduk_log/index/$paging->start_link/$o")?>" aria-label="First"><span aria-hidden="true">Awal</span></a></li>
+													<?php endif; ?>
+													<?php if ($paging->prev): ?>
+														<li><a href="<?=site_url("penduduk_log/index/$paging->prev/$o")?>" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+													<?php endif; ?>
+													<?php for ($i=$paging->start_link;$i<=$paging->end_link;$i++): ?>
+														<li <?=jecho($p, $i, "class='active'")?>><a href="<?= site_url("penduduk_log/index/$i/$o")?>"><?= $i?></a></li>
+													<?php endfor; ?>
+													<?php if ($paging->next): ?>
+														<li><a href="<?=site_url("penduduk_log/index/$paging->next/$o")?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
+													<?php endif; ?>
+													<?php if ($paging->end_link): ?>
+														<li><a href="<?=site_url("penduduk_log/index/$paging->end_link/$o")?>" aria-label="Last"><span aria-hidden="true">Akhir</span></a></li>
+													<?php endif; ?>
+												</ul>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -236,7 +235,7 @@
 		<div class='modal-content'>
 			<div class='modal-header'>
 				<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-					<h4 class='modal-title' id='myModalLabel'><i class='fa fa-exclamation-triangle text-red'></i> Konfirmasi</h4>
+				<h4 class='modal-title' id='myModalLabel'><i class='fa fa-exclamation-triangle text-red'></i> Konfirmasi</h4>
 			</div>
 			<div class='modal-body btn-info'>
 				Apakah Anda yakin ingin mengembalikan status data penduduk ini?
