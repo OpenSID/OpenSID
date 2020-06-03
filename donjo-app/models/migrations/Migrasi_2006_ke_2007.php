410a9c9b5413f87c841f386d4d2d0d30026c890e
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-06-03 11:51:51 +1000

    Hapus perubahan kolom tautan di tabel teks_berjalan

--- donjo-app/models/migrations/Migrasi_2006_ke_2007.php
+++ donjo-app/models/migrations/Migrasi_2006_ke_2007.php
@@ -3,9 +3,6 @@ class Migrasi_2006_ke_2007 extends CI_model {
 
 	public function up()
 	{
-		// Ubah tipe data tautan pd tabel teks_berjalan (tautan berdasarkan id artikel)
-		$this->db->query("ALTER TABLE teks_berjalan CHANGE COLUMN tautan tautan INT(11) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci'");;
-
 	}
 
 }
