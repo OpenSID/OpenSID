b9cdedb5da3281238af127978ac8b28c36b9eb0e
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-06-05 16:50:09 +0700

    Opsi tampilkan Nama Peserta (bukan nama Kepala Keluarga) di daftar penerima bantuan

--- donjo-app/controllers/First.php
+++ donjo-app/controllers/First.php
@@ -95,7 +95,7 @@ class First extends Web_Controller {
 		$data['artikel'] = $this->first_artikel_m->artikel_show($data['paging']->offset, $data['paging']->per_page);
 
 		$data['headline'] = $this->first_artikel_m->get_headline();
-		if (config_item('covid_rss'))
+		if ($this->setting->covid_rss)
 		{
 			$data['feed'] = array(
 				'items' => $this->first_artikel_m->get_feed(),
@@ -104,7 +104,7 @@ class First extends Web_Controller {
 			);
 		}
 
-		if (config_item('apbdes_footer'))
+		if ($this->setting->apbdes_footer)
 		{
 			$data['transparansi'] = $this->keuangan_grafik_model->grafik_keuangan_tema();
 		}
@@ -397,15 +397,30 @@ class First extends Web_Controller {
 		$data = array();
 		$no = $_POST['start'];
 
-		foreach ($peserta as $baris)
+		if ($this->setting->daftar_bantuan_nama)
 		{
-			$no++;
-			$row = array();
-			$row[] = $no;
-			$row[] = $baris['program'];
-			$row[] = $baris['peserta'];
-			$row[] = $baris['alamat'];
-			$data[] = $row;
+			foreach ($peserta as $baris)
+			{
+				$no++;
+				$row = array();
+				$row[] = $no;
+				$row[] = $baris['program'];
+				$row[] = $baris['peserta_nama'];
+				$row[] = $baris['alamat'];
+				$data[] = $row;
+			}
+		}
+		else {
+			foreach ($peserta as $baris)
+			{
+				$no++;
+				$row = array();
+				$row[] = $no;
+				$row[] = $baris['program'];
+				$row[] = $baris['peserta'];
+				$row[] = $baris['alamat'];
+				$data[] = $row;
+			}
 		}
 
 		$output = array(
@@ -625,7 +640,7 @@ class First extends Web_Controller {
 		$this->web_widget_model->get_widget_data($data);
 		$data['data_config'] = $this->config_model->get_data();
 		$data['flash_message'] = $this->session->flashdata('flash_message');
-		if (config_item('apbdes_footer') AND config_item('apbdes_footer_all'))
+		if ($this->setting->apbdes_footer AND $this->setting->apbdes_footer_all)
 		{
 			$data['transparansi'] = $this->keuangan_grafik_model->grafik_keuangan_tema();
 		}
