fcc0042590c10dc9159a3fb9d46806263ab64a06
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-06-05 17:38:44 +0700

    Opsi Tampilkan 3D Chart di Statistik

--- themes/hadakewa/layouts/header.php
+++ themes/hadakewa/layouts/header.php
@@ -14,6 +14,7 @@
 		<meta content="utf-8" http-equiv="encoding">
 		<meta name="keywords" content="OpenSID,opensid,sid,SID,SID CRI,SID-CRI,sid cri,sid-cri,Sistem Informasi Desa,sistem informasi desa, desa <?= $desa['nama_desa'];?>">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta property="og:site_name" content="<?= $desa['nama_desa'];?>"/>
 		<meta property="og:type" content="article"/>
 
@@ -32,20 +33,22 @@
 		<?php else: ?>
 			<link rel="shortcut icon" href="<?= base_url()?>favicon.ico" />
 		<?php endif; ?>
+
 		<link type='text/css' href="<?= base_url()?>assets/front/css/first.css" rel='Stylesheet' />
+		<link rel="stylesheet" href="<?= base_url()?>assets/css/leaflet.css" />
 
 		<!-- Styles untuk tema dan penyesuaiannya di folder desa -->
 		<link type='text/css' href="<?= base_url().$this->theme_folder.'/'.$this->theme.'/css/first.css'?>" rel='Stylesheet' />
-		<?php if (is_file("desa/css/".$this->theme."/desa-web.css")): ?>
-			<link type='text/css' href="<?= base_url()?>desa/css/<?= $this->theme ?>/desa-web.css" rel='Stylesheet' />
-		<?php endif; ?>
-
 		<link type='text/css' href="<?= base_url()?>assets/css/font-awesome.min.css" rel='Stylesheet' />
 		<link type='text/css' href="<?= base_url()?>assets/css/ui-buttons.css" rel='Stylesheet' />
+
 		<?php if ($single_artikel OR $gallery): ?>
 			<link type='text/css' href="<?= base_url()?>assets/front/css/colorbox.css" rel='Stylesheet' />
 		<?php endif ?>
-		<link rel="stylesheet" href="<?= base_url()?>assets/css/leaflet.css" />
+
+		<?php if (is_file("desa/css/".$this->theme."/desa-web.css")): ?>
+			<link type='text/css' href="<?= base_url()?>desa/css/<?= $this->theme ?>/desa-web.css" rel='Stylesheet' />
+		<?php endif; ?>
 
 		<script src="<?= base_url()?>assets/front/js/jquery.js"></script>
 		<script src="<?= base_url()?>assets/js/leaflet.js"></script>
@@ -69,62 +72,53 @@
 		<script type="text/javascript">
 			var BASE_URL = "<?= base_url(); ?>";
 		</script>
-
 		<?php $this->load->view('head_tags_front') ?>
 	</head>
 	<body>
 		<div id="maincontainer">
 			<div id="topsection">
+				<div id="header">
+					<div id="headercontent">
+							<div id="menuwrapper">
+								<?php $this->load->view($folder_themes.'/partials/menu.tpl.php');?>
+							</div>
+					</div>
+				</div>
+				<div id="headleft">
+					<div id="divlogo">
+						<div id="divlogo-img">
+							<div class="intube">
+								<a href="<?= site_url(); ?>first/">
+								<img src="<?= gambar_desa($desa['logo']);?>" alt="<?= $desa['nama_desa']?>"/></a>
+							</div>
+						</div>
+						<div id="divlogo-txt">
+							<div class="intube">
+								<div id="siteTitle">
+									<h1>
+										<span id="header_sebutan_desa">
+											<?= ucwords($this->setting->sebutan_desa)." "?>
+										</span>
+										<?= ucwords($desa['nama_desa'])?>
+									</h1>
+									<h3>
+										<?= ucwords($this->setting->sebutan_kecamatan." ".$desa['nama_kecamatan'])?> <?= ucwords($this->setting->sebutan_kabupaten." ".$desa['nama_kabupaten'])?><br />
+										<?= $desa['alamat_kantor']?> - Kodepos <?= $desa['kode_pos']?><br />
+										<i class="fa fa-phone-square"></i> <?= $desa['telepon']?> - <i class="fa fa-envelope"></i> <?= $desa['email_desa']?><br />
+										<div id="h1_1"><?= $desa['website']?></div>
+									</h3>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<br class="clearboth"/>
+				</div>
 				<div class="innertube">
-					<div id="header-default">
-						<div id="headercontent-default">
-							<div id="menu_vert">
-								<div id="menuwrapper">
-									<?php $this->load->view($folder_themes.'/partials/menu.tpl.php');?>
-								</div>
-							</div>
-							<div id="menu_vert2">
-								<?php $this->load->view($folder_themes.'/layouts/carousel.php'); ?>
-							</div>
-						</div>
-						<div id="headleft-default">
-							<div id="divlogo">
-								<div id="divlogo-txt">
-									<div class="intube">
-										<div id="siteTitle">
-											<h1>
-												<span id="header_sebutan_desa">
-													<?= ucwords($this->setting->sebutan_desa." ")?>
-												</span>
-												<?= ucwords($desa['nama_desa'])?>
-											</h1>
-											<h2>
-												<?= ucwords($this->setting->sebutan_kecamatan." ".$desa['nama_kecamatan'])?><br />
-												<?= ucwords($this->setting->sebutan_kabupaten." ".$desa['nama_kabupaten'])?>
-											</h2>
-											<h3><?= $desa['alamat_kantor']?></h3>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div id="divlogo-img">
-								<div class="intube">
-									<a href="<?= site_url(); ?>first/">
-									<img src="<?= gambar_desa($desa['logo']);?>" alt="<?= $desa['nama_desa']?>"/>
-									</a>
-								</div>
-							</div>
-							<br class="clearboth"/>
-						</div>
-					</div>
-
-					<?php if (!empty($teks_berjalan)): ?>
-						<?php $this->load->view($folder_themes.'/layouts/teks_berjalan.php');?>
-					<?php endif; ?>
-
-					<div id="mainmenu">
-						<?php $this->load->view($folder_themes.'/partials/menu.left.php');?>
-					</div>
 
 				</div>
 			</div>
+			<?php if ($teks_berjalan) : ?>
+					<?php $this->load->view($folder_themes.'/layouts/teks_berjalan.php'); ?>
+				<?php endif ?>
+			<br class="clearboth"/>
