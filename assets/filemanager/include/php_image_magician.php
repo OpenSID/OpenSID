c81e0415baf2701366dcdf84012af4dd30782574
afa28 <andifahruddinakas@gmail.com>
2020-06-08 08:19:23 +0700

    Update filemanager 9.14.0

--- assets/filemanager/include/php_image_magician.php
+++ assets/filemanager/include/php_image_magician.php
@@ -2719,25 +2719,27 @@ class imageLib {
 			}
 		};
 
-		// *** Get extension
-		$extension = strrchr($file, '.');
+		// *** Get extension / image type
+		$extension = mime_content_type($file);
 		$extension = fix_strtolower($extension);
+		$extension = str_replace('image/', '', $extension);
 		switch ($extension)
 		{
-			case '.jpg':
-			case '.jpeg':
+			case 'jpg':
+			case 'jpeg':
 				$img = @imagecreatefromjpeg($file);
 				break;
-			case '.gif':
+			case 'gif':
 				$img = @imagecreatefromgif($file);
 				break;
-			case '.png':
+			case 'png':
 				$img = @imagecreatefrompng($file);
 				break;
-			case '.bmp':
+			case 'bmp':
 				$img = @$this->imagecreatefrombmp($file);
 				break;
-			case '.psd':
+			case 'psd':
+			case 'vnd.adobe.photoshop':
 				$img = @$this->imagecreatefrompsd($file);
 				break;
 
