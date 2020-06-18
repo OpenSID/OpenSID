cca2776507513419397d15d69635052df2814a94
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-06-19 06:16:44 +0700

    Auto Generate Keys

--- donjo-app/controllers/Siteman.php
+++ donjo-app/controllers/Siteman.php
@@ -63,6 +63,8 @@ class Siteman extends CI_Controller
 		else
 		{
 			unset($_SESSION['request_uri']);
+			unset($_SESSION['fm_key']);
+			$this->user_model->get_key();
 			redirect('main');
 		}
 	}
