9e41786d28e915d0c4732c92524ccb6cd0201cef
kosumo <mas.kosumo@gmail.com>
2020-06-22 10:59:43 +0700

    Validasi pada analisis-kategori Fixes #3172

--- donjo-app/views/analisis_kategori/ajax_form.php
+++ donjo-app/views/analisis_kategori/ajax_form.php
@@ -8,7 +8,7 @@
 				<div class="box-body">
 					<div class="form-group">
 						<label class="control-label" for="kategori">Nama Kategori/Variabel 	</label>
-						<input  id="kategori" class="form-control input-sm required" type="text" placeholder="Kategori Indikator" name="kategori" value="<?= $analisis_kategori['kategori']?>">
+						<input  id="kategori" class="form-control input-sm required nomor_sk" maxlength="50" type="text" placeholder="Kategori Indikator" name="kategori" value="<?= $analisis_kategori['kategori']?>">
 					</div>
 				</div>
 			</div>
