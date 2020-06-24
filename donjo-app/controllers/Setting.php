19b8c560418b1a60adbdf83d771333a2a34b7b4d
afa28 <andifahruddinakas@gmail.com>
2020-06-24 13:27:49 +0700

    Hapus yg tdk perlu

--- donjo-app/controllers/Setting.php
+++ donjo-app/controllers/Setting.php
@@ -1,27 +1,29 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Setting extends Admin_Controller {
+
+	private $_header;
 
 	public function __construct()
 	{
 		parent::__construct();
-		session_start();
-		$this->load->model('header_model');
-		$this->load->model('theme_model');
+		$this->load->model(['header_model', 'theme_model']);
+		$this->_header = $this->header_model->get_data();
 		$this->modul_ini = 11;
 		$this->sub_modul_ini = 43;
 	}
 
 	public function index()
 	{
-		$header = $this->header_model->get_data();
 		$data['list_tema'] = $this->theme_model->list_all();
 		$data['judul'] = 'Pengaturan Aplikasi';
 		$data['list_setting'] = 'list_setting';
 		$this->setting_model->load_options();
 
-		$this->load->view('header', $header);
-		$this->load->view('nav', $nav);
+		$this->load->view('header', $this->_header);
+		$this->load->view('nav');
 		$this->load->view('setting/setting_form', $data);
 		$this->load->view('footer');
 	}
@@ -42,10 +44,8 @@ class Setting extends Admin_Controller {
 	{
 		$this->sub_modul_ini = 46;
 
-		$header = $this->header_model->get_data();
-
-		$this->load->view('header', $header);
-		$this->load->view('nav', $nav);
+		$this->load->view('header', $this->_header);
+		$this->load->view('nav');
 		$this->load->view('setting/info_php');
 		$this->load->view('footer');
 	}
@@ -56,14 +56,12 @@ class Setting extends Admin_Controller {
 		$this->modul_ini = 13;
 		$this->sub_modul_ini = 211;
 
-		$header = $this->header_model->get_data();
-		$data['list_tema'] = $this->theme_model->list_all();
 		$data['judul'] = 'Pengaturan Halaman Web';
 		$data['list_setting'] = 'list_setting_web';
 		$this->setting_model->load_options();
 
-		$this->load->view('header', $header);
-		$this->load->view('nav', $nav);
+		$this->load->view('header', $this->_header);
+		$this->load->view('nav');
 		$this->load->view('setting/setting_form', $data);
 		$this->load->view('footer');
 	}
