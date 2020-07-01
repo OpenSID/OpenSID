d548ebe0d48aa0f4a25e0a6d576f591370e22287
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-07-01 21:56:11 +0700

    Perbaiki Modal RFM di Menu QRCode dan Ganti UI ke Bhs. Indonesia

--- donjo-app/views/setting/setting_qr.php
+++ donjo-app/views/setting/setting_qr.php
@@ -111,14 +111,14 @@
 
 <!-- File Manager -->
 <div class="modal fade" id="myModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	<div class="modal-dialog">
+	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class='modal-header'>
 				<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
 				<h4 class='modal-title' id='myModalLabel'>Atur QR Code</h4>
 			</div>
-			<div class="modal-body" style="padding:0px; margin:0px; width: 600px;">
-				<iframe width="600" height="400" src="../../assets/filemanager/dialog.php?type=2&field_id=logoqr'&fldr='&akey=<?= config_item('file_manager')?>" frameborder="0" style="overflow: scroll; overflow-x: hidden; overflow-y: scroll; "></iframe>
+			<div class="modal-body">
+				<iframe width="100%" height="400px" src="../../assets/filemanager/dialog.php?type=1&lang=id&field_id=logoqr&fldr=&akey=<?= config_item('file_manager')?>" frameborder="0" style="overflow: scroll; overflow-x: hidden; overflow-y: scroll; "></iframe>
 			</div>
 		</div>
 	</div>
