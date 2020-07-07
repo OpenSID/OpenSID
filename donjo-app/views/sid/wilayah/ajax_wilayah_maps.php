ebf86fcb216783429c9851e45f88eaf43c81ff85
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-07-07 20:50:54 +1000

    Ubah pemberitahuan hak cipta dan lisensi

--- donjo-app/views/sid/wilayah/ajax_wilayah_maps.php
+++ donjo-app/views/sid/wilayah/ajax_wilayah_maps.php
@@ -1,17 +1,47 @@
-<!--
- * Modul Pemetaan di OpenSID
+<?php
+/*
+ * File ini:
  *
- * /donjo-app/views/sid/wilayah/ajax_wilayah_maps.php v2.0 01/07/2020
+ * View di Modul Pemetaan di OpenSID
  *
- * https://github.com/OpenSID/OpenSID
+ * /donjo-app/views/sid/wilayah/ajax_wilayah_maps.php
  *
- * Copyright (c) 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
- * Copyright (c) 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ */
+
+/**
  *
- * Dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- -->
+ * File ini bagian dari:
+ *
+ * OpenSID
+ *
+ * Sistem informasi desa sumber terbuka untuk memajukan desa
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package OpenSID
+ * @author  Tim Pengembang OpenDesa
+ * @copyright Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * @copyright Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license http://www.gnu.org/licenses/gpl.html  GPL V3
+ * @link  https://github.com/OpenSID/OpenSID
+ */
+?>
 
 <style>
   #map
