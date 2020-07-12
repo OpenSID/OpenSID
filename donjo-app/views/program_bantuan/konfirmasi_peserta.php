88ad848d2c1fc9bb6fb3be3926b1768ef056dfdb
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-07-12 11:42:42 +1000

    Tmbahkan pemberitahuan lisensi. Hapus penggunaan fixSQL. Perbaiki gunakan id_peserta untuk mencari program bagi peserta. Perbaiki penulisan script

--- donjo-app/views/program_bantuan/konfirmasi_peserta.php
+++ donjo-app/views/program_bantuan/konfirmasi_peserta.php
@@ -1,3 +1,48 @@
+<?php
+/**
+ * File ini:
+ *
+ * Bagian dari form penambahan peserta Program Bantuan
+ *
+ * donjo-app/views/program_bantuan/konfirmasi_peserta.php
+ *
+ */
+
+/**
+ *
+ * File ini bagian dari:
+ *
+ * OpenSID
+ *
+ * Sistem informasi desa sumber terbuka untuk memajukan desa
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package	OpenSID
+ * @author	Tim Pengembang OpenDesa
+ * @copyright	Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * @copyright	Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license	http://www.gnu.org/licenses/gpl.html	GPL V3
+ * @link 	https://github.com/OpenSID/OpenSID
+ */
+?>
+
 <div class="form-group">
 	<label class="col-sm-4 col-lg-3 control-label"><?=$individu['judul_nik']?></label>
 	<div class="col-sm-7">
@@ -64,7 +109,7 @@
 	<label class="col-sm-4 col-lg-3 control-label">Bantuan <?=$individu['judul']?> Yang Sedang Diterima</label>
 	<div class="col-sm-7">
 		<?php foreach ($individu['program']['programkerja'] as $item): ?>
-			<?php if($item[status] == '1'): ?>
+			<?php if ($item[status] == '1'): ?>
 				<a href="<?= site_url("program_bantuan/data_peserta/$item[peserta_id]")?>" target="_blank"><span class="label label-success"><?= $item['nama']?></span>&nbsp;</a>
 			<?php endif; ?>
 		<?php endforeach; ?>
