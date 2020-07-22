e3a06c5298a88648d088125d868869cf3bc7c214
Ariandi Ryan Kahfi <ariandi@mail.com>
2020-07-22 21:24:07 +0700

    #50 Kini widget agenda tampil pada sidebar

--- widgets/agenda.php
+++ widgets/agenda.php
@@ -1,123 +1,118 @@
 <!-- Widget Agenda -->
 
-<?php if ($agenda): ?>
-	<div class="single_bottom_rightbar">
-		<h2><a href="<?= site_url('first/kategori/1000')?>"><i class="fa fa-calendar"></i> Agenda</a></h2>
-		<div id="agenda" class="box-body">
-			<ul class="nav nav-tabs">
-				<?php if (count($agenda['hari_ini']) > 0): ?>
-					<li class="active"><a data-toggle="tab" href="#hari-ini">Hari ini</a></li>
-				<?php endif; ?>
-				<li <?php count($agenda['hari_ini']) > 0 or print('class="active"')?>><a data-toggle="tab" href="#yad">Jadwal</a></li>
-				<?php if (count($agenda['lama']) > 0): ?>
-					<li><a data-toggle="tab" href="#lalu">Lama</a></li>
-				<?php endif; ?>
-			</ul>
+<style type="text/css">
+	#agenda .tab-content { margin-top: 0px; }
+</style>
 
-			<div class="tab-content">
+<div class="single_bottom_rightbar">
+	<h2><a href="<?= site_url('first/kategori/1000')?>"><i class="fa fa-calendar"></i> Agenda</a></h2>
+	<div id="agenda" class="box-body">
+		<ul class="nav nav-tabs">
+			<?php if (count($hari_ini) > 0): ?>
+				<li class="active"><a data-toggle="tab" href="#hari-ini">Hari ini</a></li>
+			<?php endif; ?>
+			<?php if (count($yad) > 0): ?>
+				<li class="<?php count($hari_ini) == 0 and print('active')?>"><a data-toggle="tab" href="#yad">Mendatang</a></li>
+			<?php endif; ?>
+			<?php if (count($lama) > 0): ?>
+				<li class="<?php count(array_merge($hari_ini, $yad)) == 0 and print('active')?>"><a data-toggle="tab" href="#lama">Lama</a></li>
+			<?php endif; ?>
+		</ul>
+		<div class="tab-content">
+			<?php if (count(array_merge($hari_ini, $yad, $lama)) > 0): ?>
 				<div id="hari-ini" class="tab-pane fade in active">
 					<ul class="sidebar-latest">
-						<?php foreach ($agenda['hari_ini'] as $l): ?>
+						<?php foreach ($hari_ini as $agenda): ?>
 							<li>
-								<table id="table-agenda" width="100%" style="margin-bottom:10px;">
-									<tr valign="top">
-										<th id="label-meta-agenda" width="35%">Kegiatan</th>
-										<td width="5%" class="titik">:</td>
-										<td><a href="<?= site_url('artikel/'.buat_slug($l))?>"><?= $l['judul']?></a></td>
+								<table id="table-agenda" width="100%">
+									<tr>
+										<td colspan="3"><a href="<?= site_url('artikel/'.buat_slug($agenda))?>"><?= $agenda['judul']?></a></td>
 									</tr>
-									<tr valign="top">
-										<th id="label-meta-agenda">Waktu</th>
-										<td class="titik">:</td>
-										<td><div id="small-agenda"><?= tgl_indo2($l['tgl_agenda'])?></div></td>
+									<tr>
+										<th id="label-meta-agenda" width="30%">Waktu</th>
+										<td width="5%">:</td>
+										<td id="isi-meta-agenda" width="65%"><?= tgl_indo2($agenda['tgl_agenda'])?></td>
 									</tr>
-									<tr valign="top">
+									<tr>
 										<th id="label-meta-agenda">Lokasi</th>
-										<td class="titik">:</td>
-										<td><?= $l['lokasi_kegiatan']?></td>
+										<td>:</td>
+										<td id="isi-meta-agenda"><?= $agenda['lokasi_kegiatan']?></td>
 									</tr>
-									<tr valign="top">
+									<tr>
 										<th id="label-meta-agenda">Koordinator</th>
-										<td class="titik">:</td>
-										<td><?= $l['koordinator_kegiatan']?></td>
+										<td>:</td>
+										<td id="isi-meta-agenda"><?= $agenda['koordinator_kegiatan']?></td>
 									</tr>
 								</table>
-								<?php if (count($agenda['yad']) > 0): ?><hr><?php endif; ?>
 							</li>
 						<?php endforeach; ?>
 					</ul>
 				</div>
 
-				<div id="yad" class="tab-pane fade <?php count($agenda['hari_ini']) > 0 or print('in active')?> ">
+				<div id="yad" class="tab-pane fade <?php count($hari_ini) == 0 and print('in active')?>">
 					<ul class="sidebar-latest">
-						<?php if (count($agenda['yad']) > 0): ?>
-							<?php foreach ($agenda['yad'] as $l): ?>
+						<?php if (count($yad) > 0): ?>
+							<?php foreach ($yad as $agenda): ?>
 								<li>
-									<table id="table-agenda" width="100%" style="margin-bottom:10px;">
-										<tr valign="top">
-											<th id="label-meta-agenda" width="35%">Kegiatan</th>
-											<td width="5%" class="titik">:</td>
-											<td><a href="<?= site_url('artikel/'.buat_slug($l))?>"><?= $l['judul']?></a></td>
+									<table id="table-agenda" width="100%">
+										<tr>
+											<td colspan="3"><a href="<?= site_url('artikel/'.buat_slug($agenda))?>"><?= $agenda['judul']?></a></td>
 										</tr>
-										<tr valign="top">
-											<th id="label-meta-agenda">Waktu</th>
-											<td class="titik">:</td>
-											<td><div id="small-agenda"><?= tgl_indo2($l['tgl_agenda'])?></div></td>
+										<tr>
+											<th id="label-meta-agenda" width="30%">Waktu</th>
+											<td width="5%">:</td>
+											<td id="isi-meta-agenda" width="65%"><?= tgl_indo2($agenda['tgl_agenda'])?></td>
 										</tr>
-										<tr valign="top">
+										<tr>
 											<th id="label-meta-agenda">Lokasi</th>
-											<td class="titik">:</td>
-											<td><?= $l['lokasi_kegiatan']?></td>
+											<td>:</td>
+											<td id="isi-meta-agenda"><?= $agenda['lokasi_kegiatan']?></td>
 										</tr>
-										<tr valign="top">
+										<tr>
 											<th id="label-meta-agenda">Koordinator</th>
-											<td class="titik">:</td>
-											<td><?= $l['koordinator_kegiatan']?></td>
+											<td>:</td>
+											<td id="isi-meta-agenda"><?= $agenda['koordinator_kegiatan']?></td>
 										</tr>
 									</table>
-									<?php if (count($agenda['yad']) > 0): ?><hr><?php endif; ?>
 								</li>
 							<?php endforeach; ?>
-							<?php else: ?>
-								<p align="center">Belum ada agenda mendatang.</p><hr>
-							<?php endif; ?>
-						</ul>
-					</div>
-
-					<div id="lalu" class="tab-pane fade">
-						<marquee onmouseover="this.stop()" onmouseout="this.start()" scrollamount="2" direction="up" width="100%" height="100" align="center" behavior=”alternate”>
-							<ul class="sidebar-latest">
-								<?php foreach ($agenda['lama'] as $l): ?>
-									<li>
-										<table id="table-agenda" width="100%" style="margin-bottom:10px;">
-											<tr valign="top">
-												<th id="label-meta-agenda" width="35%">Kegiatan</th>
-												<td width="5%" class="titik">:</td>
-												<td><a href="<?= site_url('artikel/'.buat_slug($l))?>"><?= $l['judul']?></a></td>
-											</tr>
-											<tr valign="top">
-												<th id="label-meta-agenda">Waktu</th>
-												<td class="titik">:</td>
-												<td><div id="small-agenda"><?= tgl_indo2($l['tgl_agenda'])?></div></td>
-											</tr>
-											<tr valign="top">
-												<th id="label-meta-agenda">Lokasi</th>
-												<td class="titik">:</td>
-												<td><?= $l['lokasi_kegiatan']?></td>
-											</tr>
-											<tr valign="top">
-												<th id="label-meta-agenda">Koordinator</th>
-												<td class="titik">:</td>
-												<td><?= $l['koordinator_kegiatan']?></td>
-											</tr>
-										</table>
-										<?php if (count($agenda['yad']) > 0): ?><hr><?php endif; ?>
-									</li>
-								<?php endforeach; ?>
-							</ul>
-						</marquee>
-					</div>
+						<?php endif; ?>
+					</ul>
 				</div>
 
-			</div>
+				<div id="lama" class="tab-pane fade <?php count(array_merge($hari_ini, $yad)) == 0 and print('in active')?>">
+					<marquee onmouseover="this.stop()" onmouseout="this.start()" scrollamount="2" direction="up" width="100%" height="100" align="center" behavior=”alternate”>
+						<ul class="sidebar-latest">
+							<?php foreach ($lama as $agenda): ?>
+								<li>
+									<table id="table-agenda" width="100%">
+										<tr>
+											<td colspan="3"><a href="<?= site_url('artikel/'.buat_slug($agenda))?>"><?= $agenda['judul']?></a></td>
+										</tr>
+										<tr>
+											<th id="label-meta-agenda" width="30%">Waktu</th>
+											<td width="5%">:</td>
+											<td id="isi-meta-agenda" width="65%"><?= tgl_indo2($agenda['tgl_agenda'])?></td>
+										</tr>
+										<tr>
+											<th id="label-meta-agenda">Lokasi</th>
+											<td>:</td>
+											<td id="isi-meta-agenda"><?= $agenda['lokasi_kegiatan']?></td>
+										</tr>
+										<tr>
+											<th id="label-meta-agenda">Koordinator</th>
+											<td>:</td>
+											<td id="isi-meta-agenda"><?= $agenda['koordinator_kegiatan']?></td>
+										</tr>
+									</table>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					</marquee>
+				</div>
+			<?php else: ?>
+				<p>Belum ada agenda</p>
+			<?php endif; ?>
 		</div>
-	<?php endif; ?>
+	</div>
+</div>
