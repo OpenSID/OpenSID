59c59c4e5d2f6dcdd11ca4b9f4408fc0c3b09b87
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-07-22 23:06:03 +0700

    Aktifkan fungsi tampilkan/sembunyikan peta infrastruktur di peta website

--- donjo-app/models/Plan_area_model.php
+++ donjo-app/models/Plan_area_model.php
@@ -1,24 +1,48 @@
 <?php
-/*
- * Berkas default dari halaman web utk publik
+/**
+ * File ini:
  *
- * Copyright 2013
- * Rizka Himawan <himawan.rizka@gmail.com>
- * Muhammad Khollilurrohman <adsakle1@gmail.com>
- * Asep Nur Ajiyati <asepnurajiyati@gmail.com>
+ * Model untuk modul Pemetaan (Area)
  *
- * SID adalah software tak berbayar (Opensource) yang boleh digunakan oleh siapa saja selama bukan untuk kepentingan profit atau komersial.
- * Lisensi ini mengizinkan setiap orang untuk menggubah, memperbaiki, dan membuat ciptaan turunan bukan untuk kepentingan komersial
- * selama mereka mencantumkan asal pembuat kepada Anda dan melisensikan ciptaan turunan dengan syarat yang serupa dengan ciptaan asli.
- * Untuk mendapatkan SID RESMI, Anda diharuskan mengirimkan surat permohonan ataupun izin SID terlebih dahulu,
- * aplikasi ini akan tetap bersifat opensource dan anda tidak dikenai biaya.
- * Bagaimana mendapatkan izin SID, ikuti link dibawah ini:
- * http://lumbungkomunitas.net/bergabung/pendaftaran/daftar-onpolygon/
- * Creative Commons Attribution-NonCommercial 3.0 Unported License
- * SID Opensource TIDAK BOLEH digunakan dengan tujuan profit atau segala usaha  yang bertujuan untuk mencari keuntungan.
- * Pelanggaran HaKI (Hak Kekayaan Intelektual) merupakan tindakan  yang menghancurkan dan menghambat karya bangsa.
+ * /donjo-app/models/Plan_area_model.php
+ *
  */
-?><?php class Plan_area_model extends MY_Model {
+
+/**
+ *
+ * File ini bagian dari:
+ *
+ * OpenSID
+ *
+ * Sistem informasi desa sumber terbuka untuk memajukan desa
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package OpenSID
+ * @author  Tim Pengembang OpenDesa
+ * @copyright Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * @copyright Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license http://www.gnu.org/licenses/gpl.html  GPL V3
+ * @link  https://github.com/OpenSID/OpenSID
+ */
+
+class Plan_area_model extends MY_Model {
 
 	public function __construct()
 	{
@@ -140,7 +164,7 @@
 	{
 		$data['nama'] = nomor_surat_keputusan($post['nama']);
 		$data['ref_polygon'] = $post['ref_polygon'];
-		$data['desk'] = htmlentities($post['desk']);
+		$data['desk'] = $post['desk'];
 		$data['enabled'] = $post['enabled'];
 		return $data;
 	}
@@ -281,5 +305,19 @@
 		$data = $query->result_array();
 		return $data;
 	}
+
+	public function list_area()
+	{
+		$sql = "
+			SELECT l.*, p.nama AS kategori, m.nama AS jenis, p.simbol AS simbol, p.color AS color FROM area l
+			LEFT JOIN polygon p ON l.ref_polygon = p.id
+			LEFT JOIN polygon m ON p.parrent = m.id
+			WHERE l.enabled = 1 AND p.enabled = 1 AND m.enabled = 1";
+
+		$query = $this->db->query($sql);
+		$data = $query->result_array();
+		return $data;
+	}
+
 }
 ?>
