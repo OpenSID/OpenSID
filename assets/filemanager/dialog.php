4a7a8b0f76641d94deed0865dd75dec3b5cdc956
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-07-24 20:30:32 +0700

    Mempercepat loading dialog RFM

--- assets/filemanager/dialog.php
+++ assets/filemanager/dialog.php
@@ -358,12 +358,12 @@ $get_params = http_build_query($get_params);
         <script src="js/include.js?v=<?php echo $version; ?>"></script>
 </head>
 <body>
-    <!-- The Templates plugin is included to render the upload/download listings -->
-    <script src="//blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
-    <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/blueimp-load-image/2.18.0/load-image.all.min.js"></script>
-    <!-- The Canvas to Blob plugin is included for image resizing functionality -->
-    <script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+		<!-- The Templates plugin is included to render the upload/download listings -->
+		<script src="js/tmpl.min.js"></script>
+		<!-- The Load Image plugin is included for the preview images and image resizing functionality -->
+		<script src="js/load-image.all.min.js"></script>
+		<!-- The Canvas to Blob plugin is included for image resizing functionality -->
+		<script src="js/canvas-to-blob.min.js"></script>
     <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
     <script src="js/jquery.iframe-transport.js"></script>
     <!-- The basic File Upload plugin -->
