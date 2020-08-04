8bdd714fec30b79021c120cac4b8e3e28ce0e2f0
afa28 <andifahruddinakas@gmail.com>
2020-08-04 09:52:02 +0700

    Update slider.php

--- layouts/slider.php
+++ layouts/slider.php
@@ -1,24 +1,24 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-<style type="text/css">
-	.slick_slider img {
-		width: 100%;
-	}
-
-	.slick_slider, .cycle-slideshow {
-		max-height: 300px;
-		border: 5px solid #e5e5e5;
-		display: block;
-		position: relative;
-		/*margin: 0px auto;*/
-		overflow: hidden;
-	}
-</style>
 <div class="slick_slider" style="margin-bottom:5px;">
 	<?php foreach ($slider_gambar['gambar'] as $gambar) : ?>
 		<?php $file_gambar = $slider_gambar['lokasi'] . 'sedang_' . $gambar['gambar']; ?>
 		<?php if(is_file($file_gambar)) : ?>
 			<div class="single_iteam">
+				<style type="text/css">
+					.slick_slider img {
+						width: 100%;
+					}
+
+					.slick_slider, .cycle-slideshow {
+						max-height: 300px;
+						border: 5px solid #e5e5e5;
+						display: block;
+						position: relative;
+						/*margin: 0px auto;*/
+						overflow: hidden;
+					}
+				</style>
 				<img class="tlClogo" src="<?= base_url($file_gambar); ?>" onclick="location.href='<?= 'artikel/'.buat_slug($gambar); ?>'">
 			</div>
 		<?php endif; ?>
