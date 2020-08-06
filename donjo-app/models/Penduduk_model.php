55c10bdcf604ae4d49d9dfa4ec2173047ee97d31
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-08-06 22:44:33 +1000

    Perbaiki tampilan status kawin untuk kasus belum diisi. [bug-fix]

--- donjo-app/models/Penduduk_model.php
+++ donjo-app/models/Penduduk_model.php
@@ -359,14 +359,15 @@
 		}
 
 		$select_sql .= "u.id, u.nik, u.tanggallahir, u.tempatlahir, u.foto, u.status, u.status_dasar, u.id_kk, u.nama, u.nama_ayah, u.nama_ibu, a.dusun, a.rw, a.rt, d.alamat, d.no_kk AS no_kk, u.kk_level, u.tag_id_card, u.created_at, rc.id as status_covid,
-			(CASE when u.status_kawin <> 2
-				then k.nama
+			(CASE
+				when u.status_kawin IS NULL then ''
+				when u.status_kawin <> 2 then k.nama
 				else
 					case when u.akta_perkawinan = ''
 						then 'KAWIN BELUM TERCATAT'
 						else 'KAWIN TERCATAT'
 					end
-				end) as kawin,
+			end) as kawin,
 			(SELECT DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(`tanggallahir`)), '%Y')+0 FROM tweb_penduduk WHERE id = u.id) AS umur,
 			(SELECT DATE_FORMAT(FROM_DAYS(TO_DAYS(log.tgl_peristiwa)-TO_DAYS(u.tanggallahir)), '%Y')+0) AS umur_pada_peristiwa,
 			x.nama AS sex, sd.nama AS pendidikan_sedang, n.nama AS pendidikan, p.nama AS pekerjaan, g.nama AS agama, m.nama AS gol_darah, hub.nama AS hubungan, b.no_kk AS no_rtm, b.id AS id_rtm
@@ -951,14 +952,15 @@
 	{
 		$sql = "SELECT u.sex as id_sex, u.*, a.dusun, a.rw, a.rt, t.id AS id_status, t.nama AS status, o.nama AS pendidikan_sedang, m.nama as golongan_darah, h.nama as hubungan,
 			b.nama AS pendidikan_kk, d.no_kk AS no_kk, d.alamat, u.id_cluster as id_cluster, ux.nama as nama_pengubah, ucreate.nama as nama_pendaftar, polis.nama AS asuransi,
-			(CASE when u.status_kawin <> 2
-				then k.nama
+			(CASE
+				when u.status_kawin IS NULL then ''
+				when u.status_kawin <> 2 then k.nama
 				else
 					case when u.akta_perkawinan = ''
 						then 'KAWIN BELUM TERCATAT'
 						else 'KAWIN TERCATAT'
 					end
-				end) as kawin,
+			end) as kawin,
 			(SELECT DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(`tanggallahir`)), '%Y')+0  FROM tweb_penduduk WHERE id = u.id)
 			 AS umur, x.nama AS sex, w.nama AS warganegara,
 			 p.nama AS pekerjaan, g.nama AS agama, c.nama as cacat,
