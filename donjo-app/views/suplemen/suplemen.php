5f61f7fcfd1d37604d1ef63027f7d5a95bc9fce9
Andi Fahruddin Akas <57283157+afa28@users.noreply.github.com>
2020-09-28 19:58:20 +0700

    Style admin (#41)
    * #3591: Perbaiki tampilan pilihan jenis surat di filter Arsip Layanan surat.
    
    * Perbaikan view modul data suplement, cetak surat, surat keluar dan surat masuk.
    
    1. Hapus inline css, gunakan class select_row untuk jenis surat != 1
    2. Gunakan css class padat menggantikan nostretch
    3. Hapus tag form dan div yg tdk diperlukan
    4. Gunakan style ekssternal css yg sudah disediakan
    
    * Create afa28.txt

--- donjo-app/views/suplemen/suplemen.php
+++ donjo-app/views/suplemen/suplemen.php
@@ -55,63 +55,61 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</ol>
 	</section>
 	<section class="content" id="maincontent">
-		<form id="mainform" name="mainform" action="" method="post">
-			<div class="box box-info">
-				<div class="box-header with-border">
-					<a href="<?=site_url('suplemen/form')?>" class="btn btn-social btn-flat bg-olive btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Tambah Suplemen Baru"><i class="fa fa-plus"></i> Tambah Suplemen Baru</a>
-					<a href="<?=site_url('suplemen/panduan')?>" class="btn btn-social btn-flat btn-info btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Tambah Program Bantuan Baru"><i class="fa fa-question-circle"></i> Panduan</a>
-				</div>
-				<div class="box-body">
-					<div class="dataTables_wrapper form-inline dt-bootstrap no-footer">
-						<form id="mainform" name="mainform" action="" method="post">
-							<select class="form-control input-sm" name="sasaran" onchange="formAction('mainform', '<?= site_url('suplemen'); ?>')">
-								<option value="">Pilih Sasaran</option>
-								<?php foreach ($list_sasaran AS $key => $value): ?>
-									<?php if (in_array($key, ['1', '2'])) : ?>
-										<option value="<?= $key; ?>" <?= selected($set_sasaran, $key); ?>><?= $value?></option>
-									<?php endif; ?>
-								<?php endforeach; ?>
-							</select>
-						</form>
-						<div class="table-responsive">
-							<table class="table table-bordered table-striped dataTable table-hover tabel-daftar">
-								<thead class="bg-gray disabled color-palette">
+		<div class="box box-info">
+			<div class="box-header with-border">
+				<a href="<?=site_url('suplemen/form')?>" class="btn btn-social btn-flat bg-olive btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Tambah Suplemen Baru"><i class="fa fa-plus"></i> Tambah Suplemen Baru</a>
+				<a href="<?=site_url('suplemen/panduan')?>" class="btn btn-social btn-flat btn-info btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Tambah Program Bantuan Baru"><i class="fa fa-question-circle"></i> Panduan</a>
+			</div>
+			<div class="box-body">
+				<div class="dataTables_wrapper form-inline dt-bootstrap no-footer">
+					<form id="mainform" name="mainform" action="" method="post">
+						<select class="form-control input-sm" name="sasaran" onchange="formAction('mainform', '<?= site_url('suplemen'); ?>')">
+							<option value="">Pilih Sasaran</option>
+							<?php foreach ($list_sasaran AS $key => $value): ?>
+								<?php if (in_array($key, ['1', '2'])) : ?>
+									<option value="<?= $key; ?>" <?= selected($set_sasaran, $key); ?>><?= $value?></option>
+								<?php endif; ?>
+							<?php endforeach; ?>
+						</select>
+					</form>
+					<div class="table-responsive">
+						<table class="table table-bordered table-striped dataTable table-hover tabel-daftar">
+							<thead class="bg-gray disabled color-palette">
+								<tr>
+									<th>No</th>
+									<th>Aksi</th>
+									<th>Nama Data</th>
+									<th>Jumlah Terdata</th>
+									<th>Sasaran</th>
+									<th>Keterangan</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php	foreach ($suplemen as $key => $item):	?>
 									<tr>
-										<th>No</th>
-										<th>Aksi</th>
-										<th>Nama Data</th>
-										<th>Jumlah Terdata</th>
-										<th>Sasaran</th>
-										<th>Keterangan</th>
+										<td class="padat"><?= ($key + 1); ?></td>
+										<td class="aksi">
+											<a href="<?= site_url("suplemen/rincian/$item[id]"); ?>" class="btn bg-purple btn-flat btn-sm" title="Rincian Data"><i class="fa fa-list-ol"></i></a>
+											<a href="<?= site_url("suplemen/form/$item[id]"); ?>" class="btn bg-orange btn-flat btn-sm" title="Ubah Data"><i class='fa fa-edit'></i></a>
+											<a
+												<?php if ($item['jml'] <= 0): ?>
+													href="#" data-href="<?= site_url("suplemen/hapus/$item[id]")?>" data-toggle="modal" data-target="#confirm-delete"
+												<?php endif; ?>
+												class="btn bg-maroon btn-flat btn-sm" title="Hapus" <?= jecho($item['jml'] > 0, true, 'disabled'); ?>><i class="fa fa-trash-o"></i>
+											</a>
+										</td>
+										<td width="20%"><a href="<?= site_url("suplemen/rincian/$item[id]"); ?>"><?= $item["nama"] ?></a></td>
+										<td class="padat"><?= $item['jml']?></td>
+										<td><?= $sasaran[$item["sasaran"]]?></td>
+										<td width="40%"><?= $item['keterangan']?></td>
 									</tr>
-								</thead>
-								<tbody>
-									<?php	foreach ($suplemen as $key => $item):	?>
-										<tr>
-											<td class="padat"><?= ($key + 1); ?></td>
-											<td class="aksi">
-												<a href="<?= site_url("suplemen/rincian/$item[id]"); ?>" class="btn bg-purple btn-flat btn-sm" title="Rincian Data"><i class="fa fa-list-ol"></i></a>
-												<a href="<?= site_url("suplemen/form/$item[id]"); ?>" class="btn bg-orange btn-flat btn-sm" title="Ubah Data"><i class='fa fa-edit'></i></a>
-												<a
-													<?php if ($item['jml'] <= 0): ?>
-														href="#" data-href="<?= site_url("suplemen/hapus/$item[id]")?>" data-toggle="modal" data-target="#confirm-delete"
-													<?php endif; ?>
-													class="btn bg-maroon btn-flat btn-sm" title="Hapus" <?= jecho($item['jml'] > 0, true, 'disabled'); ?>><i class="fa fa-trash-o"></i>
-												</a>
-											</td>
-											<td width="20%"><a href="<?= site_url("suplemen/rincian/$item[id]"); ?>"><?= $item["nama"] ?></a></td>
-											<td class="padat"><?= $item['jml']?></td>
-											<td class="nostretch"><?= $sasaran[$item["sasaran"]]?></td>
-											<td><?= $item['keterangan']?></td>
-										</tr>
-									<?php endforeach; ?>
-								</tbody>
-							</table>
-						</div>
+								<?php endforeach; ?>
+							</tbody>
+						</table>
 					</div>
 				</div>
 			</div>
-		</form>
+		</div>
 	</section>
 </div>
 <?php $this->load->view('global/confirm_delete');?>
