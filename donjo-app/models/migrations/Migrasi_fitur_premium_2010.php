7c737ea99bd2b36156aaf0fef0ee4acd946f426f
fusionid <zeppyshiddiq@gmail.com>
2020-10-01 21:42:19 +0700

    Tampilkan Legenda dan Kustomisasi Warna Wilayah di Peta

--- donjo-app/models/migrations/Migrasi_fitur_premium_2010.php
+++ donjo-app/models/migrations/Migrasi_fitur_premium_2010.php
@@ -129,6 +129,35 @@ class Migrasi_fitur_premium_2010 extends MY_model {
 			'ikon_kecil' => ''
 		);
 		$this->tambah_modul($modul);
+
+		// Tambah kolom warna di tabel config
+		if (!$this->db->field_exists('warna', 'config'))
+		{
+			$fields = array(
+				'jabatan' => array(
+					'type' => 'VARCHAR',
+					'constraint' => 10,
+					'default' => NULL
+				),
+			);
+
+			$this->dbforge->add_column('config', $fields);
+		}
+
+		// Tambah kolom warna di tabel tweb_
+		if (!$this->db->field_exists('warna', 'tweb_wil_clusterdesa'))
+		{
+			$fields = array(
+				'jabatan' => array(
+					'type' => 'VARCHAR',
+					'constraint' => 10,
+					'default' => NULL
+				),
+			);
+
+			$this->dbforge->add_column('tweb_wil_clusterdesa', $fields);
+		}
+
 	}
 
 }
