e8c17b8d40ac8c66c4e669b08088d6d0cd58c4ee
afa28 <andifahruddinakas@gmail.com>
2020-10-17 20:44:08 +0700

    view code_wilayah dengan (.) contoh :  52.01.14.1005

--- donjo-app/helpers/donjolib_helper.php
+++ donjo-app/helpers/donjolib_helper.php
@@ -578,5 +578,12 @@ function sensor_nik_kk($data)
 	return $output;
 }
 
+function kode_wilayah($kode_wilayah)
+{
+	$code1 = str_split(substr($kode_wilayah, 0, 6), 2);
+	$code2 = (strlen($kode_wilayah) > 6) ? '.' . substr($kode_wilayah, 6) : '';
+	$data = implode('.', $code1) . $code2;
 
-// =======================================
+	return $data;
+
+}
