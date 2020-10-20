d7e465fc7ee894a2a09f144d6d6a037ed9f43d0a
afa28 <andifahruddinakas@gmail.com>
2020-10-21 02:41:56 +0700

    Riview : Penyesuaian Modul Kelomok > Kelompok Master

--- donjo-app/controllers/Kelompok_master.php
+++ donjo-app/controllers/Kelompok_master.php
@@ -1,6 +1,9 @@
-<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 /*
- *  File ini:
+ * File ini:
  *
  * Controller untuk modul Kelompok
  *
@@ -8,7 +11,7 @@
  *
  */
 /*
- *  File ini bagian dari:
+ * File ini bagian dari:
  *
  * OpenSID
  *
@@ -42,63 +45,60 @@
 
 class Kelompok_master extends Admin_Controller {
 
+	private $_set_page;
+	private $_list_session;
+
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('kelompok_master_model');
-
+		$this->load->model(['kelompok_master_model']);
 		$this->modul_ini = 2;
 		$this->sub_modul_ini = 24;
+		$this->_set_page = ['20', '50', '100'];
+		$this->_list_session = ['cari', 'filter'];
 	}
 
 	public function clear()
 	{
-		unset($_SESSION['cari']);
-		unset($_SESSION['filter']);
-		unset($_SESSION['state']);
+		$this->session->unset_userdata($this->_list_session);
+		$this->session->per_page = $this->_set_page[0];
 		redirect('kelompok_master');
 	}
 
-	public function index($p=1, $o=0)
+	public function index($p = 1, $o = 0)
 	{
-    unset($_SESSION['kelompok_master']);
 		$data['p'] = $p;
 		$data['o'] = $o;
 
-		if (isset($_SESSION['cari']))
-			$data['cari'] = $_SESSION['cari'];
-		else $data['cari'] = '';
+		foreach ($this->_list_session as $list)
+		{
+			$data[$list] = $this->session->$list ?: '';
+		}
 
-		if (isset($_SESSION['filter']))
-			$data['filter'] = $_SESSION['filter'];
-		else $data['filter'] = '';
-		if (isset($_SESSION['state']))
-			$data['state'] = $_SESSION['state'];
-		else $data['state'] = '';
-		if (isset($_POST['per_page']))
-			$_SESSION['per_page']=$_POST['per_page'];
-		$data['per_page'] = $_SESSION['per_page'];
+		$per_page = $this->input->post('per_page');
+		if (isset($per_page))
+			$this->session->per_page = $per_page;
 
-		$data['paging']  = $this->kelompok_master_model->paging($p,$o);
-		$data['main']    = $this->kelompok_master_model->list_data($o, $data['paging']->offset, $data['paging']->per_page);
+		$data['func'] = 'index';
+		$data['set_page'] = $this->_set_page;
+		$data['paging'] = $this->kelompok_master_model->paging($p, $o);
+		$data['main'] = $this->kelompok_master_model->list_data($o, $data['paging']->offset, $data['paging']->per_page);
 		$data['keyword'] = $this->kelompok_master_model->autocomplete();
+
 		$this->set_minsidebar(1);
 		$this->render('kelompok_master/table', $data);
 	}
 
-	public function form($p=1, $o=0, $id='')
+	public function form($id = '')
 	{
-		$data['p'] = $p;
-		$data['o'] = $o;
-
 		if ($id)
 		{
 			$data['kelompok_master'] = $this->kelompok_master_model->get_kelompok_master($id);
-			$data['form_action'] = site_url("kelompok_master/update/$p/$o/$id");
+			$data['form_action'] = site_url("kelompok_master/update/$id");
 		}
 		else
 		{
-			$data['kelompok_master'] = null;
+			$data['kelompok_master'] = NULL;
 			$data['form_action'] = site_url("kelompok_master/insert");
 		}
 
@@ -106,29 +106,12 @@ class Kelompok_master extends Admin_Controller {
 		$this->render('kelompok_master/form', $data);
 	}
 
-	public function search()
+	public function filter($filter)
 	{
-		$cari = $this->input->post('cari');
-		if ($cari != '')
-			$_SESSION['cari'] = $cari;
-		else unset($_SESSION['cari']);
-		redirect('kelompok_master');
-	}
-
-	public function filter()
-	{
-		$filter = $this->input->post('filter');
-		if ($filter != 0)
-			$_SESSION['filter'] = $filter;
-		else unset($_SESSION['filter']);
-		redirect('kelompok_master');
-	}
-
-	public function state(){
-		$filter = $this->input->post('state');
-		if ($filter != 0)
-			$_SESSION['state'] = $filter;
-		else unset($_SESSION['state']);
+		$value = $this->input->post($filter);
+		if ($value != "")
+			$this->session->$filter = $value;
+		else $this->session->unset_userdata($filter);
 		redirect('kelompok_master');
 	}
 
@@ -138,23 +121,24 @@ class Kelompok_master extends Admin_Controller {
 		redirect('kelompok_master');
 	}
 
-	public function update($p=1, $o=0, $id='')
+	public function update($id = '')
 	{
 		$this->kelompok_master_model->update($id);
-		redirect("kelompok_master/index/$p/$o");
+		redirect('kelompok_master');
 	}
 
-	public function delete($p=1, $o=0, $id='')
+	public function delete($id = '')
 	{
-		$this->redirect_hak_akses('h', "kelompok_master/index/$p/$o");
+		$this->redirect_hak_akses('h');
 		$this->kelompok_master_model->delete($id);
-		redirect("kelompok_master/index/$p/$o");
+		redirect('kelompok_master');
 	}
 
-	public function delete_all($p=1, $o=0)
+	public function delete_all()
 	{
-		$this->redirect_hak_akses('h', "kelompok_master/index/$p/$o");
+		$this->redirect_hak_akses('h');
 		$this->kelompok_master_model->delete_all();
-		redirect("kelompok_master/index/$p/$o");
+		redirect('kelompok_master');
 	}
+
 }
