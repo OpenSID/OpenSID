304e02b5da9681a24bb75f6bb0e4538be12d0a1e
afa28 <andifahruddinakas@gmail.com>
2020-10-22 05:09:41 +0700

    Tambahkan keterangan tittle/petunjuk pencarian
    TODO : ubah autocomplete menampilkan nik dan nama penduduk

--- donjo-app/views/sid/kependudukan/penduduk.php
+++ donjo-app/views/sid/kependudukan/penduduk.php
@@ -84,7 +84,7 @@
 									</div>
 									<div class="col-sm-3">
 										<div class="input-group input-group-sm pull-right">
-											<input name="cari" id="cari" class="form-control" placeholder="Cari..." type="text" value="<?=html_escape($cari); ?>" onkeypress="if (event.keyCode == 13){$('#'+'mainform').attr('action', '<?= site_url("penduduk/filter/cari"); ?>');$('#'+'mainform').submit();}">
+											<input name="cari" id="cari" class="form-control" placeholder="Cari..." type="text" title="Pencarian berdasarkan nama penduduk" value="<?=html_escape($cari); ?>" onkeypress="if (event.keyCode == 13){$('#'+'mainform').attr('action', '<?= site_url("penduduk/filter/cari"); ?>');$('#'+'mainform').submit();}">
 											<div class="input-group-btn">
 												<button type="submit" class="btn btn-default" onclick="$('#'+'mainform').attr('action', '<?= site_url("penduduk/filter/cari"); ?>');$('#'+'mainform').submit();"><i class="fa fa-search"></i></button>
 											</div>
