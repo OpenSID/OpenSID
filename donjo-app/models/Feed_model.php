2882de2c45484c7b181254a2a00d09a56bed566c
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-12-18 18:56:57 +0700

    Perbaiki feed rss content type & query

--- donjo-app/models/Feed_model.php
+++ donjo-app/models/Feed_model.php
@@ -1,5 +1,43 @@
-<?php class Feed_model extends CI_Model
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ *  File ini bagian dari: 
+ * 
+ * OpenSID
+ * 
+ * Sistem informasi desa sumber terbuka untuk memajukan desa
+ * 
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ * 
+ * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * 
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+ * 
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+ * 
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ * 
+ * @package	OpenSID
+ * @author	Tim Pengembang OpenDesa
+ * @copyright	Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * @copyright	Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license	http://www.gnu.org/licenses/gpl.html	GPL V3
+ * @link 	https://github.com/OpenSID/OpenSID
+ */
+class Feed_model extends CI_Model
 {
+	const BERITA_DESA = 1;
+	const ENABLE = 1;
 
 	public function list_feeds()
 	{
@@ -7,13 +45,10 @@
 			->from('artikel a')
 			->join('user u', 'a.id_user = u.id', 'left')
 			->join('kategori k', 'a.id_kategori = k.id', 'left')
-			->where('a.enabled', 1)
-			->where('a.id_kategori NOT IN (1000)')
-			->where('tgl_upload < NOW()')
-			->order_by('a.tgl_upload', DESC)
-			->limit('20');
-
-			return $this->db->get()->result();
+			->where('a.enabled', static::ENABLE)
+			->where('a.id_kategori', static::BERITA_DESA)
+			->order_by('a.tgl_upload', 'DESC');
+		
+		return $this->db->get()->result();
 	}
 }
-?>
