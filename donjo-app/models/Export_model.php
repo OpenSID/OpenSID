57c9cefea36cea522e31e01341a20fe114c4131c
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-12-21 14:27:09 +0700

    Upload dgn foto ukuran file kecil dan gunakan helper kode_wilayah

--- donjo-app/models/Export_model.php
+++ donjo-app/models/Export_model.php
@@ -295,8 +295,7 @@
 		->select('kode_desa')
 		->get('config')
 		->row();
-
-		$kode_desa = substr($desa->kode_desa, 0, 2) . '.' . substr($desa->kode_desa, 2, 2) . '.' . substr($desa->kode_desa, 4, 2) . '.' . substr($desa->kode_desa, 6);
+		$kode_desa = kode_wilayah($desa->kode_desa);
 
 		$data_hapus = $this->db->select([
 		  "CONCAT('{$kode_desa}') as desa_id",
@@ -316,8 +315,7 @@
 		->select('kode_desa')
 		->get('config')
 		->row();
-
-		$kode_desa = substr($desa->kode_desa, 0, 2) . '.' . substr($desa->kode_desa, 2, 2) . '.' . substr($desa->kode_desa, 4, 2) . '.' . substr($desa->kode_desa, 6);
+		$kode_desa = kode_wilayah($desa->kode_desa);
 
 		$data = $this->db->select([
 			'k.alamat', 'c.dusun', 'c.rw', 'c.rt', 'p.nama', 'k.no_kk', 'p.nik', 'p.sex', 'p.tempatlahir', 'p.tanggallahir', 'p.agama_id', 'p.pendidikan_kk_id', 'p.pendidikan_sedang_id', 'p.pekerjaan_id', 'p.status_kawin', 'p.kk_level', 'p.warganegara_id', 'p.nama_ayah', 'p.nama_ibu', 'p.golongan_darah_id', 'p.akta_lahir', 'p.dokumen_pasport', 'p.tanggal_akhir_paspor', 'p.dokumen_kitas', 'p.ayah_nik', 'p.ibu_nik', 'p.akta_perkawinan', 'p.tanggalperkawinan', 'p.akta_perceraian', 'p.tanggalperceraian', 'p.cacat_id', 'p.cara_kb_id', 'p.hamil', 'p.id', 'p.foto', 'p.status_dasar', 'p.ktp_el', 'p.status_rekam', 'p.alamat_sekarang', 'p.created_at', 'p.updated_at', "CONCAT('{$kode_desa}') as desa_id", 'YEAR(p.created_at) as tahun'])
@@ -345,6 +343,8 @@
 				$baris->rt = '-';
 			if (empty($baris->rw))
 				$baris->rw = '-';
+			if (!empty($baris->foto))
+				$baris->foto = 'kecil_' . $baris->foto;
 			$data[$i] = $baris;
 		}
 
