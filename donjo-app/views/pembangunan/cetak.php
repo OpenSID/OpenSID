dfe5a87ec38fcab05d4348939522a71d8f2d8e5b
Eddie Ridwan <eddie.ridwan@gmail.com>
2021-01-06 21:48:30 +1100

    Perbaiki kurang spasi

--- donjo-app/views/pembangunan/cetak.php
+++ donjo-app/views/pembangunan/cetak.php
@@ -14,7 +14,7 @@
 				<img class="logo" src="<?= gambar_desa($config['logo']); ?>" alt="logo-desa">
 				<h1 class="judul">DOKUMENTASI BIDANG PELAKSANAAN PEMBANGUNAN</h1>
 				<h1 class="judul">
-					PEMERINTAH<?= strtoupper($this->setting->sebutan_kabupaten . ' ' . $config['nama_kabupaten'] . ' <br>' . $this->setting->sebutan_kecamatan . ' ' . $config['nama_kecamatan'] . ' <br>' . $this->setting->sebutan_desa . ' ' . $config['nama_desa']); ?>
+					PEMERINTAH <?= strtoupper($this->setting->sebutan_kabupaten . ' ' . $config['nama_kabupaten'] . ' <br>' . $this->setting->sebutan_kecamatan . ' ' . $config['nama_kecamatan'] . ' <br>' . $this->setting->sebutan_desa . ' ' . $config['nama_desa']); ?>
 				</h1>
 			</td>
 		</tr>
