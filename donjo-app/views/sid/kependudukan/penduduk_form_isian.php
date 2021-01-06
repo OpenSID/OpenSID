6d7d4f776c6a39871a2310dc6d6b0b973aa9e572
afa28 <andifahruddinakas@gmail.com>
2021-01-06 15:49:45 +0700

    sesuaiakn penanganan maps untuk data penduduk baru

--- donjo-app/views/sid/kependudukan/penduduk_form_isian.php
+++ donjo-app/views/sid/kependudukan/penduduk_form_isian.php
@@ -4,11 +4,7 @@
 	<?php endif; ?>
 	<div class="box box-primary">
 		<div class="box-body box-profile">
-			<?php if ($penduduk['foto']): ?>
-				 <img class="penduduk profile-user-img img-responsive img-circle" src="<?= AmbilFoto($penduduk['foto'])?>" alt="Foto">
-			<?php else: ?>
-				<img class="penduduk profile-user-img img-responsive img-circle" src="<?= base_url()?>assets/files/user_pict/kuser.png" alt="Foto">
-			<?php endif; ?>
+			<img class="penduduk profile-user-img img-responsive img-circle" src="<?= AmbilFoto($penduduk['foto'])?>" alt="Foto">
 			<br/>
 			<p class="text-muted text-center"> (Kosongkan jika tidak ingin mengubah foto)</p>
 			<br/>
@@ -29,26 +25,13 @@
 			<a href="<?=site_url()?>penduduk/clear" class="btn btn-social btn-flat btn-info btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Kembali Ke Data Penduduk"><i class="fa fa-arrow-circle-o-left"></i> Kembali Ke Daftar Penduduk</a>
 		</div>
 		<div class='box-body'>
-      <?php $this->load->view('sid/kependudukan/penduduk_form_isian_bersama'); ?>
-    </div>
-    <?php if($penduduk['status_dasar_id'] == 1 || !isset($penduduk['status_dasar_id'])): ?>
-      <div class='box-footer'>
-        <div class='col-xs-12'>
-          <button type='reset' class='btn btn-social btn-flat btn-danger btn-sm' ><i class='fa fa-times'></i> Batal</button>
-          <button type='submit' class='btn btn-social btn-flat btn-info btn-sm pull-right'><i class='fa fa-check'></i> Simpan</button>
-        </div>
-      </div>
-    <?php endif; ?>
-		<div  class="modal fade" id="rumah-penduduk" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-			<div class='modal-dialog'>
-				<div class='modal-content'>
-					<div class='modal-header'>
-						<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-						<h4 class='modal-title' id='myModalLabel'><i class='fa fa-exclamation-triangle text-red'></i> Cari Lokasi Tempat Tinggal</h4>
-					</div>
-					<div class="fetched-data"></div>
-				</div>
-			</div>
+			<?php $this->load->view('sid/kependudukan/penduduk_form_isian_bersama'); ?>
 		</div>
+		<?php if($penduduk['status_dasar_id'] == 1 || !isset($penduduk['status_dasar_id'])): ?>
+			<div class='box-footer'>
+				<button type='reset' class='btn btn-social btn-flat btn-danger btn-sm' ><i class='fa fa-times'></i> Batal</button>
+				<button type="submit" class="btn btn-social btn-flat btn-info btn-sm pull-right" onclick="$('#'+'mainform').attr('action', '<?= $form_action ?>');$('#'+'mainform').submit();"><i class="fa fa-check"></i> Simpan</button>
+			</div>
+		<?php endif; ?>
 	</div>
 </div>
