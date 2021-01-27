5278c818bdcdabd9f0136600be8afd7729888675
Eddie Ridwan <eddie.ridwan@gmail.com>
2021-01-27 12:54:43 +1100

    Ubah cache ambil status idm menggunakan library CI

--- donjo-app/controllers/Status_desa.php
+++ donjo-app/controllers/Status_desa.php
@@ -46,11 +46,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Status_desa extends Admin_Controller {
 
+	protected $cache_id = 'cache_idm';
+
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->model('header_model');
 		$this->load->library('data_publik');
+		$this->load->driver('cache');
 		$this->modul_ini = 200;
 		$this->sub_modul_ini = 101;
 	}
@@ -61,13 +64,16 @@ class Status_desa extends Admin_Controller {
 		$kode_desa = $header['desa']['kode_desa'];
 		if ($this->data_publik->has_internet_connection())
 		{
-			$this->data_publik->set_api_url("https://idm.kemendesa.go.id/open/api/desa/rumusan/$kode_desa/2020", "idm_$kode_desa")
-				->set_interval(7)
-				->set_cache_folder(FCPATH.'desa');
+			$this->data_publik->set_api_url("https://idm.kemendesa.go.id/open/api/desa/rumusan/$kode_desa/2020", "idm_$kode_desa");
 
-			$idm = $this->data_publik->get_url_content();
+			$idm = $this->cache->pakai_cache(function ()
+			{
+				return $this->data_publik->get_url_content($no_cache = true);
+			}, $this->cache_id, 604800);
+
 			if ($idm->body->error)
 			{
+				$this->cache->hapus_cache_untuk_semua($this->cache_id);
 				$idm->body->mapData->error_msg = $idm->body->message . " : " . $idm->header->url . "<br><br>" .
 					"Periksa Kode Desa di Identitas Desa. Masukkan kode lengkap, contoh '3507012006'<br>";
 			}
