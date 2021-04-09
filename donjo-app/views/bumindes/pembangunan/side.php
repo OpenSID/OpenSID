662342267fb407d5c8ecf00c5c61e1f193398112
Gusna Ikhsan <gusna.ikhsan7@gmail.com>
2021-04-10 00:30:08 +0700

    url fixing

--- donjo-app/views/bumindes/pembangunan/side.php
+++ donjo-app/views/bumindes/pembangunan/side.php
@@ -1,7 +1,7 @@
 <div class="box box-widget widget-user-2">
   <div class="box-footer no-padding">
     <ul class="nav nav-stacked">
-      <li class="<?php compared_return($selected_nav, "tanah", "active"); ?>"><a href="<?= site_url('bumindes_pembangunan/tables/tanah') ?>">Buku Tanah di Desa</a></li>
+      <li class="<?php compared_return($selected_nav, "tanah", "active"); ?>"><a href="<?= site_url('bumindes_tanah_desa/tables/tanah') ?>">Buku Tanah di Desa</a></li>
       <li class="<?php compared_return($selected_nav, "tanah_kas", "active"); ?>"><a href="<?= site_url('bumindes_tanah_kas_desa/tables/tanah_kas') ?>">Buku Tanah Kas Desa</a></li>      
     </ul>
   </div>
