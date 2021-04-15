d91376691c3c0769b061d0df72cb0b75093060d6
Apidong <apidongblog@gmail.com>
2021-04-15 15:13:16 +0700

    -format ulang

--- themes/klasik/layouts/status_sdgs.tpl.php
+++ themes/klasik/layouts/status_sdgs.tpl.php
@@ -1,25 +1,69 @@
-<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<?php if (!defined('BASEPATH')) {
+	exit('No direct script access allowed');
+}
 
-<?php $this->load->view($folder_themes.'/layouts/header.php');?>
-			<div id="contentwrapper">
-				<div id="contentcolumn">
-					<div class="innertube">
-						<?php $this->load->view(Web_Controller::fallback_default($this->theme, '/partials/kemendes_sdgs.php'));?>
-					</div>
-				</div>
-			</div>
+/**
+ * File ini:
+ *
+ * View untuk tampilan status SDGS Website Tema Klasik
+ *
+ * themes/klasik/layouts/slider.php,
+ *
+ */
 
-			<div id="rightcolumn">
-				<div class="innertube">
-					<?php $this->load->view(Web_Controller::fallback_default($this->theme, '/partials/side.right.php'));?>
-				</div>
-			</div>
+/**
+ *
+ * File ini bagian dari:
+ *
+ * OpenSID
+ *
+ * Sistem informasi desa sumber terbuka untuk memajukan desa
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+ *
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+ *
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package	OpenSID
+ * @author	Tim Pengembang OpenDesa
+ * @copyright	Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * @copyright	Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license	http://www.gnu.org/licenses/gpl.html	GPL V3
+ * @link 	https://github.com/OpenSID/OpenSID
+ */
+?>
 
-			<div id="footer">
-				<?php
-				$this->load->view($folder_themes.'/partials/copywright.tpl.php');
-				?>
-			</div>
+<?php $this->load->view($folder_themes . '/layouts/header.php'); ?>
+<div id="contentwrapper">
+	<div id="contentcolumn">
+		<div class="innertube">
+			<?php $this->load->view(Web_Controller::fallback_default($this->theme, '/partials/kemendes_sdgs.php')); ?>
 		</div>
-	</body>
-</html>
+	</div>
+</div>
+
+<div id="rightcolumn">
+	<div class="innertube">
+		<?php $this->load->view(Web_Controller::fallback_default($this->theme, '/partials/side.right.php')); ?>
+	</div>
+</div>
+
+<div id="footer">
+	<?php
+	$this->load->view($folder_themes . '/partials/copywright.tpl.php');
+	?>
+</div>
+</div>
