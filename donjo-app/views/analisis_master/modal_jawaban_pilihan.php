b28fe775892c0b2695c6437f657beeb40385144e
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2021-04-23 09:41:14 +0700

    Fix Bug Session gform_id

--- donjo-app/views/analisis_master/modal_jawaban_pilihan.php
+++ donjo-app/views/analisis_master/modal_jawaban_pilihan.php
@@ -31,7 +31,7 @@
 												<label class="control-label">Informasi Umum Form Analisis</label>
 												<input type="hidden" id="id-row-nik-kk" name="id-row-nik-kk">
 												<input type="hidden" id="gform-id-nik-kk" name="gform-id-nik-kk">
-												<input type="hidden" id="gform-form-id" name="gform-form-id" value="<?= $this->session->data_import ?>">
+												<input type="hidden" id="gform-form-id" name="gform-form-id" value="<?= $this->session->gform_id ?>">
 												<div class="table-responsive">
 													<table class="table table-bordered table-striped dataTable table-hover">
 														<tbody>
