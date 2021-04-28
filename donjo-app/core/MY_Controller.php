0972746174d2e5634851d199ad4ff3b56dbb59e3
Eddie Ridwan <eddie.ridwan@gmail.com>
2021-04-28 19:56:15 +1000

    Sesuaikan aturan penulisan donjo-app/views/bumindes/pembangunan/tanah_kas_desa/content_tanah_kas_desa.php

--- donjo-app/core/MY_Controller.php
+++ donjo-app/core/MY_Controller.php
@@ -66,7 +66,7 @@ class MY_Controller extends CI_Controller {
 		parent::__construct();
 
 		// Tampilkan profiler untuk development
-		if (defined('ENVIRONMENT') && ENVIRONMENT == 'development')	$this->load->library('profiler');
+		if (defined('ENVIRONMENT') && ENVIRONMENT == 'development')	$this->output->enable_profiler(TRUE);
 
 		$this->load->model(['setting_model']);
         $this->setting_model->init();
