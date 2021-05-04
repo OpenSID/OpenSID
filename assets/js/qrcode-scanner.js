6284709b5069e5af47e432ccd0c3cd341a53b606
afa28 <andifahruddinakas@gmail.com>
2021-05-04 14:40:47 +0700

    Update qrcode-scanner.js

--- assets/js/qrcode-scanner.js
+++ assets/js/qrcode-scanner.js
@@ -14,17 +14,20 @@ function docReady(fn) {
 
 docReady(function () {
 	var resultContainer = document.getElementById('qr-reader-results');
-
 	var lastResult, countResults = 0;
+	
 	function onScanSuccess(qrCodeMessage) {
 		if (qrCodeMessage !== lastResult) {
 			++countResults;
 			lastResult = qrCodeMessage;
-			resultContainer.innerHTML
-			+= `<div><strong>${qrCodeMessage}<strong></div>
-			<p><a href="${qrCodeMessage}" class="btn btn-social btn-flat btn-info btn-sm" target="_blank"><i class="fa fa-globe"></i>&nbsp;Kunjungi Website</a></p>`;
+			resultContainer.innerHTML += `<div><strong>${qrCodeMessage}<strong></div>`;
+			const regexList = [/http/, /https/, /www/];			
+			if (regexList.some(rx => rx.test(qrCodeMessage))) {
+				resultContainer.innerHTML += `<p><a href="${qrCodeMessage}" class="btn btn-social btn-flat btn-info btn-sm" target="_blank"><i class="fa fa-globe"></i>&nbsp;Kunjungi Website</a></p>`;
+			}
 		}
-	html5QrcodeScanner.clear();	
+		
+		html5QrcodeScanner.clear();	
 	}
 
 	var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250 });
