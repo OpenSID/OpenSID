79a233ce3b37486f6d2fd5014fd6588ee4f2a8f7
Afila <apidongblog@gmail.com>
2021-05-06 12:31:23 +0700

    Update donjo-app/controllers/Api_inventaris_tanah.php Gunakan satu space untuk arrray
    Co-authored-by: Agung Sugiarto <sugiartoagung92@gmail.com>

--- donjo-app/controllers/Api_inventaris_tanah.php
+++ donjo-app/controllers/Api_inventaris_tanah.php
@@ -85,14 +85,14 @@ class Api_inventaris_tanah extends Admin_Controller
 	public function update_mutasi($id)
 	{
 		$data = $this->inventaris_tanah_model->update_mutasi($id, array(
-			'jenis_mutasi' 		=> ($this->input->post('status_mutasi') == 'Hapus') ?  $this->input->post('mutasi') : null ,
-			'status_mutasi' 	=> $this->input->post('status_mutasi'),
-			'tahun_mutasi' 		=> $this->input->post('tahun_mutasi'),
-			'harga_jual' 			=> $this->input->post('harga_jual') || null,
-			'sumbangkan' 			=> $this->input->post('sumbangkan') || null,
-			'keterangan' 			=> $this->input->post('keterangan'),
-			'updated_at' 			=> date('Y-m-d H:i:s'),
-			'visible' 				=> 1
+			'jenis_mutasi' => ($this->input->post('status_mutasi') == 'Hapus') ?  $this->input->post('mutasi') : null ,
+			'status_mutasi' => $this->input->post('status_mutasi'),
+			'tahun_mutasi' => $this->input->post('tahun_mutasi'),
+			'harga_jual' => $this->input->post('harga_jual') || null,
+			'sumbangkan' => $this->input->post('sumbangkan') || null,
+			'keterangan' => $this->input->post('keterangan'),
+			'updated_at' => date('Y-m-d H:i:s'),
+			'visible' => 1
 			));
 		if ($data) $_SESSION['success'] = 1;
 		else $_SESSION['success'] = -1;
