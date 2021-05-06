31f06f5ebc469cf40e3a2aa96cabce5494ee3240
Afila <apidongblog@gmail.com>
2021-05-06 12:30:39 +0700

    Update donjo-app/controllers/Api_inventaris_gedung.php
    Co-authored-by: Agung Sugiarto <sugiartoagung92@gmail.com>

--- donjo-app/controllers/Api_inventaris_gedung.php
+++ donjo-app/controllers/Api_inventaris_gedung.php
@@ -91,8 +91,8 @@ class Api_inventaris_gedung extends Admin_Controller
 	public function update_mutasi($id)
 	{
 		$data = $this->inventaris_gedung_model->update_mutasi($id, array(
-			'jenis_mutasi' 		=> ($this->input->post('status_mutasi') == 'Hapus') ?  $this->input->post('mutasi') : null ,
-			'status_mutasi' 	=> $this->input->post('status_mutasi'),
+			'jenis_mutasi' => ($this->input->post('status_mutasi') == 'Hapus') ?  $this->input->post('mutasi') : null ,
+			'status_mutasi' => $this->input->post('status_mutasi'),
 			'tahun_mutasi' => $this->input->post('tahun_mutasi'),
 			'harga_jual' => $this->input->post('harga_jual') || null,
 			'sumbangkan' => $this->input->post('sumbangkan') || null,
