35aae0fd203e035337ac0896316ee4e323712af6
Eddie Ridwan <eddie.ridwan@gmail.com>
2021-05-17 21:26:44 +1000

    [#4162](https://github.com/OpenSID/OpenSID/issues/4162) Batasi tidak bisa ubah data Info Desa kalau pengguna tidak mempunyai hak ubah.

--- catatan_rilis.md
+++ catatan_rilis.md
@@ -11,7 +11,7 @@ Lengkapnya, isi rilis versi v21.05-premium - 2021-05-12 adalah sebagai berikut:
 
 
 #### Perbaikan BUG
-1. [#4162](https://github.com/OpenSID/OpenSID/issues/4162) Batasi tidak bisa ubah data Penduduk, Keluarga, Kelompok, Data Suplemen, Layanan Surat dan Rumah Tangga kalau pengguna tidak mempunyai hak ubah.
+1. [#4162](https://github.com/OpenSID/OpenSID/issues/4162) Batasi tidak bisa ubah data Penduduk, Keluarga, Kelompok, Data Suplemen, Layanan Surat, Info Desa dan Rumah Tangga kalau pengguna tidak mempunyai hak ubah.
 2. [#4170](https://github.com/OpenSID/OpenSID/issues/4170) Sekarang data keluarga tersimpan normal pada impor data penduduk.
 3. [#4165](https://github.com/OpenSID/OpenSID/issues/4165) Sekarang penandatangan tampil benar di cetak/unduh Buku Agenda - Surat Masuk dan Buku Ekspedisi.
 4. [#4167](https://github.com/OpenSID/OpenSID/issues/4167) Sekarang laporan cetak/unduh Sekretariat > Informasi Publik ada blok penandatangan.
--- donjo-app/views/sid/wilayah/wilayah_rt.php
+++ donjo-app/views/sid/wilayah/wilayah_rt.php
@@ -13,7 +13,9 @@
 			<div class="col-md-12">
 				<div class="box box-info">
 					<div class="box-header with-border">
-						<a href="<?= site_url("sid_core/form_rt/$id_dusun/$id_rw")?>" class="btn btn-social btn-flat btn-success btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Tambah Data"><i class="fa fa-plus"></i> Tambah RT</a>
+						<?php if ($this->CI->cek_hak_akses('u')): ?>
+							<a href="<?= site_url("sid_core/form_rt/$id_dusun/$id_rw")?>" class="btn btn-social btn-flat btn-success btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Tambah Data"><i class="fa fa-plus"></i> Tambah RT</a>
+						<?php endif; ?>
 						<a href="<?= site_url("sid_core/cetak_rt/$id_dusun/$id_rw")?>" class="btn btn-social btn-flat bg-purple btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Cetak Data" target="_blank"><i class="fa fa-print "></i> Cetak</a>
 						<a href="<?= site_url("sid_core/excel_rt/$id_dusun/$id_rw")?>" class="btn btn-social btn-flat bg-navy btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Unduh Data" target="_blank"><i class="fa fa-download"></i> Unduh</a>
 						<a href="<?= site_url("sid_core/sub_rw/$id_dusun")?>" class="btn btn-social btn-flat btn-info btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Kembali Ke Daftar RW">
@@ -50,13 +52,17 @@
 																<tr>
 																	<td><?= $data['no'] ?></td>
 																	<td nowrap>
-																		<?php if ($data['rt']!="-"): ?>
-																			<a href="<?= site_url("sid_core/urut/rt/$paging->page/$data[id]/1/$id_dusun/$id_rw"); ?>" class="btn bg-olive btn-flat btn-sm <?php ($data['no'] == $paging->num_rows) and print('disabled'); ?>" title="Pindah Posisi Ke Bawah"><i class="fa fa-arrow-down"></i></a>
-																			<a href="<?= site_url("sid_core/urut/rt/$paging->page/$data[id]/2/$id_dusun/$id_rw"); ?>" class="btn bg-olive btn-flat btn-sm <?php ($data['no'] == 1 AND $paging->page == $paging->start_link) and print('disabled'); ?>" title="Pindah Posisi Ke Atas"><i class="fa fa-arrow-up"></i></a>
-																			<a href="<?= site_url("sid_core/form_rt/$id_dusun/$id_rw/$data[id]")?>" class="btn bg-orange btn-flat btn-sm" title="Ubah"><i class="fa fa-edit"></i></a>
-																			<a href="#" data-href="<?= site_url("sid_core/delete/rt/$data[id]")?>" class="btn bg-maroon btn-flat btn-sm" title="Hapus" data-toggle="modal" data-target="#confirm-delete"><i class="fa fa-trash-o"></i></a>
+																		<?php if ($data['rt'] != "-"): ?>
+																			<?php if ($this->CI->cek_hak_akses('u')): ?>
+																				<a href="<?= site_url("sid_core/urut/rt/$paging->page/$data[id]/1/$id_dusun/$id_rw"); ?>" class="btn bg-olive btn-flat btn-sm <?php ($data['no'] == $paging->num_rows) and print('disabled'); ?>" title="Pindah Posisi Ke Bawah"><i class="fa fa-arrow-down"></i></a>
+																				<a href="<?= site_url("sid_core/urut/rt/$paging->page/$data[id]/2/$id_dusun/$id_rw"); ?>" class="btn bg-olive btn-flat btn-sm <?php ($data['no'] == 1 AND $paging->page == $paging->start_link) and print('disabled'); ?>" title="Pindah Posisi Ke Atas"><i class="fa fa-arrow-up"></i></a>
+																				<a href="<?= site_url("sid_core/form_rt/$id_dusun/$id_rw/$data[id]")?>" class="btn bg-orange btn-flat btn-sm" title="Ubah"><i class="fa fa-edit"></i></a>
+																			<?php endif; ?>
+																			<?php if ($this->CI->cek_hak_akses('h')): ?>
+	 																			<a href="#" data-href="<?= site_url("sid_core/delete/rt/$data[id]")?>" class="btn bg-maroon btn-flat btn-sm" title="Hapus" data-toggle="modal" data-target="#confirm-delete"><i class="fa fa-trash-o"></i></a>
+	 																		<?php endif; ?>
 																		<?php endif; ?>
-																		<?php if ($data['rt']!="-"): ?>
+																		<?php if ($data['rt'] != "-"): ?>
 																			<div class="btn-group">
 																				<button type="button" class="btn btn-social btn-flat btn-info btn-sm" data-toggle="dropdown"><i class='fa fa-arrow-circle-down'></i> Peta</button>
 																				<ul class="dropdown-menu" role="menu">
