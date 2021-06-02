a8313744abc3b8cf96adea926e5f720c8be4a7d6
Eddie Ridwan <eddie.ridwan@gmail.com>
2021-06-02 12:11:38 +1000

    Perbaiki versi

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -45,7 +45,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link 	https://github.com/OpenSID/OpenSID
  */
 
-define("VERSION", '21.06-premium');
+define("VERSION", '21.06-premium-beta');
 /**
  * Untuk migrasi database. Simpan nilai ini di tabel migrasi untuk menandakan sudah migrasi ke versi ini
  * Versi database = [yyyymmdd][nomor urut dua digit]. Ubah setiap kali mengubah struktur database.
