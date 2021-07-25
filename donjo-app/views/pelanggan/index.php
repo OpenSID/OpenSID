bd7f841dc1803bc2fc2a82bab71407b1ee429ede
afa28 <andifahruddinakas@gmail.com>
2021-07-25 14:57:46 +0800

    Simpan token pelanggan terbaru

--- donjo-app/views/pelanggan/index.php
+++ donjo-app/views/pelanggan/index.php
@@ -7,71 +7,77 @@
 		</ol>
 	</section>
 	<section class="content">
-		<div class="box box-info">
-			<div class="box-body">
-				<?php if (is_null($response)) : ?>
-					<div class="alert alert-danger alert-dismissible">
-						<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-						<h4><i class="icon fa fa-ban"></i><?= "{$this->session->error_status_langganan}"?></h4>
+		<?php if (is_null($response)) : ?>
+			<div class="box box-danger">
+				<div class="box-header with-border">
+					<i class="icon fa fa-ban"></i>
+					<h3 class="box-title"><?= "{$this->session->error_status_langganan}"?></h3>
+				</div>
+				<div class="box-body">
+					<div class="callout callout-danger">
 						<h5>Data Gagal Dimuat, Harap Periksa Dibawah Ini</h5>
-						<h5>Fitur ini khusus untuk pelanggan Layanan OpenDesa (hosting, Fitur Premium, dll), untuk menampilkan status langganan.</h5>
-						<li>Periksa logs error terakhir di menu <strong>Pengaturan > Info Sistem > Logs</strong></li>
-						<li>Token pelanggan tidak terontentikasi. Periksa [Layanan Opendesa Token] di menu <strong>Pengaturan > Aplikasi</strong></li>
-						<li>Jika masih mengalami masalah harap menghubungi pelaksana masing-masing.</h5>
+						<h5>Fitur ini khusus untuk pelanggan Layanan OpenDesa (hosting, Fitur Premium, dll) untuk menampilkan status langganan.</h5>
+						<li>Periksa logs error terakhir di menu <strong><a href="<?= site_url('setting/info_sistem#log_viewer'); ?>" style="text-decoration:none;">Pengaturan > Info Sistem > Logs</a></strong></li>
+						<li>Token pelanggan tidak terontentikasi. Periksa [Layanan Opendesa Token] di <a href="#" style="text-decoration:none;" data-remote="false" data-toggle="modal" data-title="Pengaturan <?= ucwords($this->controller); ?>" data-target="#pengaturan"><strong>Pengaturan Pelanggan&nbsp;(<i class="fa fa-gear"></i>)</strong></a></li>
+						<li>Jika masih mengalami masalah harap menghubungi pelaksana masing-masing.
 					</div>
-				<?php else : ?>
-					<div class="row">
-						<div class="col-lg-3 col-xs-6">
-							<div class="small-box bg-blue">
-								<div class="inner">
-									<h4>PEMESANAN LAYANAN</h4>
-									<h6>
-										<?php foreach ($response->body->pemesanan as $pemesanan) : ?>
-											<?php foreach ($pemesanan->layanan as $layanan) : ?>
-												<li><?= $layanan->nama ?></li>
-											<?php endforeach ?>
-										<?php endforeach ?>
-									</h6>
-								</div>
-								<div class="icon">
-									<i class="ion ion-card"></i>
-								</div>
-							</div>
+				</div>
+			</div>
+		<?php else : ?>
+			<div class="row">
+				<div class="col-lg-3 col-xs-6">
+					<div class="small-box bg-blue">
+						<div class="inner">
+							<h4>PEMESANAN LAYANAN</h4>
+							<h6>
+								<?php foreach ($response->body->pemesanan as $pemesanan) : ?>
+									<?php foreach ($pemesanan->layanan as $layanan) : ?>
+										<li><?= $layanan->nama ?></li>
+									<?php endforeach ?>
+								<?php endforeach ?>
+							</h6>
 						</div>
-						<div class="col-lg-3 col-xs-6">
-							<div class="small-box bg-yellow">
-								<div class="inner">
-									<h4>STATUS PELANGGAN</h4>
-									<h4><?= $response->body->status_langganan ?></h4>
-								</div>
-								<div class="icon">
-									<i class="ion-person-add"></i>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-3 col-xs-6">
-							<div class="small-box bg-green">
-								<div class="inner">
-									<h4>MULAI BERLANGGANAN</h4>
-									<h4><?= $response->body->tanggal_berlangganan->mulai ?></h4>
-								</div>
-								<div class="icon">
-									<i class="ion ion-unlocked"></i>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-3 col-xs-6">
-							<div class="small-box bg-red">
-								<div class="inner">
-									<h4>AKHIR BERLANGGANAN</h4>
-									<h4><?= $response->body->tanggal_berlangganan->akhir ?></h4>
-								</div>
-								<div class="icon">
-									<i class="ion ion-locked"></i>
-								</div>
-							</div>
+						<div class="icon">
+							<i class="ion ion-card"></i>
 						</div>
 					</div>
+				</div>
+				<div class="col-lg-3 col-xs-6">
+					<div class="small-box bg-yellow">
+						<div class="inner">
+							<h4>STATUS PELANGGAN</h4>
+							<h4><?= $response->body->status_langganan ?></h4>
+						</div>
+						<div class="icon">
+							<i class="ion-person-add"></i>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-xs-6">
+					<div class="small-box bg-green">
+						<div class="inner">
+							<h4>MULAI BERLANGGANAN</h4>
+							<h4><?= $response->body->tanggal_berlangganan->mulai ?></h4>
+						</div>
+						<div class="icon">
+							<i class="ion ion-unlocked"></i>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-xs-6">
+					<div class="small-box bg-red">
+						<div class="inner">
+							<h4>AKHIR BERLANGGANAN</h4>
+							<h4><?= $response->body->tanggal_berlangganan->akhir ?></h4>
+						</div>
+						<div class="icon">
+							<i class="ion ion-locked"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="box box-info">
+				<div class="box-body">
 					<h5 class="text-bold">Rincian Pelanggan Desa</h5>
 					<div class="table-responsive">
 						<table class="table table-bordered table-striped table-hover tabel-rincian">
@@ -105,10 +111,11 @@
 										<?php endforeach ?>
 									</td>
 								</tr>
+								<!-- Token pelangan kayaknya tidak perlu ditampilkan lagi dibagian ini -->
 								<tr>
 									<td>Token Pelanggan</td>
 									<td> : </td>
-									<td><textarea disabled cols="140" rows="4"><?= $this->setting->demo_mode ? '' : $response->body->token ?></textarea></td>
+									<td><textarea disabled style="width:100%; overflow: hidden; background-color: white; border-color: white;"><?= $this->setting->demo_mode ? '' : $response->body->token ?></textarea></td>
 								</tr>
 							</tbody>
 						</table>
@@ -117,22 +124,33 @@
 						<div class="col-sm-12">
 							<h5 class="text-bold">Rincian Pemesanan Layanan</h5>
 							<div class="table-responsive">
-								<table class="table table-bordered dataTable table-hover">
+								<table class="table table-bordered dataTable table-hover tabel-daftar">
 									<thead class="bg-gray">
 										<tr>
 											<th width="20px">No</th>
+											<th>Aksi</th>
 											<th>Nota. Faktur</th>
 											<th>Tanggal Mulai</th>
-											<th>Tanggal Berakhir</th>
-											<th>Status Pemesanan</th>
+											<th>Tanggal Berakhir</th>											
 											<th>Bukti Pembayaran</th>
+											<th>Status Pemesanan</th>
 										</tr>
 									</thead>
 									<tbody>
 										<?php $number = 1 ?>
 										<?php foreach ($response->body->pemesanan as $pemesanan) : ?>
 											<tr>
-												<td><?= $number ?></td>
+												<td class="padat"><?= $number ?></td>
+												<td class="aksi">
+													<?php
+														$host = ENVIRONMENT == 'development'
+															? $this->setting->layanan_opendesa_dev_server
+															: $this->setting->layanan_opendesa_server;
+
+														$token = $this->setting->layanan_opendesa_token;
+													?>
+													<a target="_blank" href="<?= "{$host}/api/v1/pelanggan/pemesanan/faktur?invoice={$pemesanan->faktur}&token={$token}"?>" class="btn btn-social btn-flat bg-purple btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Cetak" data-title="Cetak Laporan"><i class="fa fa-print"></i>Cetak</a>
+												</td>
 												<td>
 													<a href="<?= "#" . str_replace('/', '-', $pemesanan->faktur) ?>" data-toggle="modal" data-target="<?= "#" . str_replace('/', '-', $pemesanan->faktur) ?>"><?= $pemesanan->faktur ?></a>
 													<div class="modal fade" id="<?= str_replace('/', '-', $pemesanan->faktur) ?>" style="display: none;">
@@ -148,12 +166,12 @@
 																	<div class="row">
 																		<div class="col-sm-12">
 																			<div class="table-responsive">
-																				<table class="table table-bordered dataTable table-hover">
+																				<table class="table table-bordered dataTable table-hover tabel-daftar">
 																					<thead class="bg-gray">
 																						<tr>
-																							<th width="20px">No</th>
+																							<th>No</th>
 																							<th>Layanan</th>
-																							<th>Harga (Rp)</th>
+																							<th>Harga</th>
 																							<th>Keterangan</th>
 																						</tr>
 																					</thead>
@@ -161,9 +179,9 @@
 																						<?php $numberLayanan = 1 ?>
 																						<?php foreach ($pemesanan->layanan as $layanan) : ?>
 																							<tr>
-																								<td><?= $numberLayanan ?></td>
-																								<td><?= $layanan->nama ?></td>
-																								<td><?= number_format($layanan->harga, 2, ',', '.') ?></td>
+																								<td class="padat"><?= $numberLayanan ?></td>
+																								<td class="aksi"><?= $layanan->nama ?></td>
+																								<td align="right"><?= rupiah($layanan->harga) ?></td>
 																								<td><?= $layanan->deskripsi ?></td>
 																							</tr>
 																							<?php $numberLayanan++ ?>
@@ -181,11 +199,8 @@
 														</div>
 													</div>
 												</td>
-												<td><?= $pemesanan->tgl_mulai ?></td>
-												<td><?= $pemesanan->tgl_akhir ?></td>
-												<td class="text-center">
-													<span class="label label-<?= $pemesanan->status_pemesanan === 'aktif' ? 'success' : 'danger' ?>"><?= $pemesanan->status_pemesanan ?></span>
-												</td>
+												<td class="padat"><?= $pemesanan->tgl_mulai ?></td>
+												<td class="padat"><?= $pemesanan->tgl_akhir ?></td>
 												<td>
 													<a href="<?= "#{$pemesanan->id}" ?>" data-toggle="modal" data-target="<?= "#{$pemesanan->id}" ?>"><?= parse_url($pemesanan->bukti, PHP_URL_PATH) ?></a>
 													<div class="modal fade" id="<?= $pemesanan->id ?>" style="display: none;">
@@ -201,11 +216,15 @@
 																	<img class="img-thumbnail" src="<?= $pemesanan->bukti ?>" alt="<?= $pemesanan->bukti ?>">
 																</div>
 																<div class="modal-footer">
+																	<a target="_blank" href="<?= $pemesanan->bukti ?>" role="button" class="btn btn-flat btn-sm bg-navy" download="<?= $pemesanan->bukti ?>">Simpan</a>
 																	<button type="button" class="btn btn-flat btn-sm btn-info" data-dismiss="modal">Tutup</button>
 																</div>
 															</div>
 														</div>
 													</div>
+												</td>
+												<td class="padat">
+													<span class="label label-<?= $pemesanan->status_pemesanan === 'aktif' ? 'success' : 'danger' ?>"><?= $pemesanan->status_pemesanan ?></span>
 												</td>
 											</tr>
 											<?php $number++ ?>
@@ -215,8 +234,8 @@
 							</div>
 						</div>
 					</div>
-				<?php endif ?>
+				</div>
 			</div>
-		</div>
+		<?php endif ?>
 	</section>
 </div>
