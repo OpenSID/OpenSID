76374b699f7e525bf5c5bf77462ae36f1cbad40c
unknown <gundambison@gmail.com>
2021-09-18 14:46:05 +0700

    perbaikan wording pin

--- donjo-app/views/kehadiran/form/login_3.php
+++ donjo-app/views/kehadiran/form/login_3.php
@@ -4,7 +4,7 @@
 		<input type="text" class="form-control required  " required name="nik" placeholder="Masukkan NIK" id="nik_keyboard"  onclick2='changeDef("nik_keyboard")' />
 	</div>
 	<div class="form-group form-login">
-		<input type="password" class="form-control required<?= jecho($cek_anjungan['keyboard'] == 1, TRUE, 'kbvnumber'); ?>" name="pin" required placeholder="Masukkan PIN BILA ADA" id="pin_keyboard" onclick2='changeDef("pin_keyboard")' />
+		<input type="password" class="form-control required<?= jecho($cek_anjungan['keyboard'] == 1, TRUE, 'kbvnumber'); ?>" name="pin" required placeholder="Masukkan PIN " id="pin_keyboard" onclick2='changeDef("pin_keyboard")' />
 	</div>
 	<div class="form-group">
 		<center><input type="checkbox" id="checkbox"> Tampilkan PIN</center>
@@ -15,5 +15,8 @@
 </form>
 <p align="center">Silakan hubungi operator <?= $this->setting->sebutan_desa; ?> untuk mendapatkan kode PIN anda.</p>
 <div>  
+	<a href='<?=site_url('kehadiran/masuk');?>?form=2' class="btn btn-block btn-block bg-green">
+	<b>Pindai Kartu e-KTP</b>
+	</a>
 <!-- keyboard -->
 </div>
