e38d4009ef5ff65916303efec0539f60d4b730fb
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-09-28 17:22:23 +0800

    Squashed 'themes/natra/' changes from 36c9816d4..f92cee6d7
    f92cee6d7 perbaikan error console dengan document.ready
    536a464c7 tulis ulang gallery partial. ganti dengan php dalam html
    739c19382 Hapus pengkondisian umum dan premium
    f8c903294 Layanan mandiri
    61e1f1781 Update kemendes_sdgs.php
    be218f1e4 statistik pengunjung rilis umum
    b72a688d2 v4.5
    f1c170f28 Pindahkan Menu Login Pada Akhir Menu
    c07a6eab8 Fix : Mapbox Key
    c1e60b0b0 Sesuaikan tampilan IDM
    c37f8e44a Perbaiki jabatan tambahan tampil juga di web, diurut, huruf besar semua. Juga bersihkan data jabatan sebelum disimpan
    5ce3c9abf Tampilkan icon SDGs
    400169310 Merge commit '21540178f228f615095a3c925d2db0bda79899b5' into rilis-dev
    d7fef1d9b Gunakan layouts/halaman_statis_lebar.tpl.php untuk SDGs. Hapus layout khusus SDGs.
    667ab378b seusuaikan format aturan penulisan
    f1dd3b9be -
    93a7a0ab4 Merge pull request #80 from OpenSID/witget-statistik-pengunjung
    22dcad844 #3352: Perbaiki pencatatan data pengunjung web. [bug-fix]
    e7acb52fb #3352 - Refraktor statistik pengunjung
    a51c56d9d Merge pull request #76 from OpenSID/susulan-issue-74
    070237e67 Perbaiki tema natra tampilkan halaman suplemen
    107c8977f #72 - Susulan perbaikan issue kurang penutup div
    167d7dda9 Merge pull request #75 from agungsugiarto/isssue-74
    442c381b6 #74 - Link Daftar Analisis Desa Tidak Tampil
    573ca1473 Kembalikan Slider Widget Aparatur Desa
    4f515af4f Sesuaikan Tampilan Halaman Kelompok
    aa5c33759 #3992: Perbaiki beberapa error javascript di tema natra. [bug]
    
    git-subtree-dir: themes/natra
    git-subtree-split: f92cee6d7a4ba17220aeb4f81fde6f969ff80e03

---
+++ layouts/kelompok.tpl.php
@@ -0,0 +1,43 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="https://www.facebook.com/2008/fbml">
+<head>
+	<?php $this->load->view("$folder_themes/commons/meta.php"); ?>
+</head>
+<body>
+<!--
+<div id="preloader">
+		<div id="status">&nbsp;</div>
+</div>
+-->
+<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+<div class="container" style="background-color: #f6f6f6;">
+	<header id="header">
+		<?php $this->load->view("$folder_themes/partials/header.php"); ?>
+	</header>
+	<div id="navarea">
+		<?php $this->load->view("$folder_themes/partials/menu_head.php"); ?>
+	</div>
+	<div class="row">
+		<section>
+			<div class="content_middle"></div>
+			<div class="content_bottom">
+				<div class="col-lg-9 col-md-9">
+					<div class="content_bottom_left">
+					<?php $this->load->view("$folder_themes/partials/kelompok.php"); ?>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<?php $this->load->view("$folder_themes/partials/bottom_content_right.php"); ?>
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+<footer id="footer">
+	<?php $this->load->view("$folder_themes/partials/footer_top.php"); ?>
+	<?php $this->load->view("$folder_themes/partials/footer_bottom.php"); ?>
+</footer>
+<?php $this->load->view("$folder_themes/commons/meta_footer.php"); ?>
+</body>
+</html>
