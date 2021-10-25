4ed9dd47cd0a8e9f7d737e9f9c15c9e4e12de04c
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-10-25 15:19:08 +0800

    Naikkan versi

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -51,7 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * beta => premium-beta[nomor urut dua digit]
  * [nomor urut dua digit] : minggu 1 => 01, dst
  */
-define("VERSION", '21.10-premium-rev02');
+define("VERSION", '21.10-premium-rev03');
 /**
  * VERSI_DATABASE
  * Ubah setiap kali mengubah struktur database atau melakukan proses rilis (tgl 01)
@@ -59,7 +59,7 @@ define("VERSION", '21.10-premium-rev02');
  * Versi database = [yyyymmdd][nomor urut dua digit]
  * [nomor urut dua digit] : 01 => rilis umum, 51 => rilis bugfix, 71 => rilis premium,
  */
-define('VERSI_DATABASE', '2021101951');
+define('VERSI_DATABASE', '2021102551');
 define("LOKASI_LOGO_DESA", 'desa/logo/');
 define("LOKASI_ARSIP", 'desa/arsip/');
 define("LOKASI_CONFIG_DESA", 'desa/config/');
