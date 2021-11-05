90a7b4af8d7e7edda1c70c06abeb856145ef6cc0
faisyalrachman <faisal.paguntaka@gmail.com>
2021-11-05 21:49:10 +0800

    penambahan menu kader berdaya

--- donjo-app/views/bumindes/pembangunan/side.php
+++ donjo-app/views/bumindes/pembangunan/side.php
@@ -3,6 +3,7 @@
     <ul class="nav nav-stacked">
       <li class="<?php compared_return($selected_nav, "tanah", "active"); ?>"><a href="<?= site_url('bumindes_pembangunan/tables/tanah') ?>">Buku Tanah di Desa</a></li>
       <li class="<?php compared_return($selected_nav, "tanah_kas", "active"); ?>"><a href="<?= site_url('bumindes_pembangunan/tables/tanah_kas') ?>">Buku Tanah Kas Desa</a></li>
+      <li class="<?php compared_return($selected_nav, "kader_pemberdayaan", "active"); ?>"><a href="<?= site_url('bumindes_pembangunan/tables/kader_pemberdayaan') ?>">Buku Kader Pemberdayaan</a></li>
     </ul>
   </div>
 </div>
