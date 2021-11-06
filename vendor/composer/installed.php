8938a0209222f0dc4ffba5bbf8ad974cec64c214
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-11-06 22:09:18 +0800

    Composer setup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-new-composer',
+        'version' => 'dev-new-composer',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4556a4589ba8ef040cd5d991e4ff953b863d19f6',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-new-composer',
+            'version' => 'dev-new-composer',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4556a4589ba8ef040cd5d991e4ff953b863d19f6',
+            'dev_requirement' => false,
+        ),
+        'mikey179/vfsstream' => array(
+            'pretty_version' => 'v1.1.0',
+            'version' => '1.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mikey179/vfsstream',
+            'aliases' => array(),
+            'reference' => 'fc0fe8f4d0b527254a2dc45f0c265567c881d07e',
+            'dev_requirement' => true,
+        ),
+    ),
+);
