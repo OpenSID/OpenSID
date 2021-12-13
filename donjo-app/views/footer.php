e4a3383447fb3d8fbf57c41047e5a5d01f785a3b
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-12-14 07:38:27 +0800

    Infromasi tentang website demo

--- donjo-app/views/footer.php
+++ donjo-app/views/footer.php
@@ -1,9 +1,10 @@
-			<footer class="main-footer">
-				<div class="pull-right hidden-xs">
-			  	<b>Versi</b> <?= AmbilVersi()?>
-				</div>
-				<strong>Aplikasi <a href="https://github.com/OpenSID/OpenSID" target="_blank"> OpenSID</a>, dikembangkan oleh <a href="https://www.facebook.com/groups/OpenSID/" target="_blank">Komunitas OpenSID</a>.</strong>
-			</footer>
+				<footer class="main-footer">
+					<div class="pull-right hidden-xs">
+						<b>Versi</b> <?= AmbilVersi()?>
+					</div>
+					<strong>Aplikasi <a href="https://github.com/OpenSID/OpenSID" target="_blank"> OpenSID</a>, dikembangkan oleh <a href="https://www.facebook.com/groups/OpenSID/" target="_blank">Komunitas OpenSID</a>.</strong>
+				</footer>
+			</div>
 		</div>
 
 		<!-- jQuery 3 -->
@@ -46,48 +47,37 @@
 		<script src="<?= base_url('assets/js/custom-select2.js'); ?>"></script>
 		<script src="<?= base_url('assets/js/custom-datetimepicker.js'); ?>"></script>
 
+		<?php if (config_item('demo_mode')): ?>
+			<script src="<?= base_url('assets/js/demo.js'); ?>"></script>
+		<?php endif; ?>
+
+
 		<!-- NOTIFICATION-->
 		<script type="text/javascript">
 
-			$('document').ready(function()
-			{
-				if ($('#success-code').val() == 1)
-				{
+			$('document').ready(function() {
+				if ($('#success-code').val() == 1) {
 					notify = 'success';
 					notify_msg = 'Data berhasil disimpan';
-				}
-				else if ($('#success-code').val() == -1)
-				{
+				} else if ($('#success-code').val() == -1) {
 					notify = 'error';
-					notify_msg = 'Data gagal disimpan <?= $_SESSION["error_msg"]?>';
-				}
-				else if ($('#success-code').val() == -2)
-				{
+					notify_msg = 'Data gagal disimpan <?= addslashes($this->session->error_msg) ?>';
+				} else if ($('#success-code').val() == -2) {
 					notify = 'error';
 					notify_msg = 'Data gagal disimpan, nama id sudah ada!';
-				}
-				else if ($('#success-code').val() == -3)
-				{
+				} else if ($('#success-code').val() == -3) {
 					notify = 'error';
 					notify_msg = 'Data gagal disimpan, nama id sudah ada!';
-				}
-				else if ($('#success-code').val() == 4)
-				{
+				} else if ($('#success-code').val() == 4) {
 					notify = 'success';
 					notify_msg = 'Data berhasil dihapus';
-				}
-				else if ($('#success-code').val() == -4)
-				{
+				} else if ($('#success-code').val() == -4) {
 					notify = 'error';
 					notify_msg = 'Data gagal dihapus';
-				}
-				else if ($('#success-code').val() == 5)
-				{
+				} else if ($('#success-code').val() == 5) {
 					notify = 'success';
 					notify_msg = 'Data berhasil diunggah';
-				}
-				else
-				{
+				} else {
 					notify = '';
 					notify_msg = '';
 				}
@@ -95,7 +85,7 @@
 				$('#success-code').val('');
 			});
 		</script>
-		<?php $_SESSION['success']=0; ?>
+		<?php $_SESSION['success'] = 0; ?>
 	</body>
 </html>
 
