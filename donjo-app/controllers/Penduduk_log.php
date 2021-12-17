2ada47e32d29af8aebd50d4356769543abcad2e7
andifahruddinakas <andifahruddinakas@users.noreply.github.com>
2021-12-17 07:24:39 +0000

    [ci skip] Fix styling

--- donjo-app/controllers/Penduduk_log.php
+++ donjo-app/controllers/Penduduk_log.php
@@ -113,7 +113,7 @@ class Penduduk_log extends Admin_Controller
         $data['list_sex']             = $this->referensi_model->list_data('tweb_penduduk_sex');
         $data['list_agama']           = $this->referensi_model->list_data('tweb_penduduk_agama');
         $data['list_dusun']           = $this->wilayah_model->list_dusun();
-        
+
         $this->render('penduduk_log/penduduk_log', $data);
     }
 
