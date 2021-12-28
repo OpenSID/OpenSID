f7121679dad875d226f3db417f3063934b88fa4d
Toto Prayogo <mail@totoprayogo.com>
2021-12-28 19:23:54 +0700

    cs-fixer

--- donjo-app/models/Web_sosmed_model.php
+++ donjo-app/models/Web_sosmed_model.php
@@ -1,7 +1,41 @@
 <?php
 
-class Web_sosmed_model extends CI_Model {
+/*
+ * File ini bagian dari:
+ *
+ * OpenSID
+ *
+ * Sistem informasi desa sumber terbuka untuk memajukan desa
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * Hak Cipta 2016 - 2021 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+ *
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+ *
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package    OpenSID
+ * @author     Tim Pengembang OpenDesa
+ * @copyright  Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * @copyright  Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license    http://www.gnu.org/licenses/gpl.html	GPL V3
+ *
+ * @link       https://github.com/OpenSID/OpenSID
+ */
 
+class Web_sosmed_model extends CI_Model
+{
 	public function __construct()
 	{
 		parent::__construct();
@@ -11,27 +45,24 @@ class Web_sosmed_model extends CI_Model {
 	{
 		$id = $this->get_id($sosmed);
 
-		$data = $this->db->where('id', $id)->get('media_sosial')->row_array();
-
-		return $data;
+		return $this->db->where('id', $id)->get('media_sosial')->row_array();
 	}
 
 	public function list_sosmed()
 	{
-		$data = $this->db->get('media_sosial')->result_array();
-
-		return $data;
+		return $this->db->get('media_sosial')->result_array();
 	}
 
 	public function get_id($sosmed)
 	{
 		$list_sosmed = $this->list_sosmed();
 
-		foreach ($list_sosmed as $list)
-		{
+		foreach ($list_sosmed as $list) {
 			$nama = str_replace(' ', '-', strtolower($list['nama']));
 
-			if($nama == $sosmed) return $list['id'];
+			if ($nama === $sosmed) {
+				return $list['id'];
+			}
 		}
 	}
 
@@ -42,8 +73,7 @@ class Web_sosmed_model extends CI_Model {
 		$data = $this->input->post();
 		$link = trim(strip_tags($this->input->post('link')));
 
-		switch ($id)
-		{
+		switch ($id) {
 			case '6':
 				$data['link'] = preg_replace('/[^A-Za-z0-9]/', '', $link);
 				break;
@@ -66,43 +96,44 @@ class Web_sosmed_model extends CI_Model {
 	// Penanganan khusus sesuai jenis sosmed
 	public function link_sosmed($id = 0, $link = '', $tipe = 1)
 	{
-		if (empty($link)) return $link;
+		if (empty($link)) {
+			return $link;
+		}
 
-		switch (true)
-		{
-			case ($id == 1 && $tipe == 1) :
+		switch (true) {
+			case $id === 1 && $tipe === 1 :
 				$link = 'https://web.facebook.com/' . $link;
 				break;
 
-			case ($id == 1 && $tipe == 2) :
+			case $id === 1 && $tipe === 2 :
 				$link = 'https://web.facebook.com/groups/' . $link;
 				break;
 
-			case ($id == 2) :
+			case $id === 2 :
 				$link = 'https://twitter.com/' . $link;
 				break;
 
-			case ($id == 4) :
+			case $id === 4 :
 				$link = 'https://www.youtube.com/channel/' . $link;
 				break;
 
-			case ($id == 5) :
+			case $id === 5 :
 				$link = 'https://www.instagram.com/' . $link . '/';
 				break;
 
-			case ($id == 6 && $tipe == 1) :
+			case $id === 6 && $tipe === 1 :
 				$link = 'https://api.whatsapp.com/send?phone=' . $link;
 				break;
 
-			case ($id == 6 && $tipe == 2) :
+			case $id === 6 && $tipe === 2 :
 				$link = 'https://chat.whatsapp.com/' . $link;
 				break;
 
-			case ($id == 7 && $tipe == 1) :
+			case $id === 7 && $tipe === 1 :
 				$link = 'https://t.me/' . $link;
 				break;
 
-			case ($id == 7 && $tipe == 2) :
+			case $id === 7 && $tipe === 2 :
 				$link = 'https://t.me/joinchat/' . $link;
 				break;
 
@@ -112,6 +143,4 @@ class Web_sosmed_model extends CI_Model {
 
 		return $link;
 	}
-
 }
-?>
