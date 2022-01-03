85be13842cefb693dc061f9e55e50b7efec725f1
Toto Prayogo <mail@totoprayogo.com>
2022-01-03 12:03:05 +0700

    perbaikan sosmed

--- donjo-app/models/Web_sosmed_model.php
+++ donjo-app/models/Web_sosmed_model.php
@@ -1,7 +1,41 @@
 <?php
 
-class Web_sosmed_model extends CI_Model {
+/*
+ * File ini bagian dari:
+ *
+ * OpenSID
+ *
+ * Sistem informasi desa sumber terbuka untuk memajukan desa
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * Hak Cipta 2016 - 2022 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+ *
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+ *
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package    OpenSID
+ * @author     Tim Pengembang OpenDesa
+ * @copyright  Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * @copyright  Hak Cipta 2016 - 2022 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license    http://www.gnu.org/licenses/gpl.html	GPL V3
+ *
+ * @link       https://github.com/OpenSID/OpenSID
+ */
 
+class Web_sosmed_model extends CI_Model
+{
 	public function __construct()
 	{
 		parent::__construct();
@@ -11,27 +45,24 @@ class Web_sosmed_model extends CI_Model {
 	{
 		$id = $this->get_id($sosmed);
 
-		$data = $this->db->where('id', $id)->get('media_sosial')->row_array();
-
-		return $data;
+		return $this->db->where('id', $id)->get('media_sosial')->row_array();
 	}
 
 	public function list_sosmed()
 	{
-		$data = $this->db->get('media_sosial')->result_array();
-
-		return $data;
+		return $this->db->get('media_sosial')->result_array();
 	}
 
 	public function get_id($sosmed)
 	{
 		$list_sosmed = $this->list_sosmed();
 
-		foreach ($list_sosmed as $list)
-		{
+		foreach ($list_sosmed as $list) {
 			$nama = str_replace(' ', '-', strtolower($list['nama']));
 
-			if($nama == $sosmed) return $list['id'];
+			if ($nama === $sosmed) {
+				return $list['id'];
+			}
 		}
 	}
 
@@ -42,20 +73,7 @@ class Web_sosmed_model extends CI_Model {
 		$data = $this->input->post();
 		$link = trim(strip_tags($this->input->post('link')));
 
-		switch ($id)
-		{
-			case '6':
-				$data['link'] = preg_replace('/[^A-Za-z0-9]/', '', $link);
-				break;
-
-			case '7':
-				$data['link'] = preg_replace('/[^A-Za-z0-9_]/', '', $link);
-				break;
-
-			default:
-				$data['link'] = $link;
-				break;
-		}
+		$data['link'] = $link;
 
 		$this->db->where('id', $id);
 		$outp = $this->db->update('media_sosial', $data);
@@ -66,44 +84,54 @@ class Web_sosmed_model extends CI_Model {
 	// Penanganan khusus sesuai jenis sosmed
 	public function link_sosmed($id = 0, $link = '', $tipe = 1)
 	{
-		if (empty($link)) return $link;
+		if (empty($link)) {
+			return $link;
+		}
 
-		switch (true)
-		{
-			case ($id == 1 && $tipe == 1) :
-				$link = 'https://web.facebook.com/' . $link;
+		// Remove all illegal characters from a url
+		$link = filter_var($link, FILTER_SANITIZE_URL);
+
+		// remove `@` with ''
+		$link = str_replace('@', '', $link);
+
+		// validasi link
+		$valid_link = filter_var($link, FILTER_VALIDATE_URL);
+
+		switch (true) {
+			case $id === 1 && $tipe === 1 :
+				$link = ($valid_link !== false ? $valid_link : 'https://web.facebook.com/' . $link);
 				break;
 
-			case ($id == 1 && $tipe == 2) :
-				$link = 'https://web.facebook.com/groups/' . $link;
+			case $id === 1 && $tipe === 2 :
+				$link = ($valid_link !== false ? $valid_link : 'https://web.facebook.com/groups/' . $link);
 				break;
 
-			case ($id == 2) :
-				$link = 'https://twitter.com/' . $link;
+			case $id === 2 :
+				$link = ($valid_link !== false ? $valid_link : 'https://twitter.com/' . $link);
 				break;
 
-			case ($id == 4) :
-				$link = 'https://www.youtube.com/channel/' . $link;
+			case $id === 4 :
+				$link = ($valid_link !== false ? $valid_link : 'https://www.youtube.com/channel/' . $link);
 				break;
 
-			case ($id == 5) :
-				$link = 'https://www.instagram.com/' . $link . '/';
+			case $id === 5 :
+				$link = ($valid_link !== false ? $valid_link : 'https://www.instagram.com/' . $link . '/');
 				break;
 
-			case ($id == 6 && $tipe == 1) :
-				$link = 'https://api.whatsapp.com/send?phone=' . $link;
+			case $id === 6 && $tipe === 1 :
+				$link = ($valid_link !== false ? $valid_link : 'https://api.whatsapp.com/send?phone=' . $link);
 				break;
 
-			case ($id == 6 && $tipe == 2) :
-				$link = 'https://chat.whatsapp.com/' . $link;
+			case $id === 6 && $tipe === 2 :
+				$link = ($valid_link !== false ? $valid_link : 'https://chat.whatsapp.com/' . $link);
 				break;
 
-			case ($id == 7 && $tipe == 1) :
-				$link = 'https://t.me/' . $link;
+			case $id === 7 && $tipe === 1 :
+				$link = ($valid_link !== false ? $valid_link : 'https://t.me/' . $link);
 				break;
 
-			case ($id == 7 && $tipe == 2) :
-				$link = 'https://t.me/joinchat/' . $link;
+			case $id === 7 && $tipe === 2 :
+				$link = ($valid_link !== false ? $valid_link : 'https://t.me/joinchat/' . $link);
 				break;
 
 			default:
@@ -112,6 +140,4 @@ class Web_sosmed_model extends CI_Model {
 
 		return $link;
 	}
-
 }
-?>
--- donjo-app/views/sosmed/sosmed.php
+++ donjo-app/views/sosmed/sosmed.php
@@ -1,48 +1,3 @@
-<?php
-/**
- * File ini:
- *
- * View untuk modul Media Sosial
- *
- * sosmed.php
- *
- */
-
-/**
- *
- * File ini bagian dari:
- *
- * OpenSID
- *
- * Sistem informasi desa sumber terbuka untuk memajukan desa
- *
- * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
- *
- * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
- * Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
- *
- * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
- * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
- * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
- * asal tunduk pada syarat berikut:
-
- * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
- * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
- * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
-
- * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
- * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
- * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
- *
- * @package	OpenSID
- * @author	Tim Pengembang OpenDesa
- * @copyright	Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
- * @copyright	Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
- * @license	http://www.gnu.org/licenses/gpl.html	GPL V3
- * @link 	https://github.com/OpenSID/OpenSID
- */
-?>
-
 <div class="content-wrapper">
 	<section class="content-header">
 		<h1>Pengaturan <?= str_replace('-', ' ', ucwords($media))?></h1>
@@ -66,15 +21,15 @@
 							<ul class="nav nav-pills nav-stacked">
 								<?php foreach ($list_sosmed as $list) :?>
 									<?php $nama = str_replace(' ', '-', strtolower($list['nama']))?>
-									<li class="<?php ($media === $nama) and print('active')?>"><a href="<?= site_url("sosmed/tab/$nama")?>"><i class="fa fa-<?= $nama?>"></i> <?= $list['nama']?></a></li>
-								<?php endforeach;?>
+									<li class="<?php ($media === $nama) && print 'active'?>"><a href="<?= site_url("sosmed/tab/{$nama}")?>"><i class="fa fa-<?= $nama?>"></i> <?= $list['nama']?></a></li>
+								<?php endforeach; ?>
 							</ul>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-9">
 					<div class="box box-info">
-						<?php $this->load->view('sosmed/'.$media); ?>
+						<?php $this->load->view('sosmed/' . $media); ?>
 						<div class='box-footer'>
 							<div class='col-xs-12'>
 								<button type='reset' class='btn btn-social btn-flat btn-danger btn-sm reset' onclick="reset_form($(this).val());"><i class='fa fa-times'></i> Batal</button>
@@ -93,57 +48,67 @@
 	$('document').ready(function()
 	{
 		$('#tipe').change();
-	});
+	})
 
 	<?php if ($media === 'facebook'): ?>
 		function ubah_pesan(tipe)
 		{
-			if (tipe == 1)
+			if (tipe == '1')
 			{
-				$('#link').attr('placeholder', ' Personal / Halaman, contoh : https://web.facebook.com/tokoopendesa \n\n Isi kolom ini dengan username : tokoopendesa');
+				$('#link').attr('placeholder', 'tokoopendesa');
+				$('#ex_facebook').text('Contoh : https://web.facebook.com/tokoopendesa atau tokoopendesa');
 			}
 			else
 			{
-				$('#link').attr('placeholder', ' Group, contoh : https://web.facebook.com/groups/opensid \n\n Isi kolom ini dengan username : opensid');
+				$('#link').attr('placeholder', 'opensid');
+				$('#ex_facebook').text('Contoh : https://web.facebook.com/groups/opensid atau opensid');
 			}
-		};
+		}
 	<?php endif ?>
 
 	<?php if ($media === 'whatsapp'): ?>
 		function ubah_pesan(tipe)
 		{
-			if (tipe == 1)
+			if (tipe == '1')
 			{
-				$('#link').attr('placeholder', ' Personal chat, contoh : 0851234567890 (Nomor HP)) \n\n Isi kolom ini dengan nomor HP : 0851234567890');
+				$('#link').attr('pattern', '(((https?:\\\/\\\/)?api\\.whatsapp\\.com\\\/send(?:\\\/|)[?&]+(\\w+)+=([^&]+).*)|\\d+)');
+				$('#link').attr('placeholder', '0851234567890');
+				$('#ex_whatsapp').text('Contoh : 0851234567890 (Nomor HP saja)) atau https://api.whatsapp.com/send?phone=62851234567890');
 			}
 			else
 			{
-				$('#link').attr('placeholder', ' Group chat, contoh : https://chat.whatsapp.com/CryQ1VyOXghEVJUTFpwFPb \n\n Isi kolom ini dengan id chat : CryQ1VyOXghEVJUTFpwFPb');
+				$('#link').attr('pattern', '^((https?:\\\/\\\/)?chat\\.whatsapp\\.com\\\/(?:invite\\\/)?([a-zA-Z0-9_-]{22})|([a-zA-Z0-9_-]{22}))');
+				$('#link').attr('placeholder', 'CryQ1VyOXghEVJUTFpwFPb');
+				$('#ex_whatsapp').text('Contoh : https://chat.whatsapp.com/CryQ1VyOXghEVJUTFpwFPb atau CryQ1VyOXghEVJUTFpwFPb');
 			}
-		};
+		}
 	<?php endif ?>
 
 	<?php if ($media === 'telegram'): ?>
 		function ubah_pesan(tipe)
 		{
-			if (tipe == 1)
+			if (tipe == '1')
 			{
-				$('#link').attr('placeholder', ' Personal chat, contoh  : https://t.me/OpenDesa \n\n Isi kolom ini dengan username : OpenDesa');
+				$('#link').attr('placeholder', 'OpenDesa');
+				$('#link').attr('pattern', '^(?:|(https?:\\\/\\\/)?(|www)[.]?((t|telegram)\\.me)\\\/)[a-zA-Z0-9_]{5,32}$');
+				$('#ex_telegram').text('Contoh : https://t.me/OpenDesa atau OpenDesa');
 			}
 			else
 			{
-				$('#link').attr('placeholder', ' Group chat, contoh  : https://t.me/joinchat/I5antRHvea8ohaU7_RsYYQ \n\n Isi kolom ini dengan id chat : I5antRHvea8ohaU7_RsYYQ');
+				$('#link').attr('placeholder', 'I5antRHvea8ohaU7_RsYYQ');
+				$('#link').attr('pattern', '^(?:|(https?:\\\/\\\/)?(|www)[.]?((t|telegram)\\.me)\\\/joinchat\\\/)[a-zA-Z0-9_]{5,32}$');
+				$('#ex_telegram').text('Contoh : https://t.me/joinchat/I5antRHvea8ohaU7_RsYYQ atau I5antRHvea8ohaU7_RsYYQ');
 			}
-		};
+		}
 	<?php endif ?>
 
 	function reset_form()
 	{
-		<?php if ($main['enabled'] == '1'): ?>
+		<?php if ($main['enabled'] === '1'): ?>
 			$("#sx3").addClass('active');
 			$("#sx4").removeClass("active");
 		<?php endif ?>
-		<?php if ($main['enabled'] == '2'): ?>
+		<?php if ($main['enabled'] === '2'): ?>
 			$("#sx4").addClass('active');
 			$("#sx3").removeClass("active");
 		<?php endif ?>
