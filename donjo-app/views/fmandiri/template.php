ab1e6f0078e163f317861b8a0057be83db13840b
Firliani Fauziah <firlianif@gmail.com>
2022-01-04 11:20:10 +0700

    u

--- donjo-app/views/fmandiri/template.php
+++ donjo-app/views/fmandiri/template.php
@@ -95,6 +95,8 @@ defined('BASEPATH') || exit('No direct script access allowed');
 	<link rel="stylesheet" href="<?= base_url() ?>assets/front/css/jquery-confirm.min.css">
 	<!-- Jquery UI -->
 	<link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/css/jquery-ui.min.css">
+	<!-- jQuery 3 -->
+	<script src="<?= base_url() ?>assets/bootstrap/js/jquery.min.js"></script>
 	<!-- Diperlukan untuk global automatic base_url oleh external js file -->
 	<script type="text/javascript">
 		const BASE_URL = "<?= base_url(); ?>";
@@ -290,8 +292,6 @@ defined('BASEPATH') || exit('No direct script access allowed');
 			</div>
 		</footer>
 	</div>
-	<!-- jQuery 3 -->
-	<script src="<?= base_url() ?>assets/bootstrap/js/jquery.min.js"></script>
 	<!-- Jquery UI -->
 	<script src="<?= base_url() ?>assets/bootstrap/js/jquery-ui.min.js"></script>
 	<script src="<?= base_url() ?>assets/bootstrap/js/jquery.ui.autocomplete.scroll.min.js"></script>
