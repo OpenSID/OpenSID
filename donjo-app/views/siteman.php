38f1a69b9b263e6cdc85be85b1d01a0d3fb70fb4
Toto Prayogo <mail@totoprayogo.com>
2022-01-07 21:09:10 +0700

    pindahkan `script` kedalam `body` ..
    & reformat

--- donjo-app/views/siteman.php
+++ donjo-app/views/siteman.php
@@ -137,44 +137,41 @@
 				</div>
 			</div>
 		</div>
+
+		<script>
+			function start_countdown() {
+				var times = eval(<?= json_encode($this->session->siteman_timeout) ?>) - eval(<?= json_encode(time()) ?>);
+				var menit = Math.floor(times / 60);
+				var detik = times % 60;
+				timer = setInterval(function() {
+					detik--;
+					if (detik <= 0 && menit >= 1) {
+						detik = 60;
+						menit--;
+					}
+					if (menit <= 0 && detik <= 0) {
+						clearInterval(timer);
+						location.reload();
+					} else {
+						document.getElementById("countdown").innerHTML = "<b>Gagal 3 kali silakan coba kembali dalam " + menit + " MENIT " + detik + " DETIK </b>";
+					}
+				}, 1000)
+			}
+
+			$('document').ready(function() {
+				var pass = $("#password");
+				$('#checkbox').click(function() {
+					if (pass.attr('type') === "password") {
+						pass.attr('type', 'text');
+					} else {
+						pass.attr('type', 'password')
+					}
+				});
+
+				if ($('#countdown').length) {
+					start_countdown();
+				}
+			});
+		</script>
 	</body>
 </html>
-<script>
-
-	function start_countdown(){
-		var times = eval(<?= json_encode($this->session->siteman_timeout)?>) - eval(<?= json_encode(time())?>);
-		var menit = Math.floor(times / 60);
-		var detik = times % 60;
-		timer = setInterval(function(){
-			detik--;
-			if (detik <= 0 && menit >=1){
-				detik = 60;
-				menit--;
-			}
-			if (menit <= 0 && detik <= 0){
-				clearInterval(timer);
-				location.reload();
-			} else {
-				document.getElementById("countdown").innerHTML = "<b>Gagal 3 kali silakan coba kembali dalam "+menit+" MENIT "+detik+" DETIK </b>";
-			}
-		}, 1000)
-	}
-
-	$('document').ready(function()
-	{
-		var pass = $("#password");
-		$('#checkbox').click(function(){
-			if (pass.attr('type') === "password"){
-				pass.attr('type', 'text');
-			} else {
-				pass.attr('type', 'password')
-			}
-		});
-
-		if ($('#countdown').length)
-		{
-			start_countdown();
-		}
-	});
-
-</script>
