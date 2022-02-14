1d911ca792a05d643a946281e060e7a62ebec853
Agung Sugiarto <me.agungsugiarto@gmail.com>
2022-02-14 16:54:00 +0700

    Perbaiki mode production, tidak menampilkan pesan errornya. (#740)
    * Jangan tampilkan error query di mode production
    
    * [ci skip] Fix styling
    
    * Strip tags message query
    
    * mutakhirkan catatan rilis
    
    Co-authored-by: agungsugiarto <agungsugiarto@users.noreply.github.com>
    Co-authored-by: sarwono <scqolbu@gmail.com>

--- donjo-app/views/errors/html/error_db.php
+++ donjo-app/views/errors/html/error_db.php
@@ -8,39 +8,34 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Database Error</title>
-	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url()?>assets/bootstrap/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url()?>assets/css/font-awesome.min.css" />
-	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url()?>assets/css/AdminLTE.css" />
+	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url() ?>assets/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url() ?>assets/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url() ?>assets/css/AdminLTE.css" />
 </head>
+
 <body>
-<div class="container">
-	<div class="error-page">
-		<h2 class="headline text-danger"> 500</h2>
+	<div class="container">
+		<div class="error-page">
+			<h2 class="headline text-danger"> 500</h2>
 
-		<div class="error-content">
-			<h3><i class="fa fa-warning text-danger"></i> <?= strip_tags($heading); ?></h3>
-			<?php
-               error_log(strip_tags($message));
-            ?>
-			<p>
-				<?= $message; ?>
+			<div class="error-content">
+				<h3><i class="fa fa-warning text-danger"></i> <?= strip_tags($heading); ?></h3>
+				<p>
+					Versi OpenSID <?= AmbilVersi() ?>. PHP versi <?= preg_split('/-/', PHP_VERSION)[0] ?>.<br><br>
 
-				Versi OpenSID <?= AmbilVersi()?>. PHP versi <?= preg_split('/-/', PHP_VERSION)[0]?>.<br><br>
-
-				Harap laporkan masalah ini, agar kami dapat mencarikan solusinya.
-				Untuk sementara Anda dapat kembali ke halaman <a href="<?= APP_URL ?>">awal</a>.
-			</p>
+					Harap laporkan masalah ini, agar kami dapat mencarikan solusinya.
+					Untuk sementara Anda dapat kembali ke halaman <a href="<?= APP_URL ?>">awal</a>.
+				</p>
+			</div>
+			<?php if (ENVIRONMENT == 'development') : ?>
+				<pre><?= $message ?></pre>
+				<pre><?= strip_tags((new \Exception())->getTraceAsString()) ?></pre>
+			<?php endif ?>
 		</div>
-		<?php
-            // Tampilkan stack trace
-            if (ENVIRONMENT == 'development') {
-                $e = new \Exception();
-                var_dump($e->getTraceAsString());
-            }
-        ?>
 	</div>
-</div>
 </body>
+
 </html>
