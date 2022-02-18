bdfaa5fbc7683cd29acc9d9e2071f9016912d78b
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-02-18 13:53:23 +0800

    Penyesuaian commit a8be719d07cd4abc667f7df61e4d1e6b8bd64a84

--- donjo-app/views/keuangan/tabel_laporan_rp_apbd.php
+++ donjo-app/views/keuangan/tabel_laporan_rp_apbd.php
@@ -13,13 +13,11 @@
 		</table>
 
 		<?php
-
-            if ($this->session->submenu == 'Laporan Keuangan Akhir') {
-                $this->load->view('keuangan/tabel_laporan_rp_apbd_isi');
-            } elseif ($this->session->submenu == 'Laporan Keuangan Akhir Bidang') {
+						if ($this->session->submenu == 'Laporan Keuangan Akhir Bidang') {
                 $this->load->view('keuangan/tabel_laporan_rp_apbd_isi', ['jenis' => 'bidang']);
-            }
-
+            } else {
+                $this->load->view('keuangan/tabel_laporan_rp_apbd_isi');
+            } 
         ?>
 
 	</div>
