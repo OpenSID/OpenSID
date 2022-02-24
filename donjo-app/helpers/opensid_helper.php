acebba7aefaeb7f0fa15fdbbcf0efa3b88bcf39f
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-02-24 09:21:38 +0800

    beta4

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -43,7 +43,7 @@ defined('BASEPATH') || exit('No direct script access allowed');
  * beta => premium-beta[nomor urut dua digit]
  * [nomor urut dua digit] : minggu 1 => 01, dst
  */
-define('VERSION', '22.02-premium-beta03');
+define('VERSION', '22.02-premium-beta04');
 /**
  * VERSI_DATABASE
  * Ubah setiap kali mengubah struktur database atau melakukan proses rilis (tgl 01)
