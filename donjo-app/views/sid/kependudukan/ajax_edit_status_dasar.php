cf97741fc35e00c5ea35f3449a41fcd882f66ddf
sarwono <scqolbu@gmail.com>
2022-02-25 09:17:26 +0700

    keterangan select yang merangkan kematian

--- donjo-app/views/sid/kependudukan/ajax_edit_status_dasar.php
+++ donjo-app/views/sid/kependudukan/ajax_edit_status_dasar.php
@@ -1,12 +1,12 @@
-<?php if ($this->CI->cek_hak_akses('u')): ?>
-<?php $this->load->view('global/validasi_form'); ?>
+<?php if ($this->CI->cek_hak_akses('u')) : ?>
+	<?php $this->load->view('global/validasi_form'); ?>
 	<?php
-        if ($log_status_dasar['tgl_peristiwa'] != ''):
-            $sekarang = $log_status_dasar['tgl_peristiwa'];
-        else:
-            $sekarang = date('d-m-Y');
-        endif;
-    ?>
+	if ($log_status_dasar['tgl_peristiwa'] != '') :
+		$sekarang = $log_status_dasar['tgl_peristiwa'];
+	else :
+		$sekarang = date('d-m-Y');
+	endif;
+	?>
 	<form action="<?= $form_action ?>" method="post" id="validasi" class="tgl_lapor_peristiwa">
 		<div class='modal-body'>
 			<div class="box box-danger">
@@ -15,7 +15,7 @@
 						<label for="status_dasar">Status Dasar Baru</label>
 						<select id="status_dasar" name="status_dasar" class="form-control select2 input-sm required">
 							<option value="">Pilih Status Dasar</option>
-							<?php foreach ($list_status_dasar as $data): ?>
+							<?php foreach ($list_status_dasar as $data) : ?>
 								<option value="<?= $data['id'] ?>" <?= selected($data['id'], $nik['status_dasar_id']) ?>><?= $data['nama'] ?></option>
 							<?php endforeach; ?>
 						</select>
@@ -37,16 +37,16 @@
 						<label for="sebab">Penyebab Kematian</label>
 						<select id="sebab" name="sebab" class="form-control select2 input-sm required">
 							<option value="">Pilih Penyebab Kematian</option>
-							<?php foreach ($sebab as $key => $value): ?>
+							<?php foreach ($sebab as $key => $value) : ?>
 								<option value="<?= $key ?>"><?= $value ?></option>
 							<?php endforeach ?>
 						</select>
 					</div>
 					<div class="form-group mati">
-						<label for="penolong_mati">Yang menerangkan kematian</label>
+						<label for="penolong_mati">Yang Menerangkan Kematian</label>
 						<select id="penolong_mati" name="penolong_mati" class="form-control select2 input-sm required">
-							<option value="">Pilih Penyebab Kematian</option>
-							<?php foreach ($penolong_mati as $key => $value): ?>
+							<option value="">Pilih Yang Menerangkan Kematian</option>
+							<?php foreach ($penolong_mati as $key => $value) : ?>
 								<option value="<?= $key ?>"><?= $value ?></option>
 							<?php endforeach ?>
 						</select>
@@ -62,9 +62,9 @@
 					<div class="form-group pindah">
 						<div class="form-group">
 							<label for="ref_pindah">Tujuan Pindah</label>
-							<select  name="ref_pindah" class="form-control select2 input-sm required">
+							<select name="ref_pindah" class="form-control select2 input-sm required">
 								<option value="">Pilih Tujuan Pindah</option>
-								<?php foreach ($list_ref_pindah as $data): ?>
+								<?php foreach ($list_ref_pindah as $data) : ?>
 									<option value="<?= $data['id'] ?>" <?= selected($data['id'], $nik['ref_pindah']) ?>><?= $data['nama'] ?></option>
 								<?php endforeach; ?>
 							</select>
@@ -106,14 +106,14 @@
 		</div>
 	</form>
 	<script>
-		$('#tgl_1').datetimepicker( {
+		$('#tgl_1').datetimepicker({
 			format: 'DD-MM-YYYY',
-			locale:'id'
+			locale: 'id'
 		});
 
-		$('#tgl_lapor').datetimepicker( {
+		$('#tgl_lapor').datetimepicker({
 			format: 'DD-MM-YYYY',
-			locale:'id'
+			locale: 'id'
 		});
 
 		$('document').ready(function() {
@@ -128,7 +128,7 @@
 						$("input[name='jam_mati']").removeClass('required');
 						$("select[name='sebab']").removeClass('required');
 						$("select[name='penolong_mati']").removeClass('required');
-						$("input[name='anak_ke']").removeClass('required').removeAttr( "min" );;
+						$("input[name='anak_ke']").removeClass('required').removeAttr("min");;
 					} else {
 						$('.mati').show();
 						$("input[name='meninggal_di']").addClass('required');
@@ -138,7 +138,7 @@
 						$("input[name='jam_mati']").show().addClass('required');
 						$("select[name='sebab']").addClass('required');
 						$("select[name='penolong_mati']").addClass('required');
-						$("input[name='anak_ke']").addClass('required').attr( "min", 1 );
+						$("input[name='anak_ke']").addClass('required').attr("min", 1);
 					}
 				} else {
 					$('.pindah').hide();
@@ -149,7 +149,7 @@
 					$("input[name='jam_mati']").removeClass('required');
 					$("select[name='sebab']").removeClass('required');
 					$("select[name='penolong_mati']").removeClass('required');
-					$("input[name='anak_ke']").removeClass('required').removeAttr( "min" );;
+					$("input[name='anak_ke']").removeClass('required').removeAttr("min");;
 				}
 			});
 			$('#status_dasar').trigger('change');
