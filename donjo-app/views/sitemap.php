fe582138d16d34277b5c971f11285551468bc590
Toto Prayogo <mail@totoprayogo.com>
2022-03-01 16:52:20 +0700

    fix url sitemap url

--- donjo-app/views/sitemap.php
+++ donjo-app/views/sitemap.php
@@ -2,15 +2,15 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url>
-		<loc><?= base_url() ?></loc>
+		<loc><?= site_url() ?></loc>
 		<priority>1.0</priority>
 		<changefreq>daily</changefreq>
 	</url>
 
 	<!-- Sitemap -->
-	<?php foreach($artikel as $a): ?>
+	<?php foreach ($artikel as $a): ?>
 	<url>
-		<loc><?= base_url('index.php/artikel/'.buat_slug($a)); ?></loc>
+		<loc><?= site_url('artikel/' . buat_slug($a)); ?></loc>
 		<lastmod><?= date_format(date_create($a['tgl_upload']), 'Y-m-d') ?></lastmod>
 		<priority>0.5</priority>
 		<changefreq>weekly</changefreq>
