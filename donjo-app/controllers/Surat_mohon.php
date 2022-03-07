26463db4ba0ef32d5b100051cf5a33441ede58d7
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-03-08 00:27:57 +0800

    Penyesuaian viewPath, if else, datatables

--- donjo-app/controllers/Surat_mohon.php
+++ donjo-app/controllers/Surat_mohon.php
@@ -41,6 +41,8 @@ defined('BASEPATH') || exit('No direct script access allowed');
 
 class Surat_mohon extends Admin_Controller
 {
+    private $viewPath = 'admin.syaratan_surat';
+
     public function __construct()
     {
         parent::__construct();
@@ -50,33 +52,37 @@ class Surat_mohon extends Admin_Controller
 
     public function index()
     {
-        return view('admin.syaratan_surat.index');
+        return view("{$this->viewPath}.index");
     }
 
     public function datatables()
     {
-        return datatables()->of(SyaratSurat::select(['ref_syarat_id', 'ref_syarat_nama']))
-            ->addColumn('ceklist', static function ($row) {
-                if (can('h')) {
-                    return '<input type="checkbox" name="id_cb[]" value="' . $row->ref_syarat_id . '"/>';
-                }
-            })
-            ->addIndexColumn()
-            ->addColumn('aksi', static function ($row) {
-                $aksi = '';
+        if ($this->input->is_ajax_request()) {
+            return datatables()->of(SyaratSurat::query())
+                ->addColumn('ceklist', static function ($row) {
+                    if (can('h')) {
+                        return '<input type="checkbox" name="id_cb[]" value="' . $row->ref_syarat_id . '"/>';
+                    }
+                })
+                ->addIndexColumn()
+                ->addColumn('aksi', static function ($row) {
+                    $aksi = '';
 
-                if (can('u')) {
-                    $aksi .= '<a href="' . route('surat_mohon.form', $row->ref_syarat_id) . '" class="btn btn-warning btn-sm"  title="Ubah Data"><i class="fa fa-edit"></i></a> ';
-                }
+                    if (can('u')) {
+                        $aksi .= '<a href="' . route('surat_mohon.form', $row->ref_syarat_id) . '" class="btn btn-warning btn-sm"  title="Ubah Data"><i class="fa fa-edit"></i></a> ';
+                    }
 
-                if (can('h')) {
-                    $aksi .= '<a href="#" data-href="' . route('surat_mohon.delete', $row->ref_syarat_id) . '" class="btn bg-maroon btn-sm"  title="Hapus Data" data-toggle="modal" data-target="#confirm-delete"><i class="fa fa-trash"></i></a> ';
-                }
+                    if (can('h')) {
+                        $aksi .= '<a href="#" data-href="' . route('surat_mohon.delete', $row->ref_syarat_id) . '" class="btn bg-maroon btn-sm"  title="Hapus Data" data-toggle="modal" data-target="#confirm-delete"><i class="fa fa-trash"></i></a> ';
+                    }
 
-                return $aksi;
-            })
-            ->rawColumns(['ceklist', 'aksi'])
-            ->make();
+                    return $aksi;
+                })
+                ->rawColumns(['ceklist', 'aksi'])
+                ->make();
+        }
+
+        return show_404();
     }
 
     public function form($id = '')
@@ -94,7 +100,7 @@ class Surat_mohon extends Admin_Controller
             $ref_syarat_surat = null;
         }
 
-        return view('admin.syaratan_surat.form', compact('action', 'form_action', 'ref_syarat_surat'));
+        return view("{$this->viewPath}.form", compact('action', 'form_action', 'ref_syarat_surat'));
     }
 
     public function insert()
@@ -103,9 +109,9 @@ class Surat_mohon extends Admin_Controller
 
         if (SyaratSurat::insert(static::validate($this->request))) {
             redirect_with('success', 'Berhasil Tambah Data');
-        } else {
-            redirect_with('error', 'Gagal Tambah Data');
         }
+
+        redirect_with('error', 'Gagal Tambah Data');
     }
 
     public function update($id = '')
@@ -117,9 +123,9 @@ class Surat_mohon extends Admin_Controller
 
         if ($data->update(static::validate($this->request))) {
             redirect_with('success', 'Berhasil Ubah Data');
-        } else {
-            redirect_with('error', 'Gagal Ubah Data');
         }
+
+        redirect_with('error', 'Gagal Ubah Data');
     }
 
     public function delete($id = '')
@@ -128,9 +134,9 @@ class Surat_mohon extends Admin_Controller
 
         if (SyaratSurat::destroy($id)) {
             redirect_with('success', 'Berhasil Hapus Data');
-        } else {
-            redirect_with('error', 'Gagal Hapus Data');
         }
+
+        redirect_with('error', 'Gagal Hapus Data');
     }
 
     public function deleteAll()
@@ -139,9 +145,9 @@ class Surat_mohon extends Admin_Controller
 
         if (SyaratSurat::destroy($this->request['id_cb'])) {
             redirect_with('success', 'Berhasil Hapus Data');
-        } else {
-            redirect_with('error', 'Gagal Hapus Data');
         }
+
+        redirect_with('error', 'Gagal Hapus Data');
     }
 
     // Hanya filter inputan
