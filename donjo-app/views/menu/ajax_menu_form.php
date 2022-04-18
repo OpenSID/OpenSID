ef394a819f9a5920227b0b7e041138c105aaee11
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2022-04-18 13:32:58 +0800

    Perbaiki link data kelompok dan suplemen (#971)

--- donjo-app/views/menu/ajax_menu_form.php
+++ donjo-app/views/menu/ajax_menu_form.php
@@ -122,13 +122,13 @@ defined('BASEPATH') || exit('No direct script access allowed');
 			<select id="kelompok" class="form-control input-sm jenis_link required" name="<?= jecho($menu['link_tipe'], 7, 'link'); ?>" style="<?php ($menu['link_tipe'] != 7) && print 'display:none;' ?>">
 				<option value="">Pilih Kelompok</option>
 				<?php foreach ($kelompok as $kel): ?>
-					<option value="<?= "data-kelompok/{$kel['id']}"; ?>" <?= selected($menu['link'], site_url() . "data-kelompok/{$kel['slug']}") ?>><?= $kel['nama'] . ' (' . $kel['master'] . ')'; ?></option>
+					<option value="<?= "data-kelompok/{$kel['id']}"; ?>" <?= selected($menu['link'], "data-kelompok/{$kel['id']}") ?>><?= $kel['nama'] . ' (' . $kel['master'] . ')'; ?></option>
 				<?php endforeach; ?>
 			</select>
 			<select id="suplemen" class="form-control input-sm jenis_link required" name="<?= jecho($menu['link_tipe'], 9, 'link'); ?>" style="<?php ($menu['link_tipe'] != 9) && print 'display:none;'; ?>">
 				<option value="">Pilih Suplemen</option>
 				<?php foreach ($suplemen as $sup): ?>
-					<option value="<?= "data-suplemen/{$sup['id']}"; ?>" <?= selected($menu['link'], site_url() . "data-suplemen/{$sup['slug']}") ?>><?= $sup['nama']; ?></option>
+					<option value="<?= "data-suplemen/{$sup['id']}"; ?>" <?= selected($menu['link'], "data-suplemen/{$sup['id']}") ?>><?= $sup['nama']; ?></option>
 				<?php endforeach; ?>
 			</select>
 			<select id="status_idm" class="form-control input-sm jenis_link required" name="<?= jecho($menu['link_tipe'], 10, 'link'); ?>" style="<?php ($menu['link_tipe'] != 10) && print 'display:none;'; ?>">
