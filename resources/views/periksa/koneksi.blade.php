926c368d9333c026e5cbfdfafb1400cd198af9d1
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-05-14 12:35:21 +0800

    Perbaiki informasi config database yang kurang lengkap.

--- resources/views/periksa/koneksi.blade.php
+++ resources/views/periksa/koneksi.blade.php
@@ -36,20 +36,22 @@
 <body class="hold-transition register-page">
 	<div class="register-box">
 		<div class="register-box-body">
-			<h4 class="login-box-msg"><i class="fa fa-warning text-danger"></i> Koneksi database gagal</h4>
+			<h4 class="login-box-msg">
+				<i class="fa fa-warning text-danger"></i><b> Koneksi Database Gagal</b>
+			</h4>
 			<hr>
 			<p>Atasi dengan melakukan langkah sebagai berikut:</p>
 			<ol style="text-align: justify">
 				<li>Pastikan telah membuat database untuk OpenSID.</li>
-				<li>Buka file desa/config/database menggunakan editor.</li>
+				<li>Buka file <b>desa/config/database</b> menggunakan editor.</li>
 				<li>Perbaiki dengan memastikan setiap baris terisi nilai sesuai dengan database di (1).</li>
 				<ul>
-					<li>db['default']['hostname'] = '<span class="red">isi-hostname-database</span>';</li>
-					<li>db['default']['username'] = '<span class="red">isi-username-database</span>';</li>
-					<li>db['default']['password'] = '<span class="red">isi-password-database</span>';</li>
+					<li>$db['default']['hostname'] = '<span class="red">isi-hostname-database</span>';</li>
+					<li>$db['default']['username'] = '<span class="red">isi-username-database</span>';</li>
+					<li>$db['default']['password'] = '<span class="red">isi-password-database</span>';</li>
 					<li>$db['default']['database'] = '<span class="red">isi-nama-database'</span>;</li>
 				</ul>
-				<li>Simpan file desa/config/database.</li>
+				<li>Simpan file <b>desa/config/database.</b></li>
 				<li>Kembali ke halaman <a href="{{ APP_URL }}">awal</a>.</li>
 			</ol>
 		</div>
