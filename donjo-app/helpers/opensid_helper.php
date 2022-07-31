c85a76a490443efb773c83e0b5b6ebebf68044c6
Agung Sugiarto <sugiartoagung92@gmail.com>
2022-08-01 00:40:03 +0700

    Rilis 22.08-umum

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -43,7 +43,7 @@ defined('BASEPATH') || exit('No direct script access allowed');
  * beta => premium-beta[nomor urut dua digit]
  * [nomor urut dua digit] : minggu 1 => 01, dst
  */
-define('VERSION', '22.07-pasca');
+define('VERSION', '22.08');
 /**
  * VERSI_DATABASE
  * Ubah setiap kali mengubah struktur database atau melakukan proses rilis (tgl 01)
