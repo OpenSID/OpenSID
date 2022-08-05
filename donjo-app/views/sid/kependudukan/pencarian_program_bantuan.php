8d7a8857fca3736d2e4551d7d91d40b0f2402851
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-08-05 22:43:26 +0800

    fix: perbaiki selected dan buat konsisten

--- donjo-app/views/sid/kependudukan/pencarian_program_bantuan.php
+++ donjo-app/views/sid/kependudukan/pencarian_program_bantuan.php
@@ -4,8 +4,8 @@
         <div class="form-group">
             <label for="program_bantuan">Program Bantuan</label>
             <select class="form-control input-sm select2" name="program_bantuan">
-                <option value="<?= JUMLAH ?>">Penduduk Penerima Bantuan</option>
-                <option value="<?= BELUM_MENGISI ?>" <?= selected($id_program, '777'); ?>>Penduduk Bukan Penerima Bantuan</option>
+                <option value="<?= JUMLAH ?>" <?= selected($id_program, JUMLAH); ?>>Penduduk Penerima Bantuan</option>
+                <option value="<?= BELUM_MENGISI ?>" <?= selected($id_program, BELUM_MENGISI); ?>>Penduduk Bukan Penerima Bantuan</option>
                 <?php foreach ($program_bantuan as $data) : ?>
                     <option value="<?= $data['id'] ?>" <?= selected($id_program, $data['id']); ?>><?= $data['nama']; ?></option>
                 <?php endforeach; ?>
