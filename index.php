ebc7893bf4b1c00a18fdbf9f10a88b66f8e8fefe
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-09-23 15:21:42 +0800

    Revert "sesuaikan define"
    This reverts commit 7a0e0fb5f57f4b67bb9ec9c1e7667e4e0f02f803.

--- index.php
+++ index.php
@@ -52,7 +52,8 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-define('ENVIRONMENT', $_SERVER['CI_ENV'] ?? 'production');
+
+    define('ENVIRONMENT', $_SERVER['CI_ENV'] ?? 'production');
 
 /*
  *---------------------------------------------------------------
@@ -66,13 +67,17 @@ switch (ENVIRONMENT) {
     case 'development':
         error_reporting(-1);
         ini_set('display_errors', 1);
-        break;
+    break;
 
     case 'testing':
     case 'production':
         ini_set('display_errors', 0);
-        error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-        break;
+        if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        } else {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
+    break;
 
     default:
         header('HTTP/1.1 503 Service Unavailable.', true, 503);
@@ -83,13 +88,33 @@ switch (ENVIRONMENT) {
 
 /*
  *---------------------------------------------------------------
+ * CATATAN
+ *---------------------------------------------------------------
+ *
+ * Setiap kali melakukan define folder,
+ * Lakukan juga di file assets/filemanager/init.php
+ *
+ */
+
+/*
+ *---------------------------------------------------------------
+ * DESA DIRECTORY NAME
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the name of your "desa" directory.
+ * Set the path if it is not in the same directory as this file.
+ */
+    $desa_path = 'desa';
+
+/*
+ *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
  *---------------------------------------------------------------
  *
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-$system_path = 'vendor/codeigniter/framework/system';
+    $system_path = 'vendor/codeigniter/framework/system';
 
 /*
  *---------------------------------------------------------------
@@ -102,11 +127,11 @@ $system_path = 'vendor/codeigniter/framework/system';
  * use an absolute (full) server path.
  * For more info please see the user guide:
  *
- * https://codeigniter.com/userguide3/general/managing_apps.html
+ * https://codeigniter.com/user_guide/general/managing_apps.html
  *
  * NO TRAILING SLASH!
  */
-$application_folder = 'donjo-app';
+    $application_folder = 'donjo-app';
 
 /*
  *---------------------------------------------------------------
@@ -121,7 +146,42 @@ $application_folder = 'donjo-app';
  *
  * NO TRAILING SLASH!
  */
-$view_folder = '';
+    $view_folder = '';
+
+    /**
+     *---------------------------------------------------------------
+     * WEB DIRECTORY NAME
+     *---------------------------------------------------------------
+     */
+    $web_folder = 'fweb';
+
+    /**
+     *---------------------------------------------------------------
+     * MANDIRI DIRECTORY NAME
+     *---------------------------------------------------------------
+     */
+    $mandiri_folder = 'fmandiri';
+
+    /**
+     *---------------------------------------------------------------
+     * ADMIN DIRECTORY NAME
+     *---------------------------------------------------------------
+     */
+    $admin_folder = 'fadmin';
+
+    /**
+     *---------------------------------------------------------------
+     * RESOURCES DIRECTORY NAME
+     *---------------------------------------------------------------
+     */
+    $resources_folder = 'resources';
+
+    /**
+     *---------------------------------------------------------------
+     * STORAGE DIRECTORY NAME
+     *---------------------------------------------------------------
+     */
+    $storage_folder = 'storage';
 
 /*
  * --------------------------------------------------------------------
@@ -142,15 +202,15 @@ $view_folder = '';
  *
  * Un-comment the $routing array below to use this feature
  */
-// The directory name, relative to the "controllers" directory.  Leave blank
-// if your controller is not in a sub-directory within the "controllers" one
-// $routing['directory'] = '';
+    // The directory name, relative to the "controllers" directory.  Leave blank
+    // if your controller is not in a sub-directory within the "controllers" one
+    // $routing['directory'] = '';
 
-// The controller class file name.  Example:  mycontroller
-// $routing['controller'] = '';
+    // The controller class file name.  Example:  mycontroller
+    // $routing['controller'] = '';
 
-// The controller function you wish to be called.
-// $routing['function']	= '';
+    // The controller function you wish to be called.
+    // $routing['function']	= '';
 
 /*
  * -------------------------------------------------------------------
@@ -166,7 +226,7 @@ $view_folder = '';
  *
  * Un-comment the $assign_to_config array below to use this feature
  */
-// $assign_to_config['name_of_config_item'] = 'value of config item';
+    // $assign_to_config['name_of_config_item'] = 'value of config item';
 
 // --------------------------------------------------------------------
 // END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
@@ -178,102 +238,174 @@ $view_folder = '';
  * ---------------------------------------------------------------
  */
 
-// Set the current directory correctly for CLI requests
-if (defined('STDIN')) {
-    chdir(__DIR__);
-}
+    // Set the current directory correctly for CLI requests
+    if (defined('STDIN')) {
+        chdir(__DIR__);
+    }
 
-if (($_temp = realpath($system_path)) !== false) {
-    $system_path = $_temp . DIRECTORY_SEPARATOR;
-} else {
-    // Ensure there's a trailing slash
-    $system_path = strtr(
-        rtrim($system_path, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    ) . DIRECTORY_SEPARATOR;
-}
+    if (($_temp = realpath($system_path)) !== false) {
+        $system_path = $_temp . DIRECTORY_SEPARATOR;
+    } else {
+        // Ensure there's a trailing slash
+        $system_path = strtr(
+            rtrim($system_path, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+        ) . DIRECTORY_SEPARATOR;
+    }
 
-// Is the system path correct?
-if (! is_dir($system_path)) {
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: ' . pathinfo(__FILE__, PATHINFO_BASENAME);
+    // Is the system path correct?
+    if (! is_dir($system_path)) {
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: ' . pathinfo(__FILE__, PATHINFO_BASENAME);
 
-    exit(3); // EXIT_CONFIG
-}
+        exit(3); // EXIT_CONFIG
+    }
 
 /*
  * -------------------------------------------------------------------
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-// The name of THIS file
-define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+    // The name of THIS file
+    define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-// Path to the system directory
-define('BASEPATH', $system_path);
+    // Path to the system directory
+    define('BASEPATH', $system_path);
 
-// Path to the front controller (this file) directory
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+    // Path to the front controller (this file) directory
+    define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-// Name of the "system" directory
-define('SYSDIR', basename(BASEPATH));
+    // Name of the "system" directory
+    define('SYSDIR', basename(BASEPATH));
 
-// The path to the "application" directory
-if (is_dir($application_folder)) {
-    if (($_temp = realpath($application_folder)) !== false) {
-        $application_folder = $_temp;
-    } else {
-        $application_folder = strtr(
-            rtrim($application_folder, '/\\'),
+    // The path to the "application" directory
+    if (is_dir($application_folder)) {
+        if (($_temp = realpath($application_folder)) !== false) {
+            $application_folder = $_temp;
+        } else {
+            $application_folder = strtr(
+                rtrim($application_folder, '/\\'),
+                '/\\',
+                DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+            );
+        }
+    } elseif (is_dir(BASEPATH . $application_folder . DIRECTORY_SEPARATOR)) {
+        $application_folder = BASEPATH . strtr(
+            trim($application_folder, '/\\'),
             '/\\',
             DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
         );
-    }
-} elseif (is_dir(BASEPATH . $application_folder . DIRECTORY_SEPARATOR)) {
-    $application_folder = BASEPATH . strtr(
-        trim($application_folder, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    );
-} else {
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: ' . self;
-
-    exit(3); // EXIT_CONFIG
-}
-
-define('APPPATH', $application_folder . DIRECTORY_SEPARATOR);
-
-// The path to the "views" directory
-if (! isset($view_folder[0]) && is_dir(APPPATH . 'views' . DIRECTORY_SEPARATOR)) {
-    $view_folder = APPPATH . 'views';
-} elseif (is_dir($view_folder)) {
-    if (($_temp = realpath($view_folder)) !== false) {
-        $view_folder = $_temp;
     } else {
-        $view_folder = strtr(
-            rtrim($view_folder, '/\\'),
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: ' . self;
+
+        exit(3); // EXIT_CONFIG
+    }
+
+    define('APPPATH', $application_folder . DIRECTORY_SEPARATOR);
+
+    // The path to the "views" directory
+    if (! isset($view_folder[0]) && is_dir(APPPATH . 'views' . DIRECTORY_SEPARATOR)) {
+        $view_folder = APPPATH . 'views';
+    } elseif (is_dir($view_folder)) {
+        if (($_temp = realpath($view_folder)) !== false) {
+            $view_folder = $_temp;
+        } else {
+            $view_folder = strtr(
+                rtrim($view_folder, '/\\'),
+                '/\\',
+                DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+            );
+        }
+    } elseif (is_dir(APPPATH . $view_folder . DIRECTORY_SEPARATOR)) {
+        $view_folder = APPPATH . strtr(
+            trim($view_folder, '/\\'),
             '/\\',
             DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
         );
+    } else {
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: ' . self;
+
+        exit(3); // EXIT_CONFIG
     }
-} elseif (is_dir(APPPATH . $view_folder . DIRECTORY_SEPARATOR)) {
-    $view_folder = APPPATH . strtr(
-        trim($view_folder, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    );
-} else {
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: ' . self;
 
-    exit(3); // EXIT_CONFIG
-}
+    define('VIEWPATH', $view_folder . DIRECTORY_SEPARATOR);
+    /**
+     * https://stackoverflow.com/questions/11792268/how-to-set-proper-codeigniter-base-url
+     * Define APP_URL Dynamically
+     * Write this at the bottom of index.php
+     *
+     * Automatic base url
+     */
+    define('APP_URL', ($_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http') . "://{$_SERVER['HTTP_HOST']}" . str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']));
 
-define('VIEWPATH', $view_folder . DIRECTORY_SEPARATOR);
+    /**
+     * Custom path
+     */
+    define('WEB', $web_folder);
+    define('MANDIRI', $mandiri_folder);
+    define('ADMIN', $admin_folder);
+    define('DESAPATH', $desa_path . DIRECTORY_SEPARATOR);
 
-/*
+    // The path to the "resources" directory
+    if (! isset($resources[0]) && is_dir(APPPATH . 'resources' . DIRECTORY_SEPARATOR)) {
+        $resources_folder = APPPATH . 'resources';
+    } elseif (is_dir($resources_folder)) {
+        if (($_temp = realpath($resources_folder)) !== false) {
+            $resources_folder = $_temp;
+        } else {
+            $resources_folder = strtr(
+                rtrim($resources_folder, '/\\'),
+                '/\\',
+                DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+            );
+        }
+    } elseif (is_dir(APPPATH . $resources_folder . DIRECTORY_SEPARATOR)) {
+        $resources_folder = APPPATH . strtr(
+            trim($resources_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+        );
+    } else {
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: ' . self;
+
+        exit(3); // EXIT_CONFIG
+    }
+
+    define('RESOURCES', $resources_folder . DIRECTORY_SEPARATOR);
+
+    // The path to the "storage" directory
+    if (! isset($storage[0]) && is_dir(APPPATH . 'storage' . DIRECTORY_SEPARATOR)) {
+        $storage_folder = APPPATH . 'storage';
+    } elseif (is_dir($storage_folder)) {
+        if (($_temp = realpath($storage_folder)) !== false) {
+            $storage_folder = $_temp;
+        } else {
+            $storage_folder = strtr(
+                rtrim($storage_folder, '/\\'),
+                '/\\',
+                DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+            );
+        }
+    } elseif (is_dir(APPPATH . $storage_folder . DIRECTORY_SEPARATOR)) {
+        $storage_folder = APPPATH . strtr(
+            trim($storage_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+        );
+    } else {
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: ' . self;
+
+        exit(3); // EXIT_CONFIG
+    }
+
+    define('STORAGE', $storage_folder . DIRECTORY_SEPARATOR);
+
+/**
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
  * --------------------------------------------------------------------
