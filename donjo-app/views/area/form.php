c677aa24f6d4d9cbdb65d48624258ff949ea9a80
sarwono <68048807+scqolbu24@users.noreply.github.com>
2022-12-05 17:01:01 +0700

    file input view (#1721)
    Co-authored-by: vickyrolanda <v.rolanda92@gmail.com>

--- donjo-app/views/area/form.php
+++ donjo-app/views/area/form.php
@@ -2,21 +2,21 @@
 	<section class="content-header">
 		<h1>Pengaturan Area</h1>
 		<ol class="breadcrumb">
-			<li><a href="<?= site_url('hom_sid')?>"><i class="fa fa-home"></i> Home</a></li>
-			<li><a href="<?= site_url('area')?>"><i class="fa fa-dashboard"></i> Daftar Area</a></li>
+			<li><a href="<?= site_url('hom_sid') ?>"><i class="fa fa-home"></i> Home</a></li>
+			<li><a href="<?= site_url('area') ?>"><i class="fa fa-dashboard"></i> Daftar Area</a></li>
 			<li class="active">Pengaturan Area</li>
 		</ol>
 	</section>
 	<section class="content" id="maincontent">
-	<form id="validasi" action="<?= $form_action?>" method="POST" enctype="multipart/form-data" class="form-horizontal">
+		<form id="validasi" action="<?= $form_action ?>" method="POST" enctype="multipart/form-data" class="form-horizontal">
 			<div class="row">
 				<div class="col-md-3">
-					<?php $this->load->view('plan/nav.php')?>
+					<?php $this->load->view('plan/nav.php') ?>
 				</div>
 				<div class="col-md-9">
 					<div class="box box-info">
 						<div class="box-header with-border">
-							<a href="<?= site_url('area')?>" class="btn btn-social btn-flat btn-info btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block">
+							<a href="<?= site_url('area') ?>" class="btn btn-social btn-flat btn-info btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block">
 								<i class="fa fa-arrow-circle-left "></i>Kembali ke Daftar Area
 							</a>
 						</div>
@@ -24,25 +24,25 @@
 							<div class="form-group">
 								<label class="control-label col-sm-3">Nama Area / Properti</label>
 								<div class="col-sm-7">
-									<input name="nama" class="form-control input-sm nomor_sk required" maxlength="100" type="text" value="<?=$area['nama']?>"></input>
+									<input name="nama" class="form-control input-sm nomor_sk required" maxlength="100" type="text" value="<?= $area['nama'] ?>"></input>
 								</div>
 							</div>
 							<div class="form-group">
 								<label class="control-label col-sm-3">Kategori</label>
 								<div class="col-sm-7">
 									<select class="form-control input-sm select2 required" id="ref_polygon" name="ref_polygon">
-									<option value="">Pilih Kategori</option>
-									<?php foreach ($list_polygon as $data): ?>
-										<option value="<?= $data['id']?>" <?= selected($area['ref_polygon'], $data['id']) ?>><?= $data['nama'] ?></option>
-									<?php endforeach; ?>
+										<option value="">Pilih Kategori</option>
+										<?php foreach ($list_polygon as $data) : ?>
+											<option value="<?= $data['id'] ?>" <?= selected($area['ref_polygon'], $data['id']) ?>><?= $data['nama'] ?></option>
+										<?php endforeach; ?>
 									</select>
 								</div>
 							</div>
-							<?php if ($area['foto']): ?>
+							<?php if ($area['foto']) : ?>
 								<div class="form-group">
 									<label class="control-label col-sm-3"></label>
 									<div class="col-sm-7">
-									  <img class="attachment-img img-responsive img-circle" src="<?= base_url() . LOKASI_FOTO_AREA?>kecil_<?= $area['foto']?>" alt="Foto">
+										<img class="attachment-img img-responsive img-circle" src="<?= base_url() . LOKASI_FOTO_AREA ?>kecil_<?= $area['foto'] ?>" alt="Foto">
 									</div>
 								</div>
 							<?php endif; ?>
@@ -51,7 +51,7 @@
 								<div class="col-sm-7">
 									<div class="input-group input-group-sm">
 										<input type="text" class="form-control" id="file_path">
-										<input id="file" type="file" class="hidden" name="foto">
+										<input id="file" type="file" class="hidden" name="foto" accept=".gif, .jpg, .jpeg, .png">
 										<span class="input-group-btn">
 											<button type="button" class="btn btn-info btn-flat" id="file_browser"><i class="fa fa-search"></i> Browse</button>
 										</span>
@@ -62,17 +62,17 @@
 							<div class="form-group">
 								<label class="col-sm-3 control-label">Keterangan</label>
 								<div class="col-sm-7">
-									<textarea id="desk" name="desk" class="form-control input-sm required" style="height: 200px;white-space: pre-wrap;"><?= $area['desk']?></textarea>
+									<textarea id="desk" name="desk" class="form-control input-sm required" style="height: 200px;white-space: pre-wrap;"><?= $area['desk'] ?></textarea>
 								</div>
 							</div>
 							<div class="form-group">
 								<label class="col-xs-12 col-sm-3 col-lg-3 control-label" for="status">Status</label>
 								<div class="btn-group col-xs-12 col-sm-9" data-toggle="buttons">
-									<label id="sx3" class="btn btn-info btn-flat btn-sm col-xs-6 col-sm-4 col-lg-2 form-check-label <?php if ($area['enabled'] == '1' || $area['enabled'] == null): ?>active<?php endif ?>">
-										<input id="sx1" type="radio" name="enabled" class="form-check-input" type="radio" value="1" <?php if ($area['enabled'] == '1' || $area['enabled'] == null): ?>checked <?php endif ?> autocomplete="off"> Aktif
+									<label id="sx3" class="btn btn-info btn-flat btn-sm col-xs-6 col-sm-4 col-lg-2 form-check-label <?php if ($area['enabled'] == '1' || $area['enabled'] == null) : ?>active<?php endif ?>">
+										<input id="sx1" type="radio" name="enabled" class="form-check-input" type="radio" value="1" <?php if ($area['enabled'] == '1' || $area['enabled'] == null) : ?>checked <?php endif ?> autocomplete="off"> Aktif
 									</label>
-									<label id="sx4" class="btn btn-info btn-flat btn-sm col-xs-6 col-sm-4 col-lg-2 form-check-label <?php if ($area['enabled'] == '2'): ?>active<?php endif ?>">
-										<input id="sx2" type="radio" name="enabled" class="form-check-input" type="radio" value="2" <?php if ($area['enabled'] == '2'): ?>checked<?php endif ?> autocomplete="off"> Tidak Aktif
+									<label id="sx4" class="btn btn-info btn-flat btn-sm col-xs-6 col-sm-4 col-lg-2 form-check-label <?php if ($area['enabled'] == '2') : ?>active<?php endif ?>">
+										<input id="sx2" type="radio" name="enabled" class="form-check-input" type="radio" value="2" <?php if ($area['enabled'] == '2') : ?>checked<?php endif ?> autocomplete="off"> Tidak Aktif
 									</label>
 								</div>
 							</div>
@@ -89,11 +89,11 @@
 </div>
 <script>
 	function reset_form() {
-		<?php if ($area['enabled'] == '1' || $area['enabled'] == null): ?>
+		<?php if ($area['enabled'] == '1' || $area['enabled'] == null) : ?>
 			$("#sx3").addClass('active');
 			$("#sx4").removeClass("active");
 		<?php endif ?>
-		<?php if ($area['enabled'] == '2'): ?>
+		<?php if ($area['enabled'] == '2') : ?>
 			$("#sx4").addClass('active');
 			$("#sx3").removeClass("active");
 		<?php endif ?>
