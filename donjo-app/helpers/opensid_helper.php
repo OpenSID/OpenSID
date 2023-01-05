73bf6d5b376e651f0af8453b834ae950b229e289
vickyrolanda <v.rolanda92@gmail.com>
2023-01-05 08:56:53 +0700

    [ci skip] memutahirkan catatan rilis

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -43,7 +43,7 @@ defined('BASEPATH') || exit('No direct script access allowed');
  * beta => premium-beta[nomor urut dua digit]
  * [nomor urut dua digit] : minggu 1 => 01, dst
  */
-define('VERSION', '23.01-premium-rev01');
+define('VERSION', '23.01-premium-beta01');
 
 /**
  * VERSI_DATABASE
