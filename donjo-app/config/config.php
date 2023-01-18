ed1100a17ce41d64070d80766912b2cb83870b4a
vickyrolanda <v.rolanda92@gmail.com>
2023-01-18 21:06:17 +0700

    kembalikan true csrf_protection

--- donjo-app/config/config.php
+++ donjo-app/config/config.php
@@ -495,7 +495,7 @@ $config['global_xss_filtering'] = false;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection']   = false;
+$config['csrf_protection']   = true;
 $config['csrf_token_name']   = 'sidcsrf';
 $config['csrf_cookie_name']  = 'sidcsrf';
 $config['csrf_expire']       = 7200;
