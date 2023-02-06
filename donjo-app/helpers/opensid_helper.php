e71fa076cb536b2acc3e7fb2d70dad37c9be7cab
Ruri Rizal <hash702@gmail.com>
2023-02-06 08:31:28 +0700

    Susulan issue timezone created_at dan updated_at (#1975)
    * menimbang issue timezone created_at dan updated_at
    
    * Update catatan_rilis.md
    
    * [ci skip] catatan rilis
    
    ---------
    
    Co-authored-by: Andi Fahruddin Akas <andifahruddinakas@gmail.com>

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -43,7 +43,7 @@ defined('BASEPATH') || exit('No direct script access allowed');
  * beta => premium-beta[nomor urut dua digit]
  * [nomor urut dua digit] : minggu 1 => 01, dst
  */
-define('VERSION', '23.02-premium');
+define('VERSION', '23.02-premium-rev01');
 
 /**
  * VERSI_DATABASE
