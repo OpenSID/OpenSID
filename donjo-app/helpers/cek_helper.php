b57033cb0652ef6382f72da4dd6b91a41ff1be54
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2023-05-22 10:31:12 +0800

    validasi migrasi premium website demo (#2329)

---
+++ donjo-app/helpers/cek_helper.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ *
+ * File ini bagian dari:
+ *
+ * OpenSID
+ *
+ * Sistem informasi desa sumber terbuka untuk memajukan desa
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * Hak Cipta 2016 - 2023 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+ *
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+ *
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package   OpenSID
+ * @author    Tim Pengembang OpenDesa
+ * @copyright Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
+ * @copyright Hak Cipta 2016 - 2023 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license   http://www.gnu.org/licenses/gpl.html GPL V3
+ * @link      https://github.com/OpenSID/OpenSID
+ *
+ */
+
+defined('BASEPATH') || exit('No direct script access allowed');
+
+// Website Demo OpenSID
+define('WEBSITE_DEMO', [
+    'beta.opendesa.id',
+    'beta2.opensid.or.id',
+    'berputar.opendesa.id',
+    'devpremium.opendesa.id',
+]);
+
+if (! function_exists('cek_anjungan')) {
+    /**
+     * - Fungsi validasi anjungan.
+     *
+     * @return string
+     */
+    function cek_anjungan()
+    {
+        // Lewati pengecekan jika web demo dan terdaftar sebagai pengecualian
+        if (config_item('demo_mode') && (in_array(get_domain(APP_URL), WEBSITE_DEMO))) {
+            return true;
+        }
+
+        $CI = &get_instance();
+        $CI->load->model('notif_model');
+
+        $status = $CI->notif_model->api_pelanggan_pemesanan();
+
+        return $status->body->tanggal_berlangganan->anjungan != 'aktif' ? false : true;
+    }
+}
