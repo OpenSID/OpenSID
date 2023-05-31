9b5119c0c2fdfeacc061a80df68797644e3ac258
vickyrolanda <v.rolanda92@gmail.com>
2023-05-31 08:56:37 +0700

    migrasi

--- donjo-app/models/migrations/Migrasi_2306_ke_2307.php
+++ donjo-app/models/migrations/Migrasi_2306_ke_2307.php
@@ -37,14 +37,14 @@
 
 defined('BASEPATH') || exit('No direct script access allowed');
 
-class Migrasi_2305_ke_2306 extends MY_Model
+class Migrasi_2306_ke_2307 extends MY_Model
 {
     public function up()
     {
         $hasil = true;
 
         // Migrasi fitur premium
-        $hasil = $hasil && $this->jalankan_migrasi('migrasi_fitur_premium_2211');
+        $hasil = $hasil && $this->jalankan_migrasi('migrasi_fitur_premium_2212');
 
         status_sukses($hasil);
 
