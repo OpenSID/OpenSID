388d286c3f7e8b5feb6e4b00196aa7dd073c6054
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-06-30 23:38:17 +0800

    migrasi

--- donjo-app/models/migrations/Migrasi_2307_ke_2308.php
+++ donjo-app/models/migrations/Migrasi_2307_ke_2308.php
@@ -37,14 +37,14 @@
 
 defined('BASEPATH') || exit('No direct script access allowed');
 
-class Migrasi_2306_ke_2307 extends MY_Model
+class Migrasi_2307_ke_2308 extends MY_Model
 {
     public function up()
     {
         $hasil = true;
 
         // Migrasi fitur premium
-        $hasil = $hasil && $this->jalankan_migrasi('migrasi_fitur_premium_2212');
+        $hasil = $hasil && $this->jalankan_migrasi('migrasi_fitur_premium_2301');
 
         status_sukses($hasil);
 
