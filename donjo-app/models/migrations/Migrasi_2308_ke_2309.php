13e4cf496546e286172a2a02e969ade9ef688084
Agung Sugiarto <sugiartoagung92@gmail.com>
2023-08-01 14:12:16 +0700

    Rilis v2308.0.0 (#6960)
    * Rilis v2308.0.0
    
    * perbaiki tema
    
    * Update meta.php
    
    ---------
    
    Co-authored-by: Andi Fahruddin Akas <andifahruddinakas@gmail.com>

--- donjo-app/models/migrations/Migrasi_2308_ke_2309.php
+++ donjo-app/models/migrations/Migrasi_2308_ke_2309.php
@@ -37,14 +37,14 @@
 
 defined('BASEPATH') || exit('No direct script access allowed');
 
-class Migrasi_2307_ke_2308 extends MY_Model
+class Migrasi_2308_ke_2309 extends MY_Model
 {
     public function up()
     {
         $hasil = true;
 
         // Migrasi fitur premium
-        $hasil = $hasil && $this->jalankan_migrasi('migrasi_fitur_premium_2301');
+        $hasil = $hasil && $this->jalankan_migrasi('migrasi_fitur_premium_2302');
 
         status_sukses($hasil);
 
--- vendor/themes/esensi/commons/meta.php
+++ vendor/themes/esensi/commons/meta.php
@@ -1,10 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-<?php $is_premium = preg_match('/premium/', ambilVersi()) ?>
 <?php $nama_desa = ucwords($this->setting->sebutan_desa) .' '.ucwords($desa['nama_desa']) ?>
 
 <?php defined('THEME_VERSION') or define('THEME_VERSION', 'v22.11') ?>
-<?php defined('IS_PREMIUM') or define('IS_PREMIUM', $is_premium) ?>
+<!-- Catatan: tema 6 bulan lalu adalah umum saat ini -->
+<?php defined('IS_PREMIUM') or define('IS_PREMIUM', true) ?>
 <?php defined('NAMA_DESA') or define('NAMA_DESA', $nama_desa) ?>
 
 <?php $title = preg_replace("/[^A-Za-z0-9- ]/", '', trim(str_replace('-', ' ', get_dynamic_title_page_from_path())));
