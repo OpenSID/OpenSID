ba06cead7da518f49c3164495f352d622a3f7610
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-09-30 00:14:37 +0800

    update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -218,8 +218,8 @@ class ComposerStaticInit9002454735de25e8c3cf2dfc30a1d238
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -264,8 +264,8 @@ class ComposerStaticInit9002454735de25e8c3cf2dfc30a1d238
         ),
         'Illuminate\\Support\\' => 
         array (
-            0 => __DIR__ . '/..' . '/illuminate/macroable',
-            1 => __DIR__ . '/..' . '/illuminate/collections',
+            0 => __DIR__ . '/..' . '/illuminate/collections',
+            1 => __DIR__ . '/..' . '/illuminate/macroable',
             2 => __DIR__ . '/..' . '/illuminate/support',
         ),
         'Illuminate\\Pipeline\\' => 
