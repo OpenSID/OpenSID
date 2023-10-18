cb9b3f127c37fcead57a01b4f35de1729d61ab9f
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-10-19 04:17:19 +0800

    perbaiki salah fuction dan buat konsisten di tema lain

--- vendor/themes/natra/partials/pembangunan/detail.php
+++ vendor/themes/natra/partials/pembangunan/detail.php
@@ -23,12 +23,12 @@
 								<tr>
 									<th width="150px">Nama Kegiatan</th>
 									<td width="20px">:</td>
-									<td><?= d($pembangunan->judul) ?></td>
+									<td><?= e($pembangunan->judul) ?></td>
 								</tr>
 								<tr>
 									<th>Alamat</th>
 									<td width="20px">:</td>
-									<td><?= d($pembangunan->alamat) ?></td>
+									<td><?= e($pembangunan->alamat) ?></td>
 								</tr>
 								<tr>
 									<th>Sumber Dana</th>
