5c6ecc726e868a421c228f8d443e059adb6a7df3
ahmad afandi <ahmad.afandi85@gmail.com>
2023-10-27 11:02:20 +0700

    penambahan tampilan tinymce mirip ms word (#3056)

--- resources/views/admin/pengaturan_surat/asset_tinymce.blade.php
+++ resources/views/admin/pengaturan_surat/asset_tinymce.blade.php
@@ -67,7 +67,7 @@
                 plugins: plugins_tambahan,
                 content_style: `body { font-family: ${default_font}; }`,
                 toolbar1: "removeformat | bold italic underline subscript superscript | bullist numlist outdent indent lineheight | alignleft aligncenter alignright alignjustify | blocks fontfamily fontsizeinput",
-                toolbar2: "responsivefilemanager | salintemplate | kodeisian",
+                toolbar2: "responsivefilemanager | salintemplate | kodeisian | insertpagebreak",
                 // toolbar: [{ name: 'blocks', items: [ 'p', 'h', 'menjorok' ] },],
                 image_advtab: true,
                 external_plugins: {
@@ -82,13 +82,31 @@
                 remove_script_host: false,
                 entity_encoding: 'raw',
                 // gak bisa pakai false
-                // forced_root_block: false, 
+                //forced_root_block: false, 
                 forced_root_block: ' ',
                 font_family_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black; Bookman Old Style=bookman old style; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Tahoma=tahoma,arial,helvetica,sans-serif; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; {{ $fonts }}",
-                setup: function(ed) {
+                setup: function(ed) {            
+                    ed.ui.registry.addButton('insertpagebreak', {
+                        text: 'Tambah Halaman Baru',
+                        onAction: function() {
+                            // Insert a page break when the button is clicked
+                            ed.insertContent('<div style="page-break-after: always;"><!-- pagebreak --></div>');
+                        }
+                    });        
                     ed.on('init', function(e) {
                         ed.execCommand("fontName", false, "${default_font}");
-                    });
+                    });                                    
+
+                    ed.on('keydown', function() {
+                        var contentAreaHeight = ed.getBody().offsetHeight;
+                        var lengthPaper = 1644; 
+                        var sisaBatasKertas = contentAreaHeight % lengthPaper
+                        console.log('sisaBatasKertas '+ sisaBatasKertas)
+                        // Check if a new line has been added
+                        if (sisaBatasKertas > 0 && sisaBatasKertas < 35) {                            
+                            ed.insertContent('<div style="page-break-after: always;"><!-- pagebreak --></div>');                            
+                        }                        
+                    });                    
                 },
                 content_style: `
                     body {
@@ -120,6 +138,19 @@
                         vertical-align: bottom;
                     }
                     {!! $cssFont !!}
+
+                    .mce-pagebreak {   
+                        border:none; 
+                        cursor: default;
+                        display: block;
+                        height: 25px;
+                        margin-top: 64px;
+                        margin-bottom: 64px;
+                        page-break-before: always;
+                        width: 120%;
+                        margin-left: -9.6%;
+                        background-color: #ECEEF4
+                    }
                 `
             });
 
