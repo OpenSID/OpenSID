aa895eb0966cf293af7e2abbe6e3445916a3346b
ahmad afandi <ahmad.afandi85@gmail.com>
2023-11-20 15:51:35 +0700

    penyesuaian pengaturan penduduk luar desa (#3178)

---
+++ resources/views/admin/pengaturan_surat/partials/modal_sumber_penduduk.blade.php
@@ -0,0 +1,59 @@
+<div class="modal fade in" id="form-penduduk-luar">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Isian Penduduk Luar</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label class="control-label col-sm-2">Label</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control input-sm judul" />
+                        <label>Isi dengan <code>[desa]</code> untuk menyesuaikan sebutan desa berdasarkan pengaturan aplikasi.</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="">Pilihan input</label>
+                    <div class="col-sm-10">
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="nama" checked disabled> Nama Lengkap</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="no_ktp" checked disabled> NIK / No. KTP</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="tempat_lahir"> Tempat lahir</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="tanggal_lahir"> Tanggal lahir</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="jenis_kelamin"> Jenis Kelamin</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="agama"> Agama</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="pendidikan_kk"> Pendidikan</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="pekerjaan"> Pekerjaan</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="warga_negara"> Warga Negara</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" data-id="alamat"> Alamat</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="reset" class="btn btn-social btn-danger btn-sm pull-left" data-dismiss="modal"><i class="fa fa-times"></i> Batal</button>
+                <button type="button" class="btn btn-sm btn-success form-penduduk-btn"> <i class="fa fa-check"></i>
+                    Simpan</button>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/admin/pengaturan_surat/partials/pengaturan_sumber_penduduk.blade.php
+++ resources/views/admin/pengaturan_surat/partials/pengaturan_sumber_penduduk.blade.php
@@ -3,99 +3,84 @@
         <div class="callout callout-info">
             <p>{{ sebutanDesa($list_setting->where('key', 'form_penduduk_luar')->first()->keterangan) }}</p>
         </div>
-        <div class="panel">
-            <div class="panel-header">
-                <h4 class="panel-title">
-                    <a onclick="$(this).find('i').toggleClass('fa-plus fa-minus')" data-toggle="collapse" class="btn btn-sm btn-social btn-success" href="#form-penduduk-luar"><i class="fa fa-plus"></i> Form Penduduk Luar</a>
-                </h4>
-            </div>
-            <div id="form-penduduk-luar" class="panel-collapse collapse">
-                <div class="panel-body">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="">Label</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control judul" />
-                            <label>Isi dengan <code>[desa]</code> untuk menyesuaikan sebutan desa berdasarkan pengaturan aplikasi.</label>
+        <a data-target="#form-penduduk-luar" data-remote="false" data-toggle="modal" data-backdrop="false" data-keyboard="false" class="btn btn-sm btn-social btn-success"><i class="fa fa-plus"></i> Form Penduduk Luar</a>
+        <br /><br />
+        <div id="list-form-penduduk">
+            @php $count = 0; @endphp
+            @foreach ($penduduk_luar as $index => $penduduk)
+                @if ($count % 3 === 0)
+                    <div class="row">
+                @endif
+                <div class="col-sm-4" data-index="{{ $index }}">
+                    <div class="panel panel-primary">
+                        @if ($index > 3)
+                            <div class="btn-group pull-right" style="padding:6px">
+                                <span
+                                    class="btn btn-sm btn-warning can-edit"
+                                    data-index="{{ $index }}"
+                                    data-target="#form-penduduk-luar"
+                                    data-remote="false"
+                                    data-toggle="modal"
+                                    data-backdrop="false"
+                                    data-keyboard="false"
+                                >
+                                    <i class="fa fa-pencil"></i>
+                                </span>
+                                <span class="btn  btn-sm btn-danger" onclick="$(this).closest('.col-sm-4').remove()" style="margin-left: 5px">
+                                    <i class="fa fa-trash"></i>
+                                </span>
+                            </div>
+                        @endif
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                {{ strtoupper(SebutanDesa($penduduk['title'])) }}
+                            </h4>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="">Pilihan input</label>
-                        <div class="col-sm-10">
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="nama" checked disabled> Nama Lengkap</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="no_ktp" checked disabled> NIK / No. KTP</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="tempat_lahir"> Tempat lahir</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="tanggal_lahir"> Tanggal lahir</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="jenis_kelamin"> Jenis Kelamin</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="agama"> Agama</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="pendidikan_kk"> Pendidikan</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="pekerjaan"> Pekerjaan</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="warga_negara"> Warga Negara</label>
-                            </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox" data-id="alamat"> Alamat</label>
-                            </div>
+                        <div class="panel-body">
+                            <ul>
+                                @foreach (explode(',', $penduduk['input']) as $item)
+                                    <li>{{ $item }}</li>
+                                @endforeach
+                            </ul>
+                            <input type="hidden" value="{{ $penduduk['title'] }}" name="penduduk_luar[{{ $index }}][title]" />
+                            <input type="hidden" value='{{ $penduduk['input'] }}' name="penduduk_luar[{{ $index }}][input]" />
                         </div>
                     </div>
                 </div>
-                <div class="panel-footer">
-                    <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-10">
-                            <button type="button" class="btn btn-sm btn-success form-penduduk-btn">Tambahkan</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                @php $count++; @endphp
+                @if ($count % 3 === 0 || $loop->last)
         </div>
-    </div>
-    <div class="box-body" id="list-form-penduduk">
-        @foreach ($penduduk_luar as $index => $penduduk)
-            <div class="col-sm-4">
-                <div class="panel panel-primary">
-                    <span class="btn btn-danger pull-right" onclick="$(this).closest('.col-sm-4').remove()">
-                        <i class="fa fa-trash"></i>
-                    </span>
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            {{ strtoupper(SebutanDesa($penduduk['title'])) }}
-                        </h4>
-                    </div>
-                    <div class="panel-body">
-                        <ul>
-                            @foreach (explode(',', $penduduk['input']) as $item)
-                                <li>{{ $item }}</li>
-                            @endforeach
-                        </ul>
-                        <input type="hidden" value="{{ $penduduk['title'] }}" name="penduduk_luar[{{ $index }}][title]" />
-                        <input type="hidden" value='{{ $penduduk['input'] }}' name="penduduk_luar[{{ $index }}][input]" />
-                    </div>
-                </div>
-            </div>
+        @endif
         @endforeach
     </div>
 </div>
+</div>
+@include('admin.pengaturan_surat.partials.modal_sumber_penduduk')
 @push('scripts')
     <script type="text/javascript">
         $(function() {
+            $('#form-penduduk-luar').on('shown.bs.modal', function(ev) {
+                let _btn = $(ev.relatedTarget)
+                const modal = $(this)
+                modal.data('index', null)
+                modal.find(`.modal-body :checkbox:not(:disabled)`).prop('checked', false)
+                modal.find(`.modal-body input.judul`).val('')
+                if (_btn.hasClass('can-edit')) {
+                    const _panel = _btn.closest('.panel')
+                    const _panelBody = _panel.find('.panel-body')
+                    const _title = _panelBody.find(`input[name="penduduk_luar[${_btn.attr('data-index')}][title]"]`).val()
+                    const _items = _panelBody.find(`input[name="penduduk_luar[${_btn.attr('data-index')}][input]"]`).val()
+                    modal.data('index', _btn.attr('data-index'))
+                    modal.find('.modal-body input.judul').val(_title)
+
+                    _items.split(',').forEach(function(item) {
+                        modal.find(`.modal-body :checkbox[data-id="${item}"]:not(:disabled)`).prop('checked', true)
+                    })
+                }
+            });
             $('.form-penduduk-btn').on('click', function() {
-                let _panel = $(this).closest('.panel')
-                let _panelBody = _panel.find('.panel-body')
+                let _panel = $(this).closest('.modal')
+                let _panelBody = _panel.find('.modal-body')
                 let _judul = _panelBody.find('input.judul')
                 if ($.isEmptyObject(_judul.val())) {
                     Swal.fire(
@@ -109,7 +94,7 @@
                 }
                 let _selected = ['<ul>']
                 let _input = [],
-                    _index = new Date().getTime()
+                    _index = _panel.data('index') ?? new Date().getTime()
                 _panelBody.find(':checked').each(function() {
                     _selected.push(`<li>${$(this).closest('label').text()}</li>`)
                     _input.push($(this).data('id'))
@@ -118,23 +103,40 @@
                 _selected.push(`<input type="hidden" value="${_judul.val()}" name="penduduk_luar[${_index}][title]">`)
                 _selected.push(`<input type="hidden" value="${_input}" name="penduduk_luar[${_index}][input]">`)
                 let _template = `
-            <div class="col-sm-4">
-                <div class="panel panel-primary">
-                    <span class="btn btn-danger pull-right" onclick="$(this).closest('.col-sm-4').remove()">
-                        <i class="fa fa-trash"></i>
-                    </span>
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            ${_judul.val()}
-                        </h4>
+                <div class="col-sm-4" data-index="${_index}">
+                    <div class="panel panel-primary">
+                        <div class="btn-group pull-right" style="padding:6px">
+                            <span class="btn btn-sm btn-warning can-edit" 
+                            data-index="${_index}"
+                            data-target="#form-penduduk-luar"
+                            data-remote="false"
+                            data-toggle="modal"
+                            data-backdrop="false"
+                            data-keyboard="false">
+                                <i class="fa fa-pencil"></i>
+                            </span>
+                            <span class="btn btn-sm btn-danger" onclick="$(this).closest('.col-sm-4').remove()" style="margin-left: 5px">
+                                <i class="fa fa-trash"></i>
+                            </span>
+                        </div>
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                ${_judul.val()}
+                            </h4>
+                        </div>
+                        <div class="panel-body">
+                            ${_selected.join('')}
+                        </div>
                     </div>
-                    <div class="panel-body">
-                        ${_selected.join('')}
-                    </div>
-                </div>
-            </div>`
+                </div>`
 
-                $(_template).appendTo('#list-form-penduduk')
+                if (_panel.data('index') == null) {
+                    $(_template).appendTo('#list-form-penduduk')
+                } else {
+                    $(`#list-form-penduduk div[data-index=${_index}]`).find('.panel-title').html(_judul.val())
+                    $(`#list-form-penduduk div[data-index=${_index}]`).find('.panel-body').html(_selected.join(''))
+                }
+                _panel.modal('hide')
             })
         });
     </script>
