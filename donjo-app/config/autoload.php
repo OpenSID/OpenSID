47d303fe413cefd1b43be834d5443c3e52f32b29
Agung Sugiarto <sugiartoagung92@gmail.com>
2024-01-09 00:00:03 +0700

    hapus develbar (#3363)
    Co-authored-by: Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>

--- donjo-app/config/autoload.php
+++ donjo-app/config/autoload.php
@@ -76,7 +76,6 @@ defined('BASEPATH') || exit('No direct script access allowed');
 |
 */
 $autoload['packages'] = [
-    APPPATH . 'third_party/DevelBar',
     APPPATH . 'third_party/faker',
     APPPATH . 'third_party/pelanggan',
     APPPATH . 'third_party/pendaftaran_kerjasama',
