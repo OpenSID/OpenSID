78d8270a7ad4ad8d0da8c4b4f02f0d280791bb69
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-01-10 23:21:37 +0800

    route installasi awal
    Co-Authored-By: ahmad afandi <ahmad.afandi85@gmail.com>

--- donjo-app/Routes/web.php
+++ donjo-app/Routes/web.php
@@ -169,6 +169,18 @@ Route::group('kehadiran', ['namespace' => 'kehadiran'], static function () {
     Route::get('/logout', 'Perangkat@logout')->name('kehadiran.perangkat.logout');
 });
 
+Route::group('install', static function () {
+    Route::match(['GET', 'POST'],'/', 'Install@index');
+    Route::match(['GET', 'POST'],'/index', 'Install@index');
+    Route::match(['GET', 'POST'],'/server', 'Install@server');
+    Route::match(['GET', 'POST'],'/folders', 'Install@folders');
+    Route::match(['GET', 'POST'],'/database', 'Install@database');
+    Route::match(['GET', 'POST'],'/migrations', 'Install@migrations');
+    Route::match(['GET', 'POST'],'/user', 'Install@user');
+    Route::match(['GET', 'POST'],'/finish', 'Install@finish');
+    Route::match(['GET', 'POST'],'/syarat_sandi/{password?}', 'Install@syarat_sandi');    
+});
+
 // include admin.php
 require_once APPPATH . 'Routes/Web/admin.php';
 require_once APPPATH . 'Routes/Web/mandiri.php';
