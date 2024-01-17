3075e0599a27af19439606c2cab21f4f073b0ce9
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-01-17 13:55:43 +0800

    susulan router surat
    commit b72a4d68d0debce0720411fe84e9d86a72f9d192
    Author: ahmad afandi <ahmad.afandi85@gmail.com>
    Date:   Wed Jan 17 12:51:15 2024 +0700
    
        router salin-template
    
    commit f52f0f18b0f27ab14210416a596a1cc67a9cf1df
    Author: ahmad afandi <ahmad.afandi85@gmail.com>
    Date:   Wed Jan 17 12:27:38 2024 +0700
    
        perbaikan disposisi
    
    commit 5a5747a55f3633c7958ceb706a8288ca4e68d23d
    Author: ahmad afandi <ahmad.afandi85@gmail.com>
    Date:   Wed Jan 17 11:37:01 2024 +0700
    
        Cek router surat masuk
    
    commit caa63595cdd9567dac846fa8ab12e9489475f2e6
    Merge: d1df901dd7 707dccef7c
    Author: ahmad afandi <ahmad.afandi85@gmail.com>
    Date:   Wed Jan 17 10:53:56 2024 +0700
    
        Merge branch 'bug-fix' of github.com:OpenSID/premium into bug-fix
    
    commit d1df901dd7217899cec6dad3421f365a43176ce1
    Merge: b5b5a711d2 fcf119e80b
    Author: ahmad afandi <ahmad.afandi85@gmail.com>
    Date:   Wed Jan 17 09:16:39 2024 +0700
    
        Merge branch 'bug-fix' of github.com:OpenSID/premium into bug-fix
    
    commit b5b5a711d2882be65e474af176318b119705ddd9
    Merge: 10c3fab9ae 4570474821
    Author: ahmad afandi <ahmad.afandi85@gmail.com>
    Date:   Tue Jan 16 07:45:51 2024 +0700
    
        Merge branch 'bug-fix' of github.com:OpenSID/premium into bug-fix
    
    commit 10c3fab9aedc5ca45f51dd8dc18fb6ee93f3a7aa
    Author: Andi Fahruddin Akas <andifahruddinakas@gmail.com>
    Date:   Mon Jan 15 12:34:49 2024 +0800
    
        RemoveRtfScope
    
    Co-Authored-By: ahmad afandi <ahmad.afandi85@gmail.com>

--- donjo-app/Routes/Web/admin.php
+++ donjo-app/Routes/Web/admin.php
@@ -683,7 +683,7 @@ Route::group('surat_master', static function (): void {
     Route::get('/pengaturan', 'Surat_master@pengaturan')->name('surat_master.pengaturan');
     Route::post('/edit_pengaturan', 'Surat_master@edit_pengaturan')->name('surat_master.edit_pengaturan');
     Route::match(['GET', 'POST'], '/kode_isian/{jenis?}/{id?}', 'Surat_master@kode_isian')->name('surat_master.kode_isian');
-    Route::post('/salin_template/{jenis?}', 'Surat_master@salin_template')->name('surat_master.salin_template');
+    Route::match(['GET', 'POST'],'/salin_template/{jenis?}', 'Surat_master@salin_template')->name('surat_master.salin_template');
     Route::post('/preview', 'Surat_master@preview')->name('surat_master.preview');
     Route::post('/ekspor', 'Surat_master@ekspor')->name('surat_master.ekspor');
     Route::get('/impor_filter/{data}', 'Surat_master@impor_filter')->name('surat_master.impor_filter');
