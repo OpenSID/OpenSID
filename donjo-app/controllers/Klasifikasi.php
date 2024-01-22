89ee4a6c95536d35042118d315b7c7d10a2472bc
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-01-23 01:13:26 +0800

    sesuaikan perubahan terakhir

--- donjo-app/controllers/Klasifikasi.php
+++ donjo-app/controllers/Klasifikasi.php
@@ -43,11 +43,18 @@ use OpenSpout\Writer\Common\Creator\WriterEntityFactory;
 
 class Klasifikasi extends Admin_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->modul_ini     = 'sekretariat';
+        $this->sub_modul_ini = 'klasifikasi-surat';
+    }
+
     public function index()
     {
         $data = [
-            'modul_ini'     => 'sekretariat',
-            'sub_modul_ini' => 'klasifikasi-surat',
+            'modul_ini'     => $this->modul_ini,
+            'sub_modul_ini' => $this->sub_modul_ini,
         ];
 
         return view('admin.klasifikasi.index', $data);
