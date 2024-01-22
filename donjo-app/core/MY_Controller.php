52dc94920e95cbd74a8f40447621d1a683d35017
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-01-23 03:14:28 +0800

    fix strict php 8.x

--- donjo-app/core/MY_Controller.php
+++ donjo-app/core/MY_Controller.php
@@ -414,7 +414,7 @@ class Admin_Controller extends MY_Controller
         if (! $this->session->change_password) {
             return;
         }
-        if ($this->router->class === 'pengguna') {
+        if ($this->controller === 'pengguna') {
             return;
         }
         redirect('pengguna');
@@ -440,7 +440,7 @@ class Admin_Controller extends MY_Controller
         $validasi = $this->premium->validasi();
         $force    = $this->session->force_change_password;
 
-        if ($force && $validasi && ! $kode_desa && $this->router->class != 'pengguna') {
+        if ($force && $validasi && ! $kode_desa && $this->controller !== 'pengguna') {
             redirect('pengguna#sandi');
         }
 
