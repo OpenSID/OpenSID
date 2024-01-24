ec61a236c47f6b6c4340a25f9d1bfff9630523ff
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-01-24 21:41:36 +0800

    sesuaikan perubahan dari bug-fix

--- donjo-app/controllers/Pembangunan_dokumentasi.php
+++ donjo-app/controllers/Pembangunan_dokumentasi.php
@@ -43,13 +43,12 @@ use App\Models\PembangunanDokumentasi;
 
 class Pembangunan_dokumentasi extends Admin_Controller
 {
-    protected $aliasController = 'admin_pembangunan';
+    public $modul_ini = 'pembangunan';
+    public $aliasController = 'admin_pembangunan';
 
     public function __construct()
     {
         parent::__construct();
-        $this->modul_ini   = 'pembangunan';
-        $this->akses_modul = 'pembangunan';
         isCan('b');
     }
 
