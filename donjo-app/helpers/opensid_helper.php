4a54b6c8f0ffa98d41c9c71ec6597974ffc4d8b9
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-03-05 15:19:08 +0800

    versi

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -49,7 +49,7 @@ use voku\helper\AntiXSS;
  * Format => [dua digit tahun dan dua digit bulan].[nomor urut digit beta].[nomor urut digit bugfix]
  * Untuk rilis resmi (tgl 1 tiap bulan) dimulai dari 0 (beta) dan 0 (bugfix)
  */
-define('VERSION', '2403.1.0');
+define('VERSION', '2403.0.1');
 
 /**
  * PREMIUM
@@ -65,7 +65,7 @@ define('PREMIUM', true);
  * Versi database = [yyyymmdd][nomor urut dua digit]
  * [nomor urut dua digit] : 01 => rilis umum, 51 => rilis bugfix, 71 => rilis premium,
  */
-define('VERSI_DATABASE', '2024022571');
+define('VERSI_DATABASE', '2024030171');
 
 /**
  * Minimum versi OpenSID yang bisa melakukan migrasi, backup dan restore database ke versi ini
