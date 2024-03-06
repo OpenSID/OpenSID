c84a8032af2926db9ae7e2b3afe6893dc68fe617
ahmad afandi <ahmad.afandi85@gmail.com>
2024-03-06 08:14:14 +0700

    Sesuaikan install modul, multi migrasi (#3561)

--- donjo-app/controllers/Plugin.php
+++ donjo-app/controllers/Plugin.php
@@ -182,6 +182,8 @@ class Plugin extends Admin_Controller
         $this->load->helper('directory');
         $directoryTable = $this->modulesDirectory . $name . '/Database/Migrations';
         $migrations     = directory_map($directoryTable, 1);
+        // sort by name, jika up maka urutkan dari yang paling lama namun jika down maka urutkan dari yang paling baru
+        $action == 'up' ? usort($migrations, static fn ($a, $b): int => strcmp($a, $b)) : usort($migrations, static fn ($a, $b): int => strcmp($b, $a));
 
         foreach ($migrations as $migrate) {
             $migrateFile = require $directoryTable . DIRECTORY_SEPARATOR . $migrate;
