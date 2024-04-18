c32026e0feca27eff89f842909fde27a9826f94a
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-04-18 11:11:19 +0800

    perbaikan link ke modul keluarga setelah konversi

--- donjo-app/views/penduduk_log/penduduk_log.php
+++ donjo-app/views/penduduk_log/penduduk_log.php
@@ -210,7 +210,7 @@
                                                         <a href="<?= site_url("penduduk/detail/{$p}/{$o}/{$data['id']}")?>"><?= strtoupper($data['nama'])?></a>
                                                     </td>
                                                     <td>
-                                                        <a href="<?= site_url("keluarga/kartu_keluarga/{$p}/{$o}/{$data['id_kk']}")?>"><?= $data['no_kk']?> </a> <br>
+                                                        <a href="<?= site_url("keluarga/kartu_keluarga/{$data['id_kk']}")?>"><?= $data['no_kk']?> </a> <br>
                                                         <?= $data['nama_kk']?>
                                                     </td>
                                                     <td><?= $data['dusun']?></td>
