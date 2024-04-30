0618fa6f44b94c33426c43d5960e8e537aeff442
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-05-01 02:00:48 +0800

    Update opensid_helper.php

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -64,7 +64,7 @@ define('PREMIUM', false);
  * Versi database = [yyyymmdd][nomor urut dua digit]
  * [nomor urut dua digit] : 01 => rilis umum, 51 => rilis bugfix, 71 => rilis premium,
  */
-define('VERSI_DATABASE', '20240501');
+define('VERSI_DATABASE', '2024050101');
 
 // Kode laporan statistik
 define('JUMLAH', 666);
