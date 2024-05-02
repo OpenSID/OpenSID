350703de744c138a00df69176b105b9253a46b48
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-05-02 15:26:06 +0800

    sesuaikan perubahan stuktur token untuk siappakai

--- donjo-app/libraries/Saas.php
+++ donjo-app/libraries/Saas.php
@@ -59,9 +59,10 @@ class Saas
         if ($layanan = $this->ci->cache->file->get('status_langganan')) {
             return collect($layanan->body->pemesanan)
                 ->map(static function ($data) {
-                    $kategori_siappakai = $data->kategori_siappakai ?? 'Dasbor SiapPakai';
-                    $saas               = collect($data->layanan)->firstWhere('nama', $kategori_siappakai);
+                    $kategori_siappakai = $data->layanan->kategori_siappakai ?? 'Dasbor SiapPakai';
+                    $saas               = collect($data->layanan)->firstWhere('nama_kategori', $kategori_siappakai);
 
+                    
                     if ($saas !== null) {
                         $saas->tgl_mulai        = Carbon::parse($data->tgl_mulai);
                         $saas->tgl_akhir        = Carbon::parse($data->tgl_akhir);
