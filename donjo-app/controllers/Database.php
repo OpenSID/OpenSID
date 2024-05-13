1ebcd2ac3cc49c6e45ece5e663a38dbf5e108c9d
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-05-13 12:55:11 +0800

    Hapus tidak digunakan dan duplikasi

--- donjo-app/controllers/Database.php
+++ donjo-app/controllers/Database.php
@@ -37,14 +37,15 @@
 
 defined('BASEPATH') || exit('No direct script access allowed');
 
-use App\Libraries\FlxZipArchive;
-use App\Models\LogBackup;
-use App\Models\LogRestoreDesa;
-use App\Models\Migrasi;
-use App\Models\SettingAplikasi;
-use App\Models\User;
 use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Migrasi;
+use App\Libraries\Sistem;
+use App\Models\LogBackup;
 use Illuminate\Support\Arr;
+use App\Models\LogRestoreDesa;
+use App\Models\SettingAplikasi;
+use App\Libraries\FlxZipArchive;
 use Symfony\Component\Process\Process;
 
 class Database extends Admin_Controller
@@ -71,7 +72,7 @@ class Database extends Admin_Controller
             'act_tab'      => 1,
             'inkremental'  => LogBackup::where('status', '<', 2)->latest()->first(),
             'restore'      => LogRestoreDesa::where('status', '=', 0)->exists(),
-            'memory_limit' => Arr::get($this->setting_model->cekKebutuhanSistem(), 'memory_limit.result'),
+            'memory_limit' => Arr::get(Sistem::cekKebutuhanSistem(), 'memory_limit.result'),
         ];
 
         view('admin.database.index', $data);
@@ -110,7 +111,7 @@ class Database extends Admin_Controller
 
     public function exec_backup()
     {
-        if (! Arr::get($this->setting_model->cekKebutuhanSistem(), 'memory_limit.result')) {
+        if (! Arr::get(Sistem::cekKebutuhanSistem(), 'memory_limit.result')) {
             return show_404();
         }
 
