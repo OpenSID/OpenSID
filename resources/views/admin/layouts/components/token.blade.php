238bbcd70d1017aca1f18328d904ff8738bb9622
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-05-17 15:53:04 +0800

    csrf

--- resources/views/admin/layouts/components/token.blade.php
+++ resources/views/admin/layouts/components/token.blade.php
@@ -4,9 +4,10 @@
         var csrfParam = "{{ $token_name }}";
         var csrfVal = "{{ $token_value }}";
 
-        function getCsrfToken() {
-            return csrfVal;
-        }
+        var getCsrfToken = (csrfVal) => {
+            const match = document.cookie.match(new RegExp(`${csrfParam}=([^;]+)`));
+            return match ? match[1] : csrfVal;
+        };
     </script>
     <script src="{{ asset('js/anti-csrf.js') }}"></script>
 @endif
