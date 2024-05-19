f75b42263827a9951d89664f7c0b93534805f4aa
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-05-20 00:43:23 +0800

    composer update --no-dev

--- vendor/themes/esensi/commons/meta.php
+++ vendor/themes/esensi/commons/meta.php
@@ -3,7 +3,7 @@
 <?php $is_premium = preg_match('/premium/', ambilVersi()) ?>
 <?php $nama_desa = ucwords($this->setting->sebutan_desa) .' '.ucwords($desa['nama_desa']) ?>
 
-<?php defined('THEME_VERSION') or define('THEME_VERSION', 'v2404.0.0') ?>
+<?php defined('THEME_VERSION') or define('THEME_VERSION', 'v2405.0.1') ?>
 <?php defined('IS_PREMIUM') or define('IS_PREMIUM', $is_premium) ?>
 <?php defined('NAMA_DESA') or define('NAMA_DESA', $nama_desa) ?>
 
