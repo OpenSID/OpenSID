f2bcbe75a2a1e462e1b6f988f0681e9004031338
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-05-21 16:33:19 +0800

    perbaikan media sosial pengurus untuk database gabungan

--- donjo-app/models/migrations/Migrasi_dev.php
+++ donjo-app/models/migrations/Migrasi_dev.php
@@ -35,6 +35,7 @@
  *
  */
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
 defined('BASEPATH') || exit('No direct script access allowed');
@@ -59,11 +60,11 @@ class Migrasi_dev extends MY_model
     protected function migrasi_data($hasil)
     {
         // Migrasi berdasarkan config_id
-        // $config_id = DB::table('config')->pluck('id')->toArray();
+        $config_id = DB::table('config')->pluck('id')->toArray();
 
-        // foreach ($config_id as $id) {
-
-        // }
+        foreach ($config_id as $id) {
+            $hasil = $hasil && $this->migrasi_2024052151($hasil, $id);
+        }
         $hasil = $hasil && $this->migrasi_2024051251($hasil);
         $hasil = $hasil && $this->migrasi_2024051252($hasil);
 
@@ -80,6 +81,39 @@ class Migrasi_dev extends MY_model
     protected function migrasi_2024051252($hasil)
     {
         DB::table('tweb_penduduk_umur')->where('nama', 'Di Atas 75 Tahun')->update(['nama' => '75 Tahun ke Atas']);
+
+        return $hasil;
+    }
+
+    protected function migrasi_2024052151($hasil, $id)
+    {
+        $media_sosial = DB::table('media_sosial')
+            ->where('config_id', $id)
+            ->pluck('nama')->map(function ($item) {
+                return Str::slug($item);
+        })->toArray();
+
+        $setting = DB::table('setting_aplikasi')
+            ->where('config_id', $id)
+            ->where('key', 'media_sosial_pemerintah_desa')
+            ->first();
+
+        $value = json_decode($setting->value, true);
+        $option = json_decode($setting->option, true);
+
+        if (count($value) > count($media_sosial)) {
+            $value = array_unique($value);
+            $value = array_filter($value, function ($item) use ($media_sosial) {
+                return in_array($item, $media_sosial);
+            });
+        }
+
+        if (count($option) > count($media_sosial)) {
+            $option = array_unique($option, SORT_REGULAR);
+            $option = array_filter($option, function ($item) use ($media_sosial) {
+                return in_array($item['id'], $media_sosial);
+            });
+        }
 
         return $hasil;
     }
