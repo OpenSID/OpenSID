e9607210e49b46f473d07b8ee4f3096091fec9ba
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-06-19 16:06:05 +0800

    v2406.0.2

--- donjo-app/models/migrations/Migrasi_2024061951.php
+++ donjo-app/models/migrations/Migrasi_2024061951.php
@@ -40,17 +40,30 @@ use Illuminate\Database\Schema\Blueprint;
 
 defined('BASEPATH') || exit('No direct script access allowed');
 
-class Migrasi_rev extends MY_model
+class Migrasi_2024061951 extends MY_model
 {
     public function up()
     {
         $hasil = true;
 
-        // Migrasi berdasarkan config_id
-        // $config_id = DB::table('config')->pluck('id')->toArray();
+        $hasil = $hasil && $this->migrasi_2024061951($hasil);
 
-        // foreach ($config_id as $id) {
-        // }
+        return $hasil && true;
+    }
+
+    protected function migrasi_2024061951($hasil)
+    {
+        if (! Schema::hasColumn('keuangan_ta_jurnal_umum_rinci', 'Kd_SubRinci')) {
+            Schema::table('keuangan_ta_jurnal_umum_rinci', static function (Blueprint $table) {
+                $table->string('Kd_SubRinci', 10)->nullable()->after('Kd_Rincian');
+            });
+        }
+
+        if (! Schema::hasColumn('keuangan_ta_mutasi', 'Kd_SubRinci')) {
+            Schema::table('keuangan_ta_mutasi', static function (Blueprint $table) {
+                $table->string('Kd_SubRinci', 10)->nullable()->after('Kd_Rincian');
+            });
+        }
 
         return $hasil && true;
     }
