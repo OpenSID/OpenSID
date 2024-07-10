ae3562ebe50845e91f1d25fac428553f4d6fe8d6
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2024-07-10 10:20:17 +0800

    penambahan kode isian penduduk luar berupa no_kk dan kepala_kk (#3886)
    Co-authored-by: andifahruddinakas <andifahruddinakas@users.noreply.github.com>
    Co-authored-by: Agung Sugiarto <sugiartoagung92@gmail.com>

--- catatan_rilis.md
+++ catatan_rilis.md
@@ -5,6 +5,7 @@ Terima kasih pada [untuk diisi] telah ikut berkontribusi.
 #### FITUR
 
 1. [#8143](https://github.com/OpenSID/OpenSID/issues/8143) Penambahan surat pernyataan sebagai surat dinas bawaan sistem.
+2. [#8166](https://github.com/OpenSID/OpenSID/issues/8166) Penambahan kode isian penduduk luar berupa no_kk dan kepala_kk.
 
 
 #### BUG
--- donjo-app/models/migrations/Migrasi_beta.php
+++ donjo-app/models/migrations/Migrasi_beta.php
@@ -35,6 +35,7 @@
  *
  */
 
+use App\Models\SettingAplikasi;
 use Illuminate\Support\Facades\DB;
 
 defined('BASEPATH') || exit('No direct script access allowed');
@@ -51,6 +52,20 @@ class Migrasi_beta extends MY_model
         // foreach ($config_id as $id) {
         // }
 
+        $hasil = $hasil && $this->migrasi_2024040271($hasil);
+
         return $hasil && true;
+    }
+
+    protected function migrasi_2024040271($hasil)
+    {
+        $penduduk_luar = SettingAplikasi::withoutGlobalScope(App\Scopes\ConfigIdScope::class)->where('key', '=', 'form_penduduk_luar')->first();
+        if ($penduduk_luar) {
+            $value             = json_decode($penduduk_luar->value, true);
+            $value[3]['input'] = 'nama,no_ktp,tempat_lahir,tanggal_lahir,jenis_kelamin,agama,pendidikan_kk,pekerjaan,warga_negara,alamat,golongan_darah,status_perkawinan,tanggal_perkawinan,shdk,no_paspor,no_kitas,nama_ayah,nama_ibu,no_kk,kepala_kk';
+            $penduduk_luar->update(['value' => json_encode($value)]);
+        }
+
+        return $hasil;
     }
 }
