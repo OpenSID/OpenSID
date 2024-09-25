d5e4f51343154a319ca20725768575da07c7cd58
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-09-25 16:03:53 +0800

    naikkan versi

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -51,7 +51,7 @@ use voku\helper\AntiXSS;
  * Format => [dua digit tahun dan dua digit bulan].[nomor urut digit beta].[nomor urut digit bugfix]
  * Untuk rilis resmi (tgl 1 tiap bulan) dimulai dari 0 (beta) dan 0 (bugfix)
  */
-define('VERSION', '2409.0.2');
+define('VERSION', '2409.1.0');
 
 /**
  * PREMIUM
