69a9a1f74511c1dd4c53426965bfb9a7fc9888d4
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-09-26 09:19:03 +0800

    naikkan versi

--- donjo-app/models/migrations/Migrasi_2024092651.php
+++ donjo-app/models/migrations/Migrasi_2024092651.php
@@ -35,18 +35,50 @@
  *
  */
 
+use Illuminate\Support\Facades\DB;
+
 defined('BASEPATH') || exit('No direct script access allowed');
 
-class Migrasi_rev extends MY_model
+class Migrasi_2024092651 extends MY_model
 {
     public function up()
     {
-        return true;
+        $hasil = $this->migrasi_2024090551(true);
+        $hasil = $this->migrasi_2024092051($hasil);
 
-        // Migrasi berdasarkan config_id
-        // $config_id = DB::table('config')->pluck('id')->toArray();
+        return $hasil && $this->migrasi_2024092151($hasil);
+    }
 
-        // foreach ($config_id as $id) {
-        // }
+    protected function migrasi_2024090551($hasil)
+    {
+        DB::table('setting_aplikasi')
+            ->whereIn('key', ['sebutan_dusun', 'sebutan_singkatan_kadus'])
+            ->where('kategori', '!=', 'Wilayah Administratif')
+            ->update(['kategori' => 'Wilayah Administratif']);
+
+        DB::table('setting_aplikasi')
+            ->where('key', 'sebutan_singkatan_kadus')
+            ->update([
+                'key'        => 'sebutan_kepala_dusun',
+                'keterangan' => 'Sebutan Kepala Dusun',
+            ]);
+
+        return $hasil;
+    }
+
+    public function migrasi_2024092051($hasil)
+    {
+        DB::table('widget')
+            ->where('form_admin', 'web/tab/1000')
+            ->update(['form_admin' => 'web/agenda']);
+
+        return $hasil;
+    }
+
+    public function migrasi_2024092151($hasil)
+    {
+        $hasil = $hasil && checkAndFixTable('log_notifikasi_admin');
+
+        return $hasil && checkAndFixTable('log_notifikasi_mandiri');
     }
 }
