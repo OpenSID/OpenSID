c7ee6e72bf2a5858b51003fcb52f58d102905f47
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2024-11-12 12:24:18 +0800

    website pelatihan

--- donjo-app/helpers/core_helper.php
+++ donjo-app/helpers/core_helper.php
@@ -72,7 +72,7 @@ define('WEBSITE_DEMO', [
     'devpremium.opendesa.id',
     'opensid-premium.test',
     'opensid.test',
-    'pelatihan.opendesa.id',
+    'pelatihan-opensid.opendesa.id',
 ]);
 
 if (! function_exists('cek_anjungan')) {
