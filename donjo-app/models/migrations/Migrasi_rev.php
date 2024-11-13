c8730f9fdcc9d906afc31d8410a0b65f29269b50
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2024-11-13 14:02:49 +0800

    naikkan versi

--- donjo-app/models/migrations/Migrasi_rev.php
+++ donjo-app/models/migrations/Migrasi_rev.php
@@ -35,125 +35,12 @@
  *
  */
 
-use App\Models\FormatSurat;
-use App\Models\GrupAkses;
-use App\Models\Modul;
-use App\Models\UserGrup;
-
 defined('BASEPATH') || exit('No direct script access allowed');
 
 class Migrasi_rev extends MY_model
 {
     public function up()
     {
-        $hasil = true;
-        $hasil = $this->migrasi_2024110351($hasil);
-        $hasil = $this->migrasi_2024110652($hasil);
-        $hasil = $this->migrasi_2024111251($hasil);
-
-        return $this->migrasi_2024110651($hasil);
-    }
-
-    private function migrasi_2024110651($hasil)
-    {
-        $hakAksesBawaan = [
-            'administrator' => [
-                '*' => 7,
-            ],
-            'kontributor' => [
-                'admin-web' => 0,
-                'artikel'   => 3,
-                'komentar'  => 3,
-                'galeri'    => 3,
-                'slider'    => 3,
-            ],
-            'redaksi' => [
-                'admin-web'      => 0,
-                'artikel'        => 3,
-                'widget'         => 3,
-                'menu'           => 3,
-                'komentar'       => 3,
-                'galeri'         => 3,
-                'media-sosial'   => 3,
-                'slider'         => 3,
-                'teks-berjalan'  => 3,
-                'pengunjung'     => 3,
-                'pengaturan-web' => 3,
-                'kategori'       => 3,
-                'lapak'          => 3,
-            ],
-            'operator' => [
-                '*' => 3,
-            ],
-            'satgas-covid-19' => [
-                'statistik'              => 0,
-                'statistik-kependudukan' => 3,
-                'kesehatan'              => 0,
-                'pendataan'              => 7,
-                'pemantauan'             => 7,
-            ],
-        ];
-
-        $configId = identitas('id');
-        $modul    = Modul::get();
-        $modulMap = $modul->pluck('id', 'slug');
-
-        foreach ($hakAksesBawaan as $role => $akses) {
-            $idGrup = UserGrup::where('slug', $role)->first()->id;
-            // jika id grup tidak ditemukan, lanjut saja
-            if (! $idGrup) continue;
-
-            if (count($akses) == 1) {
-                if (array_keys($akses)[0] == '*') {
-                    $modul->each(static function ($q) use ($akses, $idGrup, $configId) {
-                        $dataInsert = [
-                            'config_id' => $configId,
-                            'id_grup'   => $idGrup,
-                            'id_modul'  => $q->id,
-                            'akses'     => $akses['*'],
-                        ];
-                        GrupAkses::upsert($dataInsert, ['id_grup', 'id_modul']);
-                    });
-
-                    continue;
-                }
-            } else {
-                foreach ($akses as $slug => $itemAkses) {
-                    $idModul    = $modulMap[$slug];
-                    $dataInsert = [
-                        'config_id' => $configId,
-                        'id_grup'   => $idGrup,
-                        'id_modul'  => $idModul,
-                        'akses'     => $itemAkses,
-                    ];
-                    GrupAkses::upsert($dataInsert, ['id_grup', 'id_modul']);
-                }
-            }
-        }
-
-        return $hasil;
-    }
-
-    private function migrasi_2024110652($hasil)
-    {
-        copyFavicon();
-
-        return $hasil;
-    }
-
-    protected function migrasi_2024110351($hasil)
-    {
-        $hasil = $hasil && $this->hapus_foreign_key('lokasi', 'persil_peta_fk', 'persil');
-        $hasil = $hasil && $this->tambahForeignKey('persil_peta_fk', 'persil', 'id_peta', 'area', 'id', true);
-        $hasil = $hasil && $this->hapus_foreign_key('lokasi', 'mutasi_cdesa_peta_fk', 'mutasi_cdesa');
-
-        return $hasil && $this->tambahForeignKey('mutasi_cdesa_peta_fk', 'mutasi_cdesa', 'id_peta', 'area', 'id', true);
-    }
-
-    protected function migrasi_2024111251($hasil)
-    {
-        FormatSurat::where('url_surat', 'sistem-surat-keterangan-pengantar-rujukcerai')->where('jenis', FormatSurat::TINYMCE_SISTEM)->delete();
-
-        return $hasil;
+        return true;
     }
 }
