84fd62fe3cd7aaac0b83ca0fd1df9eadfe2f8ea2
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2024-11-20 16:27:35 +0800

    composer update --no-dev

--- vendor/symfony/http-kernel/Kernel.php
+++ vendor/symfony/http-kernel/Kernel.php
@@ -76,11 +76,11 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '6.4.14';
-    public const VERSION_ID = 60414;
+    public const VERSION = '6.4.15';
+    public const VERSION_ID = 60415;
     public const MAJOR_VERSION = 6;
     public const MINOR_VERSION = 4;
-    public const RELEASE_VERSION = 14;
+    public const RELEASE_VERSION = 15;
     public const EXTRA_VERSION = '';
 
     public const END_OF_MAINTENANCE = '11/2026';
