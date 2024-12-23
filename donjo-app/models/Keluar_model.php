0208021962dad2fb628499aeb20c96016cfab3d0
ahmad afandi <ahmad.afandi85@gmail.com>
2024-12-23 08:57:30 +0700

    Perbaikan jabatan pemerintah desa pada surat dan bagan (#4353)

--- donjo-app/models/Keluar_model.php
+++ donjo-app/models/Keluar_model.php
@@ -555,9 +555,16 @@ class Keluar_model extends MY_Model
     {
         $this->load->model('penomoran_surat_model');
 
-        // TODO : Sederhanakan, ini berulang
+        $pj = setting('sebutan_pj_kepala_desa');
+
         $data = $this->config_id('l')
-            ->select('l.*, k.nama AS perihal, k.kode_surat, k.format_nomor_global, k.format_nomor, n.nama AS nama_penduduk, l.nama_jabatan AS pamong_jabatan')
+            ->select('l.*, 
+                    k.nama AS perihal, 
+                    k.kode_surat, 
+                    k.format_nomor_global, 
+                    k.format_nomor, 
+                    n.nama AS nama_penduduk, 
+                    concat(if(s.status_pejabat = 1, \'' . $pj . ' \', \'\'), l.nama_jabatan) AS pamong_jabatan')
             ->select('nama_pamong as pamong_nama')
             ->from('log_surat l')
             ->join('tweb_penduduk n', 'l.id_pend = n.id', 'left')
