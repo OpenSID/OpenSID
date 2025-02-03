c5f3c1ee8e9906433765fad22f0b45af73379c04
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-02-03 17:18:44 +0700

    fix: perbaikan notifikasi simpan modul pengaturan slider (#4506)

--- donjo-app/controllers/Web.php
+++ donjo-app/controllers/Web.php
@@ -492,7 +492,8 @@ class Web extends Admin_Controller
         SettingAplikasi::where('key', 'sumber_gambar_slider')->update(['value' => $this->input->post('pilihan_sumber')]);
         SettingAplikasi::where('key', 'jumlah_gambar_slider')->update(['value' => $this->input->post('jumlah_gambar_slider')]);
         (new SettingAplikasi())->flushQueryCache();
-        redirect('web/slider');
+
+        redirect_with('success', 'Berhasil Ubah Data', 'web/slider');
     }
 
     public function reset($cat): void
