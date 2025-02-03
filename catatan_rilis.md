75a7e7eb3b77188a026c539fa289df4101b0e7d6
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-02-03 21:54:03 +0700

    fix: perbaikan presentase yang ditampilkan pada modul pembangunan (#4508)

--- catatan_rilis.md
+++ catatan_rilis.md
@@ -9,3 +9,4 @@ Terima kasih pada [untuk diisi] telah ikut berkontribusi.
 3. [#9003](https://github.com/OpenSID/OpenSID/issues/9003) Perbaikan dan seragamkan tampilan modal pada modul input keuangan.
 4. [#9015](https://github.com/OpenSID/OpenSID/issues/9015) Perbaikan urutan filter tahun pada modul laporan bulanan.
 5. [#8991](https://github.com/OpenSID/OpenSID/issues/8991) Perbaikan sumber dana pada modul pembangunan.
+6. [#8990](https://github.com/OpenSID/OpenSID/issues/8990) Perbaikan presentase yang ditampilkan pada modul pembangunan.
--- donjo-app/controllers/Pembangunan_dokumentasi.php
+++ donjo-app/controllers/Pembangunan_dokumentasi.php
@@ -87,7 +87,7 @@ class Pembangunan_dokumentasi extends Admin_Controller
 
                     return '';
                 })
-                ->editColumn('persentase', static fn ($row): string => $row->persentase . '%')
+                ->editColumn('persentase', static fn ($row): string => (strpos($row->persentase, '%') === false) ? $row->persentase . '%' : $row->persentase)
                 ->orderColumn('persentase', static function ($query, $order): void {
                     $query->orderByRaw("CONVERT(persentase, SIGNED) {$order}");
                 })
