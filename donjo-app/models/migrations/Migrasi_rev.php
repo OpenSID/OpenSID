f6fa6d46333a7a4521d9271b2246c649f3d8717d
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2025-02-13 21:18:54 +0800

    v2502.0.2

--- donjo-app/models/migrations/Migrasi_rev.php
+++ donjo-app/models/migrations/Migrasi_rev.php
@@ -35,13 +35,7 @@
  *
  */
 
-use App\Models\Dokumen;
-use App\Models\SettingAplikasi;
-use App\Models\Widget;
 use App\Traits\Migrator;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 defined('BASEPATH') || exit('No direct script access allowed');
 
@@ -51,124 +45,5 @@ class Migrasi_rev
 
     public function up()
     {
-        $this->updateIdPendDokumen();
-        $this->hapusTabelSakitMenahun();
-        $this->ubahLinkWidgetKeuangan();
-        $this->tambahPengaturanAPBD();
-        $this->perbaikiUrlQrcodeSurat();
-        $this->ubahWidgetIsi();
-        $this->updateKategoriTable();
-        $this->ubahPengaturanFormatTanggalSurat();
-        $this->hapusForeignKeyTidakDigunakan();
-        $this->ubahOpsiCascade();
-    }
-
-    public function updateIdPendDokumen()
-    {
-        Dokumen::where('id_pend', 0)->update(['id_pend' => null]);
-    }
-
-    protected function hapusTabelSakitMenahun()
-    {
-        Schema::dropIfExists('tweb_sakit_menahun');
-    }
-
-    public function ubahLinkWidgetKeuangan()
-    {
-        Widget::where('isi', 'keuangan.php')->update(['form_admin' => 'keuangan_manual']);
-    }
-
-    public function tambahPengaturanAPBD()
-    {
-        $this->createSetting([
-            'key'        => 'apbdes_tahun',
-            'judul'      => 'Tahun APBDes',
-            'keterangan' => 'Tahun APBDes yang akan ditampilkan dihalaman depan',
-            'value'      => date('Y'),
-            'jenis'      => 'text',
-            'kategori'   => 'conf_web',
-        ]);
-
-        SettingAplikasi::where('key', 'apbdes_manual_input')->delete();
-    }
-
-    public function perbaikiUrlQrcodeSurat()
-    {
-        DB::table('urls')
-            ->leftJoin('log_surat', 'log_surat.urls_id', '=', 'urls.id')
-            ->whereNotNull('log_surat.urls_id')
-            ->where('urls.url', 'REGEXP', '/c1/$')
-            ->where('log_surat.config_id', identitas('id'))
-            ->update([
-                'urls.url' => DB::raw('CONCAT(urls.url, log_surat.id)'),
-            ]);
-    }
-
-    public function ubahWidgetIsi()
-    {
-        DB::table('widget')
-            ->where('config_id', identitas('id'))
-            ->whereRaw('isi REGEXP "\\.blade\\.php$|\\.php$"')
-            ->update([
-                'isi' => DB::raw('REGEXP_REPLACE(SUBSTRING_INDEX(isi, "/", -1), "\\.blade\\.php$|\\.php$", "")'),
-            ]);
-    }
-
-    public function updateKategoriTable()
-    {
-        Schema::table('kategori', static function (Blueprint $table) {
-            $table->integer('parrent')->change();
-        });
-    }
-
-    public function ubahPengaturanFormatTanggalSurat()
-    {
-        $this->createSetting([
-            'judul'      => 'Format Tanggal Surat',
-            'key'        => 'format_tanggal_surat',
-            'value'      => 'd F Y',
-            'keterangan' => 'Format tanggal pada kode isian surat.',
-            'jenis'      => 'text',
-            'option'     => null,
-            'attribute'  => json_encode([
-                'class'       => 'format_tanggal required',
-                'placeholder' => 'd F Y',
-                'type'        => 'text',
-            ]),
-            'kategori' => 'format_surat',
-        ]);
-
-        $this->createSetting([
-            'judul'      => 'Format Tanggal Surat',
-            'key'        => 'format_tanggal_surat_dinas',
-            'value'      => 'd F Y',
-            'keterangan' => 'Format tanggal pada kode isian surat.',
-            'jenis'      => 'text',
-            'option'     => null,
-            'attribute'  => json_encode([
-                'class'       => 'format_tanggal required',
-                'placeholder' => 'd F Y',
-                'type'        => 'text',
-            ]),
-            'kategori' => 'format_surat',
-        ]);
-    }
-
-    public function hapusForeignKeyTidakDigunakan()
-    {
-        $this->hapusForeignKey('suplemen_terdata_suplemen_1', 'suplemen_terdata', 'suplemen');
-    }
-
-    public function ubahOpsiCascade()
-    {
-        Schema::table('mutasi_inventaris_peralatan', static function (Blueprint $table) {
-            $table->dropForeign('FK_mutasi_inventaris_peralatan');
-            $table->foreign('id_inventaris_peralatan', 'FK_mutasi_inventaris_peralatan')->references('id')->on('inventaris_peralatan')->onDelete('cascade')->onUpdate('cascade');
-        });
-
-        Schema::table('mutasi_inventaris_jalan', static function (Blueprint $table) {
-            $table->dropForeign('FK_mutasi_inventaris_jalan');
-            $table->foreign('id_inventaris_jalan', 'FK_mutasi_inventaris_jalan')->references('id')->on('inventaris_jalan')->onDelete('cascade')->onUpdate('cascade');
-        });
     }
 }
