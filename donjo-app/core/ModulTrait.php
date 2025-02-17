0684b0075e2f50471c3e09d641315d43698594ed
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2025-02-18 00:43:37 +0800

    refactor: sesuaikan pembacaan activasi modul dari layanan

--- donjo-app/core/ModulTrait.php
+++ donjo-app/core/ModulTrait.php
@@ -174,19 +174,20 @@ trait ModulTrait
      */
     protected function getLayananModul(): array
     {
-        return cache()->rememberForever('modul_aktif', static function () {
-            $cache = app('ci')->cache->file->get('status_langganan');
+        return cache()->rememberForever('modul_aktif', function () {
+            $cache = app('ci')->cache->file->get('status_langganan')->body;
 
-            return collect($cache->body->pemesanan)
-                ->filter(static fn ($data): bool => $data->status_pemesanan === 'aktif')
-                ->map(
-                    static fn ($data) => collect($data->layanan)
-                        ->filter(static fn ($layanan) => $layanan->nama_kategori === 'Modul')
-                        ->map(static fn ($layanan) => trim(str_replace('Modul', '', $layanan->nama)))
-                        ->toArray()
+            $modulPemesanan = collect($cache->pemesanan)
+                ->where('status_pemesanan', 'aktif')
+                ->flatMap(fn($data) => collect($data->layanan)
+                    ->where('nama_kategori', 'Modul')
+                    ->map(fn($layanan) => trim(str_replace('Modul', '', $layanan->nama)))
                 )
-                ->flatten()
                 ->toArray();
+
+            $modulSiapPakai = collect($cache->modul)->pluck('nama')->toArray();
+
+            return array_merge($modulPemesanan, $modulSiapPakai);
         });
     }
 }
