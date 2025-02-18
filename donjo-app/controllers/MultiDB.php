3b0f8ef537a90e9b8e55e7365a637dbb2a755eb5
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-02-18 22:01:39 +0700

    fix: perbaikan backup restore data statistik pengunjung (#4615)

--- donjo-app/controllers/MultiDB.php
+++ donjo-app/controllers/MultiDB.php
@@ -42,16 +42,249 @@ defined('BASEPATH') || exit('No direct script access allowed');
 
 class MultiDB extends Admin_Controller
 {
-    public $modul_ini                     = 'pengaturan';
-    public $sub_modul_ini                 = 'database';
+    public $modul_ini    = 'pengaturan';
+    public $sub_modul_in = 'database';
+
+    /**
+     * Daftar nama tabel yang digunakan dalam backup database.
+     */
+    private array $tableNames = [
+        'config',
+        'tweb_wil_clusterdesa',
+        'tweb_keluarga',
+        'tweb_rtm',
+        'tweb_penduduk',
+        'suplemen',
+        'suplemen_terdata',
+        'kelompok_master',
+        'kelompok',
+        'kelompok_anggota',
+        'widget',
+        'kategori',
+        'pendapat',
+        'pengaduan',
+        'pesan',
+        'kehadiran_jam_kerja',
+        'kehadiran_hari_libur',
+        'inventaris_tanah',
+        'keuangan_master',
+        'inventaris_peralatan',
+        'inventaris_kontruksi',
+        'inventaris_jalan',
+        'inventaris_gedung',
+        'inventaris_asset',
+        'inbox',
+        'point',
+        'keuangan_manual_rinci',
+        'pemilihan',
+        'polygon',
+        'log_login',
+        'alias_kodeisian',
+        'klasifikasi_surat',
+        'kontak',
+        'kontak_grup',
+        'laporan_sinkronisasi',
+        'line',
+        'log_backup',
+        'log_restore_desa',
+        'outbox',
+        'log_sinkronisasi',
+        'log_tte',
+        'login_attempts',
+        'media_sosial',
+        'menu',
+        'notifikasi',
+        'hubung_warga',
+        'kehadiran_alasan_keluar',
+        'gis_simbol',
+        'tanah_kas_desa',
+        'sys_traffic',
+        'posyandu',
+        'teks_berjalan',
+        // 'theme', // Tidak perlu, karena bisa lakukan scan ulang masing-masing desa
+        'buku_keperluan',
+        'surat_masuk',
+        'urls',
+        'tweb_surat_format',
+        'buku_pertanyaan',
+        'buku_tamu',
+        'surat_keluar',
+        'cdesa',
+        'anjungan',
+        'surat_dinas',
+        'statistics',
+        'user_grup',
+        'gambar_gallery',
+        'produk_kategori',
+        'program',
+        'ref_jabatan',
+        'ref_syarat_surat',
+        'sentitems',
+        'setting_aplikasi',
+        'setting_modul',
+        'tweb_penduduk_umur',
+        'dokumen',
+        'tweb_penduduk_map',
+        'shortcut',
+        'covid19_vaksin',
+        'anjungan_menu',
+        'keuangan',
+        'keuangan_ta_triwulan_rinci',
+        'keuangan_ta_triwulan',
+        'keuangan_ta_spp',
+        'keuangan_ta_tbp_rinci',
+        'keuangan_ta_tbp',
+        'keuangan_ta_sts_rinci',
+        'keuangan_ta_sts',
+        'keuangan_ta_spppot',
+        'keuangan_ta_sppbukti',
+        'keuangan_ta_spp_rinci',
+        'keuangan_ta_spjpot',
+        'keuangan_ta_spj_sisa',
+        'mutasi_inventaris_asset',
+        'mutasi_inventaris_tanah',
+        'pesan_mandiri',
+        'keuangan_ta_spj_bukti',
+        'pesan_detail',
+        'pembangunan_ref_dokumentasi',
+        'pembangunan',
+        'pelapak',
+        'mutasi_inventaris_peralatan',
+        'log_hapus_penduduk',
+        'mutasi_inventaris_jalan',
+        'mutasi_inventaris_gedung',
+        'tanah_desa',
+        'log_notifikasi_admin',
+        'tweb_penduduk_mandiri',
+        'log_notifikasi_mandiri',
+        'keuangan_ta_spj_rinci',
+        'keuangan_ta_spj',
+        'keuangan_ref_rek2',
+        'keuangan_ref_rek1',
+        'keuangan_ref_potongan',
+        'keuangan_ref_perangkat',
+        'keuangan_ref_neraca_close',
+        'keuangan_ref_korolari',
+        'keuangan_ref_kegiatan',
+        'keuangan_ref_kecamatan',
+        'keuangan_ref_desa',
+        'keuangan_ref_bunga',
+        'keuangan_ta_saldo_awal',
+        'keuangan_ref_bel_operasional',
+        'keuangan_ref_bank_desa',
+        'kader_pemberdayaan_masyarakat',
+        'keuangan_ref_rek4',
+        'garis',
+        'dtks_ref_lampiran',
+        'dtks_pengaturan_program',
+        'dtks_lampiran',
+        'covid19_pemudik',
+        'covid19_pantau',
+        'area',
+        'analisis_parameter',
+        'analisis_master',
+        'analisis_klasifikasi',
+        'analisis_kategori_indikator',
+        'keuangan_ref_rek3',
+        'keuangan_ref_bidang',
+        'keuangan_ref_sbu',
+        'keuangan_ta_pajak',
+        'keuangan_ta_rpjm_visi',
+        'keuangan_ta_rpjm_tujuan',
+        'keuangan_ta_rpjm_pagu_tahunan',
+        'keuangan_ta_rpjm_pagu_indikatif',
+        'keuangan_ta_rpjm_misi',
+        'keuangan_ta_rpjm_kegiatan',
+        'keuangan_ta_rpjm_bidang',
+        'keuangan_ta_rab_sub',
+        'keuangan_ta_rab_rinci',
+        'keuangan_ta_rab',
+        'keuangan_ta_perangkat',
+        'keuangan_ta_pencairan',
+        'keuangan_ta_pemda',
+        'keuangan_ta_pajak_rinci',
+        'keuangan_ta_rpjm_sasaran',
+        'keuangan_ta_mutasi',
+        'keuangan_ref_sumber',
+        'keuangan_ta_anggaran',
+        'keuangan_ta_jurnal_umum_rinci',
+        'keuangan_ta_anggaran_log',
+        'keuangan_ta_jurnal_umum',
+        'keuangan_ta_anggaran_rinci',
+        'keuangan_ta_kegiatan',
+        'keuangan_ta_bidang',
+        'keuangan_ta_desa',
+        'tweb_desa_pamong',
+        'user',
+        'artikel',
+        'komentar',
+        'agenda',
+        'cdesa_penduduk',
+        'bulanan_anak',
+        'buku_kepuasan',
+        'kehadiran_perangkat_desa',
+        'log_perubahan_penduduk',
+        'anggota_grup_kontak',
+        'analisis_periode',
+        'lokasi',
+        'log_penduduk',
+        'mutasi_cdesa',
+        'kia',
+        'permohonan_surat',
+        'persil',
+        'kehadiran_pengaduan',
+        'ibu_hamil',
+        'disposisi_surat_masuk',
+        'grup_akses',
+        'log_tolak',
+        'produk',
+        'sasaran_paud',
+        'dtks',
+        'program_peserta',
+        'analisis_respon_bukti',
+        'analisis_respon_hasil',
+        'analisis_respon',
+        'log_keluarga',
+        'analisis_partisipasi',
+        'dtks_anggota',
+        'analisis_indikator',
+        'log_surat',
+        'log_surat_dinas',
+    ];
+
+    /**
+     * Daftar nama tabel yang dikecualikan dalam backup database.
+     */
+    private array $excludeTableNames = [
+        'analisis_partisipasi',    // Tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT (dihapus tidak digunakan)
+        'analisis_respon',         // Tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT (dihapus tidak digunakan)
+        'analisis_respon_bukti',   // Tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT (dihapus tidak digunakan)
+        'dtks_ref_lampiran',       // Tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT (dihapus tidak digunakan)
+        'tweb_penduduk_map',       // Tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT
+        'tweb_penduduk_mandiri',   // Tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT
+        'log_notifikasi_mandiri',  // Tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT
+        'log_notifikasi_admin',    // Tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT
+
+        // Error saat restore
+        'fcm_token_mandiri',
+        'fcm_token',
+    ];
+
+    /**
+     * Daftar nama tabel yang berisi tergantung pada data penduduk.
+     */
     private array $tergantungDataPenduduk = [
         'tweb_keluarga'        => ['key' => 'nik_kepala', 'nik_kepala' => [], 'unique_record' => ['no_kk']],
         'tweb_rtm'             => ['key' => 'nik_kepala', 'nik_kepala' => [], 'unique_record' => ['no_kk']],
         'tweb_wil_clusterdesa' => ['key' => 'id_kepala', 'id_kepala' => [], 'unique_record' => ['rt', 'rw', 'dusun']],
     ];
 
-    // Tabel dengan kondisi khusus, memiliki child dan parent
-    public $tabelKhusus = [
+    /**
+     * Daftar nama tabel yang digunakan dengan kondisi khusus, memiliki child dan parent.
+     *
+     * @var array
+     */
+    private $tabelKhusus = [
         'gambar_gallery' => ['id', 'parrent'],
         'line'           => ['id', 'parrent'],
         'menu'           => ['id', 'parrent'],
@@ -61,7 +294,13 @@ class MultiDB extends Admin_Controller
         'setting_modul'  => ['id', 'parent'],
         'dokumen'        => ['id', 'id_parent'],
     ];
-    public $tabelRelasiJson = [
+
+    /**
+     * Daftar nama tabel yang digunakan untuk mendefinisikan relasi antara tabel-tabel dalam format JSON.
+     *
+     * @var array
+     */
+    private $tabelRelasiJson = [
         'tweb_wil_clusterdesa' => [
             'user' => 'akses_wilayah',
         ],
@@ -75,232 +314,8 @@ class MultiDB extends Admin_Controller
 
     public function backup(): void
     {
-        $tableNames = [
-            'config',
-            'tweb_wil_clusterdesa',
-            'tweb_keluarga',
-            'tweb_rtm',
-            'tweb_penduduk',
-            'suplemen',
-            'suplemen_terdata',
-            'kelompok_master',
-            'kelompok',
-            'kelompok_anggota',
-            'widget',
-            'kategori',
-            'pendapat',
-            'pengaduan',
-            'pesan',
-            'kehadiran_jam_kerja',
-            'kehadiran_hari_libur',
-            'inventaris_tanah',
-            'keuangan_master',
-            'inventaris_peralatan',
-            'inventaris_kontruksi',
-            'inventaris_jalan',
-            'inventaris_gedung',
-            'inventaris_asset',
-            'inbox',
-            'point',
-            'keuangan_manual_rinci',
-            'pemilihan',
-            'polygon',
-            'log_login',
-            'alias_kodeisian',
-            'klasifikasi_surat',
-            'kontak',
-            'kontak_grup',
-            'laporan_sinkronisasi',
-            'line',
-            'log_backup',
-            'log_restore_desa',
-            'outbox',
-            'log_sinkronisasi',
-            'log_tte',
-            'login_attempts',
-            'media_sosial',
-            'menu',
-            'notifikasi',
-            'hubung_warga',
-            'kehadiran_alasan_keluar',
-            'gis_simbol',
-            'tanah_kas_desa',
-            'sys_traffic',
-            'posyandu',
-            'teks_berjalan',
-            // 'theme', // Tidak perlu, karena bisa lakukan scan ulang masing-masing desa
-            'buku_keperluan',
-            'surat_masuk',
-            'urls',
-            'tweb_surat_format',
-            'buku_pertanyaan',
-            'buku_tamu',
-            'surat_keluar',
-            'cdesa',
-            'anjungan',
-            'surat_dinas',
-            'statistics',
-            'user_grup',
-            'gambar_gallery',
-            'produk_kategori',
-            'program',
-            'ref_jabatan',
-            'ref_syarat_surat',
-            'sentitems',
-            'setting_aplikasi',
-            'setting_modul',
-            'tweb_penduduk_umur',
-            'dokumen',
-            'tweb_penduduk_map',
-            'shortcut',
-            'covid19_vaksin',
-            'anjungan_menu',
-            'keuangan',
-            'keuangan_ta_triwulan_rinci',
-            'keuangan_ta_triwulan',
-            'keuangan_ta_spp',
-            'keuangan_ta_tbp_rinci',
-            'keuangan_ta_tbp',
-            'keuangan_ta_sts_rinci',
-            'keuangan_ta_sts',
-            'keuangan_ta_spppot',
-            'keuangan_ta_sppbukti',
-            'keuangan_ta_spp_rinci',
-            'keuangan_ta_spjpot',
-            'keuangan_ta_spj_sisa',
-            'mutasi_inventaris_asset',
-            'mutasi_inventaris_tanah',
-            'pesan_mandiri',
-            'keuangan_ta_spj_bukti',
-            'pesan_detail',
-            'pembangunan_ref_dokumentasi',
-            'pembangunan',
-            'pelapak',
-            'mutasi_inventaris_peralatan',
-            'log_hapus_penduduk',
-            'mutasi_inventaris_jalan',
-            'mutasi_inventaris_gedung',
-            'tanah_desa',
-            'log_notifikasi_admin',
-            'tweb_penduduk_mandiri',
-            'log_notifikasi_mandiri',
-            'keuangan_ta_spj_rinci',
-            'keuangan_ta_spj',
-            'keuangan_ref_rek2',
-            'keuangan_ref_rek1',
-            'keuangan_ref_potongan',
-            'keuangan_ref_perangkat',
-            'keuangan_ref_neraca_close',
-            'keuangan_ref_korolari',
-            'keuangan_ref_kegiatan',
-            'keuangan_ref_kecamatan',
-            'keuangan_ref_desa',
-            'keuangan_ref_bunga',
-            'keuangan_ta_saldo_awal',
-            'keuangan_ref_bel_operasional',
-            'keuangan_ref_bank_desa',
-            'kader_pemberdayaan_masyarakat',
-            'keuangan_ref_rek4',
-            'garis',
-            'dtks_ref_lampiran',
-            'dtks_pengaturan_program',
-            'dtks_lampiran',
-            'covid19_pemudik',
-            'covid19_pantau',
-            'area',
-            'analisis_parameter',
-            'analisis_master',
-            'analisis_klasifikasi',
-            'analisis_kategori_indikator',
-            'keuangan_ref_rek3',
-            'keuangan_ref_bidang',
-            'keuangan_ref_sbu',
-            'keuangan_ta_pajak',
-            'keuangan_ta_rpjm_visi',
-            'keuangan_ta_rpjm_tujuan',
-            'keuangan_ta_rpjm_pagu_tahunan',
-            'keuangan_ta_rpjm_pagu_indikatif',
-            'keuangan_ta_rpjm_misi',
-            'keuangan_ta_rpjm_kegiatan',
-            'keuangan_ta_rpjm_bidang',
-            'keuangan_ta_rab_sub',
-            'keuangan_ta_rab_rinci',
-            'keuangan_ta_rab',
-            'keuangan_ta_perangkat',
-            'keuangan_ta_pencairan',
-            'keuangan_ta_pemda',
-            'keuangan_ta_pajak_rinci',
-            'keuangan_ta_rpjm_sasaran',
-            'keuangan_ta_mutasi',
-            'keuangan_ref_sumber',
-            'keuangan_ta_anggaran',
-            'keuangan_ta_jurnal_umum_rinci',
-            'keuangan_ta_anggaran_log',
-            'keuangan_ta_jurnal_umum',
-            'keuangan_ta_anggaran_rinci',
-            'keuangan_ta_kegiatan',
-            'keuangan_ta_bidang',
-            'keuangan_ta_desa',
-            'tweb_desa_pamong',
-            'user',
-            'artikel',
-            'komentar',
-            'agenda',
-            'cdesa_penduduk',
-            'bulanan_anak',
-            'buku_kepuasan',
-            'kehadiran_perangkat_desa',
-            'log_perubahan_penduduk',
-            'anggota_grup_kontak',
-            'analisis_periode',
-            'lokasi',
-            'log_penduduk',
-            'mutasi_cdesa',
-            'kia',
-            'permohonan_surat',
-            'persil',
-            'kehadiran_pengaduan',
-            'ibu_hamil',
-            'disposisi_surat_masuk',
-            'grup_akses',
-            'log_tolak',
-            'produk',
-            'sasaran_paud',
-            'dtks',
-            'program_peserta',
-            'analisis_respon_bukti',
-            'analisis_respon_hasil',
-            'analisis_respon',
-            'log_keluarga',
-            'analisis_partisipasi',
-            'dtks_anggota',
-            'analisis_indikator',
-            'log_surat',
-            'log_surat_dinas',
-        ];
-
-        $kecuali = [
-            'analisis_partisipasi', // tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT (dihapus tidak digunakan)
-            'analisis_respon', // tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT (dihapus tidak digunakan)
-            'analisis_respon_bukti', // tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT (dihapus tidak digunakan)
-            'dtks_ref_lampiran', // tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT (dihapus tidak digunakan)
-            'log_login', // karena menggunakan uuid
-            'pemilihan', // karena menggunakan uuid
-            'pesan_mandiri', // karena menggunakan uuid
-            'tweb_penduduk_map', // tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT
-            'tweb_penduduk_mandiri', // tidak perlu, karena tidak ada kolom `id` AUTO_INCREMENT
-            'log_notifikasi_mandiri',
-            'log_notifikasi_admin',
-            'analisis_parameter', // skip dlu, terjadi error
-
-            // error saat restore
-            'sys_traffic',
-            'fcm_token_mandiri',
-            'fcm_token',
-        ];
-
         // Filter tabel yang boleh di-backup
-        $tableNames = collect($tableNames)->filter(static fn ($tableName): bool => ! in_array($tableName, $kecuali));
+        $tableNames = collect($this->tableNames)->filter(fn ($tableName): bool => ! in_array($tableName, $this->excludeTableNames));
 
         // Ambil max ID untuk setiap tabel
         $maxIds = $this->getMaxIdForTables($tableNames->toArray());
@@ -468,31 +483,32 @@ class MultiDB extends Admin_Controller
         $backupFile = $uploadConfig['upload_path'] . '/' . $uploadData['file_name'];
         $backupData = json_decode(file_get_contents($backupFile), true);
 
-        DB::beginTransaction();
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
-
         try {
+            $connection = DB::connection();
+            $connection->statement('SET FOREIGN_KEY_CHECKS=0');
+
             $this->validateBackupData($backupData);
             $this->restoreConfigData($backupData['tabel']['config']['data']);
             $this->deleteExistingData($backupData['tabel']);
             $this->restoreBackupData($backupData['tabel']);
 
-            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+            $connection->statement('SET FOREIGN_KEY_CHECKS=1');
 
             $this->updateDependentData($backupData['tabel']['tweb_penduduk']['data'], $backupData['info']['random']);
             $this->updateDataJsonTable($backupData['info']['random']);
 
-            DB::commit();
+            $connection->commit();
             hapus_cache('_cache_modul');
             kosongkanFolder(config_item('cache_blade'));
             cache()->flush();
 
             redirect_with('success', 'Proses restore dari backup berhasil.', ci_route('database'));
         } catch (Exception $e) {
-            DB::rollBack();
-            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+            $connection->rollBack();
+            $connection->statement('SET FOREIGN_KEY_CHECKS=1');
 
-            log_message('error', $e);
+            Log::error($e);
+
             redirect_with('error', 'Proses restore dari backup gagal. <br><br>' . $e->getMessage(), ci_route('database'));
         }
     }
