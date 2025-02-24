8a7d6baf61feeb0785d29c6d8c4017c5a1915c29
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2025-02-24 10:14:49 +0800

    fix: autoplay video anjungan

--- Modules/Anjungan/Views/frontend/index.blade.php
+++ Modules/Anjungan/Views/frontend/index.blade.php
@@ -144,9 +144,9 @@
                             </div>
                         @else
                             <div class="video-container">
-                                <iframe class="video-view" src="{{ setting('anjungan_youtube') }}?autoplay=1&controls=1&mute=1&loop=1" frameborder="0"></iframe>
+                                <iframe class="video-view" src="{{ setting('anjungan_youtube') }}?autoplay=1&controls=1&mute=0&loop=1"
+                                    frameborder="0"></iframe>
                             </div>
-
                         @endif
 
                     </div>
