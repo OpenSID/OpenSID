fb1a42d487e96050406c6334820a95f37da70451
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-06-17 10:42:51 +0700

    fix: perbaikan muncul error ketika migrasi (#5039)

--- app/Models/SettingAplikasi.php
+++ app/Models/SettingAplikasi.php
@@ -42,6 +42,7 @@ use App\Models\Galery as Galeri;
 use App\Traits\ConfigId;
 use Illuminate\Support\Facades\Schema;
 use Rennokki\QueryCache\Traits\QueryCacheable;
+use Spatie\Activitylog\ActivityLogStatus;
 use Spatie\Activitylog\Contracts\Activity;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -155,9 +156,10 @@ class SettingAplikasi extends BaseModel
     {
         // Cek apakah tabel log_activity tersedia
         if (! Schema::hasTable('log_activity')) {
-            logger()->warning('Tabel log_activity tidak tersedia, log aktivitas tidak akan dicatat.');
+            logger()->warning(sprintf('Tabel log_activity tidak tersedia, log aktivitas tidak akan dicatat pada: %s', self::class));
 
-            return;
+            $this->disableLogging();
+            app(ActivityLogStatus::class)->disable();
         }
     }
 
