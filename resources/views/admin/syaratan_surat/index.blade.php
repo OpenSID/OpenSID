c6e6e497d7f266ba49c69daeb0369fa1827fb63e
Firliani Fauziah <firlianif@gmail.com>
2025-06-25 13:52:37 +0700

    fix: perbaikan dan konsistensi komponen pada modul dokumen persyaratan surat (#5075)

--- resources/views/admin/syaratan_surat/index.blade.php
+++ resources/views/admin/syaratan_surat/index.blade.php
@@ -16,13 +16,12 @@
     @include('admin.layouts.components.notifikasi')
     <div class="box box-info">
         <div class="box-header with-border">
-            @if (can('u'))
-                <a href="{{ ci_route('surat_mohon.form') }}" class="btn btn-social btn-success btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"><i class="fa fa-plus"></i> Tambah</a>
-            @endif
-            @if (can('h'))
-                <a href="#confirm-delete" title="Hapus Data" onclick="deleteAllBox('mainform', '{{ ci_route('surat_mohon.deleteAll') }}')" class="btn btn-social btn-danger btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block hapus-terpilih"><i
-                        class='fa fa-trash-o'></i> Hapus</a>
-            @endif
+            @include('admin.layouts.components.buttons.tambah', ['url' => 'surat_mohon/form'])
+            @include('admin.layouts.components.buttons.hapus', [
+                'url' => "surat_mohon/deleteAll",
+                'confirmDelete' => true,
+                'selectData' => true,
+            ])
         </div>
         <div class="box-body">
             {!! form_open(null, 'id="mainform" name="mainform"') !!}
