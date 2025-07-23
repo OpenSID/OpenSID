227d1cf386c44c61949495d02d76921b8f917d8f
Firliani Fauziah <firlianif@gmail.com>
2025-07-23 10:11:08 +0700

    fix: perbaikan dan seragamkan komponen pada  modul pendaftar layanan mandiri (#5149)

--- resources/views/admin/layouts/components/buttons/btn.blade.php
+++ resources/views/admin/layouts/components/buttons/btn.blade.php
@@ -1,5 +1,8 @@
 @if ($modal)
-    <a
+    @if($buttonOnly)
+        <a href="{{$url}}" class="btn {{ $type }} btn-sm" {{ $attribut }} data-remote="false" data-toggle="modal" data-target="#modalBox" data-title="{{ $judul }}" title="{{ $judul }}"><i class="{{ $icon }}"></i></a>
+    @else
+        <a
         href="{{ site_url($url) }}"
         class="btn btn-social {{ $type }} btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"
         title="{{ $judul }}"
@@ -10,7 +13,9 @@
         data-keyboard="false"
         data-title="{{ $judul }}"
     ><i class="{{ $icon }}"></i>
-        {{ $judul }}</a>
+        {{ $judul }}</a>   
+    @endif
+    
 @else
     @if($buttonOnly)
         <a href="{{$url}}" class="btn {{ $type }} btn-sm"  title="{{ $judul }}"><i class="{{ $icon }}"></i></a>
