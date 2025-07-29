3793df9ba3051ff10fefab09d2cbf22f1c013fe8
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-07-29 22:57:41 +0700

    fix: pindahkan dan sesuaikan struktur folder database (#5198)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -742,11 +742,11 @@ class ComposerStaticInita039d07fded53f984d6274b4115b6433
         ),
         'Database\\Seeders\\' => 
         array (
-            0 => __DIR__ . '/../..' . '/database/seeders',
+            0 => __DIR__ . '/../..' . '/app/database/seeders',
         ),
         'Database\\Factories\\' => 
         array (
-            0 => __DIR__ . '/../..' . '/database/factories',
+            0 => __DIR__ . '/../..' . '/app/database/factories',
         ),
         'Cviebrock\\EloquentSluggable\\' => 
         array (
