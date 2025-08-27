72e2cbfd646ee3bc999723198eaed26b08ddf749
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2025-08-27 09:50:39 +0800

    fix: pindahkan migrasi

--- donjo-app/models/migrations/Migrasi_2025082651.php
+++ donjo-app/models/migrations/Migrasi_2025082651.php
@@ -38,6 +38,7 @@
 use App\Traits\Migrator;
 use App\Enums\StatusEnum;
 use Illuminate\Support\Facades\DB;
+use App\Enums\AnalisisRefSubjekEnum;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 
@@ -56,6 +57,7 @@ class Migrasi_2025082651
         $this->updateKolomWajibPendudukTidakBolehNull();
         $this->tambahPengaturanPelaporPengaduan();
         $this->tambahKolomPekerjaMigran();
+        $this->updateAnalisis();
     }
 
     public function perbaikiMigrasiModulKeuangan()
@@ -141,5 +143,90 @@ class Migrasi_2025082651
                 $table->string('pekerja_migran')->nullable()->after('adat');
             });
         }
+    }
+
+    public function updateAnalisis()
+    {
+        $this->hapusForeignKey('analisis_respon_bukti_subjek_fk', 'analisis_respon_bukti', 'analisis_ref_subjek');
+
+        $columnMappings = [
+            'penduduk_id' => 'tweb_penduduk',
+            'keluarga_id' => 'tweb_keluarga',
+            'kelompok_id' => 'kelompok',
+            'rtm_id'      => 'tweb_rtm',
+            'desa_id'     => 'config',
+            'dusun_id'    => 'tweb_wil_clusterdesa',
+            'rw_id'       => 'tweb_wil_clusterdesa',
+            'rt_id'       => 'tweb_wil_clusterdesa',
+        ];
+
+        $subjekMappings = [
+            AnalisisRefSubjekEnum::PENDUDUK     => 'penduduk_id',
+            AnalisisRefSubjekEnum::KELUARGA     => 'keluarga_id',
+            AnalisisRefSubjekEnum::RUMAH_TANGGA => 'rtm_id',
+            AnalisisRefSubjekEnum::KELOMPOK     => 'kelompok_id',
+            AnalisisRefSubjekEnum::DESA         => 'desa_id',
+            AnalisisRefSubjekEnum::DUSUN        => 'dusun_id',
+            AnalisisRefSubjekEnum::RW           => 'rw_id',
+            AnalisisRefSubjekEnum::RT           => 'rt_id',
+        ];
+
+        $targetTables = [
+            'analisis_respon',
+            'analisis_respon_bukti',
+            'analisis_respon_hasil',
+        ];
+
+        foreach ($targetTables as $tableName) {
+            Schema::table($tableName, static function (Blueprint $table) use ($tableName, $columnMappings) {
+                if (Schema::hasIndex($tableName, "{$tableName}_subjek_fk")) {
+                    $table->dropIndex("{$tableName}_subjek_fk");
+                }
+
+                foreach ($columnMappings as $columnName => $referenceTable) {
+                    if (! Schema::hasColumn($tableName, $columnName)) {
+                        $table->integer($columnName)->nullable();
+                        $table->foreign($columnName)
+                            ->references('id')
+                            ->on($referenceTable)
+                            ->onUpdate('cascade')
+                            ->onDelete('cascade');
+                    }
+                }
+            });
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        foreach ($targetTables as $table) {
+            $caseUpdate = [];
+
+            foreach ($subjekMappings as $subjekTipe => $columnName) {
+                $caseUpdate[$columnName] = DB::raw("
+                    CASE
+                        WHEN analisis_master.subjek_tipe = {$subjekTipe} AND {$table}.{$columnName} IS NULL
+                        THEN {$table}.id_subjek
+                        ELSE {$table}.{$columnName}
+                    END
+                ");
+            }
+
+            DB::table($table)
+                ->when(
+                    $table === 'analisis_respon',
+                    static function ($query) {
+                        $query->join('analisis_periode', 'analisis_periode.id', '=', 'analisis_respon.id_periode')
+                            ->join('analisis_master', 'analisis_master.id', '=', 'analisis_periode.id_master');
+                    },
+                    static function ($query) use ($table) {
+                        $query->join('analisis_master', 'analisis_master.id', '=', "{$table}.id_master");
+                    }
+                )
+                ->whereNotNull("{$table}.id_subjek")
+                ->where("{$table}.config_id", identitas('id'))
+                ->update($caseUpdate);
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
