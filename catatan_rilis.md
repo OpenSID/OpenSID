4312b2bfa66bdf98f89d5478de78975ff0606e3c
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-10-15 09:24:13 +0700

    fix: perbaikan dan penambahan informasi pada modul rekapitulasi stunting (#5461)

--- catatan_rilis.md
+++ catatan_rilis.md
@@ -4,6 +4,7 @@ Di rilis ini, versi 2510.1.0 berisi [untuk diisi] dan perbaikan lain yang dimint
 ### BUG
 1. [#9172](https://github.com/OpenSID/OpenSID/issues/9172) Perbaikan dan pembatasan perubahan SDHK pada kepala keluarga.
 2. [#10147](https://github.com/OpenSID/OpenSID/issues/10147) Perbaikan dan penambahan halaman periksa untuk data wilayah yang duplikat.
+3. [#10263](https://github.com/OpenSID/OpenSID/issues/10263) Perbaikan dan penambahan informasi pada modul rekapitulasi stunting.
 
 
 ### TEKNIS
--- resources/views/admin/stunting/filter.blade.php
+++ resources/views/admin/stunting/filter.blade.php
@@ -1,84 +1,115 @@
-<div class="col-md-12 no-padding">
-    <div class="row col-md-5">
-        <div class="col-md-5">
-            <div class="form-group">
-                <select name="awal_bulan" id="awal_bulan" required class="form-control input-sm" title="Pilih salah satu">
-                    @for ($i = 1; $i <= 12; $i++)
-                        <option @selected($awalBulan == $i) value="{{ $i }}">{{ getBulan($i) }}
-                        </option>
-                    @endfor
-                </select>
-            </div>
-        </div>
-        <div class="col-md-2">sd</div>
-        <div class="col-md-5">
-            <div class="form-group">
-                <select name="akhir_bulan" id="akhir_bulan" required class="form-control input-sm" title="Pilih salah satu">
-                    @for ($i = 1; $i <= 12; $i++)
-                        <option @selected($akhirBulan == $i) value="{{ $i }}">{{ getBulan($i) }}
-                        </option>
-                    @endfor
-                </select>
+<div class="row mepet">
+    <div class="col-md-4 col-sm-6">
+        <div class="form-group">
+            <div class="row">
+                <div class="col-xs-5">
+                    <select name="awal_bulan" id="awal_bulan" required class="form-control input-sm" 
+                            title="Bulan Awal">
+                        @for ($i = 1; $i <= 12; $i++)
+                            <option @selected($awalBulan == $i) value="{{ $i }}">{{ getBulan($i) }}
+                            </option>
+                        @endfor
+                    </select>
+                </div>
+                <div class="col-xs-2 text-center">
+                    <small class="text-muted">s/d</small>
+                </div>
+                <div class="col-xs-5">
+                    <select name="akhir_bulan" id="akhir_bulan" required class="form-control input-sm" 
+                            title="Bulan Akhir">
+                        @for ($i = 1; $i <= 12; $i++)
+                            <option @selected($akhirBulan == $i) value="{{ $i }}">{{ getBulan($i) }}
+                            </option>
+                        @endfor
+                    </select>
+                </div>
             </div>
         </div>
     </div>
-
-    <div class="col-md-2">
+    
+    <div class="col-md-2 col-sm-3 col-xs-6">
         <div class="form-group">
-            <select name="tahun" id="tahun" required class="form-control input-sm" title="Pilih salah satu">
+            <select name="tahun" id="tahun" required class="form-control input-sm" title="Tahun">
                 @foreach ($dataTahun as $item)
                     <option value="{{ $item->tahun }}">{{ $item->tahun }}</option>
                 @endforeach
             </select>
         </div>
     </div>
-    <div class="col-md-3">
+    
+    <div class="col-md-4 col-sm-6">
         <div class="form-group">
-            <select name="id" id="id" required class="form-control input-sm" title="Pilih salah satu">
-                <option value="">Semua</option>
+            <select name="id" id="id" class="form-control input-sm" title="Posyandu">
+                <option value="">-- Semua Posyandu --</option>
                 @foreach ($posyandu as $item)
                     <option value="{{ $item->id }}" {{ $item->id == $id ? 'selected' : '' }}>
-                        {{ $item->nama }}</option>
+                        {{ $item->nama }}
+                    </option>
                 @endforeach
             </select>
         </div>
     </div>
-    <div class="col-md-2 no-padding">
-        <button type="button" class="btn btn-social btn-info btn-sm" id="cari">
-            <i class="fa fa-search"></i> Cari
-        </button>
+    
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="form-group">
+            <button type="button" class="btn btn-primary btn-sm btn-block" id="cari">
+                <i class="fa fa-search"></i> 
+                <span class="hidden-xs">Cari</span>
+            </button>
+        </div>
     </div>
 </div>
 @push('scripts')
     <script>
         $(document).ready(function() {
+            // Validasi periode bulan
             $('#awal_bulan').change(function() {
-                let _val = $(this).val()
-                let _akhirBulan = $('#akhir_bulan')
-
-                _akhirBulan.find('option').prop('disabled', false)
-                $('#akhir_bulan option').each(function() {
-                    if (parseInt($(this).val()) < _val) {
+                let selectedMonth = parseInt($(this).val());
+                let akhirBulanSelect = $('#akhir_bulan');
+                
+                // Reset dan disable option yang tidak valid
+                akhirBulanSelect.find('option').prop('disabled', false);
+                akhirBulanSelect.find('option').each(function() {
+                    if (parseInt($(this).val()) < selectedMonth) {
                         $(this).prop('disabled', true);
                     }
                 });
 
-                if (_akhirBulan.val() < _val) {
-                    _akhirBulan.val(_val)
+                // Auto-adjust jika bulan akhir lebih kecil dari bulan awal
+                if (parseInt(akhirBulanSelect.val()) < selectedMonth) {
+                    akhirBulanSelect.val(selectedMonth);
                 }
-            })
-
-            $('#cari').click(function() {
-                let awal_bulan = $('#awal_bulan').val();
-                let akhir_bulan = $('#akhir_bulan').val();
-                let periode = `${awal_bulan}__${akhir_bulan}`
-                let tahun = $('#tahun option:selected').val();
-                let posyandu = $('#id option:selected').val();
-                window.location.href = "{{ $urlFilter }}/" + periode + "/" +
-                    tahun + "/" + posyandu;
             });
 
+            // Handler tombol cari
+            $('#cari').click(function() {
+                let btn = $(this);
+                let originalHtml = btn.html();
+                
+                // Validasi input
+                let awalBulan = $('#awal_bulan').val();
+                let akhirBulan = $('#akhir_bulan').val();
+                let tahun = $('#tahun').val();
+                
+                if (!awalBulan || !akhirBulan || !tahun) {
+                    alert('Mohon lengkapi periode bulan dan tahun!');
+                    return;
+                }
+
+                // Loading state
+                btn.prop('disabled', true).html('<i class="fa fa-spinner fa-spin"></i> <span class="hidden-xs">Tunggu...</span>');
+                
+                // Build URL
+                let periode = awalBulan + '__' + akhirBulan;
+                let posyandu = $('#id').val() || '';
+                let url = "{{ $urlFilter }}/" + periode + "/" + tahun + "/" + posyandu;
+                
+                // Navigate
+                window.location.href = url;
+            });
+
+            // Trigger initial validation
             $('#awal_bulan').trigger('change');
-        })
+        });
     </script>
 @endpush
