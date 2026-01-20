06ac4c1127c7d4ea55052601022a12753fa38f8b
Firliani Fauziah <firlianif@gmail.com>
2026-01-20 16:47:48 +0700

    fitur: tambah validasi bayi baru lahir shdk anak pada keluarga (#5843)

--- catatan_rilis.md
+++ catatan_rilis.md
@@ -14,6 +14,7 @@ Rilis versi 2601.0.1 ini berisi penambahan notifikasi berbasis laravel dan perba
 11. [#10644](https://github.com/OpenSID/OpenSID/issues/10644) Fitur Isian data istri terdahulu di surat keterangan nikah bisa dari penduduk luar desa.
 12. [#10643](https://github.com/OpenSID/OpenSID/issues/10643) Penambahan alamat lengkap lampiran surat keterangan nikah.
 13. [#10728](https://github.com/OpenSID/OpenSID/issues/10728) Ganti Istilah Cacat menjadi Disabilitas.
+14. [#10628](https://github.com/OpenSID/OpenSID/issues/10628) Penambahan fitur tambah validasi bayi baru lahir shdk anak pada keluarga.
 
 ### BUG
 1. [#10678](https://github.com/OpenSID/OpenSID/issues/10678) Perbaikan typo pada pesan error hapus Pemerintah Desa.
--- resources/views/admin/penduduk/penduduk_form_isian_bersama.blade.php
+++ resources/views/admin/penduduk/penduduk_form_isian_bersama.blade.php
@@ -1538,16 +1538,36 @@
         function orang_tua() {
             var id_kk = $('#id_kk').val();
             var kk_level = $('#kk_level').val();
-            if (id_kk && (kk_level == 4 || '{{ $jenis_peristiwa }}' == 1)) {
-                $('#ayah_nik').val(@json($data_ayah['nik'] ?? ''));
-                $('#nama_ayah').val(@json($data_ayah['nama'] ?? ''));
-                $('#ibu_nik').val(@json($data_ibu['nik'] ?? ''));
-                $('#nama_ibu').val(@json($data_ibu['nama'] ?? ''));
+            var jenis_peristiwa = '{{ $jenis_peristiwa }}';
+            
+            // Untuk bayi baru lahir
+            if (jenis_peristiwa == 1) {
+                // Jika SHDK adalah Anak (kk_level == 4), ambil data Kepala Keluarga dan Istri
+                if (id_kk && kk_level == 4) {
+                    $('#ayah_nik').val(@json($data_ayah['nik'] ?? '')).prop('readonly', true);
+                    $('#nama_ayah').val(@json($data_ayah['nama'] ?? '')).prop('readonly', true);
+                    $('#ibu_nik').val(@json($data_ibu['nik'] ?? '')).prop('readonly', true);
+                    $('#nama_ibu').val(@json($data_ibu['nama'] ?? '')).prop('readonly', true);
+                } else {
+                    // Jika SHDK selain Anak (Cucu, Famili Lain), kosongkan dan biarkan input manual
+                    $('#ayah_nik').val('').prop('readonly', false);
+                    $('#nama_ayah').val('').prop('readonly', false);
+                    $('#ibu_nik').val('').prop('readonly', false);
+                    $('#nama_ibu').val('').prop('readonly', false);
+                }
             } else {
-                $('#ayah_nik').val('{{ $penduduk['ayah_nik'] }}');
-                $('#nama_ayah').val('{{ $penduduk['nama_ayah'] }}'); 
-                $('#ibu_nik').val('{{ $penduduk['ibu_nik'] }}');
-                $('#nama_ibu').val('{{ $penduduk['nama_ibu'] }}');
+                // Untuk bukan bayi baru lahir, gunakan logika lama
+                if (id_kk && kk_level == 4) {
+                    $('#ayah_nik').val(@json($data_ayah['nik'] ?? '')).prop('readonly', true);
+                    $('#nama_ayah').val(@json($data_ayah['nama'] ?? '')).prop('readonly', true);
+                    $('#ibu_nik').val(@json($data_ibu['nik'] ?? '')).prop('readonly', true);
+                    $('#nama_ibu').val(@json($data_ibu['nama'] ?? '')).prop('readonly', true);
+                } else {
+                    $('#ayah_nik').val('{{ $penduduk['ayah_nik'] }}').prop('readonly', false);
+                    $('#nama_ayah').val('{{ $penduduk['nama_ayah'] }}').prop('readonly', false);
+                    $('#ibu_nik').val('{{ $penduduk['ibu_nik'] }}').prop('readonly', false);
+                    $('#nama_ibu').val('{{ $penduduk['nama_ibu'] }}').prop('readonly', false);
+                }
             }
         }
 </script>
