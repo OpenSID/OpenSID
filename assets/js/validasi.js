e2d34dbec67f08f80daca8a7983d402a2644cea8
Muhammad Hasan <hasanlq69@gmail.com>
2026-02-18 15:17:52 +0700

    Revert "security: perbaikan proses double submit untuk id "validasi" (#5965)"
    This reverts commit 5649455800d8123da6eaa0f55c975fc36cf91fdf.

--- assets/js/validasi.js
+++ assets/js/validasi.js
@@ -41,26 +41,6 @@ $(document).ready(function() {
 				}
 			}
 		},
-		submitHandler: function(form) {
-			$(form).find('button[type="submit"]').attr('disabled', 'disabled');
-
-			if (typeof Swal !== 'undefined') {
-				Swal.fire({
-					title: 'Tunggu Sebentar...',
-					html: 'Sedang memproses data...',
-					icon: 'info',
-					allowOutsideClick: false,
-					allowEscapeKey: false,
-					didOpen: () => {
-						Swal.showLoading();
-					}
-				});
-			} else {
-				alert('Sedang memproses data...');
-			}
-
-			form.submit();
-		}
 	});
 
 	// Menambahkan aturan validasi untuk input[name='nomor'] jika elemen ditemukan
