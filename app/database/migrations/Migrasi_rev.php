439a69f96f5a11d4625e318cacfe20a0126410e2
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2026-02-18 16:39:26 +0800

    revert: "Teknis: Standardisasi struktur tabel"

--- app/database/migrations/Migrasi_rev.php
+++ app/database/migrations/Migrasi_rev.php
@@ -37,23 +37,22 @@
 
 use App\Traits\Migrator;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
     use Migrator;
 
     /**
-     * Jalankan migrasi.
+     * Run the migrations.
      */
     public function up(): void
     {
-        //
     }
-    
+
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        //
+
     }
 };
