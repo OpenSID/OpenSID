fb7033d73afb48e8d1a2485d2f6698f58dade279
Agung Sugiarto <sugiartoagung92@gmail.com>
2026-03-10 15:24:34 +0700

    fix: Perbaikan link panduan untuk impor gform ke data analisis (#6076)

--- Modules/Analisis/Views/master/import_gform.blade.php
+++ Modules/Analisis/Views/master/import_gform.blade.php
@@ -2,20 +2,21 @@
     <div class='modal-body'>
         <div class="row">
             <div class="col-sm-12">
-                <div class="box box-danger">
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="file" class="control-label">ID Google Form (Panduan mendapatkan ID Google Form dapat Anda akses <span><a href="https://github.com/OpenSID/OpenSID/wiki/Analisis#impor-data-survei-google-form-ke-master-analisis" target="_blank"
-                                        style="color: blue;">[disini]</a></span>).</label>
-                            <input type="text" class="form-control input-sm" id="input-form-id" name="input-form-id" required>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="reset" class="btn btn-social btn-danger btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
-                        <button type="submit" class="btn btn-social btn-info btn-sm" id="ok"><i class='fa fa-check'></i> Impor</button>
-                    </div>
+                <div class="form-group">
+                    <label for="file" class="control-label">ID Google Form (Panduan mendapatkan ID Google Form dapat Anda akses
+                        <span>
+                            <a href="https://panduan.opendesa.id/id/opensid/halaman-administrasi/analisis/master#impor-data-survei-google-form-ke-master-analisis"
+                                target="_blank" style="color: blue;">[disini]
+                            </a>
+                        </span>).
+                    </label>
+                    <input type="text" class="form-control input-sm" id="input-form-id" name="input-form-id" required>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="modal-footer">
+        <button type="reset" class="btn btn-social btn-danger btn-sm pull-left"><i class="fa fa-times"></i> Batal</button>
+        <button type="submit" class="btn btn-social btn-info btn-sm" id="ok"><i class="fa fa-check"></i> Simpan</button>
     </div>
 </form>
--- catatan_rilis.md
+++ catatan_rilis.md
@@ -20,6 +20,7 @@ Rilis versi 2603.0.0 ini berisi [untuk diisi] dan perbaikan lainnya yang diminta
 17. [#10919](https://github.com/OpenSID/OpenSID/issues/10919) Perbaikan persentase yang diinput manual tidak menampilkan simbol (%) saat cetak dokumentasi pembangunan.
 18. [#10910](https://github.com/OpenSID/OpenSID/issues/10910) Perbaikan konsistensi pemanggilan jenis izin di persetujuan izin.
 19. [#10914](https://github.com/OpenSID/OpenSID/issues/10914) Perbaikan malformasi daftar aset pada modul pemetaan (GIS).
+20. [#10926](https://github.com/OpenSID/OpenSID/issues/10926) Perbaikan link panduan untuk impor gform ke data analisis.
 
 ### KEAMANAN
 1. [#6041](https://github.com/OpenSID/premium/issues/6041) Peningkatan keamanan reponse data pada api kelompok/lembaga.
